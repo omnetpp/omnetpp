235dd34659f4db7b1c8167af0ee16e6b6eac4ac8
Andras Varga <andras@omnetpp.org>
1999-06-05 07:43:00 +0000

    cleanup

--- src/utils/splitvec.bat
+++ src/utils/splitvec.bat
@@ -2,7 +2,7 @@
 rem splitvec.bat:
 rem   Splits an output vector (.vec) file into files with one vector in each
 rem	ie: foo.vec --> foo-1.vec, foo-2.vec, foo-3.vec,...
-rem Required gawk.exe comes with the OMNeT++ for DOS distribution.
+rem Required gawk.exe can be found in CYGWIN or DJGPP
 
 echo %0: splitting file '%1' to separate vector files
 gawk -f \omnetpp\utils\splitvec %1
