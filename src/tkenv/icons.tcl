2ea2deb8f37e0efcc907b8b159d2a405eaa7c531
Andras Varga <andras@omnetpp.org>
1999-07-12 18:50:06 +0000

    *** empty log message ***

--- src/tkenv/icons.tcl
+++ src/tkenv/icons.tcl
@@ -55,3 +55,53 @@ set icons(stop) [image create photo -data {
     Ow==
 }]
 
+set icons(as) [image create photo -data {
+    R0lGODlhFAATALMAAAAAAMbGxgAA/wAAwACA////////////////////////////////////
+    /////////yH5BAEAAAEALAAAAAAUABMAAAQiMMhJq7046827/xQAToDYlWh5pirYBm1skqEp
+    j3iu73wfRAA7
+}]
+
+set icons(asgraphics) [image create photo -data {
+    R0lGODlhFAATALMAAAAAAMbGxgAA/wAAwACA/wAA/+zNhP8AAP//AMDAwP//////////////
+    /////////yH5BAEAAAEALAAAAAAUABMAAARJMMhJq7046y1BMJIBfhognqBYiseBupXXsS0N
+    T4BsGvVZ58BcwFQwFEXHYVBIRBpVMd2RdorhprxeZve6Ybgo6He55JjP6PQmAgA7
+}]
+
+set icons(asobject) [image create photo -data {
+    R0lGODlhFAATALMAAAAAAMbGxgAA/wAAwACA/////25ubp6env//////////////////////
+    /////////yH5BAEAAAEALAAAAAAUABMAAARIMMhJq704683z+WAIXodgnqhgUIBUpuk6Ae1R
+    3HheGHRP2z7ATbgL/nRImYQWAPaGRGWg1UTqpBObFYeVGL7gMLhDLpvP6EsEADs=
+}]
+
+set icons(asoutput) [image create photo -data {
+    R0lGODlhFAATALMAAAAAAMbGxgAA/wAAwHFx/////25ubp6env//////////////////////
+    /////////yH5BAEAAAEALAAAAAAUABMAAARfMMhJq704h2O6/93FaRhXCAKhokUrGidKpKwF
+    bHCr7+4EACaAAXgQ/g7Fn1JYQP6GzuFSmePxKEdYEQk1FbC4gnEs/Va81p3FNGsT1GtY6jsT
+    9OJql45knvRJgIEUEQAAOw==
+}]
+
+set icons(gates) [image create photo -data {
+    R0lGODlhFAATALMAAAAAAMbGxv//AACA////////////////////////////////////////
+    /////////yH5BAEAAAEALAAAAAAUABMAAAQ+MMhJq60g63zr+OAAdFS4caQprMJIBiDAtq/0
+    yayb3rPexada6FaDATe7WO+lyiV5zl9z5fOYgs9Y7YQseiMAOw==
+}]
+
+set icons(params) [image create photo -data {
+    R0lGODlhFAATALMAAAAAAMbGxv+GAACA//+1Uf//////////////////////////////////
+    /////////yH5BAEAAAEALAAAAAAUABMAAARTMMhJq5Ug630nGGAoDt03npd5imlIEOA7vC9A
+    mXSe23dMALWgpjcwZYofHg+zCnUCKtRTJaPNCIEfUcfVenxAHXDJRJqRUDK06Wyx27eNnPOs
+    TyIAOw==
+}]
+
+set icons(parent) [image create photo -data {
+    R0lGODlhFAATALMAAAAAAMbGxgAA/wAAwACA////////////////////////////////////
+    /////////yH5BAEAAAEALAAAAAAUABMAAAQyMMhJq72BEMyJ+JzlfWAojWTZpezKCkM8mChs
+    TvV8Z6l+5zseyUfrBYG7kSzIbDqflwgAOw==
+}]
+
+set icons(redraw) [image create photo -data {
+    R0lGODlhFAATALMAAAAAAMbGxtIAAGdnZ/+1Uf//////////////////////////////////
+    /////////yH5BAEAAAEALAAAAAAUABMAAARFMMhJq70U4B3AEAHITZ5gCoDJlWdqahf7tpiM
+    0vHX4pbtorXO7lUraFI/WK/APPKWTKNQGYs2O6OOVZrVXrsSgBhMLl8iADs=
+}]
+
