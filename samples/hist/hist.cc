1641e27584024abdf3779e38398a802c57cb763e
Andras Varga <andras@omnetpp.org>
1999-10-10 16:02:44 +0000

    beautifying

--- samples/hist/hist.cc
+++ samples/hist/hist.cc
@@ -14,110 +14,111 @@ class HistogramDemo : public cSimpleModule
       virtual void activity();
 };
 
-Define_Module( HistogramDemo )
+Define_Module( HistogramDemo );
 
 void HistogramDemo::activity()
 {
-  int numobs = par("num_obs");
-  int numcells = par("num_cells");
-  int fixed_range = par("fixed_range");
-  int numfirst = par("num_first");
-  double range_ext = par("range_ext");
+    int numobs = par("num_obs");
+    int numcells = par("num_cells");
+    int fixed_range = par("fixed_range");
+    int numfirst = par("num_first");
+    double range_ext = par("range_ext");
 
-  int i=0;
-  WATCH(i);
+    int i=0;
+    WATCH(i);
 
-  ev << "Creating 5 distribution approximation objects, of types:\n";
-  ev << "cDoubleHistogram, cLongHistogram, cVarHistogram, cPSquare, cKSplit.\n";
-  ev << "Parameters:\n";
-  ev << " number of cells: " << numcells << endl;
-  ev << (fixed_range ? " range: [0,100)" : "automatic range estimation") << endl;
-  if (!fixed_range)
-  {
-     ev << " observations used for range estimation: " << numfirst << endl;
-     ev << " range will be extended by " << range_ext << " times" << endl;
-  }
-  ev << endl;
+    ev << "Creating 5 distribution approximation objects, of types:\n";
+    ev << "cDoubleHistogram, cLongHistogram, cVarHistogram, cPSquare, cKSplit.\n";
+    ev << "Parameters:\n";
+    ev << " number of cells: " << numcells << endl;
+    ev << (fixed_range ? " range: [0,100)" : "automatic range estimation") << endl;
+    if (!fixed_range)
+    {
+        ev << " observations used for range estimation: " << numfirst << endl;
+        ev << " range will be extended by " << range_ext << " times" << endl;
+    }
+    ev << endl;
 
-  cDoubleHistogram dblhist("DoubleHistogram", numcells);
-  cLongHistogram longhist("LongHistogram", numcells);
-  cVarHistogram varhist("VarHistogram", numcells,HIST_TR_AUTO_EPC_DBL);
-  cPSquare psquare("PSquare", numcells);
-  cKSplit ksplit("K-Split");
+    cDoubleHistogram dblhist("DoubleHistogram", numcells);
+    cLongHistogram longhist("LongHistogram", numcells);
+    cVarHistogram varhist("VarHistogram", numcells,HIST_TR_AUTO_EPC_DBL);
+    cPSquare psquare("PSquare", numcells);
+    cKSplit ksplit("K-Split");
 
-  FILE *f;
+    FILE *f;
 
-  if (fixed_range)
-  {
-     dblhist.setRange(0,100);
-     longhist.setRange(0,100);
-     ksplit.setRange(0,100);
-  }
-  else
-  {
-     // 0.0 is lower limit
-     dblhist.setRangeAutoUpper(0.0, numfirst, range_ext);
-     longhist.setRangeAutoUpper(0.0, numfirst, range_ext);
-     ksplit.setRangeAutoUpper(0.0, numfirst, range_ext);
-  }
-  varhist.setNumFirstVals(numfirst);
+    if (fixed_range)
+    {
+        dblhist.setRange(0,100);
+        longhist.setRange(0,100);
+        ksplit.setRange(0,100);
+    }
+    else
+    {
+        // 0.0 is lower limit
+        dblhist.setRangeAutoUpper(0.0, numfirst, range_ext);
+        longhist.setRangeAutoUpper(0.0, numfirst, range_ext);
+        ksplit.setRangeAutoUpper(0.0, numfirst, range_ext);
+    }
+    varhist.setNumFirstVals(numfirst);
 
-  f=fopen("hist.dat","r");
-  if (f && ev.askYesNo("HIST: Saved histogram file `hist.dat' found,"
-                       " load it and continue collecting from there?"))
-  {
-     longhist.loadFromFile(f);
-     dblhist.loadFromFile(f);
-     psquare.loadFromFile(f);
-     varhist.loadFromFile(f);
-     ksplit.loadFromFile(f);
-     fclose(f);
-  }
+    f=fopen("hist.dat","r");
+    if (f && ev.askYesNo("HIST: Saved histogram file `hist.dat' found,"
+                         " load it and continue collecting from there?"))
+    {
+        longhist.loadFromFile(f);
+        dblhist.loadFromFile(f);
+        psquare.loadFromFile(f);
+        varhist.loadFromFile(f);
+        ksplit.loadFromFile(f);
+        fclose(f);
+    }
 
-  ev << "[Tkenv] Select Options|Load inspector list or click the\n";
-  ev << "        Objects/Watches tab in the module inspector window.\n";
-  ev << "[Tvenv] Choose Inspect|Local objects... from menu then select from list.\n";
-  ev << endl;
-  wait(0);
+    ev << "[Tkenv] Select Options|Load inspector list or click the\n";
+    ev << "        Objects/Watches tab in the module inspector window.\n";
+    ev << "[Tvenv] Choose Inspect|Local objects... from menu then select from list.\n";
+    ev << endl;
+    wait(0);
 
-  ev << "Filling objects with " << numobs << " random observations...\n";
-  ev << "(exponential(30) with P=0.5 and normal(80, 10) with P=0.5)\n";
-  for (i=0; i<numobs; i++)
-  {
-    double d = (intrand(2)==0) ? exponential(30) : normal(80, 10);
-    ev << " adding " << d << endl;
+    ev << "Filling objects with " << numobs << " random observations...\n";
+    ev << "(exponential(30) with P=0.5 and normal(80, 10) with P=0.5)\n";
+    for (i=0; i<numobs; i++)
+    {
+        double d = (intrand(2)==0) ? exponential(30) : normal(80, 10);
+        ev << " adding " << d << endl;
 
-    longhist.collect( d );
-    dblhist.collect( d );
-    psquare.collect( d );
-    ksplit.collect( d );
-    varhist.collect( d );
+        longhist.collect( d );
+        dblhist.collect( d );
+        psquare.collect( d );
+        ksplit.collect( d );
+        varhist.collect( d );
 
-    wait(1);
-  }
+        wait(1);
+    }
 
-  ev << endl;
-  ev << "Cells in ksplit:\n";
-  for (i=0; i<ksplit.cells(); i++)
-  {
-    ev << " cell " << i << ":";
-    ev << " [" << ksplit.basepoint(i) << "," << ksplit.basepoint(i+1) << "]";
-    ev << "  n=" << ksplit.cell(i);
-    ev << "  PDF=" << ksplit.cellPDF(i) << endl;
-  }
+    ev << endl;
+    ev << "Cells in ksplit:\n";
+    for (i=0; i<ksplit.cells(); i++)
+    {
+        ev << " cell " << i << ":";
+        ev << " [" << ksplit.basepoint(i) << "," << ksplit.basepoint(i+1) << "]";
+        ev << "  n=" << ksplit.cell(i);
+        ev << "  PDF=" << ksplit.cellPDF(i) << endl;
+    }
 
-  ev << endl;
-  ev << "Writing snapshot file...\n";
-  snapshot(this);
+    ev << endl;
+    ev << "Writing snapshot file...\n";
+    snapshot(this);
 
-  ev << "Saving all four objects to `hist.dat'...\n";
-  f=fopen("hist.dat","w");
-  longhist.saveToFile(f);
-  dblhist.saveToFile(f);
-  psquare.saveToFile(f);
-  varhist.saveToFile(f);
-  ksplit.saveToFile(f);
-  fclose(f);
+    ev << "Saving all four objects to `hist.dat'...\n";
+    f=fopen("hist.dat","w");
+    longhist.saveToFile(f);
+    dblhist.saveToFile(f);
+    psquare.saveToFile(f);
+    varhist.saveToFile(f);
+    ksplit.saveToFile(f);
+    fclose(f);
 
-  endSimulation();
+    endSimulation();
 }
+
--- samples/nim/game.cc
+++ samples/nim/game.cc
@@ -11,16 +11,16 @@
 // derive Game from cSimpleModule
 class Game : public cSimpleModule
 {
-     Module_Class_Members(Game,cSimpleModule,8192)
+    Module_Class_Members(Game,cSimpleModule,8192)
        // this is a macro; it expands to constructor definition etc.
        // 8192 is the size for the coroutine stack (in bytes)
 
-     virtual void activity();
+    virtual void activity();
        // this redefined virtual function holds the algorithm
 };
 
 // register the simple module class to OMNeT++
-Define_Module( Game )
+Define_Module( Game );
 
 // Operation of Game:
 //  The Game module first waits for a message from both players and extracts
@@ -33,70 +33,70 @@ Define_Module( Game )
 
 void Game::activity()
 {
-        // strings to store player names; [0] is unused
-        char player[3][32];
+    // strings to store player names; [0] is unused
+    char player[3][32];
 
-        // read parameter values
-        int num_sticks = par("num_sticks");
-        int player_to_move = par("first_move");
+    // read parameter values
+    int num_sticks = par("num_sticks");
+    int player_to_move = par("first_move");
 
-        // make the variables visible from Tkenv/Tvenv
-        WATCH(num_sticks);
-        WATCH(player_to_move);
+    // make the variables visible from Tkenv/Tvenv
+    WATCH(num_sticks);
+    WATCH(player_to_move);
 
-        // waiting for players to tell their names
-        ev << "Waiting for players to tell their names...\n";
-        for (int i=0; i<2; i++)
-        {
-                cMessage *msg = receive();
-                if (msg->arrivedOn("from_player1"))
-                        strcpy( player[1], msg->name());
-                else
-                        strcpy( player[2], msg->name());
-                delete msg;
-        }
+    // waiting for players to tell their names
+    ev << "Waiting for players to tell their names...\n";
+    for (int i=0; i<2; i++)
+    {
+        cMessage *msg = receive();
+        if (msg->arrivedOn("from_player1"))
+                strcpy( player[1], msg->name());
+        else
+                strcpy( player[2], msg->name());
+        delete msg;
+    }
 
-        ev << "OK, let the game begin!\n";
-        ev << "Player 1: " << player[1] << "   Player 2: " << player[2]
-           << "\n\n";
+    ev << "OK, let the game begin!\n";
+    ev << "Player 1: " << player[1] << "   Player 2: " << player[2]
+       << "\n\n";
 
-        // main loop of the game
-        do
-        {
-                ev << "Sticks left: " << num_sticks << "\n";
-                ev << "Player " << player_to_move << " ("
-                   << player[player_to_move] << ") to move.\n";
+    // main loop of the game
+    do
+    {
+        ev << "Sticks left: " << num_sticks << "\n";
+        ev << "Player " << player_to_move << " ("
+           << player[player_to_move] << ") to move.\n";
 
-                // send out how many sticks we have
-                char msgname[32];
-                sprintf(msgname,"%d sticks left",num_sticks);
-                cMessage *msg = new cMessage(msgname);
-                msg->setKind(num_sticks);
+        // send out how many sticks we have
+        char msgname[32];
+        sprintf(msgname,"%d sticks left",num_sticks);
+        cMessage *msg = new cMessage(msgname);
+        msg->setKind(num_sticks);
 
-                if (player_to_move == 1)
-                        send(msg, "to_player1");
-                else
-                        send(msg, "to_player2");
+        if (player_to_move == 1)
+                send(msg, "to_player1");
+        else
+                send(msg, "to_player2");
 
-                // wait for the player to answer how many he takes
-                msg = receive();
-                int sticks_taken = msg->kind();
-                delete msg;
+        // wait for the player to answer how many he takes
+        msg = receive();
+        int sticks_taken = msg->kind();
+        delete msg;
 
-                num_sticks -= sticks_taken;
+        num_sticks -= sticks_taken;
 
-                ev << "Player " << player_to_move << " ("
-                   << player[player_to_move] << ") took "
-                   << sticks_taken << " stick(s).\n";
+        ev << "Player " << player_to_move << " ("
+           << player[player_to_move] << ") took "
+           << sticks_taken << " stick(s).\n";
 
-                // switch to the other player
-                player_to_move = 3 - player_to_move;
-        }
-        while (num_sticks>0);
+        // switch to the other player
+        player_to_move = 3 - player_to_move;
+    }
+    while (num_sticks>0);
 
-        ev << "\nPlayer " << player_to_move << " ("
-           << player[player_to_move] << ") won!\n";
+    ev << "\nPlayer " << player_to_move << " ("
+       << player[player_to_move] << ") won!\n";
 
-        endSimulation();
+    endSimulation();
 }
 
