e22c2556e2c2bac163ef74f7d90e1270f0f0f36d
Andras Varga <andras@omnetpp.org>
1999-10-17 14:15:21 +0000

    added "1" suffix to gen.*, sink.*, fifo.*

---
+++ samples/fifo1/gen1.h
@@ -0,0 +1,18 @@
+//-------------------------------------------------------------
+// file: gen1.h
+//        (part of Fifo1 - an OMNeT++ demo simulation)
+//-------------------------------------------------------------
+
+#ifndef __GEN_H
+#define __GEN_H
+
+#include <omnetpp.h>
+
+class FF1Generator : public cSimpleModule
+{
+    Module_Class_Members(FF1Generator,cSimpleModule,16384)
+    virtual void activity();
+    virtual void finish();
+};
+
+#endif
---
+++ samples/fifo1/sink1.h
@@ -0,0 +1,21 @@
+//-------------------------------------------------------------
+// file: sink1.h
+//        (part of Fifo1 - an OMNeT++ demo simulation)
+//-------------------------------------------------------------
+
+#ifndef __SINK_H
+#define __SINK_H
+
+#include <omnetpp.h>
+
+class FF1Sink : public cSimpleModule
+{
+    Module_Class_Members(FF1Sink,cSimpleModule,16384)
+
+    cStdDev qstats; // needs to be accessed from finish() too
+
+    virtual void activity();
+    virtual void finish();
+};
+
+#endif
