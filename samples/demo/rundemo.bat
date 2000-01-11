2f0897ffe9233aa6becb1f9d98d7121ea07c6d24
Andras Varga <andras@omnetpp.org>
2000-01-11 23:22:25 +0000

    *** empty log message ***

---
+++ samples/demo/rundemo.bat
@@ -0,0 +1,15 @@
+@echo off
+rem
+rem  Substitute the correct file names/paths in the command line below!
+rem 
+rem  wish: the Tcl/Tk interpreter. You may find it under different names,
+rem    for example "wish82.exe" in the standard Tk distribution or
+rem    "cygwish80.exe" in CYGWIN-B20. Files like "wishx.exe", "tixwish.exe",
+rem    "wishx80.exe" etc. will also do, they are just "wishes" with different
+rem    extensions compiled in.
+rem  
+rem  You should also manually correct the location of the "demo" file below
+rem  according to your OMNeT++ installation directory.
+rem
+
+c:\tk80\win\wish80.exe -f d:\work\omnetpp\samples\demo\demo
