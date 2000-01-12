ad6b7f8b1e25deffb703b8c56da2fae053bd496b
Andras Varga <andras@omnetpp.org>
2000-01-12 00:17:42 +0000

    *** empty log message ***

--- src/plove-old/plove.bat
+++ src/plove-old/plove.bat
@@ -3,7 +3,7 @@ rem
 rem  Substitute the correct file names/paths in the command line below!
 rem 
 rem  wish: the Tcl/Tk interpreter. You may find it under different names,
-rem    for example "wish80.exe" in the standard Tk distribution or
+rem    for example "wish82.exe" in the standard Tk distribution or
 rem    "cygwish80.exe" in CYGWIN-B20. Files like "wishx.exe", "tixwish.exe",
 rem    "wishx80.exe" etc. will also do, they are just "wishes" with different
 rem    extensions compiled in.
