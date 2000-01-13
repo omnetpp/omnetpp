71efbbd3b6e5eb1a207c5f41f4c30beb3a56d99b
Andras Varga <andras@omnetpp.org>
2000-01-13 19:33:46 +0000

    *** empty log message ***

---
+++ samples/cc2cpp.bat
@@ -0,0 +1,13 @@
+@echo off
+
+rem Rename all .cc files to .cpp for building the samples from the MSVC IDE.
+
+ren dyna\*.cc *.cpp
+ren fddi\*.cc *.cpp
+ren fifo1\*.cc *.cpp
+ren fifo2\*.cc *.cpp
+ren hcube\*.cc *.cpp
+ren hist\*.cc *.cpp
+ren nim\*.cc *.cpp
+ren pvmex\*.cc *.cpp
+ren token\*.cc *.cpp
