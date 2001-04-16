a593c9a89f7a9874326afdb4082ebd58b7b7f424
Andras Varga <andras@omnetpp.org>
2001-04-16 20:08:46 +0000

    *** empty log message ***

--- src/plove-old/plove.cc
+++ src/plove-old/plove.cc
@@ -24,6 +24,176 @@
 #include <time.h>
 #include <tk.h>
 
+#ifdef USE_WINMAIN
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#undef WIN32_LEAN_AND_MEAN
+#endif
+
+
+int initPlove(Tcl_Interp *interp)
+{
+    // Compiled-in TCL code
+    //
+    // The tclcode.cc file must be generated from the TCL scripts
+    // with the tcl2c program (to be compiled from tcl2c.c).
+    // The Unix makefile automatically does so; on Win95/NT, you have
+    // to do it by hand: bcc tcl2c.c; tcl2c tclcode.cc *.tcl
+    //
+#include "tclcode.cc"
+    if (Tcl_Eval(interp,(char *)tcl_code)==TCL_ERROR)
+    {
+         fprintf(stderr, "**** Error: %s\n", interp->result);
+         return TCL_ERROR;
+    }
+    if (Tcl_Eval(interp,"startPlove $argv")==TCL_ERROR)
+    {
+         fprintf(stderr, "**** Error: %s\n", interp->result);
+         return TCL_ERROR;
+    }
+    return TCL_OK;
+}
+
+
+#ifdef USE_WINMAIN
+/* setargv --
+ *
+ *	Parse the Windows command line string into argc/argv.  Done here
+ *	because we don't trust the builtin argument parser in crt0.
+ *	Windows applications are responsible for breaking their command
+ *	line into arguments.
+ *
+ *      Copied here from Tk source (win/winMain.c), appending the code
+ *      to set argv[0].
+ */
+static void setargv(int *argcPtr, char ***argvPtr)
+{
+    char *cmdLine, *p, *arg, *argSpace;
+    char **argv;
+    int argc, size, inquote, copy, slashes;
+    char buffer[MAX_PATH];
+
+    cmdLine = GetCommandLine();
+
+    /*
+     * Precompute an overly pessimistic guess at the number of arguments
+     * in the command line by counting non-space spans.
+     */
+    size = 2;
+    for (p = cmdLine; *p != '\0'; p++) {
+	if (isspace(*p)) {
+	    size++;
+	    while (isspace(*p)) {
+		p++;
+	    }
+	    if (*p == '\0') {
+		break;
+	    }
+	}
+    }
+    argSpace = (char *) ckalloc((unsigned) (size * sizeof(char *)
+	    + strlen(cmdLine) + 1));
+    argv = (char **) argSpace;
+    argSpace += size * sizeof(char *);
+    size--;
+
+    p = cmdLine;
+    for (argc = 0; argc < size; argc++) {
+	argv[argc] = arg = argSpace;
+	while (isspace(*p)) {
+	    p++;
+	}
+	if (*p == '\0') {
+	    break;
+	}
+
+	inquote = 0;
+	slashes = 0;
+	while (1) {
+	    copy = 1;
+	    while (*p == '\\') {
+		slashes++;
+		p++;
+	    }
+	    if (*p == '"') {
+		if ((slashes & 1) == 0) {
+		    copy = 0;
+		    if ((inquote) && (p[1] == '"')) {
+			p++;
+			copy = 1;
+		    } else {
+			inquote = !inquote;
+		    }
+                }
+                slashes >>= 1;
+            }
+
+            while (slashes) {
+		*arg = '\\';
+		arg++;
+		slashes--;
+	    }
+
+	    if ((*p == '\0') || (!inquote && isspace(*p))) {
+		break;
+	    }
+	    if (copy != 0) {
+		*arg = *p;
+		arg++;
+	    }
+	    p++;
+        }
+	*arg = '\0';
+	argSpace = arg + 1;
+    }
+    argv[argc] = NULL;
+
+    *argcPtr = argc;
+    *argvPtr = argv;
+
+
+    /*
+     * Replace argv[0] with full pathname of executable, and forward
+     * slashes substituted for backslashes.
+     */
+    GetModuleFileName(NULL, buffer, sizeof(buffer));
+    argv[0] = buffer;
+    for (p = buffer; *p != '\0'; p++) {
+	if (*p == '\\') {
+	    *p = '/';
+	}
+    }
+}
+
+static int Tcl_AppInit(Tcl_Interp *interp)
+{
+    if (Tcl_Init(interp) == TCL_ERROR)
+         return TCL_ERROR;
+    if (Tk_Init(interp) == TCL_ERROR)
+         return TCL_ERROR;
+    Tcl_StaticPackage(interp, "Tk", Tk_Init, Tk_SafeInit);
+
+    if (initPlove(interp)==TCL_ERROR)
+        return TCL_ERROR;
+
+    return TCL_OK;
+}
+
+int APIENTRY
+WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int nCmdShow)
+{
+    Tcl_Interp *interp;
+    char **argv;
+    int argc;
+
+    setargv(&argc, &argv);
+
+    // enter Tk event loop
+    Tk_Main(argc, argv, Tcl_AppInit);
+    return 1;
+}
+
+#else //not USE_WINMAIN
 
 static int XErrorProc( ClientData, XErrorEvent *errEventPtr)
 {
@@ -88,7 +258,6 @@ int initTk(int argc, char **argv, Tcl_Interp *&interp )
     return TCL_OK;
 }
 
-
 int main(int argc, char *argv[])
 {
     Tcl_Interp *interp;
@@ -100,27 +269,16 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    // Compiled-in TCL code
-    //
-    // The tclcode.cc file must be generated from the TCL scripts
-    // with the tcl2c program (to be compiled from tcl2c.c).
-    // The Unix makefile automatically does so; on Win95/NT, you have
-    // to do it by hand: bcc tcl2c.c; tcl2c tclcode.cc *.tcl
-    //
-#include "tclcode.cc"
-    if (Tcl_Eval(interp,(char *)tcl_code)==TCL_ERROR)
+    if (initPlove(interp)==TCL_ERROR)
     {
-         fprintf(stderr, "**** Error: %s\n", interp->result);
-         return 1;
-    }
-    if (Tcl_Eval(interp,"startPlove $argv")==TCL_ERROR)
-    {
-         fprintf(stderr, "**** Error: %s\n", interp->result);
-         return 1;
+        return 1;
     }
 
+    // enter Tk event loop
     Tk_MainLoop();
     return 0;
 }
+
+#endif //USE_WINMAIN
 
 
