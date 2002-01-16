e7d8fcd3842d3c1d85379702fbb8eda0a2156952
Ulrich Kaage <kaage@localhost>
2002-01-16 11:13:42 +0000

    strip all command line arguments except the last one so that only the ned file name is left. Use this for running grep and perl.

--- src/nedc/nedcwrapper.sh
+++ src/nedc/nedcwrapper.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 DIR=`dirname $0`
+cmdline=$*
+shift `expr $# - 1`
 nedfile=$1
 if grep '^//--subclassing--' $nedfile >/dev/null; then 
     echo "     running: perl $DIR/msgc.pl $nedfile"
     perl $DIR/msgc.pl $nedfile
 else
-    echo "     running: $DIR/nedc $nedfile"
-    $DIR/nedc $nedfile
+    echo "     running: $DIR/nedc $cmdline"
+    $DIR/nedc $cmdline
 fi
