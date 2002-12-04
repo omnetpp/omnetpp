3173d70c438cf04cab04e9fe2d7759dd544492e9
Andras Varga <andras@omnetpp.org>
2002-12-04 14:20:39 +0000

    neddoc stuff on windows refined

--- src/utils/opp_neddoc.cmd
+++ src/utils/opp_neddoc.cmd
@@ -1,11 +1,13 @@
 @echo off
-rem This is the same magic as the opp_neddoc shell script. Cannot be done
-rem without using NT command line extensions :-(
+rem This is the same magic as the opp_neddoc shell script.
+rem  Cannot be done without using NT command line extensions :-(
 if "_%OS%_"=="_Windows_NT_" goto NT
-echo opp_nedc: error: this script only works on NT! (NT4/W2K/XP)
+echo opp_neddoc: error: this script only works on NT! (NT4/W2K/XP)
 goto end
 :NT
-nedtool.exe -x -e -y -m -o %TEMP%\$$$ %*
+PATH %PATH%;%~dp0
+nedtool.exe -x -e -y -m -o %TEMP%\$$$ %* || goto CLEANUP
 xsltproc.exe %~dp0\neddoc.xsl %TEMP%\$$$
-del %TEMP%\$$$
+:CLEANUP
+del %TEMP%\$$$ 2>nul
 :END
---
+++ src/utils/opp_neddocall.cmd
@@ -0,0 +1,14 @@
+@echo off
+rem Document all ned files in the current directory and subdirectories.
+rem  Cannot be done without using NT command line extensions :-(
+if "_%OS%_"=="_Windows_NT_" goto NT
+echo opp_neddocall: error: this script only works on NT! (NT4/W2K/XP)
+goto end
+:NT
+PATH %PATH%;%~dp0
+dir /s /b *.ned > %TEMP%\$$$1 || goto CLEANUP
+nedtool.exe -x -e -y -m -o %TEMP%\$$$ @%TEMP%\$$$1 || goto CLEANUP
+xsltproc.exe %~dp0\neddoc.xsl %TEMP%\$$$
+:CLEANUP
+del %TEMP%\$$$ %TEMP%\$$$1 2>nul
+:END
