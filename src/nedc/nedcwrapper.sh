d7a88796eec30681100408ddd1c832e0e83e2665
Andras Varga <andras@omnetpp.org>
2002-12-07 08:56:50 +0000

    msgc --> opp_msgc

--- src/nedc/nedcwrapper.sh
+++ src/nedc/nedcwrapper.sh
@@ -4,8 +4,8 @@ cmdline=$*
 shift `expr $# - 1`
 nedfile=$1
 if grep '^//--subclassing--' $nedfile >/dev/null; then 
-    echo "     running: perl $DIR/msgc.pl $nedfile"
-    perl $DIR/msgc.pl $nedfile
+    echo "     running: opp_msgc $nedfile"
+    perl $DIR/opp_msgc $nedfile
 else
     echo "     running: $DIR/nedc $cmdline"
     $DIR/nedc $cmdline
