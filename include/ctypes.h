50db49ab6acb25e4330e1708679a0c4963e5a703
Andras Varga <andras@omnetpp.org>
2002-12-19 21:38:11 +0000

    copyright years updated; hint to my affiliation with TU Budapest removed (actually I left beginning 1998, 5 years ago)

--- include/ctypes.h
+++ include/ctypes.h
@@ -17,9 +17,7 @@
 //==========================================================================
 
 /*--------------------------------------------------------------*
-  Copyright (C) 1992-2002 Andras Varga
-  Technical University of Budapest, Dept. of Telecommunications,
-  Stoczek u.2, H-1111 Budapest, Hungary.
+  Copyright (C) 1992-2003 Andras Varga
 
   This file is distributed WITHOUT ANY WARRANTY. See the file
   `license' for details on this and other legal matters.
