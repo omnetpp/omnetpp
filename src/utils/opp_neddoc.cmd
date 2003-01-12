8dd768d5f5332b60d43577ffbf44e4b013a93055
Andras Varga <andras@omnetpp.org>
2003-01-12 11:42:57 +0000

    path fixed

--- src/utils/opp_neddoc.cmd
+++ src/utils/opp_neddoc.cmd
@@ -1,7 +1,7 @@
 @echo off
 rem Document NED files given as arguments.
 rem *** this file uses NT command extensions, it won't work on 95/98/ME ***
-PATH %PATH%;%~dp0
+PATH %~dp0;%PATH%
 nedtool.exe -x -e -y -m -o %TEMP%\$$$ %* || goto CLEANUP
 xsltproc.exe -o neddoc.html %~dp0\neddoc.xsl %TEMP%\$$$ || goto CLEANUP
 echo neddoc.html created
--- src/utils/opp_neddocall.cmd
+++ src/utils/opp_neddocall.cmd
@@ -1,7 +1,7 @@
 @echo off
 rem Document all NED files in the current directory and subdirectories.
 rem *** this file uses NT command extensions, it won't work on 95/98/ME ***
-PATH %PATH%;%~dp0
+PATH %~dp0;%PATH%
 dir /s /b *.ned > %TEMP%\$$$1 || goto CLEANUP
 nedtool.exe -x -e -y -m -o %TEMP%\$$$ @%TEMP%\$$$1 || goto CLEANUP
 xsltproc.exe -o neddoc.html %~dp0\neddoc.xsl %TEMP%\$$$ || goto CLEANUP
