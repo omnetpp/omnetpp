1f31c228fa88012df334e17abfbfb2091f59c7a1
Andras Varga <andras@omnetpp.org>
2003-08-21 04:40:59 +0000

    fixed bug with -o

--- src/utils/opp_neddocall.cmd
+++ src/utils/opp_neddocall.cmd
@@ -17,7 +17,7 @@ del /q html\*.* 2>nul
 rem --- and if gifs are exported, --stringparam imagesxml html/images.xml should be passed to the stylesheet ---
 xsltproc.exe --stringparam outputdir html --stringparam imagesxml "" %~dp0\neddoc.xsl %TEMP%\$$$ > nul || goto CLEANUP
 echo formatting comments...
-perl %~dp0\neddocproc.pl html\*.html || goto CLEANUP
+perl %~dp0\neddocproc.pl html || goto CLEANUP
 echo documentation created in html/ -- start page is index.html
 :CLEANUP
 del %TEMP%\$$$ %TEMP%\$$$1 2>nul
