3947f1a0809f0d7aa69c61d93437903d2ef3a7f6
Andras Varga <andras@omnetpp.org>
2003-09-02 00:58:05 +0000

    *** empty log message ***

--- src/plove-old/widgets.tcl
+++ src/plove-old/widgets.tcl
@@ -65,11 +65,13 @@ proc setupTkOptions {} {
    set tcl_nonwordchars {\W}
 
    # by default, undo/redo bindings are platform-specific -- change it:
-   event add <<Undo>> <Control-Key-z>
-   event add <<Undo>> <Control-Key-Z>
-   event add <<Redo>> <Control-Key-y>
-   event add <<Redo>> <Control-Key-Y>
-
+   catch {
+       event add <<Undo>> <Control-Key-z>
+       event add <<Undo>> <Control-Key-Z>
+       event add <<Redo>> <Control-Key-y>
+       event add <<Redo>> <Control-Key-Y>
+   }
+   
    #
    # fonts() array elements:
    #  normal:  menus, labels etc
@@ -278,7 +280,8 @@ proc label-text {w label height {text {}}} {
     # utility function: create a frame with a label+text
     frame $w
     label $w.l -anchor w -width 16 -text $label
-    text $w.t -undo true -maxundo 1000 -highlightthickness 0 -height $height -width 40
+    text $w.t -highlightthickness 0 -height $height -width 40
+    catch {$w.t config -undo true}
     pack $w.l -anchor n -expand 0 -fill none -padx 2 -pady 2 -side left
     pack $w.t -anchor center -expand 1 -fill both -padx 2 -pady 2 -side right
     $w.t insert 1.0 $text
