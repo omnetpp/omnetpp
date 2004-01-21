914eb177440abdf2c1684e52e3590433ea57238c
Andras Varga <andras@omnetpp.org>
2004-01-21 20:02:59 +0000

    topo renamed to neddemo

---
+++ samples/neddemo/README.txt
@@ -0,0 +1,8 @@
+Various NED demos
+=================
+
+Demonstrates how one can create various topologies using NED: chain, mesh, 
+star, hexagonal grid, full graph, random graph and binary tree.
+
+The models themselves don't do anything apart from setting up the necessary
+model structure.
---
+++ samples/neddemo/node.cc
@@ -0,0 +1,84 @@
+//
+// This file is part of an OMNeT++ simulation example.
+//
+// Copyright (C) 1992-2003 Andras Varga
+//
+// This file is distributed WITHOUT ANY WARRANTY. See the file
+// `license' for details on this and other legal matters.
+//
+
+
+#include <omnetpp.h>
+
+
+class ChainNode : public cSimpleModule
+{
+    Module_Class_Members(ChainNode,cSimpleModule,0)
+    virtual void handleMessage(cMessage *) {}
+};
+
+Define_Module(ChainNode);
+
+
+class MeshNode : public cSimpleModule
+{
+    Module_Class_Members(MeshNode,cSimpleModule,0)
+    virtual void handleMessage(cMessage *) {}
+};
+
+Define_Module(MeshNode);
+
+
+class StarNode : public cSimpleModule
+{
+    Module_Class_Members(StarNode,cSimpleModule,0)
+    virtual void handleMessage(cMessage *) {}
+};
+
+Define_Module(StarNode);
+
+
+class HubNode : public cSimpleModule
+{
+    Module_Class_Members(HubNode,cSimpleModule,0)
+    virtual void handleMessage(cMessage *) {}
+};
+
+Define_Module(HubNode);
+
+
+class RndNode : public cSimpleModule
+{
+    Module_Class_Members(RndNode,cSimpleModule,0)
+    virtual void handleMessage(cMessage *) {}
+};
+
+Define_Module(RndNode);
+
+
+class FullGraphNode : public cSimpleModule
+{
+    Module_Class_Members(FullGraphNode,cSimpleModule,0)
+    virtual void handleMessage(cMessage *) {}
+};
+
+Define_Module(FullGraphNode);
+
+
+class HexGridNode : public cSimpleModule
+{
+    Module_Class_Members(HexGridNode,cSimpleModule,0)
+    virtual void handleMessage(cMessage *) {}
+};
+
+Define_Module(HexGridNode);
+
+
+class BinaryTreeNode : public cSimpleModule
+{
+    Module_Class_Members(BinaryTreeNode,cSimpleModule,0)
+    virtual void handleMessage(cMessage *) {}
+};
+
+Define_Module(BinaryTreeNode);
+
