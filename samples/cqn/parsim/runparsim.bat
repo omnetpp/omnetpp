04d8ce809af828f79aba4f15b578f714d3e7660b
Andras Varga <andras@omnetpp.org>
2004-02-11 21:25:45 +0000

    *** empty log message ***

--- samples/cqn/parsim/runparsim.bat
+++ samples/cqn/parsim/runparsim.bat
@@ -3,6 +3,13 @@ rmdir /S /Q comm 2>nul
 mkdir comm
 mkdir comm\read
 
+echo.
+echo *** Distributed execution of the Closed Queueing Network ***
+echo If you're running CQN under the GUI, you'll have to click [Run] in all
+echo three windows to start the simulation. See Readme.txt for more info.
+echo.
+pause
+
 start /b /belownormal ..\cqn -p0,3
 start /b /belownormal ..\cqn -p1,3
 start /b /belownormal ..\cqn -p2,3
