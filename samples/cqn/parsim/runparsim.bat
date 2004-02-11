6bca3cebedcd8eb2e157bb86be08632f86d0b397
Andras Varga <andras@omnetpp.org>
2004-02-11 21:19:51 +0000

    *** empty log message ***

--- samples/cqn/parsim/runparsim.bat
+++ samples/cqn/parsim/runparsim.bat
@@ -3,6 +3,6 @@ rmdir /S /Q comm 2>nul
 mkdir comm
 mkdir comm\read
 
-start "Partition 0" /b /belownormal ..\cqn -p0,3
-start "Partition 1" /b /belownormal ..\cqn -p1,3
-start "Partition 2" /b /belownormal ..\cqn -p2,3
+start /b /belownormal ..\cqn -p0,3
+start /b /belownormal ..\cqn -p1,3
+start /b /belownormal ..\cqn -p2,3
