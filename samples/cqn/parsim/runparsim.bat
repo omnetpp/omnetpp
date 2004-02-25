6cf0b4c9217285f0af6e051b468e40e2098e22a2
Andras Varga <andras@omnetpp.org>
2004-02-25 11:26:04 +0000

    *** empty log message ***

--- samples/cqn/parsim/runparsim.bat
+++ samples/cqn/parsim/runparsim.bat
@@ -10,6 +10,6 @@ rmdir /S /Q comm 2>nul
 mkdir comm
 mkdir comm\read
 
-start /b /belownormal ..\cqn -p0,3
-start /b /belownormal ..\cqn -p1,3
-start /b /belownormal ..\cqn -p2,3
+start /b /belownormal ..\cqn -p0,3 %*
+start /b /belownormal ..\cqn -p1,3 %*
+start /b /belownormal ..\cqn -p2,3 %*
