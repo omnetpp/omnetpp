009a84dbfc20fcda65447815b373f2f0b04b64f5
Andras Varga <andras@omnetpp.org>
2004-10-11 09:42:19 +0000

    *** empty log message ***

--- src/tkenv/main.tcl
+++ src/tkenv/main.tcl
@@ -33,7 +33,7 @@ set config(filtobjlist-maxcount) 100000
 set config(filtobjlist-category) "mqsgvo"
 set config(last-nedfile) ""
 set config(confirm-exit) 1
-set config(rununtil-mode) "Normal"
+set config(rununtil-mode) "Express (tracing off)"
 set config(rununtil-time) ""
 set config(rununtil-event) ""
 
