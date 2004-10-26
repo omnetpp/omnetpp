551509fe42c44fa762cc49d9154abf28f507ebb9
Andras Varga <andras@omnetpp.org>
2004-10-26 17:52:40 +0000

    *** empty log message ***

---
+++ src/sim/cmersennetwister.cc
@@ -0,0 +1,83 @@
+//==========================================================================
+//  CMERSENNETWISTER.CC - part of
+//                         OMNeT++
+//              Discrete System Simulation in C++
+//
+// Contents:
+//   class cMersenneTwister
+//
+//==========================================================================
+
+/*--------------------------------------------------------------*
+  Copyright (C) 2002-2004 Andras Varga
+
+  This file is distributed WITHOUT ANY WARRANTY. See the file
+  `license' for details on this and other legal matters.
+*--------------------------------------------------------------*/
+
+#include "cenvir.h"
+#include "defs.h"
+#include "util.h"
+#include "crng.h"
+#include "cexception.h"
+#include "mt19937ar.h"
+
+
+/**
+ * Wraps the Mersenne Twister RNG by Makoto Matsumoto and Takuji Nishimura.
+ * Cycle length is 2^19937-1, and 623-dimensional equidistribution property
+ * is assured.
+ *
+ * http://www.math.sci.hiroshima-u.ac.jp/~m-mat/MT/ewhat-is-mt.html
+ */
+class cMersenneTwister : public cRNG
+{
+  protected:
+    unsigned long mt[MT19937_N];
+    int mti;
+
+    void useThisRNG() {current_mt = mt; current_mti_ptr = &mti;}
+
+  public:
+    cMersenneTwister() {}
+    virtual ~cMersenneTwister() {}
+
+    /**
+     * Sets up the RNG.
+     */
+    virtual void initialize(int runnumber, int id, cConfiguration *cfg);
+
+    /**
+     * Generates a random number on the interval [0,0x7fffffff]
+     */
+    virtual unsigned long intRand();
+
+    /**
+     * Generates a random number on the [0,1) real interval
+     */
+    virtual double doubleRand();
+};
+
+Register_Class(cMersenneTwister);
+
+
+void cMersenneTwister::initialize(int runnumber, int id, cConfiguration *cfg)
+{
+    useThisRNG();
+
+    unsigned int seed = 0; //FIXME
+    mt19937_init_genrand(seed);
+}
+
+unsigned long cMersenneTwister::intRand()
+{
+    useThisRNG();
+    return mt19937_genrand_int31();
+}
+
+double cMersenneTwister::doubleRand()
+{
+    useThisRNG();
+    return mt19937_genrand_real2();
+}
+
--- src/sim/mt19937ar.h
+++ src/sim/mt19937ar.h
@@ -28,31 +28,31 @@ extern unsigned long *current_mt;
 extern int *current_mti_ptr;
 
 /* initializes mt[N] with a seed */
-void init_genrand(unsigned long s);
+void mt19937_init_genrand(unsigned long s);
 
 /* initialize by an array with array-length */
 /* init_key is the array for initializing keys */
 /* key_length is its length */
 /* slight change for C++, 2004/2/26 */
-void init_by_array(unsigned long init_key[], int key_length);
+void mt19937_init_by_array(unsigned long init_key[], int key_length);
 
 /* generates a random number on [0,0xffffffff]-interval */
-unsigned long genrand_int32(void);
+unsigned long mt19937_genrand_int32(void);
 
 /* generates a random number on [0,0x7fffffff]-interval */
-long genrand_int31(void);
+long mt19937_genrand_int31(void);
 
 /* generates a random number on [0,1]-real-interval */
-double genrand_real1(void);
+double mt19937_genrand_real1(void);
 
 /* generates a random number on [0,1)-real-interval */
-double genrand_real2(void);
+double mt19937_genrand_real2(void);
 
 /* generates a random number on (0,1)-real-interval */
-double genrand_real3(void);
+double mt19937_genrand_real3(void);
 
 /* generates a random number on [0,1) with 53-bit resolution*/
-double genrand_res53(void);
+double mt19937_genrand_res53(void);
 
 #endif
 
