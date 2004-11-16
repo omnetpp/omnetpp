6d41aa23ae0bd92ffbec2a43d06ca6db2beb3d0e
Andras Varga <andras@omnetpp.org>
2004-11-16 17:40:42 +0000

    *** empty log message ***

--- doc/src/tictoc-tutorial/tutorial.h
+++ doc/src/tictoc-tutorial/tutorial.h
@@ -72,25 +72,25 @@ achieved by writing a C++ file txc1.cc:
 to create the executable tictoc:
 
 \code
-opp_makemake
+$ opp_makemake
 \endcode
 
 This command should have now created a Makefile in the working directory
 tictoc.
 
-@note Windows+MSVC users: the command is opp_<i>n</i>makemake,
-and it will create Makefile.vc.
+@note Windows+MSVC users: the command is <tt>opp_<i>n</i>makemake</tt>,
+and it will create <tt>Makefile.vc</tt>.
 
 5. Let's now compile and link our very first simulation by issuing the make command:
 
 \code
-make
+$ make
 \endcode
 
-@note Windows+MSVC: type nmake -f Makefile.vc. If you get <i>'nmake' is not recognized
-as an internal or external command...</i>, find vcvars32.bat somewhere in the
-MSVC directories, and run it first thing in every command window in which you
-want to compile.
+@note Windows+MSVC: type <tt>nmake -f Makefile.vc</tt>.
+If you get <i>'nmake' is not recognized as an internal or external command...</i>,
+find <tt>vcvars32.bat</tt> somewhere in the MSVC directories, and run it first thing
+in every command window in which you want to compile.
 
 If there are compilation errors, you need to rectify those and repeat the make until
 you get an error-free compilation and linking.
@@ -119,12 +119,12 @@ in a dialog when it starts).
 command:
 
 \code
-./tictoc
+$ ./tictoc
 \endcode
 
 and hopefully you should now get the OMNeT++ simulation window.
 
-@note Windows: the command is just tictoc.
+@note Windows: the command is just <tt>tictoc</tt>.
 
 8. Press the Run button on the toolbar to start the simulation. What you should
 see is that tic and toc are exchanging messages with each other.
@@ -148,8 +148,6 @@ value.
 
 @subsection s2 Step 2: adding icons and debug output
 
-Complete sources: @ref f2
-
 // Here we make the model look a bit prettier in the GUI. We assign
 // the "proc1" icon (bitmaps/proc1.gif), and paint it cyan for `tic'
 // and yellow for `toc'.
@@ -164,13 +162,14 @@ lines from these modules. Observe that the messages "Hello World! I'm tic."
 and "Hello World! I'm toc." are only printed once at the beginning of the
 simulation.
 
+Sources: @ref tictoc2.ned, @ref txc2.cc, @ref omnetpp.ini
 
 
 @subsection s3 Step 3
 
 In this class we add a counter, and delete the message after ten exchanges.
 
-Complete sources: @ref f3
+Sources: @ref tictoc3.ned, @ref txc3.cc, @ref omnetpp.ini
 
 
 @subsection s4 Step 4
@@ -181,7 +180,7 @@ we'll turn the "magic number" 10 into a parameter.
 For that, we need to declare the parameter in the NED file, and
 then to get its value from the C++ code.
 
-Complete sources: @ref f4
+Sources: @ref tictoc4.ned, @ref txc4.cc, @ref omnetpp.ini
 
 
 @subsection s5 Step 5
@@ -197,7 +196,7 @@ specify when they should arrive back at the module.
 
 We leave out the counter, to keep the source code small.
 
-Complete sources: @ref f5
+Sources: @ref tictoc5.ned, @ref txc5.cc, @ref omnetpp.ini
 
 
 @subsection s6 Step 6
@@ -211,7 +210,7 @@ For tic, we hardcode the delayTime parameter into the NED file.
 For `toc', we leave the delayTime parameter unassigned in the NED file,
 and put the value into omnetpp.ini instead, to make it easier to change.
 
-Complete sources: @ref f6
+Sources: @ref tictoc6.ned, @ref txc6.cc, @ref omnetpp.ini
 
 
 @subsection s7 Step 7
@@ -223,7 +222,7 @@ if the packet doesn't arrive within a certain period, we'll assume it
 was lost and create another one. The timeout will be handled using
 (what else?) a self-message.
 
-Complete sources: @ref f7
+Sources: @ref tictoc7.ned, @ref txc7.cc, @ref omnetpp.ini
 
 
 @subsection s8 Step 8
@@ -233,7 +232,7 @@ retransmit. This is OK because the packet didn't contain much, but
 in real life it's usually more practical to keep a copy of the original
 packet so that we can re-send it without the need to build it again.
 
-Complete sources: @ref f8
+Sources: @ref tictoc8.ned, @ref txc8.cc, @ref omnetpp.ini
 
 
 <hr>
@@ -252,7 +251,7 @@ NED file is completely different:
 
 tictoc9.ned: @include tictoc9.ned
 
-Complete sources: @ref f9
+Sources: @ref tictoc9.ned, @ref txc9.cc, @ref omnetpp.ini
 
 
 @subsection s10 Step 10
@@ -270,7 +269,7 @@ To make the model execute longer, after a message arrives to its destination
 the destination node will generate another message with a random destination
 address, and so forth.
 
-Complete sources: @ref f10
+Sources: @ref tictoc10.ned, @ref tictoc10.msg, @ref txc10.cc, @ref omnetpp.ini
 
 
 @subsection s11 Step 11
@@ -283,98 +282,84 @@ with the Plove program.
 We also collect basic statistics (min, max, mean, std.dev.) and histogram
 about the hop count which we'll print out at the end of the simulation.
 
-Complete sources: @ref f11
+Sources: @ref tictoc11.ned, @ref tictoc11.msg, @ref txc11.cc, @ref omnetpp.ini
 
 */
 
 
-/**
-@page f1 Tictoc1 files
+/// @page tictoc1.ned tictoc1.ned
+/// @include tictoc1.ned
 
-tictoc1.ned: @include tictoc1.ned
-txc1.cc: @include txc1.cc
-omnetpp.ini: @include omnetpp.ini
-*/
+/// @page tictoc2.ned tictoc2.ned
+/// @include tictoc2.ned
 
-/**
-@page f2 Tictoc2 files
+/// @page tictoc3.ned tictoc3.ned
+/// @include tictoc3.ned
 
-tictoc2.ned: @include tictoc2.ned
-txc2.cc: @include txc2.cc
-omnetpp.ini: @include omnetpp.ini
-*/
+/// @page tictoc4.ned tictoc4.ned
+/// @include tictoc4.ned
 
-/**
-@page f3 Tictoc3 files
+/// @page tictoc5.ned tictoc5.ned
+/// @include tictoc5.ned
 
-tictoc3.ned: @include tictoc3.ned
-txc3.cc: @include txc3.cc
-omnetpp.ini: @include omnetpp.ini
-*/
+/// @page tictoc6.ned tictoc6.ned
+/// @include tictoc6.ned
 
-/**
-@page f4 Tictoc4 files
+/// @page tictoc7.ned tictoc7.ned
+/// @include tictoc7.ned
 
-tictoc4.ned: @include tictoc4.ned
-txc4.cc: @include txc4.cc
-omnetpp.ini: @include omnetpp.ini
-*/
+/// @page tictoc8.ned tictoc8.ned
+/// @include tictoc8.ned
 
-/**
-@page f5 Tictoc5 files
+/// @page tictoc9.ned tictoc9.ned
+/// @include tictoc9.ned
 
-tictoc5.ned: @include tictoc5.ned
-txc5.cc: @include txc5.cc
-omnetpp.ini: @include omnetpp.ini
-*/
+/// @page tictoc10.ned tictoc10.ned
+/// @include tictoc10.ned
 
-/**
-@page f6 Tictoc6 files
+/// @page tictoc11.ned tictoc11.ned
+/// @include tictoc11.ned
 
-tictoc6.ned: @include tictoc6.ned
-txc6.cc: @include txc6.cc
-omnetpp.ini: @include omnetpp.ini
-*/
+/// @page txc1.cc txc1.cc
+/// @include txc1.cc
 
-/**
-@page f7 Tictoc7 files
+/// @page txc2.cc txc2.cc
+/// @include txc2.cc
 
-tictoc7.ned: @include tictoc7.ned
-txc7.cc: @include txc7.cc
-omnetpp.ini: @include omnetpp.ini
-*/
+/// @page txc3.cc txc3.cc
+/// @include txc3.cc
 
-/**
-@page f8 Tictoc8 files
+/// @page txc4.cc txc4.cc
+/// @include txc4.cc
 
-tictoc8.ned: @include tictoc8.ned
-txc8.cc: @include txc8.cc
-omnetpp.ini: @include omnetpp.ini
-*/
+/// @page txc5.cc txc5.cc
+/// @include txc5.cc
 
-/**
-@page f9 Tictoc9 files
+/// @page txc6.cc txc6.cc
+/// @include txc6.cc
 
-tictoc9.ned: @include tictoc9.ned
-txc9.cc: @include txc9.cc
-omnetpp.ini: @include omnetpp.ini
-*/
+/// @page txc7.cc txc7.cc
+/// @include txc7.cc
 
-/**
-@page f10 Tictoc10 files
+/// @page txc8.cc txc8.cc
+/// @include txc8.cc
 
-tictoc10.ned: @include tictoc10.ned
-txc10.cc: @include txc10.cc
-tictoc10.msg: @include tictoc10.msg
-omnetpp.ini: @include omnetpp.ini
-*/
+/// @page txc9.cc txc9.cc
+/// @include txc9.cc
 
-/**
-@page f11 Tictoc11 files
+/// @page txc10.cc txc10.cc
+/// @include txc10.cc
 
-tictoc11.ned: @include tictoc11.ned
-txc11.cc: @include txc11.cc
-tictoc11.msg: @include tictoc11.msg
-omnetpp.ini: @include omnetpp.ini
-*/
+/// @page txc11.cc txc11.cc
+/// @include txc11.cc
+
+/// @page tictoc10.msg tictoc10.msg
+/// @include tictoc10.msg
+
+/// @page tictoc11.msg tictoc11.msg
+/// @include tictoc11.msg
+
+/// @page omnetpp.ini omnetpp.ini
+/// @include omnetpp.ini
+
 
