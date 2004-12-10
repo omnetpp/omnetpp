3f0b2fe51c8df2c810ead1479609a7c93089785b
Andras Varga <andras@omnetpp.org>
2004-12-10 11:49:55 +0000

    *** empty log message ***

--- test/core/runtest.cmd
+++ test/core/runtest.cmd
@@ -2,8 +2,14 @@
 rem 
 rem usage: runtest [<testfile>...]
 rem without args, runs all *.test files in the current directory
-rem uncomment opp_test line with -N to test with dynamic NED loading
 rem
+
+rem Use -N to test with dynamic NED loading
+:set FLAG=
+set FLAG=-N
+
+
+rem --- nothing to change below this line ---
 
 set TESTFILES=%*
 if "x%TESTFILES%" == "x" set TESTFILES=*.test
@@ -12,15 +18,15 @@ path %~dp0\..\bin;%PATH%
 mkdir work 2>nul
 del work\work.exe 2>nul
 
-call opp_test -g -v %TESTFILES% || goto end
+call opp_test %OPT% -g -v %TESTFILES% || goto end
 
 cd work || goto end
-call opp_nmakemake -f -e cc -u cmdenv || goto end
+call opp_nmakemake %OPT% -f -e cc -u cmdenv || goto end
 nmake -f makefile.vc || cd .. && goto end
 cd .. || goto end
 
 :#call opp_test -r -v %TESTFILES% || goto end
-call opp_test -N -r -v %TESTFILES% || goto end
+call opp_test %OPT%-r -v %TESTFILES% || goto end
 
 echo.
 echo Results can be found in work/
