ac469cbdc33074264696335794bef6b29702a938
Andras Varga <andras@omnetpp.org>
2004-12-13 18:59:14 +0000

    added

---
+++ src/sim/parsim/parsimutil.cc
@@ -0,0 +1,44 @@
+//=========================================================================
+//
+// PARSIMUTIL.CC - part of
+//                          OMNeT++
+//           Discrete System Simulation in C++
+//
+//   Written by:  Andras Varga, 2003
+//
+//=========================================================================
+
+/*--------------------------------------------------------------*
+  Copyright (C) 2004 Andras Varga
+  Monash University, Dept. of Electrical and Computer Systems Eng.
+  Melbourne, Australia
+
+  This file is distributed WITHOUT ANY WARRANTY. See the file
+  `license' for details on this and other legal matters.
+*--------------------------------------------------------------*/
+
+#include <string.h>
+#include "parsimutil.h"
+#include "cexception.h"
+
+
+void getProcIdFromCommandLineArgs(int& myProcId, int& numPartitions, const char *caller)
+{
+    int argc = ev.argCount();
+    char **argv = ev.argVector();
+    int i;
+    for (i=1; i<argc; i++)
+        if (argv[i][0]=='-' && argv[i][1]=='p')
+            break;
+    if (i==argc)
+        throw new cException("%s: missing -p<procId>,<numPartitions> switch on the command line", caller);
+
+    char *parg = argv[i];
+    myProcId = atoi(parg+2);
+    char *s = parg;
+    while (*s!=',' && *s) s++;
+    numPartitions = (*s) ? atoi(s+1) : 0;
+    if (myProcId<0 || numPartitions<=0 || myProcId>=numPartitions)
+        throw new cException("%s: invalid switch '%s' -- should have the format -p<procId>,<numPartitions>",
+                             caller, parg);
+}
---
+++ src/sim/parsim/parsimutil.h
@@ -0,0 +1,32 @@
+//=========================================================================
+//
+// PARSIMUTIL.H - part of
+//                          OMNeT++
+//           Discrete System Simulation in C++
+//
+//   Written by:  Andras Varga
+//
+//=========================================================================
+
+/*--------------------------------------------------------------*
+  Copyright (C) 2004 Andras Varga
+  Monash University, Dept. of Electrical and Computer Systems Eng.
+  Melbourne, Australia
+
+  This file is distributed WITHOUT ANY WARRANTY. See the file
+  `license' for details on this and other legal matters.
+*--------------------------------------------------------------*/
+
+#ifndef __PARSIMUTIL_H
+#define __PARSIMUTIL_H
+
+
+/**
+ * Expects a -p<procId>,<numPartitions> command line argument, and parses it
+ * into the variables passed. Throws exception on error.
+ */
+void getProcIdFromCommandLineArgs(int& myProcId, int& numPartitions, 
+                                  const char *caller);
+
+#endif
+
