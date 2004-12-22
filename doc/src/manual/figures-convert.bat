899a2a7ac9d247271e7d4c627853e322013ddec8
Andras Varga <andras@omnetpp.org>
2004-12-22 17:05:06 +0000

    *** empty log message ***

--- doc/src/manual/figures-convert.bat
+++ doc/src/manual/figures-convert.bat
@@ -3,10 +3,14 @@ set IM6=D:\Program Files\ImageMagick-6.1.6-Q8
 
 cd figures
 
-:this sucks: for %%I in (*.wmf) do echo %%I to %%~nI.gif && "%IM6%"\convert -density 300 -trim %%I %%~nI.gif
+: *** WMF-to-GIF (doesn't work properly: hairlines get lost) ***
+: for %%I in (*.wmf) do echo %%I to %%~nI.gif && "%IM6%"\convert -density 300 -trim %%I %%~nI.gif
 
+: *** trim GIFs ***
 for %%I in (*.gif) do echo crop %%I && "%IM6%"\convert -trim %%I %%I
+
+: *** PDF conversion follows (commented out because rasterized PDFs look very bad) ***
 : convert to png because the gifs still contain some garbage meta-tag which screws up pdf
-for %%I in (*.gif) do echo %%I to %%~nI.png && "%IM6%"\convert %%I %%~nI.png
-for %%I in (*.png) do echo %%I to %%~nI.pdf && "%IM6%"\convert %%I %%~nI.pdf
-del *.png
+: for %%I in (*.gif) do echo %%I to %%~nI.png && "%IM6%"\convert %%I %%~nI.png
+: for %%I in (*.png) do echo %%I to %%~nI.pdf && "%IM6%"\convert %%I %%~nI.pdf
+: del *.png
