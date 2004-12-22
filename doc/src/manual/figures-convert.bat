ea9f21acff307f46ef148e234d8551b91648e842
Andras Varga <andras@omnetpp.org>
2004-12-22 13:05:24 +0000

    *** empty log message ***

--- doc/src/manual/figures-convert.bat
+++ doc/src/manual/figures-convert.bat
@@ -3,6 +3,6 @@ set IM6=D:\Program Files\ImageMagick-6.1.6-Q8
 
 cd figures
 
-for %%I in (*.wmf) do "%IM6%"\convert %%I -trim %%~dpnI.gif
-for %%I in (*.gif) do "%IM6%"\convert %%I %%~dpnI.pdf
+for %%I in (*.wmf) do echo %%I to %%~nI.gif && "%IM6%"\convert -density 150 -trim %%I %%~nI.gif
+for %%I in (*.gif) do echo %%I to %%~nI.pdf && "%IM6%"\convert %%I %%~nI.pdf
 
