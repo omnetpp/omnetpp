8f6d476508794876a70c2dce8709cb4c00f05dd0
Andras Varga <andras@omnetpp.org>
2005-01-10 18:25:34 +0000

    *** empty log message ***

--- include/platdep/fileglobber.h
+++ include/platdep/fileglobber.h
@@ -17,25 +17,11 @@
 #define __PLATDEP_FILEGLOBBER_H
 
 #ifdef _MSC_VER
-#include <process.h>
-#include <direct.h>
 #include <io.h>
-#include <direct.h>
 #include <stdlib.h> // _MAX_PATH
-
-#define getpid _getpid
-#define getcwd _getcwd
-#define chdir  _chdir
-#define usleep(x) _sleep((x)/1000)
-#define stat _stat
-#define mkdir(x,y) _mkdir(x)
-
 #else
-
 #include <sys/types.h>
-#include <unistd.h>  // getpid(), getcwd(), etc
 #include <glob.h>
-
 #endif
 
 #include <string>
