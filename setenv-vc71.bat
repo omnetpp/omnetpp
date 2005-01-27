ff76aa75f187cd0faf89f8243f8126a5180b6efd
Andras Varga <andras@omnetpp.org>
2005-01-27 16:15:40 +0000

    *** empty log message ***

--- setenv-vc71.bat
+++ setenv-vc71.bat
@@ -3,6 +3,8 @@ set INCLUDE=
 set LIB=
 set PATH=%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem
 
+:XXX for stock .NET 2003 installation, use:
+:XXX "%VS71COMNTOOLS%vsvars32.bat"
 call D:\home\tools\vc71\vcvars32.bat
 call d:\home\tools\setenv.cmd
 PATH %~dp0\bin;%PATH%
