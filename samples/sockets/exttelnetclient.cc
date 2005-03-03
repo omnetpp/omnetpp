60a5659d4f8f038e91cf91acfaa8284371194814
Andras Varga <andras@omnetpp.org>
2005-03-03 05:42:15 +0000

    *** empty log message ***

---
+++ samples/sockets/exttelnetclient.cc
@@ -0,0 +1,84 @@
+//
+// This file is part of an OMNeT++/OMNEST simulation example.
+//
+// Copyright (C) 1992-2005 Andras Varga
+//
+// This file is distributed WITHOUT ANY WARRANTY. See the file
+// `license' for details on this and other legal matters.
+//
+
+
+#include <omnetpp.h>
+#include "telnetpkt_m.h"
+#include "csocketrtscheduler.h"
+
+
+/**
+ * Simple model of a Telnet client.
+ */
+class ExtTelnetClient : public cSimpleModule
+{
+  public:
+    Module_Class_Members(ExtTelnetClient,cSimpleModule,0);
+
+    cMessage *rtEvent;
+    cSocketRTScheduler *rtScheduler;
+
+    char recvBuffer[4000];
+    int numRecvBytes;
+
+    int addr;
+    int srvAddr;
+
+    virtual void initialize();
+    virtual void handleMessage(cMessage *msg);
+    void handleSocketEvent();
+    void handleReply(TelnetPkt *telnetReply);
+};
+
+Define_Module(ExtTelnetClient);
+
+void ExtTelnetClient::initialize()
+{
+    rtEvent = new cMessage("rtEvent");
+    rtScheduler = check_and_cast<cSocketRTScheduler *>(simulation.scheduler());
+    rtScheduler->setInterfaceModule(this, rtEvent, recvBuffer, 4000, &numRecvBytes);
+
+    addr = par("addr");
+    srvAddr = par("srvAddr");
+
+    displayString().setTagArg("t",0,"Start: telnet localhost 4242\nThen if on Unix: ^] \"mode character\"");
+}
+
+void ExtTelnetClient::handleMessage(cMessage *msg)
+{
+    if (msg==rtEvent)
+        handleSocketEvent();
+    else
+        handleReply(check_and_cast<TelnetPkt *>(msg));
+}
+
+void ExtTelnetClient::handleSocketEvent()
+{
+    // get data from buffer
+    std::string text = std::string(recvBuffer, numRecvBytes);
+    numRecvBytes = 0;
+
+    // assemble and send Telnet packet
+    TelnetPkt *telnetPkt = new TelnetPkt();
+    telnetPkt->setPayload(text.c_str());
+    telnetPkt->setName(text.c_str());
+    telnetPkt->setDestAddress(srvAddr);
+    telnetPkt->setSrcAddress(addr);
+
+    send(telnetPkt,"out");
+}
+
+void ExtTelnetClient::handleReply(TelnetPkt *telnetReply)
+{
+    const char *reply = telnetReply->getPayload();
+    rtScheduler->sendBytes(reply, strlen(reply));
+    delete telnetReply;
+}
+
+
