3a7a9718e88c97eeccff98b44fb8795a9458d471
Andras Varga <andras@omnetpp.org>
2005-03-03 00:26:03 +0000

    *** empty log message ***

---
+++ include/platdep/fileutil.h
@@ -0,0 +1,51 @@
+//==========================================================================
+//  PLATDEP/FILEUTIL.H - part of
+//                     OMNeT++/OMNEST
+//            Discrete System Simulation in C++
+//
+//
+//==========================================================================
+
+/*--------------------------------------------------------------*
+  Copyright (C) 1992-2004 Andras Varga
+
+  This file is distributed WITHOUT ANY WARRANTY. See the file
+  `license' for details on this and other legal matters.
+*--------------------------------------------------------------*/
+
+#ifndef __PLATDEP_FILEUTIL_H
+#define __PLATDEP_FILEUTIL_H
+
+#include <string>
+
+inline void splitFileName(const char *pathname, std::string& dir, std::string& fnameonly)
+{
+    if (!pathname || !*pathname)
+    {
+         dir = ".";
+         fnameonly = "";
+         return;
+    }
+
+    dir = pathname;
+
+    // find last "/" or "\"
+    const char *s = pathname + strlen(pathname) - 1;
+    while (s>=pathname && *s!='\\' && *s!='/') s--;
+
+    // split along that
+    if (s<pathname)
+    {
+        fnameonly = pathname;
+        dir = ".";
+    }
+    else
+    {
+        fnameonly = s+1;
+        dir = "";
+        dir.append(pathname, s-pathname+1);
+    }
+}
+
+#endif
+
