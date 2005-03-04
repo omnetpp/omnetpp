12a9899ccdcf35c294148e7b61232333fc3e2841
Andras Varga <andras@omnetpp.org>
2005-03-04 12:20:12 +0000

    *** empty log message ***

--- samples/cpp2cc.bat
+++ samples/cpp2cc.bat
@@ -3,4 +3,4 @@ rem
 rem Renaming all .cpp files back to .cc
 rem
 
-for %%i in (aloha cqn dyna fddi fifo hcube hist neddemo p-to-p queueing routing queues tictoc tokenring) do ren %%i\*.cpp *.cc
+for %%i in (aloha cqn dyna fddi fifo hcube hist neddemo p-to-p queueing routing queues tictoc tokenring sockets) do ren %%i\*.cpp *.cc
