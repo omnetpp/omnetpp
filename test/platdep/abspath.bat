029c969ec29a14976e3cd9eee2f65e3f5d5fef44
Andras Varga <andras@omnetpp.org>
2005-03-18 03:44:48 +0000

    *** empty log message ***

--- test/platdep/abspath.bat
+++ test/platdep/abspath.bat
@@ -5,27 +5,32 @@ abspath c:\dir
 abspath c:/dir
 abspath \\unc\foo
 abspath //unc/foo
+echo.
 
 :: no drive + relative dir
 abspath .
 abspath dir
 abspath dir/dir2
 abspath dir\dir2
+echo.
 
 :: drive + relative dir
 abspath c:dir
 abspath c:dir/dir2
 abspath c:dir\dir2
+echo.
 
 :: nonexistent or invalid drives
 abspath ?:dir
 abspath t:dir
 abspath t:dir/dir2
 abspath t:dir\dir2
+echo.
 
 :: no drive + absolute dir
 abspath \dir
 abspath /dir
+echo.
 
 echo ---------
 
@@ -34,3 +39,17 @@ abspath c:\bla c:\dir
 abspath c:/bla c:/dir
 abspath \bla c:\dir
 abspath /bla c:/dir
+echo.
+
+:: path has drive+relative dir, basedir has same drive+absolute dir
+abspath c:\base c:dir
+abspath c:/base c:dir
+echo.
+
+:: path has drive+relative dir, but basedir cannot be applied
+abspath \base c:dir
+abspath /base c:dir
+abspath x:\base c:dir
+abspath x:/base c:dir
+abspath x:base c:dir
+echo.
