a427b5996f5143a642264bae1d5d2324c1e06bd5
Andras Varga <andras@omnetpp.org>
2005-03-18 03:38:35 +0000

    *** empty log message ***

--- test/platdep/abspath.bat
+++ test/platdep/abspath.bat
@@ -1,22 +1,36 @@
-@abspath c:\dir
-@abspath c:/dir
-@abspath \\unc\foo
-@abspath //unc/foo
+@echo off
 
-@abspath .
-@abspath dir
-@abspath dir/dir2
-@abspath dir\dir2
+:: drive + absolute dir
+abspath c:\dir
+abspath c:/dir
+abspath \\unc\foo
+abspath //unc/foo
 
-@abspath c:dir
-@abspath c:dir/dir2
-@abspath c:dir\dir2
+:: no drive + relative dir
+abspath .
+abspath dir
+abspath dir/dir2
+abspath dir\dir2
 
-@abspath t:dir
-@abspath t:dir/dir2
-@abspath t:dir\dir2
+:: drive + relative dir
+abspath c:dir
+abspath c:dir/dir2
+abspath c:dir\dir2
 
-@abspath ?:dir
+:: nonexistent or invalid drives
+abspath ?:dir
+abspath t:dir
+abspath t:dir/dir2
+abspath t:dir\dir2
 
-@abspath \dir
-@abspath /dir
+:: no drive + absolute dir
+abspath \dir
+abspath /dir
+
+echo ---------
+
+:: path (arg2) absolute, basedir (arg1) ignored
+abspath c:\bla c:\dir
+abspath c:/bla c:/dir
+abspath \bla c:\dir
+abspath /bla c:/dir
