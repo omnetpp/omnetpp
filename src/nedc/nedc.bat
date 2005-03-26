1a259dc51bd10990bfba6a93e7bcb48dc0613691
Andras Varga <andras@omnetpp.org>
2005-03-26 02:02:04 +0000

    *** empty log message ***

---
+++ src/nedc/nedc.bat
@@ -0,0 +1,5 @@
+@echo off
+echo ERROR: The nedc program no longer exists. It has been replaced by the 
+echo multi-functional and more mature nedtool. Change 'nedc' to 'nedtool'
+echo in your makefiles, or re-generate them.
+pause
