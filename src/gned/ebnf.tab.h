07ad61c5e3d8ea506f7ccb69fd8939eea8319578
Andras Varga <andras@omnetpp.org>
2005-07-10 08:58:23 +0000

    *** empty log message ***

--- src/gned/ebnf.tab.h
+++ src/gned/ebnf.tab.h
@@ -1,98 +1,212 @@
-#ifndef YYSTYPE
-#define YYSTYPE int
+/* A Bison parser, made by GNU Bison 1.875.  */
+
+/* Skeleton parser for Yacc-like parsing with Bison,
+   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.  */
+
+/* As a special exception, when this file is copied by Bison into a
+   Bison output file, you may use that output file without restriction.
+   This special exception was added by the Free Software Foundation
+   in version 1.24 of Bison.  */
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     INCLUDE = 258,
+     SIMPLE = 259,
+     CHANNEL = 260,
+     DELAY = 261,
+     ERROR = 262,
+     DATARATE = 263,
+     MODULE = 264,
+     PARAMETERS = 265,
+     GATES = 266,
+     GATESIZES = 267,
+     SUBMODULES = 268,
+     CONNECTIONS = 269,
+     DISPLAY = 270,
+     IN = 271,
+     OUT = 272,
+     NOCHECK = 273,
+     LEFT_ARROW = 274,
+     RIGHT_ARROW = 275,
+     FOR = 276,
+     TO = 277,
+     DO = 278,
+     IF = 279,
+     LIKE = 280,
+     NETWORK = 281,
+     ENDSIMPLE = 282,
+     ENDMODULE = 283,
+     ENDCHANNEL = 284,
+     ENDNETWORK = 285,
+     ENDFOR = 286,
+     MACHINES = 287,
+     ON = 288,
+     IO_INTERFACES = 289,
+     IFPAIR = 290,
+     INTCONSTANT = 291,
+     REALCONSTANT = 292,
+     NAME = 293,
+     STRING = 294,
+     _TRUE = 295,
+     _FALSE = 296,
+     INPUT = 297,
+     REF = 298,
+     ANCESTOR = 299,
+     NED_CONST = 300,
+     NUMERICTYPE = 301,
+     STRINGTYPE = 302,
+     BOOLTYPE = 303,
+     XMLTYPE = 304,
+     ANYTYPE = 305,
+     PLUS = 306,
+     MIN = 307,
+     MUL = 308,
+     DIV = 309,
+     MOD = 310,
+     EXP = 311,
+     SIZEOF = 312,
+     SUBMODINDEX = 313,
+     PLUSPLUS = 314,
+     EQ = 315,
+     NE = 316,
+     GT = 317,
+     GE = 318,
+     LS = 319,
+     LE = 320,
+     AND = 321,
+     OR = 322,
+     XOR = 323,
+     NOT = 324,
+     BIN_AND = 325,
+     BIN_OR = 326,
+     BIN_XOR = 327,
+     BIN_COMPL = 328,
+     SHIFT_LEFT = 329,
+     SHIFT_RIGHT = 330,
+     INVALID_CHAR = 331,
+     UMIN = 332
+   };
 #endif
+#define INCLUDE 258
+#define SIMPLE 259
+#define CHANNEL 260
+#define DELAY 261
+#define ERROR 262
+#define DATARATE 263
+#define MODULE 264
+#define PARAMETERS 265
+#define GATES 266
+#define GATESIZES 267
+#define SUBMODULES 268
+#define CONNECTIONS 269
+#define DISPLAY 270
+#define IN 271
+#define OUT 272
+#define NOCHECK 273
+#define LEFT_ARROW 274
+#define RIGHT_ARROW 275
+#define FOR 276
+#define TO 277
+#define DO 278
+#define IF 279
+#define LIKE 280
+#define NETWORK 281
+#define ENDSIMPLE 282
+#define ENDMODULE 283
+#define ENDCHANNEL 284
+#define ENDNETWORK 285
+#define ENDFOR 286
+#define MACHINES 287
+#define ON 288
+#define IO_INTERFACES 289
+#define IFPAIR 290
+#define INTCONSTANT 291
+#define REALCONSTANT 292
+#define NAME 293
+#define STRING 294
+#define _TRUE 295
+#define _FALSE 296
+#define INPUT 297
+#define REF 298
+#define ANCESTOR 299
+#define NED_CONST 300
+#define NUMERICTYPE 301
+#define STRINGTYPE 302
+#define BOOLTYPE 303
+#define XMLTYPE 304
+#define ANYTYPE 305
+#define PLUS 306
+#define MIN 307
+#define MUL 308
+#define DIV 309
+#define MOD 310
+#define EXP 311
+#define SIZEOF 312
+#define SUBMODINDEX 313
+#define PLUSPLUS 314
+#define EQ 315
+#define NE 316
+#define GT 317
+#define GE 318
+#define LS 319
+#define LE 320
+#define AND 321
+#define OR 322
+#define XOR 323
+#define NOT 324
+#define BIN_AND 325
+#define BIN_OR 326
+#define BIN_XOR 327
+#define BIN_COMPL 328
+#define SHIFT_LEFT 329
+#define SHIFT_RIGHT 330
+#define INVALID_CHAR 331
+#define UMIN 332
 
-#ifndef YYLTYPE
-typedef
-  struct yyltype
-    {
-      int timestamp;
-      int first_line;
-      int first_column;
-      int last_line;
-      int last_column;
-      char *text;
-   }
-  yyltype;
 
-#define YYLTYPE yyltype
+
+
+#if ! defined (YYSTYPE) && ! defined (YYSTYPE_IS_DECLARED)
+typedef int YYSTYPE;
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+# define YYSTYPE_IS_TRIVIAL 1
 #endif
-
-#define	INCLUDE	257
-#define	SIMPLE	258
-#define	CHANNEL	259
-#define	DELAY	260
-#define	ERROR	261
-#define	DATARATE	262
-#define	MODULE	263
-#define	PARAMETERS	264
-#define	GATES	265
-#define	GATESIZES	266
-#define	SUBMODULES	267
-#define	CONNECTIONS	268
-#define	DISPLAY	269
-#define	IN	270
-#define	OUT	271
-#define	NOCHECK	272
-#define	LEFT_ARROW	273
-#define	RIGHT_ARROW	274
-#define	FOR	275
-#define	TO	276
-#define	DO	277
-#define	IF	278
-#define	LIKE	279
-#define	NETWORK	280
-#define	ENDSIMPLE	281
-#define	ENDMODULE	282
-#define	ENDCHANNEL	283
-#define	ENDNETWORK	284
-#define	ENDFOR	285
-#define	MACHINES	286
-#define	ON	287
-#define	IO_INTERFACES	288
-#define	IFPAIR	289
-#define	INTCONSTANT	290
-#define	REALCONSTANT	291
-#define	NAME	292
-#define	STRING	293
-#define	_TRUE	294
-#define	_FALSE	295
-#define	INPUT	296
-#define	REF	297
-#define	ANCESTOR	298
-#define	NED_CONST	299
-#define	NUMERICTYPE	300
-#define	STRINGTYPE	301
-#define	BOOLTYPE	302
-#define	XMLTYPE	303
-#define	ANYTYPE	304
-#define	PLUS	305
-#define	MIN	306
-#define	MUL	307
-#define	DIV	308
-#define	MOD	309
-#define	EXP	310
-#define	SIZEOF	311
-#define	SUBMODINDEX	312
-#define	PLUSPLUS	313
-#define	EQ	314
-#define	NE	315
-#define	GT	316
-#define	GE	317
-#define	LS	318
-#define	LE	319
-#define	AND	320
-#define	OR	321
-#define	XOR	322
-#define	NOT	323
-#define	BIN_AND	324
-#define	BIN_OR	325
-#define	BIN_XOR	326
-#define	BIN_COMPL	327
-#define	SHIFT_LEFT	328
-#define	SHIFT_RIGHT	329
-#define	INVALID_CHAR	330
-#define	UMIN	331
-
 
 extern YYSTYPE yylval;
+
+#if ! defined (YYLTYPE) && ! defined (YYLTYPE_IS_DECLARED)
+typedef struct YYLTYPE
+{
+  int first_line;
+  int first_column;
+  int last_line;
+  int last_column;
+} YYLTYPE;
+# define yyltype YYLTYPE /* obsolescent; will be withdrawn */
+# define YYLTYPE_IS_DECLARED 1
+# define YYLTYPE_IS_TRIVIAL 1
+#endif
+
+extern YYLTYPE yylloc;
+
+
