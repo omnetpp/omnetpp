6092f8270b793991f3b12c4335e02d2faa726b6f
Andras Varga <andras@omnetpp.org>
2005-07-23 13:37:49 +0000

    *** empty log message ***

--- test/tkenv/watch/watchtest.cc
+++ test/tkenv/watch/watchtest.cc
@@ -10,7 +10,31 @@ Define_Module(WatchTest);
 
 void WatchTest::activity()
 {
+    bool b1 = true;
+    bool b2 = false;
+    char c = 'a';
+    unsigned char uc = 'b';
+    signed char sc = 'c';
+    short s = -1234;
+    unsigned short us = 1234;
+    int i = -123456;
+    unsigned int ui = 123456;
+    long l = -7654321;
+    unsigned long ul = 7654321;
+    std::string str = "some string";
 
-    for(;;) {new cMessage("heyho"); wait(1);}
+    WATCH(b1);
+    WATCH(b2);
+    WATCH(c;
+    WATCH(uc);
+    WATCH(sc);
+    WATCH(s);
+    WATCH(us);
+    WATCH(i);
+    WATCH(ui);
+    WATCH(l);
+    WATCH(ul);
+
+    for(;;) wait(1);
 }
 
