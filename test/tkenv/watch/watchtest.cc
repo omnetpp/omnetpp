6ccc701ace3b69fd2c440599870de868581e5f99
Andras Varga <andras@omnetpp.org>
2005-07-23 14:35:42 +0000

    *** empty log message ***

--- test/tkenv/watch/watchtest.cc
+++ test/tkenv/watch/watchtest.cc
@@ -8,6 +8,23 @@ class WatchTest : public cSimpleModule
 
 Define_Module(WatchTest);
 
+struct Point
+{
+    int x; int y;
+    Point(int x1, int y1) {x=x1; y=y1;}
+};
+
+std::ostream& operator<<(std::ostream& os, const Point& p)
+{
+    return os << "(" << p.x << "," << p.y << ")";
+}
+
+std::istream& operator>>(std::istream& is, Point& p)
+{
+    char dummy;
+    return is >> dummy >> p.x >> dummy >> p.y >> dummy;
+}
+
 void WatchTest::activity()
 {
     bool b1 = true;
@@ -34,6 +51,13 @@ void WatchTest::activity()
     WATCH(ui);
     WATCH(l);
     WATCH(ul);
+    WATCH(str);
+
+    Point point(100,200);
+    WATCH(point);
+
+    Point point_rw(100,200);
+    WATCH_RW(point_rw);
 
     for(;;) wait(1);
 }
