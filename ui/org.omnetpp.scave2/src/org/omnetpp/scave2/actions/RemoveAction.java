9d65558a6e6298b0d2a8dbb50d5e4975db00a809
Tamas Borbely <tamas.borbely@omnest.com>
2006-07-13 16:29:13 +0000

    todo added

--- ui/org.omnetpp.scave2/src/org/omnetpp/scave2/actions/RemoveAction.java
+++ ui/org.omnetpp.scave2/src/org/omnetpp/scave2/actions/RemoveAction.java
@@ -1,6 +1,7 @@
 package org.omnetpp.scave2.actions;
 
 import org.eclipse.emf.common.command.Command;
+import org.eclipse.emf.edit.command.DeleteCommand;
 import org.eclipse.emf.edit.command.RemoveCommand;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.omnetpp.scave2.editors.ScaveEditor;
@@ -17,7 +18,11 @@ public class RemoveAction extends AbstractScaveAction {
 	@Override
 	protected void doRun(ScaveEditor scaveEditor, IStructuredSelection structuredSelection) {
 		// use EMF.Edit Framework do to the removal (this makes it undoable)
-		Command command = RemoveCommand.create(scaveEditor.getEditingDomain(), structuredSelection.toList());
+		// the DeleteCommand removes the references to the deleted items too
+		// XXX: if the selection done through a reference delete the reference only (now the referenced object is deleted)
+		Command command = DeleteCommand.create(
+							scaveEditor.getEditingDomain(),
+							structuredSelection.toList());
 		scaveEditor.getEditingDomain().getCommandStack().execute(command);
 	}
 
