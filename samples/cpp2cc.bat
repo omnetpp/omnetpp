ccf8ad3bc04bac6bd07e9f7e8c7b0203503f3f63
Andras Varga <andras@omnetpp.org>
2006-12-14 20:28:15 +0000

    removed MSVC 6.0 project files, and now there's no need to rename .cc files to .cpp either!

--- samples/cpp2cc.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-rem
-rem Renaming all .cpp files back to .cc
-rem
-
-for %%i in (aloha cqn dyna fifo hcube hist neddemo p-to-p queueing routing queues tictoc tokenring sockets) do ren %%i\*.cpp *.cc
