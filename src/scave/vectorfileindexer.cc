57b9b47537fec1d56658ee9ef5c05adf75bf0426
Andras Varga <andras@omnetpp.org>
2007-03-11 21:08:47 +0000

    indentation

--- src/scave/vectorfileindexer.cc
+++ src/scave/vectorfileindexer.cc
@@ -47,94 +47,95 @@ static std::string createTempFileName(const std::string baseFileName)
 
 void VectorFileIndexer::generateIndex(const char* fileName)
 {
-        // load file
-        ResultFileManager resultFileManager;
-        ResultFile *f = resultFileManager.loadFile(fileName); // TODO: limit number of lines read
-        if (!f)
-        {
-            throw opp_runtime_error("Error: %s: load() returned null", fileName);
-        }
-        else if (f->numUnrecognizedLines>0)
-        {
-            fprintf(stderr, "WARNING: %s: %d invalid/incomplete lines out of %d\n", fileName, f->numUnrecognizedLines, f->numLines);
-        }
+    // load file
+    ResultFileManager resultFileManager;
+    ResultFile *f = resultFileManager.loadFile(fileName); // TODO: limit number of lines read
+    if (!f)
+    {
+        throw opp_runtime_error("Error: %s: load() returned null", fileName);
+    }
+    else if (f->numUnrecognizedLines>0)
+    {
+        fprintf(stderr, "WARNING: %s: %d invalid/incomplete lines out of %d\n", fileName, f->numUnrecognizedLines, f->numLines);
+    }
 
-        RunList runs = resultFileManager.getRunsInFile(f);
-        if (runs.size() != 1)
-        {
-            if (runs.size() == 0)
-                fprintf(stderr, "WARNING: %s: contains no runs\n", fileName);
-            else
-                fprintf(stderr, "WARNING: %s: contains %d runs, expected 1\n", fileName, (int)runs.size());
-            return;
-        }
+    RunList runs = resultFileManager.getRunsInFile(f);
+    if (runs.size() != 1)
+    {
+        if (runs.size() == 0)
+            fprintf(stderr, "WARNING: %s: contains no runs\n", fileName);
+        else
+            fprintf(stderr, "WARNING: %s: contains %d runs, expected 1\n", fileName, (int)runs.size());
+        return;
+    }
 
-        Run *runRef = runs[0];
+    Run *runRef = runs[0];
 
-        //
-        // assemble dataflow network for vectors
-        //
-        DataflowManager *dataflowManager = new DataflowManager;
+    //
+    // assemble dataflow network for vectors
+    //
+    DataflowManager *dataflowManager = new DataflowManager;
 
-        // create filereader node
-        NodeType *readerNodeType=NodeTypeRegistry::instance()->getNodeType("vectorfilereader");
-        StringMap attrs;
-        attrs["filename"] = fileName;
-        VectorFileReaderNode *reader = (VectorFileReaderNode*)readerNodeType->create(dataflowManager, attrs);
+    // create filereader node
+    NodeType *readerNodeType=NodeTypeRegistry::instance()->getNodeType("vectorfilereader");
+    StringMap attrs;
+    attrs["filename"] = fileName;
+    VectorFileReaderNode *reader = (VectorFileReaderNode*)readerNodeType->create(dataflowManager, attrs);
 
-        // create filewriter node
-        NodeType *writerNodeType=NodeTypeRegistry::instance()->getNodeType("indexedvectorfilewriter");
-        std::string tmpFileName=createTempFileName(fileName);
-        std::string indexFileName=IndexFile::getIndexFileName(fileName);
-        std::string tmpIndexFileName=createTempFileName(indexFileName);
-        attrs.clear();
-        attrs["fileheader"]= generateHeader(runRef);
-        attrs["blocksize"]="65536";
-        attrs["filename"]=tmpFileName;
-        attrs["indexfilename"]=tmpIndexFileName;
-        IndexedVectorFileWriterNode *writer = (IndexedVectorFileWriterNode*)writerNodeType->create(dataflowManager, attrs);
-        writer->setRun(runRef->runName.c_str(), runRef->attributes, runRef->moduleParams);
+    // create filewriter node
+    NodeType *writerNodeType=NodeTypeRegistry::instance()->getNodeType("indexedvectorfilewriter");
+    std::string tmpFileName=createTempFileName(fileName);
+    std::string indexFileName=IndexFile::getIndexFileName(fileName);
+    std::string tmpIndexFileName=createTempFileName(indexFileName);
+    attrs.clear();
+    attrs["fileheader"]= generateHeader(runRef);
+    attrs["blocksize"]="65536";
+    attrs["filename"]=tmpFileName;
+    attrs["indexfilename"]=tmpIndexFileName;
+    IndexedVectorFileWriterNode *writer = (IndexedVectorFileWriterNode*)writerNodeType->create(dataflowManager, attrs);
+    writer->setRun(runRef->runName.c_str(), runRef->attributes, runRef->moduleParams);
 
-        // create a ports for each vector on reader node and writer node and connect them
-        IDList vectorIDList = resultFileManager.getAllVectors();
-        for (int i=0; i<(int)vectorIDList.size(); i++)
-        {
-             const VectorResult& vector = resultFileManager.getVector(vectorIDList.get(i));
-             Port *readerPort = reader->addVector(vector);
-             Port *writerPort = writer->addVector(vector);
-             dataflowManager->connect(readerPort, writerPort);
-        }
+    // create a ports for each vector on reader node and writer node and connect them
+    IDList vectorIDList = resultFileManager.getAllVectors();
+    for (int i=0; i<(int)vectorIDList.size(); i++)
+    {
+         const VectorResult& vector = resultFileManager.getVector(vectorIDList.get(i));
+         Port *readerPort = reader->addVector(vector);
+         Port *writerPort = writer->addVector(vector);
+         dataflowManager->connect(readerPort, writerPort);
+    }
 
-        // run!
-        try
-        {
-            dataflowManager->execute();
-        }
-        catch (std::exception &e)
-        {
-            fprintf(stderr, "Exception during indexing: %s\n", e.what());
-            if (unlink(tmpFileName.c_str())!=0 && errno!=ENOENT)
-                fprintf(stderr, "Cannot remove temporary file: %s\n", tmpFileName.c_str());
-            if (unlink(tmpIndexFileName.c_str())!=0 && errno!=ENOENT)
-                fprintf(stderr, "Cannot remove temporary index file: %s\n", tmpIndexFileName.c_str());
-            delete dataflowManager;
-            throw e;
-        }
-
+    // run!
+    try
+    {
+        dataflowManager->execute();
+    }
+    catch (std::exception &e)
+    {
+        fprintf(stderr, "Exception during indexing: %s\n", e.what());
+        if (unlink(tmpFileName.c_str())!=0 && errno!=ENOENT)
+            fprintf(stderr, "Cannot remove temporary file: %s\n", tmpFileName.c_str());
+        if (unlink(tmpIndexFileName.c_str())!=0 && errno!=ENOENT)
+            fprintf(stderr, "Cannot remove temporary index file: %s\n", tmpIndexFileName.c_str());
         delete dataflowManager;
+        throw e;
+    }
 
-        // rename vector file and index file
-        if (unlink(fileName)!=0 && errno!=ENOENT)
-            throw opp_runtime_error("Cannot remove original vector file `%s': %s", fileName, strerror(errno));
-        if (unlink(indexFileName.c_str())!=0 && errno!=ENOENT)
-            throw opp_runtime_error("Cannot remove original index file `%s': %s", indexFileName, strerror(errno));
-        if (rename(tmpIndexFileName.c_str(), indexFileName.c_str())!=0)
-            throw opp_runtime_error("Cannot rename index file from '%s' to '%s': %s", tmpIndexFileName.c_str(), indexFileName.c_str(), strerror(errno));
-        if (rename(tmpFileName.c_str(), fileName)!=0)
-            throw opp_runtime_error("Cannot rename vector file from '%s' to '%s': %s", tmpFileName.c_str(), fileName, strerror(errno));
+    delete dataflowManager;
+
+    // rename vector file and index file
+    if (unlink(fileName)!=0 && errno!=ENOENT)
+        throw opp_runtime_error("Cannot remove original vector file `%s': %s", fileName, strerror(errno));
+    if (unlink(indexFileName.c_str())!=0 && errno!=ENOENT)
+        throw opp_runtime_error("Cannot remove original index file `%s': %s", indexFileName, strerror(errno));
+    if (rename(tmpIndexFileName.c_str(), indexFileName.c_str())!=0)
+        throw opp_runtime_error("Cannot rename index file from '%s' to '%s': %s", tmpIndexFileName.c_str(), indexFileName.c_str(), strerror(errno));
+    if (rename(tmpFileName.c_str(), fileName)!=0)
+        throw opp_runtime_error("Cannot rename vector file from '%s' to '%s': %s", tmpFileName.c_str(), fileName, strerror(errno));
 }
 
-std::string VectorFileIndexer::generateHeader(Run *runRef) {
+std::string VectorFileIndexer::generateHeader(Run *runRef)
+{
     std::stringstream header;
 
     header << "# generated by scave\n";
