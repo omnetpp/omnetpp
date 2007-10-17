9c6322874a87b1ca4be71b67a324c914e96fa69e
Tamas Borbely <tamas.borbely@omnest.com>
2007-10-17 15:23:07 +0000

    add Predicate.itemWithText

--- ui/com.simulcraft.test.gui/src/com/simulcraft/test/gui/util/Predicate.java
+++ ui/com.simulcraft.test.gui/src/com/simulcraft/test/gui/util/Predicate.java
@@ -1,6 +1,7 @@
 package com.simulcraft.test.gui.util;
 
 import org.eclipse.core.runtime.Assert;
+import org.eclipse.swt.widgets.Item;
 import org.eclipse.swt.widgets.Widget;
 import org.omnetpp.common.util.IPredicate;
 
@@ -19,6 +20,17 @@ public class Predicate {
 					return id.equals(widgetID);
 				}
 				return false;
+			}
+		};
+	}
+	
+	public static IPredicate itemWithText(final String text) {
+		Assert.isLegal(text != null);
+		
+		return new IPredicate() {
+			public boolean matches(Object object) {
+				return object instanceof Item &&
+						((Item)object).getText().matches(text);
 			}
 		};
 	}
