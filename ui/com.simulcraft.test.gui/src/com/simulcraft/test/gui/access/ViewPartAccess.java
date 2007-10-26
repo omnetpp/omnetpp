191325dcba89edcb3053afd22685b40787abb092
Tamas Borbely <tamas.borbely@omnest.com>
2007-10-26 08:19:59 +0000

    access view's toolbar

--- ui/com.simulcraft.test.gui/src/com/simulcraft/test/gui/access/ViewPartAccess.java
+++ ui/com.simulcraft.test.gui/src/com/simulcraft/test/gui/access/ViewPartAccess.java
@@ -1,6 +1,11 @@
 package com.simulcraft.test.gui.access;
 
+import junit.framework.Assert;
+
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.IWorkbenchPartSite;
+import org.eclipse.ui.internal.ViewSite;
 
 public class ViewPartAccess extends WorkbenchPartAccess {
 
@@ -12,4 +17,12 @@ public class ViewPartAccess extends WorkbenchPartAccess {
 	public IViewPart getPart() {
 		return (IViewPart)workbenchPart;
 	}
+    
+    public ToolBarAccess getViewToolbar() {
+		IWorkbenchPartSite site = workbenchPart.getSite();
+		Assert.assertTrue("Site is not a ViewSite", site instanceof ViewSite);
+		Control toolbar = ((ViewSite)site).getPane().getToolBar();
+		Assert.assertNotNull("No toolbar found", toolbar);
+		return (ToolBarAccess)createAccess(toolbar);
+    }
 }
