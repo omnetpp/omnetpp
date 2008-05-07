cff68c524d11da592384ea99aaeb5622fc7b5e49
Andras Varga <andras@omnetpp.org>
2008-05-07 17:02:57 +0000

    brought up to date with migrate/migrateini.pl

--- ui/org.omnetpp.inifile.editor/src/org/omnetpp/inifile/editor/model/InifileConverter.java
+++ ui/org.omnetpp.inifile.editor/src/org/omnetpp/inifile/editor/model/InifileConverter.java
@@ -7,23 +7,27 @@ import org.omnetpp.common.util.StringUtils;
  * @author Andras
  */
 public class InifileConverter {
-	public static String[] OBSOLETE_SECTIONS = {"Parameters", "Cmdenv", "Tkenv", "OutVectors", "Partitioning", "DisplayStrings"};
+    public static String[] OBSOLETE_SECTIONS = {"Parameters", "Cmdenv", "Tkenv", "OutVectors", "Partitioning", "DisplayStrings"};
 
-	public static String[] CMDENV_NAMES = {"autoflush", "event-banner-details", "event-banners", "express-mode",
-			                	"message-trace", "module-messages", "output-file", "performance-display",
-			                	"runs-to-execute", "status-frequency"};
+    public static String[] CMDENV_NAMES = {"autoflush", "event-banner-details", "event-banners", "express-mode",
+        "message-trace", "module-messages", "output-file", "performance-display",
+        "runs-to-execute", "status-frequency"};
 
-	public static String[] TKENV_NAMES = {"anim-methodcalls", "animation-enabled", "animation-msgclassnames",
-				               "animation-msgcolors", "animation-msgnames", "animation-speed",
-				               "default-run", "expressmode-autoupdate", "image-path", "methodcalls-delay",
-				               "next-event-markers", "penguin-mode", "plugin-path", "print-banners",
-				               "senddirect-arrows", "show-bubbles", "show-layouting", "slowexec-delay",
-				               "update-freq-express", "update-freq-fast", "use-mainwindow",
-				               "use-new-layouter"};
-	public static String[] NUMBERS = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",
-							   "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", 
-							   "Seventeen", "Eighteen", "Nineteen", "Twenty" };
+    public static String[] TKENV_NAMES =  {"default-run", "image-path", "plugin-path"};
 
+    public static String[] REMOVE_NAMES = {"anim-methodcalls", "animation-enabled", "animation-msgclassnames",
+        "animation-msgcolors", "animation-msgnames", "animation-speed",
+        "expressmode-autoupdate", "methodcalls-delay", "next-event-markers",
+        "penguin-mode", "print-banners", "senddirect-arrows", "show-bubbles",
+        "show-layouting", "slowexec-delay", "update-freq-express",
+        "update-freq-fast", "use-mainwindow", "use-new-layouter"};
+
+    // note: these should be the NEW option names!
+    public static String[] COMMENT_OUT_NAMES = {"tkenv-default-run", "cmdenv-runs-to-execute"};
+
+    public static String[] NUMBERS = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",
+        "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", 
+        "Seventeen", "Eighteen", "Nineteen", "Twenty" };
 
 	/**
 	 * Tests whether the given inifile text is in old format (OMNeT++ 3.x) and 
@@ -41,14 +45,16 @@ public class InifileConverter {
 	 *  - rename the sections [Parameters], [Cmdenv], [Tkenv], [OutVectors], [Partitioning] to [General]
 	 *  - merge the resulting multiple occurrences of [General] into one
 	 *  - prefix [Cmdenv]/[Tkenv] config names with "cmdenv-" and "tkenv-" (unless already begins with that)
+	 *  - remove obsolete [Tkenv] keys
 	 *  - rename sections [Run 1], [Run 2] etc to [Config config1], etc.
-	 *  - rename output vector configuration sections "**.interval=" to "**.record-interval="
-	 *  - rename "**.use-default" to "**.apply-default"
+	 *  - rename per-object config options: "**.enabled", "**.interval", "**.use-default", etc
+	 *  
+	 * Note: this method was created from the Perl script omnetpp/migrate/migrate-inifile. 
 	 */
 	public static String convert(String text) {
-		// Note: this method was created from the Perl script _scripts\migrate\migrateinifile.pl 
 		String MULTILINE = "(?m)";  // turns on regex MULTILINE mode (Perl: s///m) 
 		
+        // rename Cmdenv keys
 	    String allCmdenvNames = StringUtils.join(CMDENV_NAMES, "|");
 	    text = text.replaceAll(MULTILINE+"^(\\s*[;#]?\\s*)("+allCmdenvNames+")\\b", "$1cmdenv-$2");
 
@@ -56,23 +62,37 @@ public class InifileConverter {
 	    String allTkenvNames = StringUtils.join(TKENV_NAMES, "|");
 	    text = text.replaceAll(MULTILINE+"^(\\s*[;#]?\\s*)("+allTkenvNames+")\\b", "$1tkenv-$2");
 
-	    // rename sections to [General]
-	    String allObsoleteSections = StringUtils.join(OBSOLETE_SECTIONS, "|");
-	    text = text.replaceAll(MULTILINE+"^(\\s*[;#]?\\s*)\\[("+allObsoleteSections+")\\]", "$1\\[General\\]");
+	    // remove obsolete keys
+        String allRemoveNames = StringUtils.join(REMOVE_NAMES, "|");
+        text.replaceAll("^\\s*[;#]?\\s*("+allRemoveNames+")\\b.*$", "");
 
-	    // rename [Run X] sections to [Config configX]
-	    for (int i=1; i<NUMBERS.length; i++)
-	    	text = text.replaceAll(MULTILINE+"^(\\s*[;#]?\\s*)\\[Run "+i+"\\]", "$1\\[Config "+NUMBERS[i]+"\\]");
-    	text = text.replaceAll(MULTILINE+"^(\\s*[;#]?\\s*)\\[Run ([0-9]+)\\]", "$1\\[Config config$2\\]");
+	    // comment out some keys
+	    String allCommentOutNames = StringUtils.join(COMMENT_OUT_NAMES, "|");
+	    text.replaceAll("^\\s*(("+allCommentOutNames+")\\b)", "# $1");
+	    
+	    // rename per-object keys
+    	text = text.replaceAll(MULTILINE+"\\.use-default\\s*=", "$1.apply-default =");
+    	text = text.replaceAll(MULTILINE+"\\.ev-output\\s*=", "$1.cmdenv-ev-output =");
+    	text = text.replaceAll(MULTILINE+"\\.enabled\\s*=", "$1.vector-recording =");
+    	text = text.replaceAll(MULTILINE+"\\.interval\\s*=", "$1.vector-recording-interval =");
+    	text = text.replaceAll(MULTILINE+"\\.pingApp\\.vector-recording =", ".pingApp.enabled =");  // change back false hit
+    	text = text.replaceAll(MULTILINE+"\\.pingApp\\.vector-recording-interval =", ".pingApp.interval =");  // change back false hit
+    	text = text.replaceAll(MULTILINE+"\\.akaroa\\s*=", "$1.with-akaroa =");
+    	
+        // convert yes/no and on/off to true/false
+    	text = text.replaceAll(MULTILINE+"=\\s*yes\\b", "= true");
+    	text = text.replaceAll(MULTILINE+"=\\s*no\\b",  "= false");
+        text = text.replaceAll(MULTILINE+"=\\s*on\\b",  "= true");
+        text = text.replaceAll(MULTILINE+"=\\s*off\\b", "= false");
 
-    	// rename .use-default= to .apply-default=, .interval= to .record-interval etc. 
-    	text = text.replaceAll(MULTILINE+"^([^;#\"]*)\\.use-default\\b", "$1.apply-default");
-    	text = text.replaceAll(MULTILINE+"^([^;#\"]*)\\.interval\\b", "$1.record-interval");
-    	text = text.replaceAll(MULTILINE+"^([^;#\"]*)\\.enabled\\b", "$1.record-enabled"); //XXX or what became the new name??
+        // rename sections to [General]
+        String allObsoleteSections = StringUtils.join(OBSOLETE_SECTIONS, "|");
+        text = text.replaceAll(MULTILINE+"^(\\s*[;#]?\\s*)\\[("+allObsoleteSections+")\\]", "$1\\[General\\]");
 
-    	// replace yes/no with true/false
-    	text = text.replaceAll(MULTILINE+"(=\\s*)yes(\\s*([;#].*)?)$", "$1true$2");
-    	text = text.replaceAll(MULTILINE+"(=\\s*)no(\\s*([;#].*)?)$", "$1false$2");
+        // rename [Run X] sections to [Config configX]
+        for (int i=1; i<NUMBERS.length; i++)
+            text = text.replaceAll(MULTILINE+"^(\\s*[;#]?\\s*)\\[Run "+i+"\\]", "$1\\[Config "+NUMBERS[i]+"\\]");
+        text = text.replaceAll(MULTILINE+"^(\\s*[;#]?\\s*)\\[Run ([0-9]+)\\]", "$1\\[Config config$2\\]");
 
 	    // make exactly one space on both sides of the "=" sign (optional, just cosmetics)
 	    text = text.replaceAll(MULTILINE+"^([^//;=]*?) *= *", "$1 = ");
