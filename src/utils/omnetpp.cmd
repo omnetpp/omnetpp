94dbfaafb28ab530f1afc80654f916a85c694f8c
Rudolf Hornig <rudi@omnetpp.org>
2008-05-09 11:24:24 +0000

    omnetpp.cmd included on windows alwas

--- src/utils/omnetpp.cmd
+++ src/utils/omnetpp.cmd
@@ -1,6 +1,6 @@
 @echo off
 SET IDEDIR=%~dp0\..\ide                                                                                        
-cd $IDEDIR
+cd %IDEDIR%
 SET PATH=%~dp0;%~dp0\..\msys\bin;%PATH%
 echo "Starting the OMNeT++ IDE..."
 start omnetpp.exe >out.log
