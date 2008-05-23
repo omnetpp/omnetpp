ca0feb5bd69a90ad767b546b87f965bfa37ba0eb
Andras Varga <andras@omnetpp.org>
2008-05-23 21:26:14 +0000

    removed obsolete scripts

--- doc/src/manual/eps2pdf.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-PATH C:\home\tools\gs\gs8.11\bin;C:\home\tools\gs\gs8.11\lib;%PATH%
-ps2pdf12 %*
--- doc/src/manual/wmf2gif.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-C:\Program Files\IrfanView\i_view32.exe" %~dpn1.wmf  /convert=%~dpn1.gif
-
