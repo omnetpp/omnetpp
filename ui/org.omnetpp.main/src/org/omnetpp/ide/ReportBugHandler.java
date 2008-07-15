62f1a096c686b178685fa249f00661dfb505efa4
Rudolf Hornig <rudi@omnetpp.org>
2008-07-15 14:24:29 +0000

    added bug reporting menu

---
+++ ui/org.omnetpp.main/src/org/omnetpp/ide/ReportBugHandler.java
@@ -0,0 +1,32 @@
+package org.omnetpp.ide;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.jface.dialogs.MessageDialog;
+
+/**
+ * 
+ */
+public class ReportBugHandler extends AbstractHandler {
+	/**
+	 * The constructor.
+	 */
+	public ReportBugHandler() {
+	}
+
+	/**
+	 * the command has been executed, so extract extract the needed information
+	 * from the application context.
+	 */
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+		MessageDialog.openInformation(
+				window.getShell(),
+				"OMNeT++ Integrated Development Environment",
+				"Reporting bugs");
+		return null;
+	}
+}
