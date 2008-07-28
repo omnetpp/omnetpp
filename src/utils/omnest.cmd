43234b0013f44fc415620581fd769cf45178457d
Rudolf Hornig <rudi@omnetpp.org>
2008-07-28 09:57:28 +0000

    OMNEST build support added

--- src/utils/omnest.cmd
+++ src/utils/omnest.cmd
@@ -2,5 +2,5 @@
 SET IDEDIR=%~dp0\..\ide                                                                                        
 cd %IDEDIR%
 SET PATH=%~dp0;%~dp0\..\msys\bin;%~dp0\..\mingw\bin;%PATH%
-echo Starting the OMNEST IDE...
-start omnest.exe >out.log
+echo Starting the OMNeT++ IDE...
+start omnetpp.exe >out.log
