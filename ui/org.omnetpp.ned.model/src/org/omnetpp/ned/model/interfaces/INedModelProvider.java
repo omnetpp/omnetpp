1975c2208e6be774e8a659bbda61891fa603487c
Andras Varga <andras@omnetpp.org>
2008-10-23 07:59:21 +0000

    renamed IModelProvider to INedModelProvider

---
+++ ui/org.omnetpp.ned.model/src/org/omnetpp/ned/model/interfaces/INedModelProvider.java
@@ -0,0 +1,14 @@
+package org.omnetpp.ned.model.interfaces;
+
+import org.omnetpp.ned.model.INEDElement;
+
+/**
+ * Can provide a NED Model. Allows that we access the model from any selection
+ * without depending on the GEF plugin. GEF EditParts should implement this, as an
+ * IStructuredSelection contains GEF EditParts in the graphical NED editor.
+ *
+ * @author rhornig
+ */
+public interface INedModelProvider {
+    public INEDElement getNedModel();
+}
