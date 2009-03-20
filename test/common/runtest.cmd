7d6829817a3ffe01055721f2dfb46ed77200bd02
Tamas Borbely <tamas.borbely@omnest.com>
2009-03-20 11:56:20 +0000

    rwlock tests

--- test/common/runtest.cmd
+++ test/common/runtest.cmd
@@ -13,7 +13,7 @@ mkdir work 2>nul
 call opp_test %OPT% -g -v %TESTFILES% || goto end
 
 cd work || goto end
-call opp_nmakemake -f -e cc --deep --no-deep-includes -I../../../src/common -I../../../src/envir -I../../../include/platdep || goto end
+call opp_nmakemake -f -e cc --deep --no-deep-includes -I../../../src/common -I../../../src/envir -I../../../include/platdep -I $(PTHREADS_DIR)/include -lpthreadVC2 -L$(PTHREADS_DIR)/lib || goto end
 nmake -f makefile.vc || cd .. && goto end
 cd .. || goto end
 
