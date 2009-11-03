ea0f1f65337579dba08332f90f52633ac0f56eba
Andras Varga <andras@omnetpp.org>
2009-11-03 21:57:42 +0100

    topology generation example now works

--- samples/neddemo/templates/project/topogen/Topogen.java
+++ samples/neddemo/templates/project/topogen/Topogen.java
@@ -2,10 +2,11 @@ package org.example;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Random;
 import java.util.Set;
 
 /**
- * Simply topology generator. It can produce a connected graph with a given 
+ * Simple topology generator. It can produce a connected graph with a given 
  * number of nodes and edges, and no multiple connections between any two nodes.
  * 
  * @author Andras
@@ -18,17 +19,19 @@ public class Topogen {
      * Generates a connected graph with the given number of nodes and edges, and no multiple
      * connections between any two nodes.
      */
-    public Topogen(int nodes, int edges) {
+    public Topogen(int nodes, int edges, long seed) {
         if (nodes > edges+1) // tree
             throw new IllegalArgumentException("Not enough edges to form a connected tree; need at least #nodes-1");
         if (edges > nodes*(nodes-1)/2) // full graph
             throw new IllegalArgumentException("Cannot have that many edges without having multiple edges between nodes");
         
+        Random rng = new Random(seed);
+        
         // add nodes and links to form a connected tree
         for (int i = 0; i < nodes; i++) {
             neighbors.add(new HashSet<Integer>());
             if (i > 0) {
-                int existing = (int)Math.floor(Math.random()*i);
+                int existing = rng.nextInt(i);
                 neighbors.get(i).add(existing);
                 neighbors.get(existing).add(i);
             }
@@ -39,7 +42,7 @@ public class Topogen {
             // pick a random node which is not connected to all others yet
             int src;
             do {
-                src = (int)Math.floor(Math.random()*nodes);
+                src = rng.nextInt(nodes);
             } while (neighbors.get(src).size() == nodes-1);
                 
             // then pick one node to connect to
@@ -47,7 +50,7 @@ public class Topogen {
             for (int j = 0; j < nodes; j++)
                 if (j != src && !neighbors.get(src).contains(j))
                     destCandidates.add(j);
-            int dest = destCandidates.get((int)Math.floor(Math.random()*destCandidates.size()));
+            int dest = destCandidates.get(rng.nextInt(destCandidates.size()));
             
             // add link
             neighbors.get(src).add(dest);
@@ -55,12 +58,19 @@ public class Topogen {
         }
         
     }
-    
+
+    /**
+     * Static factory method for BeanWrapper access
+     */
+    public static Topogen create(int nodes, int edges, long seed) {
+        return new Topogen(nodes, edges, seed);
+    }
+
     /**
      * Returns the edge list representation of the generated graph.
      * For each node index i, the list contains the list of its neighbors.
      */
-    public List<Set<Integer>> getNeighbors() {
+    public List<Set<Integer>> getNeighborLists() {
         return neighbors;
     }
     
@@ -79,6 +89,9 @@ public class Topogen {
     }
 
     public static void main(String[] args) {
-        System.out.println(new Topogen(10, 45));
+        int nodes = args.length>=1 ? Integer.parseInt(args[0]) : 10;
+        int edges = args.length>=2 ? Integer.parseInt(args[1]) : 2*nodes;
+        long seed = args.length>=3 ? Long.parseLong(args[2]) : System.currentTimeMillis();
+        System.out.println(new Topogen(nodes, edges, seed));
     }
 }
