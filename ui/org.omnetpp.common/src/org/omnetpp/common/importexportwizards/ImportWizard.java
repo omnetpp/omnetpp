aea9fb7a8cb97685f29f6333b2afb3c918f5ff26
Andras Varga <andras@omnetpp.org>
2009-12-06 22:13:36 +0100

    added "import" to supportedWizardTypes of import wizard -- the OMNeT++ Import dialog will filter by this

--- ui/org.omnetpp.common/src/org/omnetpp/common/importexportwizards/ImportWizard.java
+++ ui/org.omnetpp.common/src/org/omnetpp/common/importexportwizards/ImportWizard.java
@@ -1,17 +1,32 @@
 package org.omnetpp.common.importexportwizards;
 
+import org.eclipse.jface.dialogs.IDialogSettings;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.IWizardPage;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.IImportWizard;
 import org.eclipse.ui.IWorkbench;
+import org.omnetpp.common.CommonPlugin;
 import org.omnetpp.common.wizard.IContentTemplate;
 import org.omnetpp.common.wizard.TemplateSelectionPage;
 
+/**
+ * Import OMNeT++ File wizard. Brings up the IContentTemplates that support the
+ * wizard type "import", lets the user pick one, then delegates to a wizard
+ * that can run the given template.
+ *  
+ * @author Andras
+ */
 public class ImportWizard extends Wizard implements IImportWizard {
     private TemplateSelectionPage templateSelectionPage;
 
     public ImportWizard() {
+        // code copied from PreferencesExportWizard
+        IDialogSettings workbenchSettings = CommonPlugin.getDefault().getDialogSettings();
+        IDialogSettings section = workbenchSettings.getSection("ImportWizard");
+        if (section == null)
+            section = workbenchSettings.addNewSection("ImportWizard");
+        setDialogSettings(section);
     }
 
     public void init(IWorkbench workbench, IStructuredSelection selection) {
@@ -19,19 +34,38 @@ public class ImportWizard extends Wizard implements IImportWizard {
 
     @Override
     public void addPages() {
-        addPage(templateSelectionPage = new TemplateSelectionPage(null, true));
+        addPage(templateSelectionPage = new TemplateSelectionPage("import", true)); 
+        templateSelectionPage.setTitle("Select Importer");
     }
 
     @Override
     public IWizardPage getNextPage(IWizardPage page) {
         if (page == templateSelectionPage) {
             IContentTemplate template = templateSelectionPage.getSelectedTemplate();
+            String wizardType = chooseWizardTypeFor(template);
             //TODO create the corresponding wizard... and return its first page
             return null;
         }
         return super.getNextPage(page);
     }
     
+    protected String chooseWizardTypeFor(IContentTemplate template) {
+        // by default we'd like to invoke the "New Network" wizard; if not supported, 
+        // here's a decreasing preference order of wizards
+        String[] preferredOrder = "network nedfile compoundmodule simplemodule msgfile inifile simulation project".split(" ");
+        for (String type : preferredOrder)
+            if (template.getSupportedWizardTypes().contains(type))
+                return type;
+        
+        // ok, return what it supports
+        for (String type : template.getSupportedWizardTypes())
+            if (!type.equals("import"))
+                return type;
+        
+        // looks like it does not support anything
+        return null;
+    }
+
     @Override
     public boolean performFinish() {
         return false;
