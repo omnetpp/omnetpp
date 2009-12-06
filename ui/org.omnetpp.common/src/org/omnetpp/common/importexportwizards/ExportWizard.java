2c286a6563a6ee61cfc47ce0af635ebdb96e3fed
Andras Varga <andras@omnetpp.org>
2009-12-06 13:08:35 +0100

    started implementing export/import wizards

---
+++ ui/org.omnetpp.common/src/org/omnetpp/common/importexportwizards/ExportWizard.java
@@ -0,0 +1,25 @@
+package org.omnetpp.common.importexportwizards;
+
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.IExportWizard;
+import org.eclipse.ui.IWorkbench;
+
+public class ExportWizard extends Wizard implements IExportWizard {
+
+    public ExportWizard() {
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public boolean performFinish() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public void init(IWorkbench workbench, IStructuredSelection selection) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ ui/org.omnetpp.common/src/org/omnetpp/common/importexportwizards/ImportWizard.java
@@ -0,0 +1,25 @@
+package org.omnetpp.common.importexportwizards;
+
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.IImportWizard;
+import org.eclipse.ui.IWorkbench;
+
+public class ImportWizard extends Wizard implements IImportWizard {
+
+    public ImportWizard() {
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public boolean performFinish() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public void init(IWorkbench workbench, IStructuredSelection selection) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
