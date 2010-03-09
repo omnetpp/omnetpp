0ba82f9da964241157e3473563442b7704999da3
Rudolf Hornig <rudi@omnetpp.org>
2010-03-09 12:58:44 +0100

    Fixed OMNEST/OMNET++ branding issue in launcher scripts.
    X-Committed-on-Branch: master

--- src/utils/omnest.cmd
+++ src/utils/omnest.cmd
@@ -3,5 +3,5 @@ SET IDEDIR=%~dp0\..\ide
 SET LAUNCHER=omnest
 cd %IDEDIR%
 SET PATH=%~dp0;%~dp0\..\msys\bin;%~dp0\..\mingw\bin;%PATH%
-echo Starting the OMNeT++ IDE...
+echo Starting the OMNEST IDE...
 start .\win32\%LAUNCHER% >out.log
