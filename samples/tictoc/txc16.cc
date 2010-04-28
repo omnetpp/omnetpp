de1c040393a4513b15d24e69d47dd25f9b4b15a8
Rudolf Hornig <rudi@omnetpp.org>
2010-04-28 16:07:40 +0200

    Modified the tictoc example and tutorial to include a step about gathering statistics using signals.
    X-Committed-on-Branch: master

---
+++ samples/tictoc/txc16.cc
@@ -0,0 +1,119 @@
+//
+// This file is part of an OMNeT++/OMNEST simulation example.
+//
+// Copyright (C) 2003 Ahmet Sekercioglu
+// Copyright (C) 2003-2008 Andras Varga
+//
+// This file is distributed WITHOUT ANY WARRANTY. See the file
+// `license' for details on this and other legal matters.
+//
+
+#include <stdio.h>
+#include <string.h>
+#include <omnetpp.h>
+#include "tictoc16_m.h"
+
+
+/**
+ * The main problem with the previous step is that we must modify the model's
+ * code if we want to change what statistics are gathered. Statistic calculation
+ * is woven deeply into the model code which is hard to modify and understand.
+ *
+ * OMNeT++ 4.1 provides a different mechanism called 'signals' that we can use
+ * to gather statistics. First we have to identify the events where the state
+ * of the model changes. We can emit signals at these points that carry the value
+ * of chosen state variables. This way the C++ code only emits signals, but how those
+ * signals are processed are determined only by the listeners that are attached to them.
+ *
+ * The signals the model emits and the listeners that process them can be defined in
+ * the NED file using the 'signal' and 'statistic' property.
+ *
+ * We will gather the same statistics as in the previous step, but notice that we will not need
+ * any private member variables to calculate these values. We will use only a single signal that
+ * is emitted when a message arrives and carries the hopcount in the message.
+ */
+class Txc16 : public cSimpleModule
+{
+  private:
+	simsignal_t arrivalSignal;
+
+  protected:
+    virtual TicTocMsg16 *generateMessage();
+    virtual void forwardMessage(TicTocMsg16 *msg);
+    virtual void initialize();
+    virtual void handleMessage(cMessage *msg);
+};
+
+Define_Module(Txc16);
+
+void Txc16::initialize()
+{
+    arrivalSignal = registerSignal("arrival");
+    // Module 0 sends the first message
+    if (getIndex()==0)
+    {
+        // Boot the process scheduling the initial message as a self-message.
+        TicTocMsg16 *msg = generateMessage();
+        scheduleAt(0.0, msg);
+    }
+}
+
+void Txc16::handleMessage(cMessage *msg)
+{
+    TicTocMsg16 *ttmsg = check_and_cast<TicTocMsg16 *>(msg);
+
+    if (ttmsg->getDestination()==getIndex())
+    {
+        // Message arrived
+        int hopcount = ttmsg->getHopCount();
+        // send a signal
+        emit(arrivalSignal, hopcount);
+
+        EV << "Message " << ttmsg << " arrived after " << hopcount << " hops.\n";
+        bubble("ARRIVED, starting new one!");
+
+        delete ttmsg;
+
+        // Generate another one.
+        EV << "Generating another message: ";
+        TicTocMsg16 *newmsg = generateMessage();
+        EV << newmsg << endl;
+        forwardMessage(newmsg);
+    }
+    else
+    {
+        // We need to forward the message.
+        forwardMessage(ttmsg);
+    }
+}
+
+TicTocMsg16 *Txc16::generateMessage()
+{
+    // Produce source and destination addresses.
+    int src = getIndex();
+    int n = size();
+    int dest = intuniform(0,n-2);
+    if (dest>=src) dest++;
+
+    char msgname[20];
+    sprintf(msgname, "tic-%d-to-%d", src, dest);
+
+    // Create message object and set source and destination field.
+    TicTocMsg16 *msg = new TicTocMsg16(msgname);
+    msg->setSource(src);
+    msg->setDestination(dest);
+    return msg;
+}
+
+void Txc16::forwardMessage(TicTocMsg16 *msg)
+{
+    // Increment hop count.
+    msg->setHopCount(msg->getHopCount()+1);
+
+    // Same routing as before: random gate.
+    int n = gateSize("gate");
+    int k = intuniform(0,n-1);
+
+    EV << "Forwarding message " << msg << " on gate[" << k << "]\n";
+    send(msg, "gate$o", k);
+}
