6f1bc43a6dcb310ba05633205d74f17a32bf1ff1
Rudolf Hornig <rudi@omnetpp.org>
2010-05-21 12:33:00 +0200

    InslallGuide update.
    X-Committed-on-Branch: master

---
+++ vcenv.cmd
@@ -0,0 +1,21 @@
+@echo off
+rem Specify your MS Visual C++ 8/9 installation here.
+rem  e.g. C:\Program Files\Microsoft Visual Studio 9.0\VC
+set VCINSTALLDIR=
+
+if NOT EXIST "%VCINSTALLDIR%\bin\cl.exe" goto MESSAGE
+
+set OMNETPP_ROOT=%~dp0
+set OMNETPP_IMAGE_PATH=.\images;%OMNETPP_ROOT%\images
+set PATH=%OMNETPP_ROOT%\bin;%OMNETPP_ROOT%\msys\bin;%OMNETPP_ROOT%\mingw\bin;%PATH%
+
+call "%VCINSTALLDIR%"\bin\vcvars32.bat
+
+goto EOF
+
+:MESSAGE
+echo You must specify the location of your Visual C++ installation.
+echo Open 'vcenv.cmd' with your editor and set the VCINSTALLDIR
+echo variable to point to your Visual C++ installation directory.
+
+:EOF
