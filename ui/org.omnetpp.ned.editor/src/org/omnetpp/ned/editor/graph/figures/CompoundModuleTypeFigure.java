ca407e76ddf4eef34c20bad5855edbf2628323fa
Rudolf Hornig <rudi@omnetpp.org>
2011-01-13 15:02:48 +0100

    NED Editor: Added missing files.
    X-Committed-on-Branch: master

---
+++ ui/org.omnetpp.ned.editor/src/org/omnetpp/ned/editor/graph/figures/CompoundModuleTypeFigure.java
@@ -0,0 +1,126 @@
+/*--------------------------------------------------------------*
+  Copyright (C) 2006-2011 OpenSim Ltd.
+
+  This file is distributed WITHOUT ANY WARRANTY. See the file
+  'License' for details on this and other legal matters.
+*--------------------------------------------------------------*/
+
+package org.omnetpp.ned.editor.graph.figures;
+
+import static org.omnetpp.figures.CompoundModuleFigure.BORDER_SNAP_WIDTH;
+
+import org.eclipse.draw2d.ColorConstants;
+import org.eclipse.draw2d.Figure;
+import org.eclipse.draw2d.Graphics;
+import org.eclipse.draw2d.IFigure;
+import org.eclipse.draw2d.MarginBorder;
+import org.eclipse.draw2d.ToolbarLayout;
+import org.eclipse.draw2d.geometry.Insets;
+import org.eclipse.draw2d.geometry.Point;
+import org.eclipse.draw2d.geometry.Rectangle;
+import org.eclipse.gef.handles.HandleBounds;
+import org.eclipse.swt.graphics.Image;
+import org.omnetpp.common.displaymodel.IDisplayString;
+import org.omnetpp.figures.CompoundModuleFigure;
+import org.omnetpp.ned.editor.NedEditorPlugin;
+
+/**
+ * A figure representing a compound module type (or network). Displays a caption with the name and the default
+ * image (if any) and an area where inner types can be added plus the area used to display submodules
+ * (inner types compartment is inserted on demand from the TypesEditPart) 
+ *
+ * @author rhornig
+ */
+public class CompoundModuleTypeFigure extends ModuleTypeFigure implements HandleBounds {
+    protected static final Image IMG_DEFAULT_COMPOUND = NedEditorPlugin.getCachedImage("icons/type/compound.png");
+    protected static final Image IMG_DEFAULT_COMPOUND_S = NedEditorPlugin.getCachedImage("icons/type/compound_s.png");
+    protected static final Image IMG_DEFAULT_NETWORK = NedEditorPlugin.getCachedImage("icons/type/network.png");
+    protected static final Image IMG_DEFAULT_NETWORK_S = NedEditorPlugin.getCachedImage("icons/type/network_s.png");
+
+    protected boolean isNetwork;
+    protected CompoundModuleFigure submoduleArea = new CompoundModuleFigure();
+    
+    /**
+     * A figure that contains the inner types inside a compound module type
+     *
+     */
+    public static class InnerTypesFigure extends Figure {
+        /**
+         * A border that draws a line at the left side of the figure
+         */
+        public static class InnerTypesBorder extends MarginBorder {
+            InnerTypesBorder(int t, int l, int b, int r) {
+                super(t, l, b, r);
+            }
+
+            @Override
+            public void paint(IFigure f, Graphics g, Insets i) {
+                Rectangle r = getPaintRectangle(f, i);
+                g.setForegroundColor(ColorConstants.buttonDarker);
+                int x = r.x + insets.left/4;
+                g.drawLine(x, r.y, x, r.bottom());
+            }
+        }
+        
+        public InnerTypesFigure() {
+            setBorder(new InnerTypesBorder(0, 20, 0, 0 ));
+            ToolbarLayout typesLayout = new ToolbarLayout();
+            typesLayout.setStretchMinorAxis(false);
+            typesLayout.setSpacing(5);
+            setLayoutManager(typesLayout);
+        }
+    }
+
+    // ----------------------------------------------------------
+    public CompoundModuleTypeFigure() {
+        super();
+        add(submoduleArea);
+    }
+    
+    public CompoundModuleFigure getSubmoduleArea() {
+        return submoduleArea;
+    }
+    
+    public boolean isNetwork() {
+        return isNetwork;
+    }
+
+    public void setNetwork(boolean isNetwork) {
+        this.isNetwork = isNetwork;
+    }
+
+    @Override
+    protected Image getDefaultImage() {
+        Image image = isNetwork() ? 
+                (isInnerType() ? IMG_DEFAULT_NETWORK_S : IMG_DEFAULT_NETWORK) : 
+                    (isInnerType() ? IMG_DEFAULT_COMPOUND_S : IMG_DEFAULT_COMPOUND);
+        return image;
+    }
+    
+	@Override
+	public void setDisplayString(IDisplayString displayString) {
+	    // set the properties for the submodules compartment
+	    getSubmoduleArea().setDisplayString(displayString);
+	    // set the properties for the icon in the title
+	    super.setDisplayString(displayString);
+	}
+
+    /**
+     * Returns whether the point is on the border area, where dragging and selection and connection start/end is possible.
+     * Coordinates are viewport relative.
+     */
+    public boolean isOnBorder(int x, int y) {
+        // translate the mouse coordinates to be relative to the root figure
+        Point mouse = new Point(x,y);
+        translateToRelative(mouse);
+        Rectangle r = getHandleBounds().shrink(2*BORDER_SNAP_WIDTH, 2*BORDER_SNAP_WIDTH);
+        return getBounds().contains(mouse) && !r.contains(mouse);
+    }
+
+    public Rectangle getHandleBounds() {
+        Rectangle clientArea = submoduleArea.getHandleBounds();
+        translateToParent(clientArea);  // because we are using local coordinates
+        return clientArea;
+    }
+
+}
---
+++ ui/org.omnetpp.ned.editor/src/org/omnetpp/ned/editor/graph/figures/NedTypeFigure.java
@@ -0,0 +1,202 @@
+/*--------------------------------------------------------------*
+  Copyright (C) 2006-2008 OpenSim Ltd.
+
+  This file is distributed WITHOUT ANY WARRANTY. See the file
+  'License' for details on this and other legal matters.
+*--------------------------------------------------------------*/
+
+package org.omnetpp.ned.editor.graph.figures;
+
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.draw2d.Figure;
+import org.eclipse.draw2d.FigureUtilities;
+import org.eclipse.draw2d.ImageFigure;
+import org.eclipse.draw2d.Label;
+import org.eclipse.draw2d.Layer;
+import org.eclipse.draw2d.PositionConstants;
+import org.eclipse.draw2d.TextUtilities;
+import org.eclipse.draw2d.ToolbarLayout;
+import org.eclipse.draw2d.XYLayout;
+import org.eclipse.draw2d.geometry.Dimension;
+import org.eclipse.draw2d.geometry.Rectangle;
+import org.eclipse.gef.tools.CellEditorLocator;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.ui.ISharedImages;
+import org.eclipse.ui.PlatformUI;
+import org.omnetpp.common.displaymodel.IDisplayString;
+import org.omnetpp.common.util.StringUtils;
+import org.omnetpp.figures.IProblemDecorationSupport;
+import org.omnetpp.figures.ITooltipTextProvider;
+import org.omnetpp.ned.editor.graph.misc.IDirectEditSupport;
+import org.omnetpp.ned.editor.graph.misc.LabelCellEditorLocator;
+
+
+/**
+ * Common base for ModuleTypeFigure and ConnectionTypeFigure. Provides an editable name
+ * in a vertical toolbar layout so additional figures can be added if needed (i.g. compound module)
+ * below the title. 
+ *
+ * @author rhornig
+ */
+abstract public class NedTypeFigure extends Figure implements IDirectEditSupport,   
+                IProblemDecorationSupport, ITooltipTextProvider {
+
+	// This is a global hack to increase the text size returned by the textExtents function
+	// on Windows, because it is too small if anti aliased fonts are used. Once 
+	// textExtent works correctly it can be removed
+	static {
+		TextUtilities.INSTANCE = new TextUtilities() {
+		    public Dimension getStringExtents(String s, Font f) {
+		        Dimension stringExtents = FigureUtilities.getStringExtents(s, f);
+				return Platform.OS_WIN32.equals(Platform.getOS()) ? stringExtents.expand(2,0) : stringExtents;
+		    }
+
+		    public Dimension getTextExtents(String s, Font f) {
+		        Dimension textExtents = FigureUtilities.getTextExtents(s, f);
+				return Platform.OS_WIN32.equals(Platform.getOS()) ? textExtents.expand(2,0) : textExtents;
+		    }
+		};
+	}
+	
+	// FIXME move it to ImageFactory
+	protected static final Image ICON_ERROR = PlatformUI.getWorkbench().getSharedImages().getImage(ISharedImages.IMG_OBJS_ERROR_TSK); //ImageFactory.getImage(ImageFactory.DECORATOR_IMAGE_ERROR);
+	protected static final Image ICON_WARNING = PlatformUI.getWorkbench().getSharedImages().getImage(ISharedImages.IMG_OBJS_WARN_TSK); //ImageFactory.getImage(ImageFactory.DECORATOR_IMAGE_WARNING);
+	protected static final Image ICON_INFO = PlatformUI.getWorkbench().getSharedImages().getImage(ISharedImages.IMG_OBJS_INFO_TSK); //ImageFactory.getImage(ImageFactory.DECORATOR_IMAGE_INFO);
+
+    protected String tmpName;
+    protected Label nameFigure = new Label();
+    protected Layer titleArea = new Layer();
+    protected ImageFigure problemMarkerFigure = new ImageFigure(); // FIXME create it on demand
+    protected ITooltipTextProvider problemMarkerTextProvider;
+    protected String tooltipText;
+
+    private boolean isInterface; 
+    private boolean isInnerType; 
+
+    public NedTypeFigure() {
+        // all ned types has a vertical toolbar layout. The topmost child is the 
+        // title figure (containing an icon,the name of the type and an error indicator figure)
+        // Usually this is the only child, but CompoundModuleType contains
+        // may additional children where the submodules are displayed or
+        // a compartment where the inner types are present. 
+        ToolbarLayout tb = new ToolbarLayout();
+        tb.setSpacing(2);
+        tb.setStretchMinorAxis(false);
+        setLayoutManager(tb);
+
+        // name label alignment
+        nameFigure.setLabelAlignment(PositionConstants.LEFT);
+        nameFigure.setTextAlignment(PositionConstants.EAST);
+        nameFigure.setTextPlacement(PositionConstants.MIDDLE);
+        
+        titleArea.setLayoutManager(new XYLayout());  // XXX we could use a horizontal toolbar possibly if the problem figure could be placed before the icon (instead of over) 
+        titleArea.add(problemMarkerFigure, new Rectangle(-1, 0, 16, 16));
+        titleArea.add(nameFigure);
+        
+        add(titleArea);
+    }
+
+    @Override
+    protected boolean useLocalCoordinates() {
+        return true;
+    }
+
+    /**
+     * Stores the display string, and updates the figure accordingly.
+     * @param dps
+     */
+    abstract public void setDisplayString(IDisplayString dps);
+
+	// FIXME move the icons to ImageFactory
+    public static Image getProblemImageFor(int severity) {
+		Image image;
+    	switch (severity) {
+	    	case -1: image = null; break;
+	    	case SEVERITY_ERROR: image = ICON_ERROR; break;
+	    	case SEVERITY_WARNING: image = ICON_WARNING; break;
+	    	case SEVERITY_INFO: image = ICON_INFO; break;
+	    	default: throw new RuntimeException("invalid severity value");
+    	}
+		return image;
+	}
+    
+    public boolean isInterface() {
+        return isInterface;
+    }
+
+    public void setInterface(boolean isInterface) {
+        this.isInterface = isInterface;
+    }
+
+    public boolean isInnerType() {
+        return isInnerType;
+    }
+
+    public void setInnerType(boolean isInnerType) {
+        this.isInnerType = isInnerType;
+    }
+
+    public void setTooltipText(String tttext) {
+    	tooltipText = tttext;
+    }
+
+	public String getTooltipText(int x, int y) {
+		// if there is a problem marker and an associated tooltip text provider
+		// and the cursor is over the marker, delegate to the problem marker text provider
+		if (problemMarkerTextProvider != null && problemMarkerFigure != null) {
+			Rectangle markerBounds = problemMarkerFigure.getBounds().getCopy();
+			translateToParent(markerBounds);
+			if (markerBounds.contains(x, y)) {
+				String text = problemMarkerTextProvider.getTooltipText(x, y);
+				if (text != null)
+					return text;
+			}
+		}
+		return tooltipText;
+	}
+
+    public void setProblemDecoration(int maxSeverity, ITooltipTextProvider textProvider) {
+        Image image = NedTypeFigure.getProblemImageFor(maxSeverity);
+        if (image != null)
+            problemMarkerFigure.setImage(image);
+        problemMarkerFigure.setVisible(image != null);
+
+        problemMarkerTextProvider = textProvider;
+        repaint();
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName()+" "+nameFigure.getText();
+    }
+
+    public void setName(String text) {
+        nameFigure.setText(text);
+    }
+    
+    // Direct edit support
+
+    public String getName() {
+        return nameFigure.getText();
+    }
+    
+    public CellEditorLocator getDirectEditCellEditorLocator() {
+        return new LabelCellEditorLocator(nameFigure);
+    }
+
+    public void showLabelUnderCellEditor(boolean visible) {
+        // HACK to hide the text part only of the label
+        if (!visible) {
+            tmpName = nameFigure.getText();
+            nameFigure.setText("");
+        }
+        else {
+            if (StringUtils.isEmpty(nameFigure.getText()))
+                nameFigure.setText(tmpName);
+        }
+        invalidate();
+        validate();
+    }
+
+}
