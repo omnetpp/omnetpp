3c9f01a527d017bf52fe8ad770427608fe259480
Rudolf Hornig <rudi@omnetpp.org>
2011-01-14 12:21:41 +0100

    NED Editor: Type validator works correctly with inner types and packages.
    X-Committed-on-Branch: master

--- ui/org.omnetpp.ned.editor/src/org/omnetpp/ned/editor/graph/properties/util/TypeNameValidator.java
+++ ui/org.omnetpp.ned.editor/src/org/omnetpp/ned/editor/graph/properties/util/TypeNameValidator.java
@@ -7,12 +7,17 @@
 
 package org.omnetpp.ned.editor.graph.properties.util;
 
+import java.util.Set;
+
 import org.eclipse.core.resources.IProject;
+import org.omnetpp.ned.core.INedResources;
 import org.omnetpp.ned.core.NedResourcesPlugin;
+import org.omnetpp.ned.model.ex.CompoundModuleElementEx;
 import org.omnetpp.ned.model.interfaces.INedTypeElement;
+import org.omnetpp.ned.model.interfaces.INedTypeResolver;
 
 /**
- * Validator used for checking new top level type names. NOTE: Inner types should be validated differently.
+ * Validator used for checking new type names.
  * 
  * @author rhornig, andras (cleanup)
  */
@@ -24,12 +29,18 @@ public class TypeNameValidator extends NedNameValidator {
     }
 
     @Override
-    public String isValid(Object newText) {
-        IProject project = NedResourcesPlugin.getNedResources().getNedFile(typeElement.getContainingNedFileElement()).getProject();
-        if (NedResourcesPlugin.getNedResources().getReservedQNames(project).contains(newText) &&
-                !newText.equals(typeElement.getName()))
-            return "Name is already in use. There is already a type with the same name.";  //FIXME ez szar: package-ekkel mi van???
+    public String isValid(Object simpleName) {
+        INedResources resolver = NedResourcesPlugin.getNedResources();
+        IProject project = resolver.getNedFile(typeElement.getContainingNedFileElement()).getProject();
+        String namePrefix = typeElement.getNedTypeInfo().getNamePrefix();
+        Set<String> existingQTypeNames = resolver.getReservedQNames(project);  // top level types
+        if (typeElement.getEnclosingLookupContext() instanceof CompoundModuleElementEx) // add inner types if needed
+            existingQTypeNames.addAll(resolver.getLocalTypeNames(typeElement.getEnclosingLookupContext(), INedTypeResolver.ALL_FILTER));
+        
+        if (existingQTypeNames.contains(namePrefix+simpleName) &&
+                !simpleName.equals(typeElement.getName()))
+            return "Name is already in use. There is already a type with the same name.";  
 
-        return super.isValid(newText);
+        return super.isValid(simpleName);
     }
 }
