1793cbd7ab45e78eb7529a78533c8157b5e69e82
Andras Varga <andras@omnetpp.org>
2011-01-20 11:08:25 +0100

    NedTypeSelectionDialog: extended functionality: more control over which types to offer
    X-Committed-on-Branch: master

--- ui/org.omnetpp.ned.core/src/org/omnetpp/ned/core/ui/misc/NedTypeSelectionDialog.java
+++ ui/org.omnetpp.ned.core/src/org/omnetpp/ned/core/ui/misc/NedTypeSelectionDialog.java
@@ -1,5 +1,8 @@
 package org.omnetpp.ned.core.ui.misc;
 
+import java.util.Collection;
+
+import org.eclipse.core.resources.IProject;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.swt.graphics.Image;
@@ -15,15 +18,22 @@ import org.omnetpp.ned.core.INedResources;
 import org.omnetpp.ned.core.NedResourcesPlugin;
 import org.omnetpp.ned.model.NedTreeUtil;
 import org.omnetpp.ned.model.interfaces.INedTypeInfo;
+import org.omnetpp.ned.model.interfaces.INedTypeResolver;
 import org.omnetpp.ned.model.notification.INedChangeListener;
 import org.omnetpp.ned.model.notification.NedModelEvent;
 
 /**
- * NED type selection dialog.
+ * NED type selection dialog. It can offer all types, a list of types, types visible 
+ * from a project and/or filtered by a predicate. By default this dialog allows 
+ * one item to be selected; call setMultipleSelection(true) to allow multiple selection.
  * 
  * @author Andras
  */
 public class NedTypeSelectionDialog extends ElementListSelectionDialog {
+    private INedTypeResolver.IPredicate typeFilter; 
+    private IProject contextProject;
+    private INedTypeInfo[] typesToOffer; // overrides previous two 
+
 
     INedChangeListener nedChangeListener = new INedChangeListener() {
         public void modelChanged(NedModelEvent event) {
@@ -36,8 +46,7 @@ public class NedTypeSelectionDialog extends ElementListSelectionDialog {
         public void run() {
             if (getShell() != null && !getShell().isDisposed()) {
                 Debug.println("NedTypeSelectionDialog: running delayed update");
-                INedResources nedResources = NedResourcesPlugin.getNedResources();
-                updateElements(nedResources.getNedTypesFromAllProjects().toArray());
+                updateElements(getElementsToShow());
             }
         }
     };
@@ -71,16 +80,79 @@ public class NedTypeSelectionDialog extends ElementListSelectionDialog {
         super(parent, new NedLabelProvider());
         setMessage("Select NED type:");
         setTitle("Select NED Type");
-        INedResources nedResources = NedResourcesPlugin.getNedResources();
-        setElements(nedResources.getNedTypesFromAllProjects().toArray());
         setDialogBoundsSettings(UIUtils.getDialogSettings(NedResourcesPlugin.getDefault(), "SelectNEDTypeDialog"), Dialog.DIALOG_PERSISTSIZE + Dialog.DIALOG_PERSISTLOCATION);
+        INedResources nedResources = NedResourcesPlugin.getNedResources();
         nedResources.addNedModelChangeListener(nedChangeListener);
     }
 
     /**
-     * Updates an existing dialog's contents
+     * Sets the context project. When non-null, only types visible from the given project are offered.
      */
-    public void updateElements(Object[] elements) {
+    public void setContextProject(IProject contextProject) {
+        this.contextProject = contextProject;
+    }
+    
+    public IProject getContextProject() {
+        return contextProject;
+    }
+
+    /**
+     * Sets a filter predicate. When non-null, only types matching the predicate are offered. 
+     * Can be combined with setContextProject().
+     */
+    public void setTypeFilter(INedTypeResolver.IPredicate typeFilter) {
+        this.typeFilter = typeFilter;
+    }
+
+    public INedTypeResolver.IPredicate getTypeFilter() {
+        return typeFilter;
+    }
+
+    /**
+     * Sets the list of types to offer. This overrides the context project and the type filter.
+     */
+    public void setTypesToOffer(INedTypeInfo[] typesToOffer) {
+        this.typesToOffer = typesToOffer;
+    }
+    
+    public INedTypeInfo[] getTypesToOffer() {
+        return typesToOffer;
+    }
+
+    protected INedTypeInfo[] getElementsToShow() {
+        if (getTypesToOffer() != null)
+            return getTypesToOffer();
+
+        INedResources nedResources = NedResourcesPlugin.getNedResources();
+        Collection<INedTypeInfo> result;
+
+        if (getContextProject() == null) {
+            if (getTypeFilter() == null)
+                result = nedResources.getNedTypesFromAllProjects();
+            else 
+                result = nedResources.getNedTypesFromAllProjects(getTypeFilter());
+        }
+        else {
+            if (getTypeFilter() == null)
+                result = nedResources.getNedTypes(getContextProject());
+            else 
+                result = nedResources.getNedTypes(getTypeFilter(), getContextProject());
+        }
+        return result.toArray(new INedTypeInfo[]{});   
+    }
+    
+    @Override
+    public int open() {
+        setElements(getElementsToShow());
+        return super.open();
+    }
+
+    /**
+     * Updates an existing dialog's contents. Most useful when NED types are being loaded
+     * while the dialog is already open and the user can interact with it. (This can occur
+     * just after IDE startup when no editors are open, and user hits the Open NED Type button.)   
+     */
+    protected void updateElements(Object[] elements) {
         setListElements(elements);
         if (elements.length == 0) {
             handleEmptyList();
