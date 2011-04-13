22e500353d60e5ddd7318684fac53c29c6bc6984
Levente Mészáros <levente.meszaros@gmail.com>
2011-04-13 11:23:03 +0200

    Infinite animation primitives end in the infinity.
    The end of the whole animation is still at the very last event and simulation time.
    
    X-Committed-on-Branch: topic/animation

--- ui/org.omnetpp.animation/src/org/omnetpp/animation/primitives/AbstractInfiniteAnimation.java
+++ ui/org.omnetpp.animation/src/org/omnetpp/animation/primitives/AbstractInfiniteAnimation.java
@@ -1,4 +1,5 @@
 /*--------------------------------------------------------------*
+
   Copyright (C) 2006-2008 OpenSim Ltd.
 
   This file is distributed WITHOUT ANY WARRANTY. See the file
@@ -8,6 +9,7 @@
 package org.omnetpp.animation.primitives;
 
 import org.omnetpp.animation.widgets.AnimationController;
+import org.omnetpp.common.engine.BigDecimal;
 import org.omnetpp.eventlog.engine.IEvent;
 
 /**
@@ -20,9 +22,10 @@ public class AbstractInfiniteAnimation extends AbstractAnimationPrimitive {
 	public AbstractInfiniteAnimation(AnimationController animationController) {
 		super(animationController);
 		IEvent lastEvent = animationController.getEventLogInput().getEventLog().getLastEvent();
+		// NOTE: there are no events after the last one, but we can pretend that simulation time goes on
+		// this is necessary, because some animation might go beyond that (e.g. a message send)
 		setEndEventNumber(lastEvent.getEventNumber());
-		setEndSimulationTime(lastEvent.getSimulationTime());
-		// TODO: how do we figure out the correct maximum frame relative animation time?
-		setFrameRelativeEndAnimationTime(Double.MAX_VALUE);
+	    setEndSimulationTime(BigDecimal.getPositiveInfinity());
+	    setFrameRelativeEndAnimationTime(Double.POSITIVE_INFINITY);
 	}
 }
