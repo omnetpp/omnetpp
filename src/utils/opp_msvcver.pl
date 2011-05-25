e2b5f731f56e8f386750f67082c7023d391e945e
Andras Varga <andras@omnetpp.org>
2011-05-25 13:43:59 +0200

    opp_msvcver.pl: fix #75: Chinese version of Visual C++ not recognized
    X-Committed-on-Branch: master

--- src/utils/opp_msvcver.pl
+++ src/utils/opp_msvcver.pl
@@ -3,7 +3,9 @@
 $prog = $ARGV[0];
 $logo=`$prog 2>&1`;
 
-if ($logo =~ /Microsoft.*32-bit.*Version ([0-9]+)\./) {
+# note: the regex below must match for internationalized versions of MSVC as well,
+# so e.g. it cannot contain the words "bit" (as in "32-bit") or "version".
+if ($logo =~ /Microsoft.*\s([0-9]+)\./) { 
   exit($1);
 } else {
   exit(-1);
