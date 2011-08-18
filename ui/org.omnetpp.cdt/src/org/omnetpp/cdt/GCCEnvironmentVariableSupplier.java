cf347c7f3518322a21a05e1e074a6bf19178d9d2
Andras Varga <andras@omnetpp.org>
2011-08-18 18:15:32 +0200

    IDE: merged MingwEnvironmentVariableSupplier_COPY into GCCEnvironmentVariableSupplier, and simplified
    X-Committed-on-Branch: topic/msvc-build

--- ui/org.omnetpp.cdt/src/org/omnetpp/cdt/GCCEnvironmentVariableSupplier.java
+++ ui/org.omnetpp.cdt/src/org/omnetpp/cdt/GCCEnvironmentVariableSupplier.java
@@ -9,35 +9,44 @@ package org.omnetpp.cdt;
 
 import org.eclipse.cdt.managedbuilder.core.IConfiguration;
 import org.eclipse.cdt.managedbuilder.envvar.IBuildEnvironmentVariable;
+import org.eclipse.cdt.managedbuilder.envvar.IConfigurationEnvironmentVariableSupplier;
 import org.eclipse.cdt.managedbuilder.envvar.IEnvironmentVariableProvider;
 import org.eclipse.core.runtime.Platform;
-
+import org.omnetpp.common.util.StringUtils;
+import org.omnetpp.ide.OmnetppMainPlugin;
 
 /**
  * We use the same toolchain for gcc on all platforms, so we need a combined
- * environment variable supplier that works on all platforms. As of CDT 5.0,
- * CDT contains a MingwEnvironmentVariableSupplier but no supplier for other platforms
- * (except Cygwin which we don't support). So here we invoke the MinGW one if platform
- * is win32, and do nothing in other cases.
+ * environment variable supplier that works on all platforms.
+ *
+ * Note: we use omnetpp/msys/bin and omnetpp/mingw/bin instead of the registry reading code
+ * in CDT's MingwEnvironmentVariableSupplier.
  *
  * @author Andras
  */
-public class GCCEnvironmentVariableSupplier extends MingwEnvironmentVariableSupplier_COPY {
+public class GCCEnvironmentVariableSupplier implements IConfigurationEnvironmentVariableSupplier {
+	public IBuildEnvironmentVariable getPathVariable() {
+	    StringBuilder buff = new StringBuilder();
 
-    @Override
-    public IBuildEnvironmentVariable getVariable(String variableName, IConfiguration configuration, IEnvironmentVariableProvider provider) {
-        if (Platform.getOS().equals(Platform.OS_WIN32))
-            return super.getVariable(variableName, configuration, provider);
-        else
-            return null;
-    }
+	    if (Platform.getOS().equals(Platform.OS_WIN32)) {
+	        String mingwBinDir = OmnetppMainPlugin.getMingwBinDir();
+	        if (!StringUtils.isEmpty(mingwBinDir))
+	            buff.append(mingwBinDir).append(';');
 
-    @Override
-    public IBuildEnvironmentVariable[] getVariables(IConfiguration configuration, IEnvironmentVariableProvider provider) {
-        if (Platform.getOS().equals(Platform.OS_WIN32))
-            return super.getVariables(configuration, provider);
-        else
-            return new IBuildEnvironmentVariable[0];
-    }
+	        String msysBinDir = OmnetppMainPlugin.getMsysBinDir();
+	        if (!StringUtils.isEmpty(msysBinDir))
+	            buff.append(msysBinDir).append(';');
+	    }
+
+	    return new BuildEnvironmentVariable("PATH", buff.toString(), IBuildEnvironmentVariable.ENVVAR_PREPEND);
+	}
+
+	public IBuildEnvironmentVariable getVariable(String variableName, IConfiguration configuration, IEnvironmentVariableProvider provider) {
+		return variableName.equals("PATH") ? getPathVariable() : null;
+	}
+
+	public IBuildEnvironmentVariable[] getVariables(IConfiguration configuration, IEnvironmentVariableProvider provider) {
+		return new IBuildEnvironmentVariable[] { getPathVariable() };
+	}
 
 }
