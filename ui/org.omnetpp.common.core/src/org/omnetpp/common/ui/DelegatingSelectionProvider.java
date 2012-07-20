f47b46e3b3a2647264c4611aeb450a95aae28f2a
Andras Varga <andras@omnetpp.org>
2012-07-20 16:20:54 +0200

    Simulation: implemented DelegatingSelectionProvider
    So we can switch editor selection between selections provided
    by AnimationCanvas and SimulationCanvas.
    
    X-Committed-on-Branch: anim_tmp_livesim
    
    X-Committed-on-Branch: (no branch)

---
+++ ui/org.omnetpp.common.core/src/org/omnetpp/common/ui/DelegatingSelectionProvider.java
@@ -0,0 +1,65 @@
+/*--------------------------------------------------------------*
+  Copyright (C) 2006-2008 OpenSim Ltd.
+
+  This file is distributed WITHOUT ANY WARRANTY. See the file
+  'License' for details on this and other legal matters.
+*--------------------------------------------------------------*/
+
+package org.omnetpp.common.ui;
+
+import org.eclipse.core.runtime.ListenerList;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.ISelectionProvider;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
+
+/**
+ * A selection provider that delegates to another selection provider. One can switch
+ * selection providers. Useful e.g. with multi-page editors.
+ *
+ * @author andras
+ */
+public class DelegatingSelectionProvider implements ISelectionProvider {
+	private ListenerList listeners = new ListenerList();
+	private ISelectionProvider currentSelectionProvider = null;
+
+	private ISelectionChangedListener listener = new ISelectionChangedListener() {
+        public void selectionChanged(SelectionChangedEvent event) {
+            fireSelectionChanged(event);
+        }
+    }; 
+	
+	public DelegatingSelectionProvider() {
+	}
+
+	public void setSelectionProvider(ISelectionProvider selectionProvider) {
+	    if (currentSelectionProvider != null)
+	        currentSelectionProvider.removeSelectionChangedListener(listener);
+	    currentSelectionProvider = selectionProvider;
+	    currentSelectionProvider.addSelectionChangedListener(listener);
+	    
+	    SelectionChangedEvent e = new SelectionChangedEvent(selectionProvider, selectionProvider.getSelection());
+        fireSelectionChanged(e);
+	}
+	
+	private void fireSelectionChanged(SelectionChangedEvent event) {
+		for (Object listener : listeners.getListeners())
+			((ISelectionChangedListener)listener).selectionChanged(event);
+	}
+
+	public void setSelection(ISelection selection) {
+	    currentSelectionProvider.setSelection(selection);
+	}
+	
+	public ISelection getSelection() {
+		return currentSelectionProvider.getSelection();
+	}
+
+	public void addSelectionChangedListener(ISelectionChangedListener listener) {
+        listeners.add(listener);
+    }
+
+    public void removeSelectionChangedListener(ISelectionChangedListener listener) {
+        listeners.remove(listener);
+    }
+}
