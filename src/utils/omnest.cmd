6e79709ec0899527f4688297419b7bb18be76fe3
Rudolf Hornig <rudi@omnetpp.org>
2012-11-30 17:57:09 +0100

    IDE: modified start scripts to pass the command line parameters to the IDE
    X-Committed-on-Branch: master

--- src/utils/omnest.cmd
+++ src/utils/omnest.cmd
@@ -1,7 +1,6 @@
 @echo off
 SET IDEDIR=%~dp0\..\ide
 SET LAUNCHER=omnest
-cd %IDEDIR%
 SET PATH=%~dp0;%~dp0\..\msys\bin;%~dp0\..\mingw\bin;%PATH%
 echo Starting the OMNEST IDE...
-start .\%LAUNCHER% >out.log
+start %IDEDIR%\%LAUNCHER% %* >out.log
