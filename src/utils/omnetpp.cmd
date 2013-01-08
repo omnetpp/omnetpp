803a44b1b06eb2ebebe69940bd8f20eca9fc4b5f
Rudolf Hornig <rudi@omnetpp.org>
2013-01-08 16:25:31 +0100

    command line: ide launcher scripts now provide correct default workspace location.
    X-Committed-on-Branch: master

--- src/utils/omnetpp.cmd
+++ src/utils/omnetpp.cmd
@@ -2,5 +2,6 @@
 SET IDEDIR=%~dp0\..\ide
 SET LAUNCHER=omnetpp
 SET PATH=%~dp0;%~dp0\..\msys\bin;%~dp0\..\mingw\bin;%PATH%
+SET DEFAULT_WORKSPACE_ARGS="-vmargs -Dosgi.instance.area.default=%IDEDIR%\..\samples"
 echo Starting the OMNeT++ IDE...
-start %IDEDIR%\%LAUNCHER% %* >out.log
+start %IDEDIR%\%LAUNCHER% %* %DEFAULT_WORKSPACE_ARGS% >out.log
