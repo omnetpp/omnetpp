4aa15965fb4f6f0eb6b542a06b372ca0c6c790d0
Andras Varga <andras@omnetpp.org>
2013-02-18 11:44:04 +0100

    upped version number to 5.0
    X-Committed-on-Branch: misc

--- include/simkerneldefs.h
+++ include/simkerneldefs.h
@@ -26,7 +26,7 @@
 #include "platdep/intxtypes.h"
 
 // OMNeT++ version -- must match NEDC_VERSION and MSGC_VERSION in nedxml
-#define OMNETPP_VERSION 0x0403
+#define OMNETPP_VERSION 0x0500
 
 #if defined(SIM_EXPORT)
 #  define SIM_API OPP_DLLEXPORT
