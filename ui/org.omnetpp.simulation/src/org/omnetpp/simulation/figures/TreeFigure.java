aa1a100168544475f13b9d30ec91d5eb028ef79e
Tamas Borbely <tomi.borbely@gmail.com>
2013-03-03 20:04:21 +0100

    draw2d tree
    X-Committed-on-Branch: (no branch)

---
+++ ui/org.omnetpp.simulation/src/org/omnetpp/simulation/figures/TreeFigure.java
@@ -0,0 +1,416 @@
+package org.omnetpp.simulation.figures;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.runtime.Assert;
+import org.eclipse.draw2d.GridData;
+import org.eclipse.draw2d.GridLayout;
+import org.eclipse.draw2d.IFigure;
+import org.eclipse.draw2d.KeyEvent;
+import org.eclipse.draw2d.KeyListener;
+import org.eclipse.draw2d.LineBorder;
+import org.eclipse.draw2d.MouseEvent;
+import org.eclipse.draw2d.MouseListener;
+import org.eclipse.draw2d.Panel;
+import org.eclipse.draw2d.ScrollPane;
+import org.eclipse.draw2d.geometry.Dimension;
+import org.eclipse.draw2d.geometry.Point;
+import org.eclipse.draw2d.geometry.Rectangle;
+import org.eclipse.jface.viewers.DelegatingStyledCellLabelProvider.IStyledLabelProvider;
+import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.jface.viewers.StyledString;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Cursor;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Display;
+import org.omnetpp.simulation.figures.TreeItemFigure.ToggleFigure;
+
+// TODO styles: SINGLE, MULTI, CHECK, FULL_SELECTION, VIRTUAL, NO_SCROLL
+public class TreeFigure extends ScrollPane {
+
+    private static final Cursor CURSOR_ARROW = new Cursor(Display.getDefault(), SWT.CURSOR_ARROW);
+
+    // panel containing TreeItemFigures
+    class ContentsPanel extends Panel {
+        @Override
+        public void remove(IFigure figure) {
+            handleItemRemoved(figure);
+            super.remove(figure);
+        }
+
+        @Override
+        protected boolean useLocalCoordinates() {
+            return true;
+        }
+    }
+
+    Object input;
+    ITreeContentProvider contentProvider;
+    IStyledLabelProvider labelProvider;
+    TreeItemFigure lastSelectedItem;
+    Panel contentsPane;
+
+    MouseListener toggleMouseListener = new MouseListener.Stub() {
+        @Override
+        public void mouseReleased(MouseEvent event) {
+            handleMouseReleasedOnToggle(event);
+        }
+    };
+
+    MouseListener itemMouseClickListener = new MouseListener.Stub() {
+        @Override
+        public void mousePressed(MouseEvent event) {
+            handleMousePressedOnItem(event);
+        }
+    };
+
+    KeyListener itemKeyPressedListener = new KeyListener.Stub() {
+        @Override
+        public void keyPressed(KeyEvent event) {
+            handleKeyPressedEvent(event);
+        }
+    };
+
+    public TreeFigure() {
+        setRequestFocusEnabled(true);
+        setFocusTraversable(true);
+        setBorder(new LineBorder(Display.getDefault().getSystemColor(SWT.COLOR_WIDGET_BORDER)));
+        setCursor(CURSOR_ARROW); // XXX should not be necessary
+
+        contentsPane = new ContentsPanel();
+        GridLayout layout = new GridLayout(1, false);
+        layout.marginWidth = 0;
+        layout.marginHeight = 0;
+        layout.verticalSpacing = 0;
+        contentsPane.setLayoutManager(layout);
+        setContents(contentsPane);
+
+        addKeyListener(itemKeyPressedListener);
+    }
+
+    public Dimension getMaximumSize() {
+        return getPreferredSize();
+    }
+
+    public Object getInput() {
+        return input;
+    }
+
+    public void setInput(Object input) {
+        if (this.input != input) {
+            this.input = input;
+            rebuild();
+            revalidate();
+            repaint();
+        }
+    }
+
+    public ITreeContentProvider getContentProvider() {
+        return contentProvider;
+    }
+
+    public void setContentProvider(ITreeContentProvider contentProvider) {
+        if (this.contentProvider != contentProvider) {
+            this.contentProvider = contentProvider;
+            rebuild();
+            revalidate();
+            repaint();
+        }
+    }
+
+    public IStyledLabelProvider getLabelProvider() {
+        return labelProvider;
+    }
+
+    public void setLabelProvider(IStyledLabelProvider labelProvider) {
+        if (this.labelProvider != labelProvider) {
+            this.labelProvider = labelProvider;
+            revalidate();
+            repaint();
+        }
+    }
+
+    protected void rebuild() {
+        contentsPane.removeAll();
+        if (contentProvider == null || labelProvider == null || input == null)
+            return;
+
+        Object roots[] = contentProvider.getElements(input);
+        for (Object root : roots)
+            addTreeItems(root, 0);
+    }
+
+    protected void addTreeItems(Object node, int level) {
+        boolean hasChildren = contentProvider.hasChildren(node);
+        TreeItemFigure item = addTreeItem(node, level, hasChildren);
+        if (hasChildren && item.isExpanded()) {
+            Object[] children = contentProvider.getChildren(node);
+            for (Object child : children)
+                addTreeItems(child, level+1);
+        }
+    }
+
+    protected TreeItemFigure addTreeItem(Object node, int level, boolean hasChildren) {
+        return addTreeItem(node, level, hasChildren, -1);
+    }
+
+    protected TreeItemFigure addTreeItem(Object node, int level, boolean hasChildren, int index) {
+        TreeItemFigure item = createTreeItem(node, level, hasChildren);
+        contentsPane.add(item, index);
+        contentsPane.setConstraint(item, new GridData(GridData.FILL, GridData.CENTER, true, false));
+        item.addMouseListener(itemMouseClickListener);
+        if (item.getToggle() != null)
+            item.getToggle().addMouseListener(toggleMouseListener);
+        return item;
+    }
+
+    protected TreeItemFigure createTreeItem(Object node, int level, boolean hasChildren) {
+        Image image = labelProvider.getImage(node);
+        StyledString label = labelProvider.getStyledText(node);
+        TreeItemFigure item = new TreeItemFigure(node, level, hasChildren);
+        item.setImage(image);
+        item.setLabel(label);
+        return item;
+    }
+
+    protected void handleItemRemoved(IFigure item) {
+        if (item instanceof TreeItemFigure) {
+            item.removeMouseListener(itemMouseClickListener);
+            IFigure toggle = ((TreeItemFigure)item).getToggle();
+            if (toggle != null)
+                toggle.removeMouseListener(toggleMouseListener);
+            if (item == lastSelectedItem)
+                lastSelectedItem = null;
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    protected List<IFigure> getItems() {
+        return contentsPane.getChildren();
+    }
+
+    protected int getItemIndex(TreeItemFigure item) {
+        return getItems().indexOf(item);
+    }
+
+    protected TreeItemFigure getNextItem(TreeItemFigure item) {
+        List<IFigure> items = getItems();
+        int index = items.indexOf(item);
+        return index < items.size() - 1 ? (TreeItemFigure)items.get(index+1) : null;
+    }
+
+    protected TreeItemFigure getPreviousItem(TreeItemFigure item) {
+        List<IFigure> items = getItems();
+        int index = items.indexOf(item);
+        return index > 0 ? (TreeItemFigure)items.get(index-1) : null;
+    }
+
+    protected void collapseItem(TreeItemFigure item) {
+        Assert.isTrue(item.getParent() == contentsPane && !item.isExpanded());
+        int level = item.getLevel();
+        List<IFigure> children = getItems();
+        List<IFigure> descendants = new ArrayList<IFigure>();
+        for (int i = children.indexOf(item) + 1; i < children.size(); ++i) {
+            TreeItemFigure childItem = (TreeItemFigure)children.get(i);
+            if (childItem.getLevel() > level)
+                descendants.add(childItem);
+            else
+                break;
+        }
+
+        for (IFigure descendant : descendants)
+            contentsPane.remove(descendant);
+
+        revalidate();
+        repaint();
+    }
+
+    protected void expandItem(TreeItemFigure item) {
+        Assert.isTrue(item.getParent() == contentsPane && item.isExpanded());
+        Object content = item.getContent();
+        int level = item.getLevel();
+        Object[] children = contentProvider.getChildren(content);
+        int index = getItems().indexOf(item);
+        for (Object child : children) {
+            boolean hasChildren = contentProvider.hasChildren(child);
+            addTreeItem(child, level+1, hasChildren, ++index);
+        }
+        revalidate();
+        repaint();
+    }
+
+    protected void toggleSelection(TreeItemFigure item) {
+        lastSelectedItem = item;
+        item.setSelected(!item.isSelected());
+    }
+
+    protected void extendSelection(TreeItemFigure item) {
+        if (lastSelectedItem != null) {
+            List<IFigure> items = getItems();
+            int itemIndex = items.indexOf(item);
+            int lastSelectedIndex = items.indexOf(lastSelectedItem);
+            if (itemIndex >= 0 && lastSelectedIndex >= 0) { // TODO assert?
+                int start = Math.min(itemIndex, lastSelectedIndex);
+                int end = Math.max(itemIndex, lastSelectedIndex);
+                for (int i = 0; i < items.size(); ++i) {
+                    TreeItemFigure ithItem = (TreeItemFigure)items.get(i);
+                    ithItem.setSelected(start <= i && i <= end);
+                }
+            }
+            lastSelectedItem = item;
+        }
+        else {
+            setSelection(item);
+        }
+    }
+
+    protected void setSelection(TreeItemFigure item) {
+        lastSelectedItem = item;
+        for (IFigure child : getItems()) {
+            ((TreeItemFigure)child).setSelected(child == item);
+        }
+    }
+
+    protected void collapseCurrentItem() {
+        if (lastSelectedItem != null && lastSelectedItem.isExpandable() && lastSelectedItem.isExpanded()) {
+            lastSelectedItem.setExpanded(false);
+            collapseItem(lastSelectedItem);
+        }
+    }
+
+    protected void expandCurrentItem() {
+        if (lastSelectedItem != null && lastSelectedItem.isExpandable() && !lastSelectedItem.isExpanded()) {
+            lastSelectedItem.setExpanded(true);
+            expandItem(lastSelectedItem);
+        }
+    }
+
+    protected void toggleCurrentItem() {
+        if (lastSelectedItem != null && lastSelectedItem.isExpandable()) {
+            if (!lastSelectedItem.isExpanded())
+                expandCurrentItem();
+            else
+                collapseCurrentItem();
+        }
+    }
+
+    protected void showItem(TreeItemFigure item) {
+        Point itemPos = item.getLocation();
+        int itemHeight = item.getSize().height;
+        Point scrollPos = getViewport().getViewLocation();
+        int viewportHeight = getViewport().getClientArea(new Rectangle()).getSize().height;
+
+        if (itemPos.y < scrollPos.y) {
+            scrollVerticalTo(itemPos.y);
+            repaint();
+        }
+        else if (itemPos.y + itemHeight > scrollPos.y + viewportHeight) {
+            scrollVerticalTo(itemPos.y + itemHeight - viewportHeight);
+            repaint();
+        }
+    }
+
+    protected void handleMouseReleasedOnToggle(MouseEvent event) {
+        if (event.getSource() instanceof ToggleFigure && event.button == 1) {
+            TreeItemFigure item = (TreeItemFigure)((ToggleFigure)event.getSource()).getParent();
+            if (item.isExpanded())
+                expandItem(item);
+            else
+                collapseItem(item);
+        }
+    }
+
+    protected void handleMousePressedOnItem(MouseEvent event) {
+        requestFocus();
+        if (event.getSource() instanceof TreeItemFigure) {
+            TreeItemFigure item = (TreeItemFigure)event.getSource();
+            int state = event.getState();
+            if ((state & SWT.CTRL) != 0) {
+                toggleSelection(item);
+            }
+            else if ((state & SWT.SHIFT) != 0) {
+                extendSelection(item);
+            }
+            else if ((state & SWT.ALT) == 0) {
+                setSelection(item);
+            }
+        }
+    }
+
+    protected void handleKeyPressedEvent(KeyEvent event) {
+        int key = event.keycode;
+        int state = event.getState();
+        boolean shift = (state & SWT.SHIFT) != 0;
+        boolean ctrl = (state & SWT.CTRL) != 0;
+        switch (key) {
+        case SWT.ARROW_LEFT:
+            collapseCurrentItem();  // Linux (both Gnome and KDE) require Shift for expand/collapse, but neither Windows not OS X does -- there's no harm in ignoring the Shift state
+            break;
+        case SWT.ARROW_RIGHT:
+            expandCurrentItem();
+            break;
+        case SWT.ARROW_DOWN:
+            handleKeyDown(shift, ctrl);
+            break;
+        case SWT.ARROW_UP:
+            handleKeyUp(shift, ctrl);
+            break;
+        case ' ':
+            toggleCurrentItem();  // seems to be Linux only, but there's not harm doing it on all platforms
+            break;
+        }
+    }
+
+    protected void handleKeyUp(boolean shift, boolean ctrl) {
+        if (lastSelectedItem != null && !lastSelectedItem.isSelected()) {
+            setSelection(lastSelectedItem);
+        }
+        else if (lastSelectedItem == null) {
+            List<IFigure> items = getItems();
+            if (!items.isEmpty())
+                setSelection((TreeItemFigure)items.get(0));
+        }
+        else if (shift) {
+            TreeItemFigure prevItem = getPreviousItem(lastSelectedItem);
+            if (prevItem != null) {
+                prevItem.setSelected(true);
+                lastSelectedItem = prevItem;
+                showItem(prevItem);
+            }
+        }
+        else {
+            TreeItemFigure prevItem = getPreviousItem(lastSelectedItem);
+            if (prevItem != null) {
+                setSelection(prevItem);
+                showItem(prevItem);
+            }
+        }
+    }
+
+    protected void handleKeyDown(boolean shift, boolean ctrl) {
+        if (lastSelectedItem != null && !lastSelectedItem.isSelected()) {
+            setSelection(lastSelectedItem);
+        }
+        else if (lastSelectedItem == null) {
+            List<IFigure> items = getItems();
+            if (!items.isEmpty())
+                setSelection((TreeItemFigure)items.get(0));
+        }
+        else if (shift) {
+            TreeItemFigure nextItem = getNextItem(lastSelectedItem);
+            if (nextItem != null) {
+                nextItem.setSelected(true);
+                lastSelectedItem = nextItem;
+                showItem(nextItem);
+            }
+        }
+        else {
+            TreeItemFigure nextItem = getNextItem(lastSelectedItem);
+            if (nextItem != null) {
+                setSelection(nextItem);
+                showItem(nextItem);
+            }
+        }
+    }
+}
---
+++ ui/org.omnetpp.simulation/src/org/omnetpp/simulation/figures/TreeItemFigure.java
@@ -0,0 +1,225 @@
+package org.omnetpp.simulation.figures;
+
+import org.eclipse.draw2d.AbstractLayout;
+import org.eclipse.draw2d.Figure;
+import org.eclipse.draw2d.Graphics;
+import org.eclipse.draw2d.IFigure;
+import org.eclipse.draw2d.ImageFigure;
+import org.eclipse.draw2d.MouseEvent;
+import org.eclipse.draw2d.MouseListener;
+import org.eclipse.draw2d.MouseMotionListener;
+import org.eclipse.draw2d.geometry.Dimension;
+import org.eclipse.draw2d.geometry.Rectangle;
+import org.eclipse.jface.viewers.StyledString;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Display;
+import org.omnetpp.common.color.ColorFactory;
+
+class TreeItemFigure extends Figure {
+
+    private static final Color SELECTION_BACKGROUND_COLOR = Display.getDefault().getSystemColor(SWT.COLOR_LIST_SELECTION);
+
+    static class ToggleFigure extends Figure{
+
+        boolean isExpanded;
+        boolean isMouseOver;
+        int [] vertices = new int[6];
+
+        MouseListener mouseListener = new MouseListener.Stub() {
+            @Override
+            public void mouseReleased(MouseEvent event) {
+                onMouseReleased(event);
+            }
+        };
+
+        MouseMotionListener mouseMotionListener = new MouseMotionListener.Stub() {
+            @Override
+            public void mouseEntered(MouseEvent me) {
+                if (!isMouseOver) {
+                    isMouseOver = true;
+                    repaint();
+                }
+            }
+
+            @Override
+            public void mouseExited(MouseEvent me) {
+                if (isMouseOver) {
+                    isMouseOver = false;
+                    repaint();
+                }
+            }
+        };
+
+        ToggleFigure() {
+            addMouseListener(mouseListener);
+            addMouseMotionListener(mouseMotionListener);
+        }
+
+        @Override
+        public Dimension getPreferredSize(int wHint, int hHint) {
+            return isExpanded ? new Dimension(9, 7) : new Dimension(7,9);
+        }
+
+        @Override
+        public void paint(Graphics graphics) {
+            graphics.setForegroundColor(ColorFactory.BLACK);
+            graphics.setBackgroundColor(ColorFactory.BLACK);
+            Rectangle area = getBounds();
+            int x = area.x, y = area.y, w = area.width, h = area.height;
+            if (isExpanded) {
+                vertices[0] = x;
+                vertices[1] = y;
+                vertices[2] = x+w;
+                vertices[3] = y;
+                vertices[4] = x+w/2;
+                vertices[5] = y+h;
+            }
+            else {
+                vertices[0] = x;
+                vertices[1] = y;
+                vertices[2] = x;
+                vertices[3] = y+h;
+                vertices[4] = x+w;
+                vertices[5] = y+h/2;
+            }
+
+            if (isMouseOver)
+                graphics.fillPolygon(vertices);
+            else
+                graphics.drawPolygon(vertices);
+        }
+
+        private void onMouseReleased(MouseEvent event) {
+            if (event.button == 1) {
+                isExpanded = !isExpanded;
+                revalidate();
+                repaint();
+            }
+        }
+    }
+
+    class TreeItemLayout extends AbstractLayout {
+        final static int hmargin = 5;
+        final static int vmargin = 2;
+        private static final int indent = 10;
+        final static int spacing = 2;
+        final static int toggleWidth = 12;
+        final static int toggleHeight = 10;
+
+        @Override
+        protected Dimension calculatePreferredSize(IFigure container, int wHint, int hHint) {
+            int w = toggleWidth + 2*hmargin + spacing;
+            int h = toggleHeight;
+            Dimension imageSize = imageFigure.getPreferredSize();
+            Dimension labelSize = labelFigure.getPreferredSize();
+            w += level * indent + imageSize.width + labelSize.width;
+            h = Math.max(h, imageSize.height);
+            h = Math.max(h, labelSize.height);
+            h += 2 * vmargin;
+            return new Dimension(w,h);
+        }
+
+        @Override
+        public void layout(IFigure container) {
+            Rectangle clientArea = container.getClientArea();
+            Dimension imageSize = imageFigure.getPreferredSize();
+            Dimension labelSize = labelFigure.getPreferredSize();
+
+            Rectangle newBounds = new Rectangle();
+            newBounds.x = clientArea.x + hmargin + level * indent;
+            newBounds.y = clientArea.y + vmargin;
+
+            if (toggleFigure != null) {
+                Dimension toggleSize = toggleFigure.getPreferredSize();
+                newBounds.width = toggleSize.width;
+                newBounds.height = toggleSize.height;
+                toggleFigure.setBounds(newBounds);
+            }
+
+            newBounds.x += toggleWidth;
+            newBounds.width = imageSize.width;
+            newBounds.height = imageSize.height;
+            imageFigure.setBounds(newBounds);
+
+            newBounds.x += imageSize.width + spacing;
+            newBounds.width = labelSize.width;
+            newBounds.height = labelSize.height;
+            labelFigure.setBounds(newBounds);
+        }
+    }
+
+    private Object content;
+    private ToggleFigure toggleFigure;
+    private ImageFigure imageFigure;
+    private StyledLabel labelFigure;
+    private boolean isSelected;
+    private int level;
+
+    public TreeItemFigure(Object content, int level, boolean hasChildren) {
+        this.content = content;
+        this.level = level;
+        if (hasChildren)
+            this.toggleFigure = new ToggleFigure();
+        this.imageFigure = new ImageFigure();
+        this.labelFigure = new StyledLabel();
+        setLayoutManager(new TreeItemLayout());
+        if (toggleFigure != null)
+            add(toggleFigure);
+        add(imageFigure);
+        add(labelFigure);
+    }
+
+    public Object getContent() {
+        return content;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setImage(Image image) {
+        imageFigure.setImage(image);
+    }
+
+    public void setLabel(StyledString label) {
+        labelFigure.setStyledString(label);
+    }
+
+    public boolean isExpandable() {
+        return toggleFigure != null;
+    }
+
+    public boolean isExpanded() {
+        return toggleFigure != null && toggleFigure.isExpanded;
+    }
+
+    public void setExpanded(boolean isExpanded) {
+        if (toggleFigure != null)
+            toggleFigure.isExpanded = isExpanded;
+    }
+
+    public void setSelected(boolean isSelected) {
+        if (this.isSelected == isSelected)
+            return;
+
+        this.isSelected = isSelected;
+        if (isSelected) {
+            setOpaque(true);
+            setBackgroundColor(SELECTION_BACKGROUND_COLOR);
+        }
+        else {
+            setOpaque(false);
+            setBackgroundColor(null);
+        }
+    }
+
+    public boolean isSelected() {
+        return isSelected;
+    }
+
+    public Figure getToggle() {
+        return toggleFigure;
+    }
+}
