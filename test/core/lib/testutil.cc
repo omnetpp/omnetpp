3d346646dd80985c74837bd3240593506ea74cd8
Levente Mészáros <levy@omnetpp.org>
2013-04-04 11:01:24 +0200

    logging: Fixed compilation error.
    X-Committed-on-Branch: (no branch)

--- test/core/lib/testutil.cc
+++ test/core/lib/testutil.cc
@@ -5,5 +5,5 @@ void printEnum(const char *enumname)
     cEnum *e = cEnum::find(enumname);
     ASSERT(e!=NULL);
     ASSERT(strcmp(e->getFullName(), enumname)==0);
-    EV << e->getFullName() << ": " << e->info() << "\n";
+    EV_S << e->getFullName() << ": " << e->info() << "\n";
 }
