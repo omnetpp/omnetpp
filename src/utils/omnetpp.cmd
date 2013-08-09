d7e392a7532636c11f6f2330128254d17cad28cc
Rudolf Hornig <rudi@omnetpp.org>
2013-08-09 11:13:18 +0200

    command line: fix for #670 -vmargs conflict between omnetpp.ini and the launcher script.
    X-Committed-on-Branch: omnetpp-4.3.x

--- src/utils/omnetpp.cmd
+++ src/utils/omnetpp.cmd
@@ -2,6 +2,6 @@
 SET IDEDIR=%~dp0\..\ide
 SET LAUNCHER=omnetpp
 SET PATH=%~dp0;%~dp0\..\msys\bin;%~dp0\..\mingw\bin;%PATH%
-SET DEFAULT_WORKSPACE_ARGS="-vmargs -Dosgi.instance.area.default=%IDEDIR%\..\samples"
+SET DEFAULT_WORKSPACE_ARGS="-vmargs -Xms256m -Xmx1024m -XX:MaxPermSize=320m -Dosgi.instance.area.default=%IDEDIR%\..\samples"
 echo Starting the OMNeT++ IDE...
 start %IDEDIR%\%LAUNCHER% %* %DEFAULT_WORKSPACE_ARGS% >out.log
