2d921b9753fc994ee442f99ee8f537735231460f
Rudolf Hornig <rudi@omnetpp.org>
2014-04-25 17:25:14 +0200

    productizing: updated to version 4.5 - DO NOT merge this with master!
    X-Committed-on-Branch: omnetpp-4.x

--- include/simkerneldefs.h
+++ include/simkerneldefs.h
@@ -26,7 +26,7 @@
 #include "platdep/intxtypes.h"
 
 // OMNeT++ version -- must match NEDC_VERSION and MSGC_VERSION in nedxml
-#define OMNETPP_VERSION 0x0404
+#define OMNETPP_VERSION 0x0405
 
 #if defined(SIM_EXPORT)
 #  define SIM_API OPP_DLLEXPORT
