1b4cd829a7352f7333464541256b90205d7310b1
Rudolf Hornig <rudi@omnetpp.org>
2014-08-04 16:01:38 +0200

    build: visualc environment modified to work with msys2
    X-Committed-on-Branch: omnetpp-4.x

--- vcenv.cmd
+++ vcenv.cmd
@@ -1,13 +1,13 @@
 @echo off
-rem Specify your MS Visual C++ 8/9 installation here.
-rem  e.g. C:\Program Files\Microsoft Visual Studio 9.0\VC
+rem Specify your MS Visual C++ installation here.
+rem  e.g. C:\Program Files\Microsoft Visual Studio 12.0\VC
 set VCINSTALLDIR=
 
 if NOT EXIST "%VCINSTALLDIR%\bin\vcvars32.bat" goto MESSAGE
 
 set OMNETPP_ROOT=%~dp0
 set OMNETPP_IMAGE_PATH=.\images;%OMNETPP_ROOT%\images
-set PATH=%OMNETPP_ROOT%\bin;%OMNETPP_ROOT%\msys\bin;%PATH%
+set PATH=%OMNETPP_ROOT%\bin;%OMNETPP_ROOT%\msys\usr\bin;%OMNETPP_ROOT%\msys\mingw32\bin;%PATH%
 
 call "%VCINSTALLDIR%"\bin\vcvars32.bat
 
