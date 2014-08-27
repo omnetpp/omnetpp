b3aca84abc6e87378188ec5c42471131741376f7
Rudolf Hornig <rudi@omnetpp.org>
2014-08-27 16:52:09 +0200

    productizing: updated product.ini to check for JVM 1.7
    X-Committed-on-Branch: omnetpp-4.x

--- src/utils/omnest.cmd
+++ src/utils/omnest.cmd
@@ -1,7 +1,7 @@
 @echo off
 SET IDEDIR=%~dp0\..\ide
 SET LAUNCHER=omnest
-SET PATH=%~dp0;%~dp0\..\msys\bin;%~dp0\..\mingw\bin;%PATH%
-SET DEFAULT_WORKSPACE_ARGS="-vmargs -Xms256m -Xmx1024m -XX:MaxPermSize=320m -Dosgi.instance.area.default=%IDEDIR%\..\samples"
+SET PATH=%~dp0;%~dp0\..\msys\usr\bin;%~dp0\..\msys\mingw32\bin;%PATH%
+SET DEFAULT_WORKSPACE_ARGS="-vmargs -Dosgi.instance.area.default=%IDEDIR%\..\samples"
 echo Starting the OMNEST IDE...
 start %IDEDIR%\%LAUNCHER% %* %DEFAULT_WORKSPACE_ARGS% >out.log
