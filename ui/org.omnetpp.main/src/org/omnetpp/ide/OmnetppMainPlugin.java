bd840e4c23e85b6b5b7c716f7919dbf9301bb621
Rudolf Hornig <rudi@omnetpp.org>
2014-10-03 13:08:34 +0200

    productizing: new msys location (tools/win32), updated bash.bashrc startscript
    X-Committed-on-Branch: omnetpp-4.x

--- ui/org.omnetpp.main/src/org/omnetpp/ide/OmnetppMainPlugin.java
+++ ui/org.omnetpp.main/src/org/omnetpp/ide/OmnetppMainPlugin.java
@@ -163,7 +163,7 @@ public class OmnetppMainPlugin extends AbstractUIPlugin {
         String oppRoot = getOmnetppRootDir();
         if (StringUtils.isBlank(oppRoot))
             return "";
-        return new Path(oppRoot).append("msys").append("usr").append("bin").toOSString();
+        return new Path(oppRoot).append("tools").append("win32").append("usr").append("bin").toOSString();
     }
 
     /**
@@ -175,7 +175,7 @@ public class OmnetppMainPlugin extends AbstractUIPlugin {
         String oppRoot = getOmnetppRootDir();
         if (StringUtils.isBlank(oppRoot))
             return "";
-        return new Path(oppRoot).append("msys").append("mingw32").append("bin").toOSString();
+        return new Path(oppRoot).append("tools").append("win32").append("mingw32").append("bin").toOSString();
     }
 
     /**
