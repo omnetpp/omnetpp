3d74a28ac1a9d5ab2e93407b26fc398fb2ccafb6
Andras Varga <andras@omnetpp.org>
2016-02-23 11:52:05 +0100

    usman: appendix-config updated
    X-Committed-on-Branch: topic/stats-config

--- doc/src/manual/tools/run.cmd
+++ doc/src/manual/tools/run.cmd
@@ -1,5 +1,5 @@
 call ..\..\..\..\setenv-vc110.bat
-opp_run -h configdetails > config-in.txt
+opp_run -h latexconfig > config-in.txt
 perl stripgrammar.pl ../../../../src/nedxml/ned2.y >ned.txt
 perl stripgrammar.pl ../../../../src/nedxml/msg2.y >msg.txt
 perl stripeventlog.pl ../../../../src/eventlog/eventlogentries.txt >eventlog.txt
