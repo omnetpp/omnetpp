683090587f6752a2d6d9715512ae879ea4dfe488
Rudolf Hornig <rudi@omnetpp.org>
2016-03-25 15:08:10 +0100

    doc: fixed missing manual in the Eclipse Help system
    X-Committed-on-Branch: master

--- doc/src/manual/tools/processnedfuncs.pl
+++ doc/src/manual/tools/processnedfuncs.pl
@@ -6,10 +6,10 @@ $txt = "";
 while (<>) { chomp; s/\r$//; $txt .= $_ . "\n"; }
 
 $txt =~ s/([_\$])/\\$1/sg;
-$txt =~ s/^ (Category.*)/\\end{description}\n\n\\subsubsection{$1}\n\n\\begin{description}/mg;
+$txt =~ s/^ (Category.*)/\\end{description}\n\n\\section{$1}\n\n\\begin{description}/mg;
 $txt =~ s/^  ([^ :]+) *: *(.*)/\\item[$1]: \\ttt{$2} \\\\/mg;
 
-$txt =~ s/^.*?(\\subsubsection)/$1/s;
+$txt =~ s/^.*?(\\section)/$1/s;
 $txt =~ s/End\.\s*//s;
 $txt =~ s/\s*$/\n\\end{description}\n/s;
 
