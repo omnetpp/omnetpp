597e0a204e6154aa995201dc3e28bdb3142063d4
Andras Varga <andras@omnetpp.org>
2016-04-11 17:14:16 +0200

    samples: osg-satellites: methods reordered, local vars renamed

--- samples/osg-satellites/ChannelController.cc
+++ samples/osg-satellites/ChannelController.cc
@@ -45,6 +45,103 @@ ChannelController *ChannelController::getInstance()
     return instance;
 }
 
+void ChannelController::initialize(int stage)
+{
+    switch (stage) {
+    case 0:
+        satToSatColor = par("satToSatColor").stringValue();
+        satToSatWidth = par("satToSatWidth").doubleValue();
+        satToGroundColor = par("satToGroundColor").stringValue();
+        satToGroundWidth = par("satToGroundWidth").doubleValue();
+        break;
+    case 1:
+        scene = OsgEarthScene::getInstance()->getScene()->asGroup();
+        connections = new osg::Geode();
+        scene->addChild(connections);
+        break;
+    case 2:
+        // note: satellites and ground stations must have been added by now
+        for (int i = 0; i < (int)satellites.size(); ++i) {
+            for (int j = 0; j < (int)stations.size(); ++j)
+                addLineOfSight(satellites[i]->getLocatorNode(), stations[j]->getLocatorNode(), 0);
+            for (int j = i+1; j < (int)satellites.size(); ++j)
+                addLineOfSight(satellites[i]->getLocatorNode(), satellites[j]->getLocatorNode(), 1);
+        }
+        break;
+    }
+}
+
+
+void ChannelController::addSatellite(Satellite *p)
+{
+    ASSERT(!contains(satellites, p));
+    ASSERT(losNodes.empty()); // we are before init stage 2
+    satellites.push_back(p);
+}
+
+void ChannelController::addGroundStation(GroundStation *p)
+{
+    ASSERT(!contains(stations, p));
+    ASSERT(losNodes.empty()); // we are before init stage 2
+    stations.push_back(p);
+}
+
+void ChannelController::addLineOfSight(osg::Node *a, osg::Node *b, int type)
+{
+    auto mapNode = osgEarth::MapNode::findMapNode(scene);
+    osgEarth::Util::LinearLineOfSightNode *los = new osgEarth::Util::LinearLineOfSightNode(mapNode);
+    losNodes.push_back(los);
+
+    // not drawing the line of sight nodes' lines
+    los->setGoodColor(osg::Vec4f(0, 0, 0, 0));
+    los->setBadColor(osg::Vec4f(0, 0, 0, 0));
+
+    auto stateSet = los->getOrCreateStateSet();
+    stateSet->setRenderingHint(osg::StateSet::TRANSPARENT_BIN);
+    auto depth = new osg::Depth;
+    depth->setWriteMask(false);
+    stateSet->setAttributeAndModes(depth, osg::StateAttribute::ON);
+
+    los->setUserValue("type", type);
+
+    los->setUpdateCallback(new osgEarth::Util::LineOfSightTether(a, b));
+    los->setTerrainOnly(true); // so the dish model itself won't occlude
+
+    scene->addChild(los);
+}
+
+void ChannelController::refreshDisplay() const
+{
+    connections->removeDrawables(0, connections->getNumDrawables());
+
+    int numSatToSat = 0;
+    int numSatToGround = 0;
+
+    for (auto losNode : losNodes) {
+        if (losNode->getHasLOS()) {
+            auto start = losNode->getStartWorld();
+            auto end = losNode->getEndWorld();
+
+            int type;
+            losNode->getUserValue("type", type);
+            switch (type) {
+            case 0:
+                ++numSatToGround;
+                if (!satToGroundColor.empty())
+                    connections->addDrawable(createLineBetweenPoints(start, end, satToGroundWidth, osgEarth::Color(satToGroundColor)));
+                break;
+            case 1:
+                ++numSatToSat;
+                if (!satToSatColor.empty())
+                    connections->addDrawable(createLineBetweenPoints(start, end, satToSatWidth, osgEarth::Color(satToSatColor)));
+                break;
+            }
+        }
+    }
+
+    EV << "Active connections: " << numSatToSat << " sat-to-sat and " << numSatToGround << " sat-to-ground \n";
+}
+
 osg::ref_ptr<osg::Drawable> ChannelController::createLineBetweenPoints(osg::Vec3 start, osg::Vec3 end, float width, osg::Vec4 color)
 {
     osg::ref_ptr<osg::Geometry> orbitGeom = new osg::Geometry;
@@ -72,99 +169,6 @@ osg::ref_ptr<osg::Drawable> ChannelController::createLineBetweenPoints(osg::Vec3
     return orbitGeom;
 }
 
-void ChannelController::addLineOfSight(osg::Node *a, osg::Node *b, int type)
-{
-    auto mapNode = osgEarth::MapNode::findMapNode(scene);
-    osgEarth::Util::LinearLineOfSightNode *los = new osgEarth::Util::LinearLineOfSightNode(mapNode);
-    losNodes.push_back(los);
-
-    // not drawing the line of sight nodes' lines
-    los->setGoodColor(osg::Vec4f(0, 0, 0, 0));
-    los->setBadColor(osg::Vec4f(0, 0, 0, 0));
-
-    auto stateSet = los->getOrCreateStateSet();
-    stateSet->setRenderingHint(osg::StateSet::TRANSPARENT_BIN);
-    auto depth = new osg::Depth;
-    depth->setWriteMask(false);
-    stateSet->setAttributeAndModes(depth, osg::StateAttribute::ON);
-
-    los->setUserValue("type", type);
-
-    los->setUpdateCallback(new osgEarth::Util::LineOfSightTether(a, b));
-    los->setTerrainOnly(true); // so the dish model itself won't occlude
-
-    scene->addChild(los);
-}
-
-void ChannelController::addSatellite(Satellite *p)
-{
-    ASSERT(!contains(satellites, p));
-    satellites.push_back(p);
-}
-
-void ChannelController::addGroundStation(GroundStation *p)
-{
-    ASSERT(!contains(stations, p));
-    stations.push_back(p);
-}
-
-void ChannelController::initialize(int stage)
-{
-    switch (stage) {
-    case 0:
-        satToSatColor = par("satToSatColor").stringValue();
-        satToSatWidth = par("satToSatWidth").doubleValue();
-        satToGroundColor = par("satToGroundColor").stringValue();
-        satToGroundWidth = par("satToGroundWidth").doubleValue();
-        connections = new osg::Geode();
-        break;
-    case 1:
-        scene = OsgEarthScene::getInstance()->getScene()->asGroup();
-        scene->addChild(connections);
-        break;
-    case 2:
-        for (int i = 0; i < (int)satellites.size(); ++i) {
-            for (int j = 0; j < (int)stations.size(); ++j)
-                addLineOfSight(satellites[i]->getLocatorNode(), stations[j]->getLocatorNode(), 0);
-
-            for (int j = i+1; j < (int)satellites.size(); ++j)
-                addLineOfSight(satellites[i]->getLocatorNode(), satellites[j]->getLocatorNode(), 1);
-        }
-        break;
-    }
-}
-
-void ChannelController::refreshDisplay() const
-{
-    connections->removeDrawables(0, connections->getNumDrawables());
-
-    int ns2s = 0;
-    int ns2g = 0;
-
-    for (auto n : losNodes) {
-        if (n->getHasLOS()) {
-            auto start = n->getStartWorld();
-            auto end = n->getEndWorld();
-
-            int type;
-            n->getUserValue("type", type);
-            switch (type) {
-            case 0:
-                ++ns2g;
-                if (!satToGroundColor.empty())
-                    connections->addDrawable(createLineBetweenPoints(start, end, satToGroundWidth, osgEarth::Color(satToGroundColor)));
-                break;
-            case 1:
-                ++ ns2s;
-                if (!satToSatColor.empty())
-                    connections->addDrawable(createLineBetweenPoints(start, end, satToSatWidth, osgEarth::Color(satToSatColor)));
-                break;
-            }
-        }
-    }
-
-    EV << "Active connections: " << ns2s << " sat-to-sat and " << ns2g << " sat-to-ground \n";
-}
 
 void ChannelController::handleMessage(cMessage *msg)
 {
