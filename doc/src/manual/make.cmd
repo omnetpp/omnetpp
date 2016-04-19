83d0d887712530d8412fa457048b45b97ee0b1f4
Andras Varga <andras@omnetpp.org>
2016-04-19 14:36:27 +0200

    usman: makefiles synced; mixtex upgrade

--- doc/src/manual/make.cmd
+++ doc/src/manual/make.cmd
@@ -1,3 +1,3 @@
 call ..\..\..\setenv-vc.bat
-set PATH=%PATH%;C:\Latex\MiKTeX-2.8\miktex\bin
+set PATH=%PATH%;C:\Latex\MiKTeX 2.9\miktex\bin
 nmake -f makefile.vc %* &&  : start ..\..\manual\usman.html
