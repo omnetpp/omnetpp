07b65607cec02cb8a2b7fa3a87ec4c7e71ce0f16
Zoltan Bojthe <zbojthe@omnetpp.org>
2016-11-25 11:11:21 +0100

    incremented OMNETPP_BUILDNUM : for detecting Register_Figure/Register_Class incompatibility

--- include/omnetpp/simkerneldefs.h
+++ include/omnetpp/simkerneldefs.h
@@ -26,7 +26,7 @@
 #define OMNETPP_VERSION  0x0501
 
 // OMNeT++ build number
-#define OMNETPP_BUILDNUM   1007
+#define OMNETPP_BUILDNUM   1008
 
 // uncomment to switch fingerprint computation algorithm to OMNeT++ 4.x compatible mode
 //#define USE_OMNETPP4x_FINGERPRINTS
