407907fcb0fd69a322da12095c941a4c428919b7
Andras Varga <andras@omnetpp.org>
2016-11-29 17:38:41 +0100

    test: added vector file writing performance tester

---
+++ test/vectorrecordingperf/vectorrecorder.cc
@@ -0,0 +1,31 @@
+#include "vectorrecorder.h"
+
+Define_Module(VectorRecorder);
+
+
+void VectorRecorder::initialize()
+{
+    cEnvir *ev = getEnvir();
+    std::vector<void*> vectorHandles;
+
+    int numVectors = par("numVectors");
+    for (int i = 0; i < numVectors; i++) {
+        char vectorName[32];
+        sprintf(vectorName, "dummy-vector-%d", i);
+        vectorHandles.push_back(ev->registerOutputVector("dummy-module", vectorName));
+    }
+
+    int numValues = par("numValues");;
+    simtime_t t = 0;
+    simtime_t dt = 0.001;
+    double vectorIndexMean = numVectors / 5.0;
+    double geomP = 1 / vectorIndexMean;
+    for (int i = 0; i < numValues; i++) {
+        int vectorIndex = geometric(geomP);
+        if (vectorIndex >= numVectors)
+            vectorIndex = intuniform(0, numVectors-1);
+        double value = dblrand();
+        ev->recordInOutputVector(vectorHandles[vectorIndex], t, value);
+        t += dt;
+    }
+}
---
+++ test/vectorrecordingperf/vectorrecorder.h
@@ -0,0 +1,14 @@
+#ifndef __VECTORRECORDER_H_
+#define __VECTORRECORDER_H_
+
+#include <omnetpp.h>
+
+using namespace omnetpp;
+
+class VectorRecorder : public cSimpleModule
+{
+  protected:
+    virtual void initialize();
+};
+
+#endif
