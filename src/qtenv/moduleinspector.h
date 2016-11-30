116741f5f8bfc63f784b928bb1e0692ca744c968
TÖRÖK Attila <torokati44@gmail.com>
2016-11-30 11:13:00 +0100

    qtenv: moduleinspector.h formatted from 3 and 6 spaces to 0 and 4.

--- src/qtenv/moduleinspector.h
+++ src/qtenv/moduleinspector.h
@@ -45,134 +45,134 @@ class GraphicsLayer;
 
 class QTENV_API ModuleInspector : public Inspector
 {
-   Q_OBJECT
+    Q_OBJECT
 
-   private slots:
-      void runUntil();
-      void fastRunUntil();
-      void relayout();
-      void zoomIn(int x = 0, int y = 0);
-      void zoomOut(int x = 0, int y = 0);
-      void increaseIconSize() { zoomIconsBy(1.25); }
-      void decreaseIconSize() { zoomIconsBy(0.8); }
-      void runPreferencesDialog();
+private slots:
+    void runUntil();
+    void fastRunUntil();
+    void relayout();
+    void zoomIn(int x = 0, int y = 0);
+    void zoomOut(int x = 0, int y = 0);
+    void increaseIconSize() { zoomIconsBy(1.25); }
+    void decreaseIconSize() { zoomIconsBy(0.8); }
+    void runPreferencesDialog();
 
-      void click(QMouseEvent *event);
-      void doubleClick(QMouseEvent *event);
-      void onViewerDragged(QPointF center);
+    void click(QMouseEvent *event);
+    void doubleClick(QMouseEvent *event);
+    void onViewerDragged(QPointF center);
 
-      void onObjectsPicked(const std::vector<cObject*>&);
-      void onMarqueeZoom(QRectF rect);
+    void onObjectsPicked(const std::vector<cObject*>&);
+    void onMarqueeZoom(QRectF rect);
 
-      void createContextMenu(const std::vector<cObject*> &objects, const QPoint &globalPos);
+    void createContextMenu(const std::vector<cObject*> &objects, const QPoint &globalPos);
 
-      void layers();
-      void showLabels(bool show);
-      void showArrowheads(bool show);
-      void zoomIconsBy(double mult);
-      void resetOsgView();
+    void layers();
+    void showLabels(bool show);
+    void showArrowheads(bool show);
+    void zoomIconsBy(double mult);
+    void resetOsgView();
 
-      void switchToOsgView();
-      void switchToCanvasView();
+    void switchToOsgView();
+    void switchToCanvasView();
 
-      void onFontChanged();
-      void updateToolbarLayout(); // mostly the margins, to prevent occluding the scrollbar
+    void onFontChanged();
+    void updateToolbarLayout(); // mostly the margins, to prevent occluding the scrollbar
 
-      void exportToPdf();
-      void print();
+    void exportToPdf();
+    void print();
 
-   protected:
-      static const QString PREF_MODE; // 0 is 2D, 1 is OSG
-      static const QString PREF_CENTER; // of the viewport in the 2D scene
-      static const QString PREF_ZOOMFACTOR;
-      static const QString PREF_ZOOMBYFACTOR;
-      static const QString PREF_ICONSCALE;
-      static const QString PREF_SHOWLABELS;
-      static const QString PREF_SHOWARROWHEADS;
+protected:
+    static const QString PREF_MODE; // 0 is 2D, 1 is OSG
+    static const QString PREF_CENTER; // of the viewport in the 2D scene
+    static const QString PREF_ZOOMFACTOR;
+    static const QString PREF_ZOOMBYFACTOR;
+    static const QString PREF_ICONSCALE;
+    static const QString PREF_SHOWLABELS;
+    static const QString PREF_SHOWARROWHEADS;
 
-      const int toolbarSpacing = 4; // from the edges, in pixels, the scrollbar size will be added to this
+    const int toolbarSpacing = 4; // from the edges, in pixels, the scrollbar size will be added to this
 
-      QAction *switchToOsgViewAction;
-      QAction *switchToCanvasViewAction;
+    QAction *switchToOsgViewAction;
+    QAction *switchToCanvasViewAction;
 
-      QAction *canvasRelayoutAction;
-      QAction *canvasZoomInAction;
-      QAction *canvasZoomOutAction;
-      QAction *resetOsgViewAction;
-      QAction *showModuleNamesAction;
-      QAction *showArrowheadsAction;
-      QAction *increaseIconSizeAction;
-      QAction *decreaseIconSizeAction;
+    QAction *canvasRelayoutAction;
+    QAction *canvasZoomInAction;
+    QAction *canvasZoomOutAction;
+    QAction *resetOsgViewAction;
+    QAction *showModuleNamesAction;
+    QAction *showArrowheadsAction;
+    QAction *increaseIconSizeAction;
+    QAction *decreaseIconSizeAction;
 
-      QStackedLayout *stackedLayout;
-      QGridLayout *toolbarLayout = nullptr; // not used in topLevel mode
-      QToolBar *toolbar;
+    QStackedLayout *stackedLayout;
+    QGridLayout *toolbarLayout = nullptr; // not used in topLevel mode
+    QToolBar *toolbar;
 
-      ModuleCanvasViewer *canvasViewer;
-
-#ifdef WITH_OSG
-      OsgViewer *osgViewer;
-#endif
-
-   public slots:
-      void onLayoutVisualizationStarts(cModule *module, QGraphicsScene *layoutingScene);
-      void onLayoutVisualizationEnds(cModule *module);
-      void onModuleLayoutChanged(cModule *module);
-
-   protected:
-      cCanvas *getCanvas();
-
-      void createViews(bool isTopLevel);
-      QToolBar *createToolbar(bool isTopLevel);
-      void refreshOsgViewer();
+    ModuleCanvasViewer *canvasViewer;
 
 #ifdef WITH_OSG
-      cOsgCanvas *getOsgCanvas();
-      void setOsgCanvas(cOsgCanvas *osgCanvas);
+    OsgViewer *osgViewer;
 #endif
 
-      QSize sizeHint() const override { return QSize(600, 500); }
-      void wheelEvent(QWheelEvent *event) override;
-      void resizeEvent(QResizeEvent *event) override;
-      void zoomBy(double mult, bool snaptoone = false, int x = 0, int y = 0);
-      void firstObjectSet(cObject *obj) override;
+public slots:
+    void onLayoutVisualizationStarts(cModule *module, QGraphicsScene *layoutingScene);
+    void onLayoutVisualizationEnds(cModule *module);
+    void onModuleLayoutChanged(cModule *module);
 
-      void renderToPrinter(QPrinter &printer);
+protected:
+    cCanvas *getCanvas();
 
-      std::map<cComponent *, std::string> bubblesToShow;
+    void createViews(bool isTopLevel);
+    QToolBar *createToolbar(bool isTopLevel);
+    void refreshOsgViewer();
 
-      // drawing methods:
-      virtual void redraw();
+#ifdef WITH_OSG
+    cOsgCanvas *getOsgCanvas();
+    void setOsgCanvas(cOsgCanvas *osgCanvas);
+#endif
 
-   public:
-      ModuleInspector(QWidget *parent, bool isTopLevel, InspectorFactory *f);
-      ~ModuleInspector();
-      virtual void doSetObject(cObject *obj) override;
-      virtual void refresh() override;
-      virtual void clearObjectChangeFlags() override;
+    QSize sizeHint() const override { return QSize(600, 500); }
+    void wheelEvent(QWheelEvent *event) override;
+    void resizeEvent(QResizeEvent *event) override;
+    void zoomBy(double mult, bool snaptoone = false, int x = 0, int y = 0);
+    void firstObjectSet(cObject *obj) override;
 
+    void renderToPrinter(QPrinter &printer);
 
-      // implementations of inspector commands:
-      virtual int getDefaultLayoutSeed();
+    std::map<cComponent *, std::string> bubblesToShow;
 
-      QImage getScreenshot();
+    // drawing methods:
+    virtual void redraw();
 
-      // notifications from envir:
-      virtual void submoduleCreated(cModule *newmodule);
-      virtual void submoduleDeleted(cModule *module);
-      virtual void connectionCreated(cGate *srcgate);
-      virtual void connectionDeleted(cGate *srcgate);
-      virtual void displayStringChanged();
-      virtual void displayStringChanged(cModule *submodule);
-      virtual void displayStringChanged(cGate *gate);
-      virtual void bubble(cComponent *subcomponent, const char *text) { bubblesToShow[subcomponent] = text; }
+public:
+    ModuleInspector(QWidget *parent, bool isTopLevel, InspectorFactory *f);
+    ~ModuleInspector();
 
-      double getZoomFactor();
-      double getImageSizeFactor();
+    virtual void doSetObject(cObject *obj) override;
+    virtual void refresh() override;
+    virtual void clearObjectChangeFlags() override;
 
-      GraphicsLayer *getAnimationLayer();
-      QPointF getSubmodCoords(cModule *mod);
-      QLineF getConnectionLine(cGate *gate);
+    // implementations of inspector commands:
+    virtual int getDefaultLayoutSeed();
+
+    QImage getScreenshot();
+
+    // notifications from envir:
+    virtual void submoduleCreated(cModule *newmodule);
+    virtual void submoduleDeleted(cModule *module);
+    virtual void connectionCreated(cGate *srcgate);
+    virtual void connectionDeleted(cGate *srcgate);
+    virtual void displayStringChanged();
+    virtual void displayStringChanged(cModule *submodule);
+    virtual void displayStringChanged(cGate *gate);
+    virtual void bubble(cComponent *subcomponent, const char *text) { bubblesToShow[subcomponent] = text; }
+
+    double getZoomFactor();
+    double getImageSizeFactor();
+
+    GraphicsLayer *getAnimationLayer();
+    QPointF getSubmodCoords(cModule *mod);
+    QLineF getConnectionLine(cGate *gate);
 };
 
 } // namespace qtenv
