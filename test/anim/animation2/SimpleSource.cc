eac536da904a24714a56b186e4cc9aa8c1b7b883
TÖRÖK Attila <torokati44@gmail.com>
2017-01-20 18:17:01 +0100

    test: Added a sweet second animation test.

---
+++ test/anim/animation2/SimpleSource.cc
@@ -0,0 +1,38 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#include "SimpleSource.h"
+
+namespace animation2 {
+
+Define_Module(SimpleSource);
+
+void SimpleSource::initialize()
+{
+    scheduleAt(simTime() + 1, new cMessage("tick"));
+}
+
+void SimpleSource::handleMessage(cMessage *msg)
+{
+    delete msg;
+
+    cPacket *jobA = new cPacket("cake");
+    jobA->setByteLength(64000);
+    send(jobA, "out");
+
+    scheduleAt(simTime() + 1, new cMessage("tick"));
+}
+
+} //namespace
