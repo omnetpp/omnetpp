0b099eba81119198b2c3c4244c88b02e169decb4
Andras Varga <andras@omnetpp.org>
2017-11-13 09:20:42 +0100

    msgc: fix missing default initialization of fields

--- src/nedxml/msganalyzer.cc
+++ src/nedxml/msganalyzer.cc
@@ -388,10 +388,15 @@ void MsgAnalyzer::analyzeField(ClassInfo& classInfo, FieldInfo *field, const std
         return;
     }
 
-    if (field->fisarray && field->farraysize.empty() && !field->fval.empty())
-        errors->addError(field->nedElement, "unaccepted default value for variable length vector field '%s %s' in class '%s'", field->ftype.c_str(), field->fname.c_str(), classInfo.msgname.c_str());
-    if (field->fispointer) {
-        if (field->fval.empty())
+    if (field->fisarray) {
+        //TODO note: currently the grammar does not accept arrays with initializers!
+        if (field->farraysize.empty() && !field->fval.empty())  //TODO but what about fval for fixed-size arrays??? CHECK!!!
+            errors->addError(field->nedElement, "unaccepted default value for variable length vector field '%s %s' in class '%s'", field->ftype.c_str(), field->fname.c_str(), classInfo.msgname.c_str());
+    }
+    else {
+        if (field->fval.empty() && !fieldClassInfo.defaultvalue.empty())
+            field->fval = fieldClassInfo.defaultvalue;
+        if (field->fispointer && field->fval.empty())
             field->fval = "nullptr";
     }
 
