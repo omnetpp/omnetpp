b985da7db57c7cd9ecf9116194508b8f20605b7b
Andras Varga <andras@omnetpp.org>
2017-12-05 07:52:19 +0100

    msgc: updated list of reserved words

--- src/nedxml/msganalyzer.cc
+++ src/nedxml/msganalyzer.cc
@@ -90,12 +90,29 @@ P check_and_cast_nullable(T *p)
 }
 
 MsgAnalyzer::StringSet MsgAnalyzer::RESERVED_WORDS = {
-        "namespace", "cplusplus", "struct", "message", "packet", "class", "noncobject",
-        "enum", "extends", "abstract", "readonly", "properties", "fields", "unsigned",
-        "true", "false", "for", "while", "if", "else", "do", "enum", "class", "struct",
-        "typedef", "public", "private", "protected", "auto", "register", "sizeof", "void",
-        "new", "delete", "explicit", "static", "extern", "return", "try", "catch",
+        // C++ keywords
+        "alignas", "alignof", "and", "and_eq", "asm", "atomic_cancel",
+        "atomic_commit", "atomic_noexcept", "auto", "bitand", "bitor", "bool",
+        "break", "case", "catch", "char", "char16_t", "char32_t", "class",
+        "compl", "concept", "const", "constexpr", "const_cast", "continue",
+        "co_await", "co_return", "co_yield", "decltype", "default", "delete",
+        "do", "double", "dynamic_cast", "else", "enum", "explicit", "export",
+        "extern", "false", "float", "for", "friend", "goto", "if", "import",
+        "inline", "int", "long", "module", "mutable", "namespace", "new",
+        "noexcept", "not", "not_eq", "nullptr", "operator", "or", "or_eq",
+        "private", "protected", "public", "register", "reinterpret_cast",
+        "requires", "return", "short", "signed", "sizeof", "static",
+        "static_assert", "static_cast", "struct", "switch", "synchronized",
+        "template", "this", "thread_local", "throw", "true", "try", "typedef",
+        "typeid", "typename", "union", "unsigned", "using", "virtual", "void",
+        "volatile", "wchar_t", "while", "xor", "xor_eq",
+
+        // MSG keywords that are not also C++ keywords
+        "cplusplus", "message", "packet", "noncobject", "extends", "abstract"
 };
+
+
+
 
 MsgAnalyzer::MsgAnalyzer(const MsgCompilerOptions& opts, MsgTypeTable *typeTable, ErrorStore *errors) : errors(errors), typeTable(typeTable), opts(opts)
 {
