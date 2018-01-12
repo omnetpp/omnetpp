9de4373eebf1813316ed281b2c7daf90cc7fe02d
Andras Varga <andras@omnetpp.org>
2018-01-12 12:31:36 +0100

    bump OMNETPP_BUILDNUM

--- include/omnetpp/simkerneldefs.h
+++ include/omnetpp/simkerneldefs.h
@@ -26,7 +26,7 @@
 #define OMNETPP_VERSION  0x0503
 
 // OMNeT++ build number
-#define OMNETPP_BUILDNUM   1013
+#define OMNETPP_BUILDNUM   1014
 
 // uncomment to switch fingerprint computation algorithm to OMNeT++ 4.x compatible mode
 //#define USE_OMNETPP4x_FINGERPRINTS
