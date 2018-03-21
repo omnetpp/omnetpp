1e570f429ae7b2fd472290e8136a389705a5b002
Andras Varga <andras@omnetpp.org>
2018-03-21 14:30:19 +0100

    unitconversion: no longer accept 0 without measurement unit
    reason: confusing when the unit is dB or dBm (e.g. **.power=0 --> 0W or 0dBm??)

--- src/common/unitconversion.cc
+++ src/common/unitconversion.cc
@@ -339,10 +339,6 @@ double UnitConversion::convertUnit(double value, const char *unit, const char *t
     if (std::isnan(value) && opp_isempty(unit))
         return value;
 
-    // if value is zero, unit may be omitted (if target unit is linear, e.g. not for dB) --TODO remove this in 6.0
-    if (value == 0 && opp_isempty(unit) && !opp_isempty(targetUnit) && isLinearUnit(targetUnit))
-        return 0;
-
     // if only one unit is given, that's an error
     if (opp_isempty(unit) || opp_isempty(targetUnit))
         cannotConvert(unit, targetUnit);
