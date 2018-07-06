fba679a8c6341d1027fbb829f9008fc4e19f5103
Rudolf Hornig <rudi@omnetpp.org>
2018-07-06 18:12:08 +0200

    ide: fixed the trashy cursors during connection drag and new module creation with GTK3
    The IDE now runs with GTK3. Both the cursor issue and the missing 3state
    checkbox issue in the launcher dialog has been fixed.
    
    X-Committed-on-Branch: topic/omnetpp6

---
+++ ui/org.omnetpp.ned.editor/src/org/omnetpp/ned/editor/graph/misc/NedSharedCursors.java
@@ -0,0 +1,50 @@
+package org.omnetpp.ned.editor.graph.misc;
+
+import org.eclipse.swt.graphics.Cursor;
+import org.omnetpp.ned.editor.NedEditorPlugin;
+import org.eclipse.jface.resource.ImageDescriptor;
+
+import org.eclipse.draw2d.Cursors;
+
+/**
+ * A shared collection of Cursors.
+ *
+ * @since 2.0
+ */
+public class NedSharedCursors extends Cursors {
+
+    /**
+     * Cursor for valid connection
+     */
+    public static final Cursor CURSOR_PLUG;
+    /**
+     * Cursor for invalid connection
+     */
+    public static final Cursor CURSOR_PLUG_NOT;
+    /**
+     * Cursor for adding to a tree
+     */
+    public static final Cursor CURSOR_TREE_ADD;
+    /**
+     * Cursor for dragging in a tree
+     */
+    public static final Cursor CURSOR_TREE_MOVE;
+
+    static {
+        CURSOR_PLUG = createCursor("cursors/plug.bmp", //$NON-NLS-1$
+                "cursors/plugmask.gif"); //$NON-NLS-1$
+        CURSOR_PLUG_NOT = createCursor("cursors/plugnot.bmp", //$NON-NLS-1$
+                "cursors/plugmasknot.gif"); //$NON-NLS-1$
+        CURSOR_TREE_ADD = createCursor("cursors/Tree_Add.gif", //$NON-NLS-1$
+                "cursors/Tree_Add_Mask.gif"); //$NON-NLS-1$
+        CURSOR_TREE_MOVE = createCursor("cursors/Tree_Move.gif", //$NON-NLS-1$
+                "cursors/Tree_Move_Mask.gif"); //$NON-NLS-1$
+    }
+
+    private static Cursor createCursor(String sourceName, String maskName) {
+        ImageDescriptor src = NedEditorPlugin.getImageDescriptor(sourceName);
+        ImageDescriptor mask = NedEditorPlugin.getImageDescriptor(maskName);
+        return new Cursor(null, src.getImageData(100), mask.getImageData(100), 0, 0);
+    }
+
+}
