9ca5a8f6961bf122fa7766c11a9f3537913ef219
TÖRÖK Attila <torokati44@gmail.com>
2018-08-14 14:49:02 +0200

    scave: separate results.pivotCharts into two halves (the first is not used elsewhere yet)
    plus some whitespace formatting changes

--- ui/org.omnetpp.scave.pychart/src/org/omnetpp/scave/pychart/python/scave.py
+++ ui/org.omnetpp.scave.pychart/src/org/omnetpp/scave/pychart/python/scave.py
@@ -4,10 +4,13 @@ import numpy as np
 import Gateway
 import pickle as pickle
 
+from TimeAndGuard import TimeAndGuard
+
 import functools
 print = functools.partial(print, flush=True)
 
 import time
+
 
 def getScalars(filter):
     time0 = time.perf_counter()
@@ -21,33 +24,43 @@ def getScalars(filter):
     df = pd.DataFrame(unpickled, columns=["run", "type", "module", "name", "attrname", "attrvalue", "value"])
 
     time3 = time.perf_counter()
-    #print("times: getting pickle: " + str((time1 - time0) * 1000.0) + " ms, unpickling: " + str((time2 - time1) * 1000.0) + " df constr:" + str((time3 - time2) * 1000.0) + " ms")
+    # print("times: getting pickle: " + str((time1 - time0) * 1000.0) + " ms, unpickling: " + str((time2 - time1) * 1000.0) + " df constr:" + str((time3 - time2) * 1000.0) + " ms")
 
-    #pickle.dump(df, open("/home/attila/scalars_df.pkl", "wb"))
+    # pickle.dump(df, open("/home/attila/scalars_df.pkl", "wb"))
 
     return df
 
 
-def pivotScalars(df, columns=["module"], index=["name"], values = None):
+def pivotScalars1(df):
     attrs = df[df.type.isin(["runattr", "itervar"])][["run", "attrname", "attrvalue"]]
-    
+
     scalars = df[df.type == "scalar"][["run", "module", "name", "value"]]
 
     runattrs_pivot = attrs.pivot(index="run", columns="attrname", values="attrvalue")
 
     scalars_merged = scalars.merge(runattrs_pivot, left_on="run", right_index=True)
     scalars_merged.set_index(["run", "module", "name"])
-    
+
     df = scalars_merged[scalars_merged.module != "_runattrs_"].set_index(["run", "module", "name"])
-    print(df, flush=True)
-    df = df.pivot_table(columns = columns, index = index, values = values)
-    
-    print(df, flush=True)
-    
-    df.columns = df.columns.droplevel() # the "value" label
-    
+    # print(df, flush=True)
     return df
 
+
+# @TimeAndGuard(measureTime=False)
+def pivotScalars(df, columns=["module"], index=["name"], values=None):
+
+    df = pivotScalars1(df)
+
+    df = df.pivot_table(columns=columns, index=index, values=values)
+
+    # print(df, flush=True)
+
+    if isinstance(df.columns, pd.MultiIndex):
+        if df.columns.nlevels > 1:
+            if not df.columns.names[0]:
+                df.columns = df.columns.droplevel()  # the "value" label
+
+    return df
 
 
 def getVectors(filter):
@@ -66,9 +79,9 @@ def getVectors(filter):
     df["attrvalue"] = pd.to_numeric(df["attrvalue"], errors='ignore')
 
     time3 = time.perf_counter()
-    #print("times: getting pickle: " + str((time1 - time0) * 1000.0) + " ms, unpickling: " + str((time2 - time1) * 1000.0) + " df constr:" + str((time3 - time2) * 1000.0) + " ms")
+    # print("times: getting pickle: " + str((time1 - time0) * 1000.0) + " ms, unpickling: " + str((time2 - time1) * 1000.0) + " df constr:" + str((time3 - time2) * 1000.0) + " ms")
 
-    #pickle.dump(df, open("/home/attila/vectors_df.pkl", "wb"))
+    # pickle.dump(df, open("/home/attila/vectors_df.pkl", "wb"))
 
     return df
 
@@ -85,9 +98,9 @@ def getStatistics(filter):
     df = pd.DataFrame(unpickled, columns=["run", "type", "module", "name", "attrname", "attrvalue", "count", "sumweights", "mean", "stddev", "min", "max"])
 
     time3 = time.perf_counter()
-    #print("times: getting pickle: " + str((time1 - time0) * 1000.0) + " ms, unpickling: " + str((time2 - time1) * 1000.0) + " df constr:" + str((time3 - time2) * 1000.0) + " ms")
+    # print("times: getting pickle: " + str((time1 - time0) * 1000.0) + " ms, unpickling: " + str((time2 - time1) * 1000.0) + " df constr:" + str((time3 - time2) * 1000.0) + " ms")
 
-    #pickle.dump(df, open("/home/attila/statistics_df.pkl", "wb"))
+    # pickle.dump(df, open("/home/attila/statistics_df.pkl", "wb"))
 
     return df
 
@@ -107,9 +120,9 @@ def getHistograms(filter):
     df["binvalues"] = df["binvalues"].map(lambda v: np.frombuffer(v, dtype=np.dtype('>f8')), na_action='ignore')
 
     time3 = time.perf_counter()
-    #print("times: getting pickle: " + str((time1 - time0) * 1000.0) + " ms, unpickling: " + str((time2 - time1) * 1000.0) + " df constr:" + str((time3 - time2) * 1000.0) + " ms")
+    # print("times: getting pickle: " + str((time1 - time0) * 1000.0) + " ms, unpickling: " + str((time2 - time1) * 1000.0) + " df constr:" + str((time3 - time2) * 1000.0) + " ms")
 
-    #pickle.dump(df, open("/home/attila/histograms_df.pkl", "wb"))
+    # pickle.dump(df, open("/home/attila/histograms_df.pkl", "wb"))
 
     return df
 
