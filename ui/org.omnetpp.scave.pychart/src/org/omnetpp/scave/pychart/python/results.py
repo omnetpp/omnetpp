62fa411d2ed417bc0bb88139b001464a6a1ad8bf
TÖRÖK Attila <torokati44@gmail.com>
2018-08-17 15:45:08 +0200

    scave: handle all result types in transform_results, add filter_metadata

--- ui/org.omnetpp.scave.pychart/src/org/omnetpp/scave/pychart/python/results.py
+++ ui/org.omnetpp.scave.pychart/src/org/omnetpp/scave/pychart/python/results.py
@@ -37,42 +37,104 @@ def transform_results(df):
     # print(itervars_pivot)
 
     # the stuff recorded as scalars with _runattrs_ as module is redundant, since we include the runattrs anyways
-    scalars = df[(df.type == "scalar") & (df.module != '_runattrs_')][["run", "module", "name", "value"]]
-    scalars.set_index(['run', 'module', 'name'], inplace=True)
-    scalars.columns = pd.MultiIndex.from_product([['result'], scalars.columns])
-
+    scalars = df[(df.type == "scalar") & (df.module != '_runattrs_')]
+    if not scalars.empty:
+        scalars = scalars[["run", "module", "name", "value"]]
+        scalars.set_index(['run', 'module', 'name'], inplace=True)
+        scalars.columns = pd.MultiIndex.from_product([['result'], scalars.columns])
     # print(scalars)
 
-    attrs = df[df.type == "attr"][["run", "module", "name", "attrname", "attrvalue"]]
+    vectors = df[df.type == "vector"]
+    if not vectors.empty:
+        vectors = vectors[["run", "module", "name", "vectime", "vecvalue"]]
+        vectors.set_index(['run', 'module', 'name'], inplace=True)
+        vectors.columns = pd.MultiIndex.from_product([['result'], vectors.columns])
+    # print(vectors)
 
-    # print(attrs)
+    histograms = df[df.type == "histogram"]
+    if not histograms.empty:
+        histograms = histograms[["run", "module", "name", "count", "sumweights", "mean", "stddev", "min", "max", "binedges", "binvalues"]]
+        histograms.set_index(['run', 'module', 'name'], inplace=True)
+        histograms.columns = pd.MultiIndex.from_product([['result'], histograms.columns])
+    # print(histograms)
 
-    # we know that the attrname values are unique for every run-module-name index, but pandas doesn't, so it will
-    # give us a series of 1 element length to aggregate. we will simply return the single element of that series
-    attrs_pivot = attrs.pivot_table(index=["run", "module", "name"], columns="attrname", values="attrvalue", aggfunc=lambda s: s[s.first_valid_index()])
-    attrs_pivot.columns = pd.MultiIndex.from_product([['attr'], attrs_pivot.columns])
+    statistics = df[df.type == "statistic"]
+    if not statistics.empty:
+        statistics = statistics[["run", "module", "name", "count", "sumweights", "mean", "stddev", "min", "max"]]
+        statistics.set_index(['run', 'module', 'name'], inplace=True)
+        statistics.columns = pd.MultiIndex.from_product([['result'], statistics.columns])
+    # print(statistics)
 
-    # print(attrs)
+    attrs = df[df.type == "attr"]
+    if not attrs.empty:
+        attrs = attrs[["run", "module", "name", "attrname", "attrvalue"]]
+        # print(attrs)
 
-    joined = scalars
-    joined = joined.join(attrs_pivot)
-    joined = joined.join(itervars_pivot, on='run')
-    joined = joined.join(params_pivot, on='run')
-    joined = joined.join(runattrs_pivot, on='run')
+        # we know that the attrname values are unique for every run-module-name index, but pandas doesn't, so it will
+        # give us a series of 1 element length to aggregate. we will simply return the single element of that series
+        first = lambda s: s[s.first_valid_index()]
+        attrs_pivot = attrs.pivot_table(index=["run", "module", "name"], columns="attrname", values="attrvalue", aggfunc=first)
+        attrs_pivot.columns = pd.MultiIndex.from_product([['attr'], attrs_pivot.columns])
 
+        # print(attrs_pivot)
+
+    joined = pd.DataFrame()
+
+    if not scalars.empty:
+        joined = scalars if joined.empty else joined.join(scalars)
+    if not vectors.empty:
+        joined = vectors if joined.empty else joined.join(vectors)
+    if not histograms.empty:
+        joined = histograms if joined.empty else joined.join(histograms)
+    if not statistics.empty:
+        joined = statistics if joined.empty else joined.join(statistics)
+
+    if not attrs_pivot.empty:
+        joined = joined.join(attrs_pivot)
+
+    if not itervars_pivot.empty:
+        joined = joined.join(itervars_pivot, on='run')
+    if not params_pivot.empty:
+        joined = joined.join(params_pivot, on='run')
+    if not runattrs_pivot.empty:
+        joined = joined.join(runattrs_pivot, on='run')
     # print(joined)
-    joined.reset_index(inplace=True)
-    joined.set_index([('runattr', 'experiment'), ('runattr', 'measurement'), ('runattr', 'replication'), ('module', ""), ('name', "")], inplace=True)
-    #joined.sort_index(inplace=True)
-    #joined.index.names = ["experiment", "measurement", "replication", "module", "name"]
 
-    # joined.rename({'':'runid'}, axis="columns", level=1, inplace=True)
-    # joined.rename({'run':'runattr'}, axis="columns", level=0, inplace=True)
+    if not joined.empty:
+        joined.reset_index(inplace=True)
 
+        joined.set_index([('runattr', 'experiment'), ('runattr', 'measurement'), ('runattr', 'replication'), ('module', ""), ('name', "")], inplace=True)
+        joined.sort_index(inplace=True)
+        joined.index.names = ["experiment", "measurement", "replication", "module", "name"]
+
+        joined.rename({'':'runid'}, axis="columns", level=1, inplace=True)
+        joined.rename({'run':'runattr'}, axis="columns", level=0, inplace=True)
+
+        joined.sort_index(axis="columns", inplace=True)
+
+        joined = joined.reindex(['result', 'attr', 'itervar', 'param', 'runattr'], axis="columns", level=0)
     # print(joined.columns)
     # print(joined)
 
     return joined
+
+
+def filter_metadata(df, attrs=[], itervars=[], params=[], runattrs=[], droplevel=True):
+    keys = [k for k in list(df.columns) if k[0] == 'result']
+
+    l = lambda s, b: [k for k in list(df.columns) if k[0] == s] if b == "all" else [(s, a) for a in b]
+
+    keys += l("attr", attrs)
+    keys += l("param", params)
+    keys += l("itervar", itervars)
+    keys += l("runattr", runattrs)
+
+    df = df[keys]
+
+    if droplevel:
+        df.columns = df.columns.droplevel()
+
+    return df
 
 
 def getScalars(filter):
