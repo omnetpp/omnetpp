24f5ef8751568ef7869946fec515c12f7cfaab28
TÖRÖK Attila <torokati44@gmail.com>
2018-10-25 15:32:35 +0200

    scave: opp_charttool: add more result querying functions, add partial chart API, call plt.show() if needed

---
+++ src/utils/chart.py
@@ -0,0 +1,185 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import math
+import results
+
+properties = dict()
+name = ""
+
+def getProperties():
+    return properties
+
+def setProperties(*vargs, **kwargs):
+    for a in vargs:
+        properties.update(a)
+    properties.update(kwargs)
+
+def setProperty(key, value):
+    properties[key] = value
+
+def getDefaultProperties():
+    return dict() # TODO
+
+def getName():
+    return name
+
+
+def _to_label(x):
+    if isinstance(x, str):
+        return x
+    elif isinstance(x, tuple):
+        return ", ".join(list(x))
+    elif isinstance(x, list):
+        return ", ".join(x)
+    else:
+        return str(x)
+
+
+def _plotScalars_lists(row_label, labels, values):
+    plt.bar(np.arange(len(values)), values, tick_label=labels, label=row_label)
+    plt.legend()
+    plt.grid()
+
+def _plotScalars_DF_simple(df):
+    column_labels = [_to_label(c) for c in list(df.columns)]
+
+    N = len(df.index)
+    M = 0
+    i = 0
+    for index, row in df.iterrows():
+        v = row.values
+        M = len(v)
+        plt.bar(np.arange(M) + i * 0.8 / N, v, width=0.8/N, tick_label=column_labels, label=_to_label(index))
+        i += 1
+
+    if N and M:
+        plt.xticks(np.arange(M) + 0.8 / N * ((N-1)/2))
+
+    plt.grid()
+    plt.legend()
+
+
+def _plotScalars_DF_scave(df):
+    _plotScalars_DF_simple(results.pivotScalars(df))
+
+
+def _plotScalars_DF_2(df):
+    names = df.index.get_level_values('name').tolist()
+    modules = df.index.get_level_values('module').tolist()
+
+    paths = list(map(lambda t: '.'.join(t), zip(modules, names)))
+
+    values = df[('result', 'value')]
+
+    _plotScalars_lists(None, paths, values)
+
+
+def plotScalars(df_or_values, labels=None, row_label=None):
+    if isinstance(df_or_values, pd.DataFrame):
+        df = df_or_values
+        if "value" in df.columns and "type" in df.columns and "module" in df.columns and "name" in df.columns:
+            _plotScalars_DF_scave(df)
+        elif "experiment" in df.index.names and "measurement" in df.index.names and "replication" in df.index.names and "module" in df.index.names and "name" in df.index.names:
+            _plotScalars_DF_2(df)
+        else:
+            _plotScalars_DF_simple(df)
+    else:
+        _plotScalars_lists(row_label, labels, df_or_values)
+
+
+def plotVector(label, xs, ys):
+    plt.plot(xs, ys, label=_to_label(label))
+    plt.legend()
+
+
+def _plotVectors_tuplelist(vectors):
+    for v in vectors:
+        plt.plot(v[1], v[2], label=_to_label(v[0]))
+    plt.legend()
+
+
+def _plotVectors_DF_simple(df):
+    xs = None
+    if "time" in df:
+        xs = list(df["time"])
+    else:
+        xs = list(range(len(df[df.columns[0]])))
+
+    for column in df:
+        if column != "time":
+            plt.plot(xs, list(df[column]), label=_to_label(column))
+    plt.legend()
+
+
+def _plotVectors_DF_scave(df):
+    for row in df.itertuples(index=False):
+        if row.type == "vector":
+            plt.plot(list(row.vectime), list(row.vecvalue), label=row.module + ':' + row.name)
+    plt.legend()
+
+def _plotVectors_DF_2(df):
+    for index, row in df.iterrows():
+        style = dict()
+        if ('attr', 'interpolationmode') in row:
+            interp = row[('attr', 'interpolationmode')]
+            if interp == "none":
+                style['linestyle'] = ' '
+                style['marker'] = '.'
+            elif interp == "linear":
+                pass
+                # nothing to do
+            elif interp == "sample-hold":
+                style['drawstyle'] = 'steps-post'
+            elif interp == "backward-sample-hold":
+                style['drawstyle'] = 'steps-pre'
+
+
+        plt.plot(list(row[('result', 'vectime')]), list(row[('result', 'vecvalue')]), label=row[('attr', 'title')], **style)
+
+
+
+def plotVectors(df_or_list):
+    if isinstance(df_or_list, pd.DataFrame):
+        df = df_or_list
+        if "vectime" in df.columns and "vecvalue" in df.columns and "type" in df.columns and "module" in df.columns and "name" in df.columns:
+            _plotVectors_DF_scave(df)
+        elif "experiment" in df.index.names and "measurement" in df.index.names and "replication" in df.index.names and "module" in df.index.names and "name" in df.index.names:
+            _plotVectors_DF_2(df)
+        else:
+            _plotVectors_DF_simple(df)
+    else:
+        _plotVectors_tuplelist(df_or_list)
+
+
+
+
+def plotHistogram(label, edges, values, count=-1, lowest=math.nan, highest=math.nan):
+    plt.hist(bins=edges, x=edges[:-1], weights=values, label=label)
+    plt.legend()
+
+
+def _plotHistograms_DF(df):
+    for row in df.itertuples(index=False):
+        if row[1] == "histogram":
+
+            edges = list(row[12])
+            values = list(row[13])
+
+            plt.hist(bins=edges, x=edges[:-1], weights=values, label=row[2] + ":" + row[3])
+    plt.legend()
+
+
+def _plotHistograms_DF_2(df):
+    for index, row in df.iterrows():
+        edges = list(row[('result', 'binedges')])
+        values = list(row[('result', 'binvalues')])
+        plt.hist(bins=edges, x=edges[:-1], weights=values, label=row[('attr', 'title')])
+
+
+
+def plotHistograms(df):
+    if "experiment" in df.index.names and "measurement" in df.index.names and "replication" in df.index.names and "module" in df.index.names and "name" in df.index.names:
+        _plotHistograms_DF_2(df)
+    else:
+        _plotHistograms_DF(df)
--- src/utils/results.py
+++ src/utils/results.py
@@ -7,6 +7,172 @@ import pandas as pd
 wd = "."
 inputs = list()
 
+
+
+
+def transform_results(df):
+    # print(df)
+
+    runattrs = df[df.type == "runattr"][["run", "attrname", "attrvalue"]]
+    params = df[df.type == "param"][["run", "attrname", "attrvalue"]]
+    itervars = df[df.type == "itervar"][["run", "attrname", "attrvalue"]]
+
+    # print(runattrs)
+    # print(params)
+    # print(itervars)
+
+    # we only keep the effective (first in the .sca file) parameter assignments, sorry...
+    params.drop_duplicates(subset=["run", "attrname"], inplace=True)
+
+    runattrs_pivot = runattrs.pivot(index="run", columns="attrname", values="attrvalue")
+    params_pivot = params.pivot(index="run", columns="attrname", values="attrvalue")
+    itervars_pivot = itervars.pivot(index="run", columns="attrname", values="attrvalue")
+
+    runattrs_pivot.columns = pd.MultiIndex.from_product([['runattr'], runattrs_pivot.columns])
+    params_pivot.columns = pd.MultiIndex.from_product([['param'], params_pivot.columns])
+    itervars_pivot.columns = pd.MultiIndex.from_product([['itervar'], itervars_pivot.columns])
+
+    # print(runattrs_pivot)
+    # print(params_pivot)
+    # print(itervars_pivot)
+
+    # the stuff recorded as scalars with _runattrs_ as module is redundant, since we include the runattrs anyways
+    scalars = df[df.type == "scalar"]
+    if not scalars.empty:
+        scalars = scalars[["run", "module", "name", "value"]]
+        scalars.set_index(['run', 'module', 'name'], inplace=True)
+        scalars.columns = pd.MultiIndex.from_product([['result'], scalars.columns])
+    # print(scalars)
+
+    vectors = df[df.type == "vector"]
+    if not vectors.empty:
+        vectors = vectors[["run", "module", "name", "vectime", "vecvalue"]]
+        vectors.set_index(['run', 'module', 'name'], inplace=True)
+        vectors.columns = pd.MultiIndex.from_product([['result'], vectors.columns])
+    # print(vectors)
+
+    histograms = df[df.type == "histogram"]
+    if not histograms.empty:
+        histograms = histograms[["run", "module", "name", "count", "sumweights", "mean", "stddev", "min", "max", "binedges", "binvalues"]]
+        histograms.set_index(['run', 'module', 'name'], inplace=True)
+        histograms.columns = pd.MultiIndex.from_product([['result'], histograms.columns])
+    # print(histograms)
+
+    statistics = df[df.type == "statistic"]
+    if not statistics.empty:
+        statistics = statistics[["run", "module", "name", "count", "sumweights", "mean", "stddev", "min", "max"]]
+        statistics.set_index(['run', 'module', 'name'], inplace=True)
+        statistics.columns = pd.MultiIndex.from_product([['result'], statistics.columns])
+    # print(statistics)
+
+    attrs = df[df.type == "attr"]
+    if not attrs.empty:
+        attrs = attrs[["run", "module", "name", "attrname", "attrvalue"]]
+        # print(attrs)
+
+        # we know that the attrname values are unique for every run-module-name index, but pandas doesn't, so it will
+        # give us a series of 1 element length to aggregate. we will simply return the single element of that series
+        first = lambda s: s[s.first_valid_index()]
+        attrs_pivot = attrs.pivot_table(index=["run", "module", "name"], columns="attrname", values="attrvalue", aggfunc=first)
+        attrs_pivot.columns = pd.MultiIndex.from_product([['attr'], attrs_pivot.columns])
+
+        # print(attrs_pivot)
+    else:
+        attrs_pivot = pd.DataFrame()
+
+    joined = pd.DataFrame()
+
+    if not scalars.empty:
+        joined = scalars if joined.empty else joined.join(scalars)
+    if not vectors.empty:
+        joined = vectors if joined.empty else joined.join(vectors)
+    if not histograms.empty:
+        joined = histograms if joined.empty else joined.join(histograms)
+    if not statistics.empty:
+        joined = statistics if joined.empty else joined.join(statistics)
+
+    if not attrs_pivot.empty:
+        joined = joined.join(attrs_pivot)
+
+    if not itervars_pivot.empty:
+        joined = joined.join(itervars_pivot, on='run')
+    if not params_pivot.empty:
+        joined = joined.join(params_pivot, on='run')
+    if not runattrs_pivot.empty:
+        joined = joined.join(runattrs_pivot, on='run')
+    # print(joined)
+
+    if not joined.empty:
+        joined.reset_index(inplace=True)
+
+        joined.set_index([('runattr', 'experiment'), ('runattr', 'measurement'), ('runattr', 'replication'), ('module', ""), ('name', "")], inplace=True)
+        joined.sort_index(inplace=True)
+        joined.index.names = ["experiment", "measurement", "replication", "module", "name"]
+
+        joined.rename({'':'runid'}, axis="columns", level=1, inplace=True)
+        joined.rename({'run':'runattr'}, axis="columns", level=0, inplace=True)
+
+        joined.sort_index(axis="columns", inplace=True)
+
+        joined = joined.reindex(['result', 'attr', 'itervar', 'param', 'runattr'], axis="columns", level=0)
+    # print(joined.columns)
+    # print(joined)
+
+    return joined
+
+
+def filter_metadata(df, attrs=[], itervars=[], params=[], runattrs=[], droplevel=True):
+    keys = [k for k in list(df.columns) if k[0] == 'result']
+
+    l = lambda s, b: [k for k in list(df.columns) if k[0] == s] if b == "all" else [(s, a) for a in b]
+
+    keys += l("attr", attrs)
+    keys += l("param", params)
+    keys += l("itervar", itervars)
+    keys += l("runattr", runattrs)
+
+    df = df[keys]
+
+    if droplevel:
+        df.columns = df.columns.droplevel()
+
+    return df
+
+
+
+def pivotScalars1(df):
+    attrs = df[df.type.isin(["runattr", "itervar"])][["run", "attrname", "attrvalue"]]
+
+    scalars = df[df.type == "scalar"][["run", "module", "name", "value"]]
+
+    runattrs_pivot = attrs.pivot(index="run", columns="attrname", values="attrvalue")
+
+    scalars_merged = scalars.merge(runattrs_pivot, left_on="run", right_index=True)
+    scalars_merged.set_index(["run", "module", "name"])
+
+    df = scalars_merged[scalars_merged.module != "_runattrs_"].set_index(["run", "module", "name"])
+    # print(df, flush=True)
+    return df
+
+
+# @TimeAndGuard(measureTime=False)
+def pivotScalars(df, columns=["module"], index=["name"], values=None):
+
+    df = pivotScalars1(df)
+
+    df = df.pivot_table(columns=columns, index=index, values=values)
+
+    # print(df, flush=True)
+
+    if isinstance(df.columns, pd.MultiIndex):
+        if df.columns.nlevels > 1:
+            if not df.columns.names[0]:
+                df.columns = df.columns.droplevel()  # the "value" label
+
+    return df
+
+
+
 def parse_if_number(s):
     try: return float(s)
     except: return True if s=="true" else False if s=="false" else s if s else None
@@ -35,5 +201,76 @@ def getVectors(filter_expression):
         'vecvalue': parse_ndarray
     })
     # print(df)
+
+    return df
+
+
+def getScalars(filter_expression):
+    # print("wd: " + wd + " inputs " + ", ".join(inputs))
+
+    inputfiles = [glob.glob(wd + i) for i in inputs]
+
+    # flattening the list of lists and making it unique
+    inputfiles = list(set([item for sublist in inputfiles for item in sublist if item.endswith('.sca')]))
+    # print(inputfiles)
+
+    output = subprocess.check_output(["opp_scavetool", "x", *inputfiles, '-T', 's', '-f', filter_expression, "-F", "CSV-R", "-o", "-"], cwd=wd)
+    # print(output.decode('utf-8'))
+
+    df = pd.read_csv(io.BytesIO(output), converters = {
+        'attrvalue': parse_if_number,
+        'binedges': parse_ndarray,
+        'binvalues': parse_ndarray,
+        'vectime': parse_ndarray,
+        'vecvalue': parse_ndarray
+    })
+    # print(df)
+
+    return df
+
+
+def getStatistics(filter_expression):
+    # print("wd: " + wd + " inputs " + ", ".join(inputs))
+
+    inputfiles = [glob.glob(wd + i) for i in inputs]
+
+    # flattening the list of lists and making it unique
+    inputfiles = list(set([item for sublist in inputfiles for item in sublist if item.endswith('.sca')]))
+    # print(inputfiles)
+
+    output = subprocess.check_output(["opp_scavetool", "x", *inputfiles, '-T', 't', '-f', filter_expression, "-F", "CSV-R", "-o", "-"], cwd=wd)
+    # print(output.decode('utf-8'))
+
+    df = pd.read_csv(io.BytesIO(output), converters = {
+        'attrvalue': parse_if_number,
+        'binedges': parse_ndarray,
+        'binvalues': parse_ndarray,
+        'vectime': parse_ndarray,
+        'vecvalue': parse_ndarray
+    })
+    # print(df)
+
+    return df
+
+
+def getHistograms(filter_expression):
+    # print("wd: " + wd + " inputs " + ", ".join(inputs))
+
+    inputfiles = [glob.glob(wd + i) for i in inputs]
+
+    # flattening the list of lists and making it unique
+    inputfiles = list(set([item for sublist in inputfiles for item in sublist if item.endswith('.sca')]))
+    # print(inputfiles)
+
+    output = subprocess.check_output(["opp_scavetool", "x", *inputfiles, '-T', 'h', '-f', filter_expression, "-F", "CSV-R", "-o", "-"], cwd=wd)
+    # print(output.decode('utf-8'))
+
+    df = pd.read_csv(io.BytesIO(output), converters = {
+        'attrvalue': parse_if_number,
+        'binedges': parse_ndarray,
+        'binvalues': parse_ndarray,
+        'vectime': parse_ndarray,
+        'vecvalue': parse_ndarray
+    })
 
     return df
