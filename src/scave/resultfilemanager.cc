d7c7e33a915738a97a2c02d5ec7f8ecc6e44cb1f
TÖRÖK Attila <torokati44@gmail.com>
2018-11-05 11:51:47 +0100

    scave: Fix build with C++17. Contribution by tomasz.jankowski.mail@gmail.com.

--- src/scave/resultfilemanager.cc
+++ src/scave/resultfilemanager.cc
@@ -1297,7 +1297,7 @@ inline bool strdictLess(const std::string& first, const std::string& second)
 
 struct StrDictCompare
 {
-    bool operator()(const std::string& first, const std::string& second)
+    bool operator()(const std::string& first, const std::string& second) const
     {
         return strdictLess(first, second);
     }
