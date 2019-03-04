639ddce0a3dbe14fe08d9eae2b8e190de3fb6495
Rudolf Hornig <rudi@omnetpp.org>
2019-03-04 12:17:36 +0100

    ide: renamed the launcher executable to opp_ide
    The same launcher name will be used in both OMNeT++ and OMNEST

--- src/utils/omnest.cmd
+++ src/utils/omnest.cmd
@@ -1,6 +1,6 @@
 @echo off
 SET IDEDIR=%~dp0\..\ide
-SET LAUNCHER=omnest
+SET LAUNCHER=opp_ide
 SET DEFAULT_WORKSPACE_ARGS=-vmargs -Dosgi.instance.area.default=%IDEDIR%\..\samples
 echo Starting the OMNEST IDE...
 start %IDEDIR%\%LAUNCHER% %* %DEFAULT_WORKSPACE_ARGS% >out.log
