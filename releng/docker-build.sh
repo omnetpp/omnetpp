4f05766d61de36d12d66ced254ffad7f02fa3625
Rudolf Hornig <rudi@omnetpp.org>
2019-03-28 11:42:02 +0100

    prod: use fully offline build with network turned off in the docker container

--- releng/docker-build.sh
+++ releng/docker-build.sh
@@ -3,8 +3,8 @@
 cd `dirname $0`/..
 OMNETPP_ROOT=`pwd`
 
-docker run -v "$OMNETPP_ROOT":/root/omnetpp-repo -e what -e GIT_VERSION \
+docker run --network none -v "$OMNETPP_ROOT":/root/omnetpp-repo -e what -e GIT_VERSION \
        -e BUILD_CORE_DISTRO -e BUILD_LINUX_DISTRO -e BUILD_WIN64_DISTRO -e BUILD_MACOSX_DISTRO \
-       omnetpp/distrobuild:eclipse480-tools180301-181214 && \
+       omnetpp/distrobuild:eclipse4.11-tools180301-190328 && \
 docker cp $(docker ps -lq):/root/omnetpp/out/dist/ releng && \
 docker rm $(docker ps -lq)
