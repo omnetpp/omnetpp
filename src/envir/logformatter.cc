8645974c171b3658e3980ba87ceac08ffa7b16fc
Andras Varga <andras@omnetpp.org>
2019-07-03 14:23:49 +0200

    qtenv: fix: prevent aborting when user enters invalid log prefix in Preferences
    To try, add '%-' to log prefix.

--- src/envir/logformatter.cc
+++ src/envir/logformatter.cc
@@ -94,7 +94,7 @@ void LogFormatter::parseFormat(const char *format)
 LogFormatter::FormatDirective LogFormatter::getDirective(char ch)
 {
     if (strchr("lcetgvanmosqNMOSQGRXYZpbdzuxyfiwWHIEUCKJL", ch) == nullptr)
-        throw cRuntimeError("Unknown log format character '%c'", ch);
+        throw opp_runtime_error("Unknown log format character '%c'", ch);
     return (LogFormatter::FormatDirective)ch;
 }
 
@@ -420,7 +420,7 @@ std::string LogFormatter::formatPrefix(cLogEntry *entry)
             }
 
             default:
-                throw cRuntimeError("Unknown format directive '%d'", part.directive);
+                throw opp_runtime_error("Unknown format directive '%d'", part.directive);
         }
     }
     return stream.str();
