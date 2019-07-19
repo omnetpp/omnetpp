74c8e7084ef9ec4e4d0d7cdede73c50b764ee023
Rudolf Hornig <rudi@omnetpp.org>
2019-07-19 08:51:59 +0200

    prod: updated dist scripts to use docker images with new tools.

--- releng/docker-build.sh
+++ releng/docker-build.sh
@@ -5,6 +5,6 @@ OMNETPP_ROOT=`pwd`
 
 docker run --network none -v "$OMNETPP_ROOT":/root/omnetpp-repo -e what -e GIT_VERSION \
        -e BUILD_CORE_DISTRO -e BUILD_LINUX_DISTRO -e BUILD_WIN64_DISTRO -e BUILD_MACOSX_DISTRO \
-       omnetpp/distrobuild:eclipse4.12-tools190710-190710 && \
+       omnetpp/distrobuild:eclipse4.12-tools190710-190722 && \
 docker cp $(docker ps -lq):/root/omnetpp/out/dist/ releng && \
 docker rm $(docker ps -lq)
