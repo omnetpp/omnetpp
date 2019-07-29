562ff48094c98d8ea86720db466e737c5e6b0498
TÖRÖK Attila <torokati44@gmail.com>
2019-07-29 17:08:43 +0200

    test/scave: Add a model to generate a small test dataset for scave.

---
+++ test/scave/node.cc
@@ -0,0 +1,22 @@
+#include <omnetpp.h>
+
+using namespace omnetpp;
+
+class Node : public cSimpleModule
+{
+    virtual void initialize() override {
+        int bar = par("bar");
+
+        simsignal_t foo1 = registerSignal("foo1");
+        simsignal_t foo2 = registerSignal("foo2");
+        for (int i = 0; i < 100; i++) {
+            cTimestampedValue tmp1((simtime_t)i, (double)binomial(40, 0.5) * bar);
+            emit(foo1, &tmp1);
+
+            cTimestampedValue tmp2((simtime_t)i, (double)exponential(10) * bar);
+            emit(foo2, &tmp2);
+        }
+    }
+};
+
+Define_Module(Node);
