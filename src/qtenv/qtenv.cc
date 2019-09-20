e32603e96a637a9ed73b67e246ba3019e9fd2500
TÖRÖK Attila <torokati44@gmail.com>
2019-09-20 17:47:49 +0200

    qtenv: Start the simulation at t=0, not right before the first event.
    Again.

--- src/qtenv/qtenv.cc
+++ src/qtenv/qtenv.cc
@@ -1127,7 +1127,6 @@ void Qtenv::newNetwork(const char *networkname)
     mainNetworkView->setObject(module);
     mainInspector->setObject(module);
 
-    displayUpdateController->skipToNextEvent(); // should be changed to animate at some point...
     animating = true;  // affects how network graphics is drawn!
     messageAnimator->redrawMessages();
     messageAnimator->setMarkedModule(getSimulation()->guessNextModule());
@@ -1185,7 +1184,6 @@ void Qtenv::newRun(const char *configname, int runnumber)
     mainNetworkView->setObject(module);
     mainInspector->setObject(module);
 
-    displayUpdateController->skipToNextEvent(); // should be changed to animate at some point...
     animating = true;  // affects how network graphics is drawn!
     messageAnimator->redrawMessages();
     messageAnimator->setMarkedModule(getSimulation()->guessNextModule());
