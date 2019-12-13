c476ec27bef24c1390de23b04e2eb6aa7bf9e7e1
TÖRÖK Attila <torokati44@gmail.com>
2019-12-13 17:29:27 +0100

    ide/scave: Restore chart.get_default_properties() functionality

--- ui/org.omnetpp.scave/src/org/omnetpp/scave/editors/PropertiesProvider.java
+++ ui/org.omnetpp.scave/src/org/omnetpp/scave/editors/PropertiesProvider.java
@@ -3,6 +3,13 @@ package org.omnetpp.scave.editors;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.eclipse.swt.graphics.FontData;
+import org.eclipse.swt.graphics.RGB;
+import org.eclipse.ui.views.properties.IPropertyDescriptor;
+import org.eclipse.ui.views.properties.IPropertySource;
+import org.omnetpp.common.util.Converter;
+import org.omnetpp.scave.charting.properties.ChartDefaults;
+import org.omnetpp.scave.charting.properties.ChartVisualProperties;
 import org.omnetpp.scave.model.Chart;
 import org.omnetpp.scave.model.Property;
 import org.omnetpp.scave.pychart.IChartPropertiesProvider;
@@ -26,41 +33,43 @@ public class PropertiesProvider implements IChartPropertiesProvider {
 
     @Override
     public Map<String, String> getDefaultChartProperties() {
-        // TODO
-//        ChartProperties props = ChartProperties.createPropertySource(chart, null);
-//
-//        IPropertyDescriptor[] descriptors = props.getPropertyDescriptors();
-//
-//        Map<String, String> result = new HashMap<String, String>();
-//
-//        for (IPropertyDescriptor desc : descriptors) {
-//            String name = desc.getId().toString();
-//            Object def = ChartDefaults.getDefaultPropertyValue(name);
-//
-//            if (def == null)
-//                result.put(name, null);
-//            else {
-//                String stringDefault = null;
-//
-//                if (def instanceof FontData)
-//                    stringDefault = Converter.fontdataToString((FontData) def);
-//                else if (def instanceof Boolean)
-//                    stringDefault = Converter.booleanToString((Boolean) def);
-//                else if (def instanceof Double)
-//                    stringDefault = Converter.doubleToString((Double) def);
-//                else if (def instanceof Integer)
-//                    stringDefault = Converter.integerToString((Integer) def);
-//                else if (def instanceof RGB)
-//                    stringDefault = Converter.rgbToString((RGB) def);
-//                else
-//                    stringDefault = def.toString();
-//
-//                result.put(name, stringDefault);
-//            }
-//        }
-        //return result;
 
-        return null;
+        IPropertySource props = ChartVisualProperties.createPropertySource(chart);
+
+        IPropertyDescriptor[] descriptors = props.getPropertyDescriptors();
+
+        Map<String, String> result = new HashMap<String, String>();
+
+        for (IPropertyDescriptor desc : descriptors) {
+            String name = desc.getId().toString();
+            Object def = ChartDefaults.getDefaultPropertyValue(name);
+
+            if (def == null)
+                result.put(name, null);
+            else {
+                String stringDefault = null;
+
+                if (def instanceof FontData)
+                    stringDefault = Converter.fontdataToString((FontData) def);
+                else if (def instanceof Boolean)
+                    stringDefault = Converter.booleanToString((Boolean) def);
+                else if (def instanceof Double)
+                    stringDefault = Converter.doubleToString((Double) def);
+                else if (def instanceof Integer)
+                    stringDefault = Converter.integerToString((Integer) def);
+                else if (def instanceof RGB)
+                    stringDefault = Converter.rgbToString((RGB) def);
+                else
+                    stringDefault = def.toString();
+
+                result.put(name, stringDefault);
+            }
+        }
+
+        // TODO: Also add the declared form properties of the template of the chart.
+        // ... except there is not much point since changing them will not have any effect?
+
+        return result;
     }
 
     @Override
