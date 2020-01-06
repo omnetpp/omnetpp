b1f613e3535628eac2855f524baeffca1dd04998
Rudolf Hornig <rudi@omnetpp.org>
2020-01-06 11:38:00 +0100

    prod: dependency update to eclipse 4.14 and CDT 9.10

--- releng/docker-build.sh
+++ releng/docker-build.sh
@@ -12,7 +12,7 @@ OMNETPP_ROOT=`pwd`
 
 docker run --network none -v "$OMNETPP_ROOT/.git":/root/omnetpp-repo -e what -e GIT_VERSION \
        -e BUILD_CORE_DISTRO -e BUILD_LINUX_DISTRO -e BUILD_WIN64_DISTRO -e BUILD_MACOSX_DISTRO \
-       omnetpp/distrobuild:eclipse4.13-tools180301-191212 \
+       omnetpp/distrobuild:eclipse4.14-tools180301-200106 \
        /bin/sh -c 'cd .. && rm -rf $OMNETPP_DIR && git clone $OMNETPP_REPO $OMNETPP_DIR && cd $OMNETPP_DIR && git checkout -f $GIT_VERSION && _scripts/build-omnetpp-src-dist' && \
 docker cp $(docker ps -lq):/root/omnetpp/out/dist/ releng && \
 docker rm $(docker ps -lq)
