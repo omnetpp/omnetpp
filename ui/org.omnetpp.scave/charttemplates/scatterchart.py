e5a16c4fb25d5b8b78dae96d89b96d938a30eb44
TÖRÖK Attila <torokati44@gmail.com>
2020-01-20 15:58:57 +0100

    ide/scave: fix scatter chart template to use MPL and new API

--- ui/org.omnetpp.scave/charttemplates/scatterchart.py
+++ ui/org.omnetpp.scave/charttemplates/scatterchart.py
@@ -7,38 +7,41 @@ pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
 
-params = chart.get_properties()
+params = chart.get_configured_properties()
+
 
 def aggfunc(x):
     if type(x.values[0]) == float:
         return np.nanmean(x, dtype=np.float64)
-    
+
     if type(x.values[0]) == str:
         return ", ".join(x.unique())
-    
+
     return np.nanmean(x, dtype=np.float64)
+
 
 def get_data(filter):
     sc = results.get_scalars(filter, include_attrs=True)
     iv = results.get_itervars(filter)
     ra = results.get_run_attrs(filter)
-    
+
     df = pd.concat([sc, iv, ra])
-    #print(df)
-    
+    # print(df)
+
     df["value"] = pd.to_numeric(df["value"], errors="ignore")
     df = pd.pivot_table(df, columns="name", index="runID", dropna=False, aggfunc=aggfunc)
-    #print(df)
+    # print(df)
 
     return df
 
+
 def filter_for(what):
-    return "(" + params["common_filter"] + ") AND (" +  params[what] + ")"
+    return "(" + params["common_filter"] + ") AND (" + params[what] + ")"
+
 
 xa = get_data(filter_for("xaxis_filter"))
 ya = get_data(filter_for("yaxis_filter"))
 iso = get_data(filter_for("iso_filter"))
-
 
 cols = xa["value"].columns.values
 if len(cols) != 1:
@@ -48,7 +51,7 @@ xaxis_column = cols[0]
 cols = ya["value"].columns.values
 if len(cols) != 1:
     raise Exception("Y axis filter must result in exactly one variable, instead of: " + ", ".join(cols) + "\nTry narrowing down the filter expression to one of these by name!")
-yaxis_column = cols[0] 
+yaxis_column = cols[0]
 
 cols = iso["value"].columns.values
 if len(cols) != 1:
@@ -66,22 +69,17 @@ values = df["value"]
 titles = df["title"] if "title" in df else None
 units = df["unit"] if "unit" in df else None
 
-
-
 values[yaxis_column] = pd.to_numeric(values[yaxis_column], errors="ignore")
 
-
-
-#values[iso_column] = values[iso_column].apply(lambda x: 1 if x == 'true' else 0)
-#values.plot.scatter( x=xaxis_column,y=yaxis_column, c=iso_column, colormap='rainbow')
-
+# values[iso_column] = values[iso_column].apply(lambda x: 1 if x == 'true' else 0)
+# values.plot.scatter( x=xaxis_column,y=yaxis_column, c=iso_column, colormap='rainbow')
 
 df = pd.pivot_table(values, columns=iso_column, index=xaxis_column, values=yaxis_column, dropna=False)
 
 if False:
     ax = plt.subplot(1, 2, 1)
     ax.table(cellText=df.values, colLabels=df.columns, loc='center')
-    
+
     df.plot.line(ax=plt.subplot(1, 2, 2))
 else:
     df.plot.line()
