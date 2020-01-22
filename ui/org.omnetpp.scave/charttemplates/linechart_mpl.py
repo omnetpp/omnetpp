1b1e3f216f31bea48722bf4baa6b95532f6eb282
TÖRÖK Attila <torokati44@gmail.com>
2020-01-22 16:15:25 +0100

    ide/scave: add drawstyle property to linechart_mpl template

--- ui/org.omnetpp.scave/charttemplates/linechart_mpl.py
+++ ui/org.omnetpp.scave/charttemplates/linechart_mpl.py
@@ -1,6 +1,7 @@
 from omnetpp.scave import results, chart, utils, plot, vectorops as ops
 import matplotlib.pyplot as plt
 import math
+import sys
 
 props = chart.get_properties()
 utils.update_matplotlib_rcparams(props)
@@ -28,17 +29,43 @@ title, legend = utils.extract_label_columns(df)
 
 for t in df.itertuples(index=False):
     style = dict()
-    interp = t.interpolationmode if 'interpolationmode' in df else 'sample-hold' if 'enum' in df else None
-    if interp:
-        if interp == "none":
-            style['linestyle'] = ' '
-            style['marker'] = '.'
-        elif interp == "linear":
-            pass  # nothing to do
-        elif interp == "sample-hold":
-            style['drawstyle'] = 'steps-post'
-        elif interp == "backward-sample-hold":
-            style['drawstyle'] = 'steps-pre'
+
+    ds = props["drawstyle"]
+    if not ds or ds == "auto":
+        interp = t.interpolationmode if 'interpolationmode' in df else 'sample-hold' if 'enum' in df else None
+        if interp:
+            if interp == "none":
+                ds = "dots"
+            elif interp == "linear":
+                ds = "linear"
+            elif interp == "sample-hold":
+                ds = "steps-post"
+            elif interp == "backward-sample-hold":
+                ds = 'steps-pre'
+            else:
+                print("Unknown interpolationmode for", t, ":", interp, file=sys.stderr)
+                ds = None
+        else:
+            ds = "linear"
+
+    if ds == "dots":
+        style['linestyle'] = ' '
+        style['marker'] = '.'
+    elif ds == "points":
+        style['linestyle'] = ' '
+        style['marker'] = '.'
+        style['markersize'] = 1
+    elif ds == "linear":
+        pass  # nothing to do
+    elif ds == 'steps-pre':
+        style['drawstyle'] = 'steps-pre'
+    elif ds == "steps-mid":
+        style['drawstyle'] = 'steps-mid'
+    elif ds == "steps-post":
+        style['drawstyle'] = 'steps-post'
+    else:
+        if ds:
+            print("Unknown drawstyle:", ds, file=sys.stderr)
 
     plt.plot(t.vectime, t.vecvalue, label=utils.make_legend_label(legend, t), **style)
 
