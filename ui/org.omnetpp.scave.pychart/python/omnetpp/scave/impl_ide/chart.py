d039f769b3c01bbd313d10fd1894806015368e59
TÖRÖK Attila <torokati44@gmail.com>
2020-02-10 18:01:44 +0100

    ide/scave: cache chart properties on the Python side, avoid a Py4J error while querying

--- ui/org.omnetpp.scave.pychart/python/omnetpp/scave/impl_ide/chart.py
+++ ui/org.omnetpp.scave.pychart/python/omnetpp/scave/impl_ide/chart.py
@@ -7,14 +7,29 @@ print = functools.partial(print, flush=True)
 from omnetpp.scave import results, plot
 from omnetpp.internal import Gateway
 
+# this is a copy of the property map queried from Java, since
+# it is not supposed to be modifiable by the script
+_properties = None
 
 def get_properties():
-    # converting it into a regular dict, instead of the Py4J "Java Map emulator",
-    # so we get a KeyError when getting a non-existent key, instead of None
-    return dict(Gateway.chart_provider.getChartProperties())
+    global _properties
+    if _properties is None:
+        # converting it into a regular dict, instead of the Py4J "Java Map emulator",
+        # so we get a KeyError when getting a non-existent key, instead of None
+        props = Gateway.chart_provider.getChartProperties()
+
+        _properties = dict()
+        for k in props:
+            _properties[k] = props[k]
+
+        # this would be simpler, and it is _supposed_ to work, but sometimes
+        # it triggers a "Received empty command" error inside Py4J...
+        # _properties = dict(props)
+
+    return _properties
 
 def get_property(key):
-    return get_properties()[key]  # TODO: could be optimized
+    return get_properties()[key]
 
 def get_name():
     return Gateway.chart_provider.getChartName()
