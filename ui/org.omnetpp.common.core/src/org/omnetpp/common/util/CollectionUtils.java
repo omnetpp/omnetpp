91982376f9b01afdb762f8c28577fa4bf401ff1e
Andras Varga <andras@omnetpp.org>
2020-02-18 21:21:12 +0100

    common: CollectionUtils: added type parameters

--- ui/org.omnetpp.common.core/src/org/omnetpp/common/util/CollectionUtils.java
+++ ui/org.omnetpp.common.core/src/org/omnetpp/common/util/CollectionUtils.java
@@ -43,42 +43,39 @@ public class CollectionUtils {
         return sortable;
     }
 
-    @SuppressWarnings({ "unchecked", "rawtypes" })
-    public static Map getDeepCopyOf(Map map) {
-        Map result = new HashMap();
-        for (Object key : map.keySet())
+    public static <K,V> Map<K,V> getDeepCopyOf(Map<K,V> map) {
+        Map<K,V> result = new HashMap<>();
+        for (K key : map.keySet())
             result.put(deepCopyOf(key), deepCopyOf(map.get(key)));
         return result;
     }
 
-    @SuppressWarnings({ "unchecked", "rawtypes" })
-    public static Object getDeepCopyOf(Set set) {
-        Set result = new HashSet();
-        for (Object value : set)
+    public static <T> Set<T> getDeepCopyOf(Set<T> set) {
+        Set<T> result = new HashSet<>();
+        for (T value : set)
             result.add(deepCopyOf(value));
         return result;
     }
 
-    @SuppressWarnings({ "unchecked", "rawtypes" })
-    public static Object getDeepCopyOf(List list) {
-        List result = new ArrayList();
-        for (Object value : list)
+    public static <T> List<T> getDeepCopyOf(List<T> list) {
+        List<T> result = new ArrayList<>();
+        for (T value : list)
             result.add(deepCopyOf(value));
         return result;
     }
 
-    @SuppressWarnings("rawtypes")
-    public static Object deepCopyOf(Object object) {
+    @SuppressWarnings("unchecked")
+    public static <T> T deepCopyOf(T object) {
         if (object instanceof String || object instanceof Boolean || object instanceof Number)
             return object; // immutable (probably; except for exotic stuff like apache.MutableDouble)
         if (object instanceof Map)
-            return getDeepCopyOf((Map)object);
+            return (T)getDeepCopyOf((Map<?,?>)object);
         if (object instanceof List)
-            return getDeepCopyOf((List)object);
+            return (T)getDeepCopyOf((List<?>)object);
         if (object instanceof Set)
-            return getDeepCopyOf((Set)object);
+            return (T)getDeepCopyOf((Set<?>)object);
         if (object instanceof Cloneable)
-            return ReflectionUtils.invokeMethod(object, "clone");
+            return (T)ReflectionUtils.invokeMethod(object, "clone");
         throw new IllegalArgumentException("dunno how to clone " + object.toString());
     }
 
