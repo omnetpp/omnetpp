a2a536dc05590b12cfeaad028e94f7df360ceec8
TÖRÖK Attila <torokati44@gmail.com>
2020-02-20 14:27:54 +0100

    ide/scave: Add automatic series/groups and xaxis/isoline parameter guessing to barchart and scatterchart templates.

--- ui/org.omnetpp.scave/charttemplates/barchart.py
+++ ui/org.omnetpp.scave/charttemplates/barchart.py
@@ -5,17 +5,28 @@ from omnetpp.scave import results, chart, plot, utils
 props = chart.get_properties()
 utils.preconfigure_plot(props)
 
-if not props["groups"] or not props["series"]:
-    plot.set_warning("Please select group and series attributes in the Properties dialog!")
-    exit(1)
-
 # collect parameters for query
 filter_expression = props["filter"]
-groups = props["groups"].split(",")
-series = props["series"].split(",")
 
 # query scalar data into dataframe
 df = results.get_scalars(filter_expression, include_attrs=True, include_itervars=True, include_runattrs=True)
+
+groups = props["groups"].split(",")
+series = props["series"].split(",")
+
+if not groups[0] and not series[0]:
+    print("The Groups and Series options were not set in the dialog, inferring them from the data.")
+    title_col, label_cols = utils.extract_label_columns(df)
+    label_cols = [l[1] for l in label_cols]
+    if len(label_cols) == 0:
+        plot.set_warning("Please set the Groups and Series options in the dialog!")
+        exit(1)
+    if len(label_cols) == 1:
+        groups = [title_col]
+        series = [label_cols[0]]
+    if len(label_cols) >= 2:
+        groups = [label_cols[0]]
+        series = [label_cols[1]]
 
 for c in groups + series:
     df[c] = pd.to_numeric(df[c], errors="ignore")
