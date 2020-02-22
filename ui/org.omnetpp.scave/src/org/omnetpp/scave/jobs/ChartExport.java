e7ec185b2e1ca8dfbe778010a5aa0323470150d5
Andras Varga <andras@omnetpp.org>
2020-02-22 15:53:23 +0100

    ide/scave: ChartExport: collect args into a Context class to reduce number of places to change when a new one is added

--- ui/org.omnetpp.scave/src/org/omnetpp/scave/jobs/ChartExport.java
+++ ui/org.omnetpp.scave/src/org/omnetpp/scave/jobs/ChartExport.java
@@ -8,7 +8,6 @@ import java.util.Map;
 import java.util.concurrent.Callable;
 
 import org.eclipse.core.resources.IContainer;
-import org.eclipse.core.runtime.Assert;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.OperationCanceledException;
@@ -41,30 +40,40 @@ import org.omnetpp.scave.pychart.PythonProcessPool;
  * @author andras
  */
 public class ChartExport {
-
-    protected static class ChartExportJob extends Job {
-        private Chart chart;
-        private Map<String, String> extraProperties;
-        private File chartsDir;
-        private ResultFileManager manager;
-
-        public ChartExportJob(Chart chart, Map<String, String> extraProperties, File chartsDir, ResultFileManager manager) {
-            super("Exporting chart '" + chart.getName() + "'");
-            this.chart = chart;
+    private static class Context {
+        public Context(Map<String, String> extraProperties, File chartsDir, ResultFileManager manager, boolean stopOnError, int numConcurrentProcesses) {
             this.extraProperties = extraProperties;
             this.chartsDir = chartsDir;
             this.manager = manager;
+            this.stopOnError = stopOnError;
+            this.numConcurrentProcesses = numConcurrentProcesses;
+        }
+        final Map<String, String> extraProperties;
+        final File chartsDir;
+        final ResultFileManager manager;
+        final boolean stopOnError;
+        final int numConcurrentProcesses;
+    }
+
+    protected static class ChartExportJob extends Job {
+        private Chart chart;
+        private Context context;
+
+        public ChartExportJob(Chart chart, Context context) {
+            super("Exporting chart '" + chart.getName() + "'");
+            this.chart = chart;
+            this.context = context;
         }
 
         @Override
         protected IStatus run(final IProgressMonitor monitor) {
             try {
-                ResultFileManager.callWithReadLock(manager, new Callable<Object>() {
+                ResultFileManager.callWithReadLock(context.manager, new Callable<Object>() {
                     @Override
                     public Object call() throws Exception {
                         PythonProcessPool processPool = new PythonProcessPool(1);
                         processPool.setShouldSetOmnetppMplBackend(false);
-                        runChartScript(chart, extraProperties, processPool, chartsDir, manager, monitor);
+                        runChartScript(chart, processPool, context, monitor);
                         return null;
                     }
                 });
@@ -81,32 +90,24 @@ public class ChartExport {
 
     protected static class BatchChartExportJob extends Job {
         private List<Chart> charts;
-        private Map<String, String> extraProperties;
-        private File chartsDir;
-        private ResultFileManager manager;
-        private boolean stopOnError = false;
-        private int numConcurrentProcesses = 2;
+        private Context context;
 
-        public BatchChartExportJob(List<Chart> charts, Map<String, String> extraProperties, File chartsDir, ResultFileManager manager, boolean stopOnError, int numConcurrentProcesses) {
+        public BatchChartExportJob(List<Chart> charts, Context context) {
             super("Exporting " + charts.size() + " charts");
             this.charts = charts;
-            this.extraProperties = extraProperties;
-            this.chartsDir = chartsDir;
-            this.manager = manager;
-            this.stopOnError = stopOnError;
-            this.numConcurrentProcesses = numConcurrentProcesses;
+            this.context = context;
         }
 
         @Override
         protected IStatus run(IProgressMonitor monitor) {
-            JobGroup jobGroup = new JobGroup("Exporting charts", numConcurrentProcesses, charts.size()) {
+            JobGroup jobGroup = new JobGroup("Exporting charts", context.numConcurrentProcesses, charts.size()) {
                 @Override
                 protected boolean shouldCancel(IStatus lastCompletedJobResult, int numberOfFailedJobs, int numberOfCanceledJobs) {
-                    return numberOfCanceledJobs > 0 || (stopOnError && numberOfFailedJobs > 0);
+                    return numberOfCanceledJobs > 0 || (context.stopOnError && numberOfFailedJobs > 0);
                 }
             };
             for (Chart chart : charts) {
-                Job job1 = new ChartExportJob(chart, extraProperties, chartsDir, manager);
+                Job job1 = new ChartExportJob(chart, context);
                 job1.setJobGroup(jobGroup);
                 job1.setPriority(Job.BUILD);
                 job1.setSystem(false);
@@ -125,18 +126,18 @@ public class ChartExport {
 
     };
 
-    protected static void startExportJob(Chart chart, Map<String, String> extraProperties, File chartsDir, ResultFileManager manager) {
+    protected static void startExportJob(Chart chart, Context context) {
         chart = (Chart)chart.dup(); // since job runs in another thread, and we don't want locking
-        Job job = new ChartExportJob(chart, extraProperties, chartsDir, manager);
+        Job job = new ChartExportJob(chart, context);
         job.setPriority(Job.BUILD);
         job.setSystem(false);
         job.setUser(true);
         job.schedule();
     }
 
-    protected static void startBatchExportJob(List<Chart> charts, Map<String, String> extraProperties, File chartsDir, ResultFileManager manager, boolean stopOnError, int numConcurrentProcesses) {
+    protected static void startBatchExportJob(List<Chart> charts, Context context) {
         charts = CollectionUtils.getDeepCopyOf(charts); // since job runs in another thread, and we don't want locking
-        Job job = new BatchChartExportJob(charts, extraProperties, chartsDir, manager, stopOnError, numConcurrentProcesses);
+        Job job = new BatchChartExportJob(charts, context);
         job.setPriority(Job.BUILD);
         job.setSystem(false);
         job.setUser(true);
@@ -153,20 +154,22 @@ public class ChartExport {
 
     public static void exportChartImage(Chart chart, IContainer targetFolder, String format, File chartsDir, ResultFileManager manager) {
         Map<String, String> extraProperties = makeExtraPropsForImageExport(targetFolder, format);
-        startExportJob(chart, extraProperties, chartsDir, manager);
+        Context context = new Context(extraProperties, chartsDir, manager, true, 2);
+        startExportJob(chart, context);
         //TODO folder.refreshLocal(IResource.DEPTH_INFINITE, monitor); // because we're creating the file behind Eclipse's back
     }
 
     public static void exportChartImages(List<Chart> charts, IContainer targetFolder, String format, File chartsDir, ResultFileManager manager, boolean stopOnError, int numConcurrentProcesses) {
         Map<String, String> extraProperties = makeExtraPropsForImageExport(targetFolder, format);
+        Context context = new Context(extraProperties, chartsDir, manager, stopOnError, numConcurrentProcesses);
         if (charts.size() == 1)
-            startExportJob(charts.get(0), extraProperties, chartsDir, manager);
+            startExportJob(charts.get(0), context);
         else if (charts.size() >= 2)
-            startBatchExportJob(charts, extraProperties, chartsDir, manager, stopOnError, numConcurrentProcesses);
+            startBatchExportJob(charts, context);
         //TODO folder.refreshLocal(IResource.DEPTH_INFINITE, monitor); // because we're creating the file behind Eclipse's back
     }
 
-    private static void runChartScript(Chart chart, Map<String,String> extraProperties, PythonProcessPool processPool, File chartsDir, ResultFileManager rfm, IProgressMonitor monitor) {
+    private static void runChartScript(Chart chart, PythonProcessPool processPool, Context context, IProgressMonitor monitor) {
         IOConsole console = new IOConsole("'" + chart.getName() + "' - chart script output", null);
         IConsoleManager consoleManager = ConsolePlugin.getDefault().getConsoleManager();
         consoleManager.addConsoles(new IConsole[] { console });
@@ -191,8 +194,8 @@ public class ChartExport {
         proc.outputMonitoringThread.addOutputListener(outputListener);
         proc.errorMonitoringThread.addOutputListener(outputListener);
 
-        proc.getEntryPoint().setResultsProvider(new ResultsProvider(rfm, proc.getInterruptedFlag()));
-        proc.getEntryPoint().setChartProvider(new ChartProvider(chart, extraProperties));
+        proc.getEntryPoint().setResultsProvider(new ResultsProvider(context.manager, proc.getInterruptedFlag()));
+        proc.getEntryPoint().setChartProvider(new ChartProvider(chart, context.extraProperties));
 
         final boolean[] executionDone = new boolean[] { false };
         Runnable runAfterDone = () -> {
@@ -210,8 +213,8 @@ public class ChartExport {
 
         proc.pythonCallerThread.asyncExec(() -> {
             // Ensure the chart script can load source files and Python modules from the anf file's directory
-            proc.getEntryPoint().execute("import os; os.chdir(r\"\"\"" + chartsDir.getAbsolutePath() + "\"\"\"); del os;");
-            proc.getEntryPoint().execute("import site; site.addsitedir(r\"\"\"" + chartsDir.getAbsolutePath() + "\"\"\"); del site;");
+            proc.getEntryPoint().execute("import os; os.chdir(r\"\"\"" + context.chartsDir.getAbsolutePath() + "\"\"\"); del os;");
+            proc.getEntryPoint().execute("import site; site.addsitedir(r\"\"\"" + context.chartsDir.getAbsolutePath() + "\"\"\"); del site;");
             proc.getEntryPoint().execute(chart.getScript());
         }, runAfterDone, runAfterError);
 
@@ -227,7 +230,6 @@ public class ChartExport {
 
         proc.kill();
         processPool.dispose();
-
     }
 
 
