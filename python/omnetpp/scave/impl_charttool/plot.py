afa0a209c422bd82cdb1703e50296f77a4ec129b
TÖRÖK Attila <torokati44@gmail.com>
2020-03-03 15:30:49 +0100

    ide/scave: Remove the old and incomplete plotting API in plot.py, keep only the MPL-ish API.

--- python/omnetpp/scave/impl_charttool/plot.py
+++ python/omnetpp/scave/impl_charttool/plot.py
@@ -1,163 +1,29 @@
 import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
 import math
-from omnetpp.scave import results, plot
 from omnetpp.scave import utils # for small misc. methods
-
-properties = dict()
-name = ""
 
 
 def set_properties(*vargs, **kwargs):
-    for a in vargs:
-        properties.update(a)
-    properties.update(kwargs)
-
+    pass
 
 def set_property(key, value):
-    properties[key] = value
+    pass
 
-
-def _to_label(x):
-    if isinstance(x, str):
-        return x
-    elif isinstance(x, tuple):
-        return ", ".join(list(x))
-    elif isinstance(x, list):
-        return ", ".join(x)
-    else:
-        return str(x)
-
-
-def _plot_scalars_lists(row_label, labels, values):
-    plt.bar(np.arange(len(values)), values, tick_label=labels, label=row_label)
-    plt.legend()
-    plt.grid()
-
-
-def _plot_scalars_DF_simple(df):
-    column_labels = [_to_label(c) for c in list(df.columns)]
-
-    N = len(df.index)
-    M = 0
-    i = 0
-    for index, row in df.iterrows():
-        v = row.values
-        M = len(v)
-        plt.bar(np.arange(M) + i * 0.8 / N, v, width=0.8 / N, tick_label=column_labels, label=_to_label(index))
-        i += 1
-
-    if N and M:
-        plt.xticks(np.arange(M) + 0.8 / N * ((N - 1) / 2))
-
-    plt.grid()
-    plt.legend()
-
-
-def _plot_scalars_DF_scave(df):
-    df.plot.bar(y="value")
-    plt.legend()
-
-
-def plot_scalars(df_or_values, labels=None, row_label=None):
-    if isinstance(df_or_values, pd.DataFrame):
-        df = df_or_values
-        if "value" in df.columns and "module" in df.columns and "name" in df.columns:
-            _plot_scalars_DF_scave(df)
-        else:
-            _plot_scalars_DF_simple(df)
-    else:
-        _plot_scalars_lists(row_label, labels, df_or_values)
-
-
-def plot_vector(label, xs, ys, key=None, props=dict()):
-    plt.plot(xs, ys, label=_to_label(label))
-    plt.legend()
-
-    # TODO: deal with key and props
-
-
-def _plot_vectors_tuplelist(vectors):
-    for v in vectors:
-        plt.plot(v[1], v[2], label=_to_label(v[0]))
-    plt.legend()
-
-
-def _plot_vectors_DF_simple(df):
-    xs = None
-    if "time" in df:
-        xs = list(df["time"])
-    else:
-        xs = list(range(len(df[df.columns[0]])))
-
-    for column in df:
-        if column != "time":
-            plt.plot(xs, list(df[column]), label=_to_label(column))
-    plt.legend()
-
-
-def _plot_vectors_DF_scave(df):
-    for row in df.itertuples(index=False):
-        plt.plot(list(row.vectime), list(row.vecvalue), label=row.module + ':' + row.name)
-    plt.legend()
-
-
-def plot_vectors(df_or_list):
-    if isinstance(df_or_list, pd.DataFrame):
-        df = df_or_list
-        if "vectime" in df.columns and "vecvalue" in df.columns and "module" in df.columns and "name" in df.columns:
-            _plot_vectors_DF_scave(df)
-        else:
-            _plot_vectors_DF_simple(df)
-    else:
-        _plot_vectors_tuplelist(df_or_list)
-
-
-def plot_histogram(label, binedges, binvalues, underflows=0.0, overflows=0.0, minvalue=math.nan, maxvalue=math.nan):
-    plt.hist(bins=edges, x=edges[:-1], weights=values, label=label)
-    plt.legend()
-
-
-def _plot_histograms_DF_scave(df):
-    for row in df.itertuples(index=False):
-        edges = list(row.binedges)
-        values = list(row.binvalues)
-
-        plt.hist(bins=edges, x=edges[:-1], weights=values, label=row.module + ":" + row.name)
-    plt.legend()
-
-
-def _plot_histograms_DF(df):
-    for row in df.itertuples(index=False):
-        if row[1] == "histogram":
-
-            edges = list(row[12])
-            values = list(row[13])
-
-            plt.hist(bins=edges, x=edges[:-1], weights=values, label=row[2] + ":" + row[3])
-    plt.legend()
-
-
-def plot_histograms(df):
-    if "binedges" in df and "binvalues" in df and "module" in df and "name" in df:
-        _plot_histograms_DF_scave(df)
-    else:
-        _plot_histograms_DF(df)
 
 def get_supported_property_keys():
     return [] # TODO
+
 
 def set_warning(warning):
     # TODO this might not always work, depending on whether it's called before or after plotting?
     plt.annotate(warning, xy=(20, 40), xycoords="figure pixels", color="red")
 
-# params?
+
 def plot(xs, ys, key=None, label=None, drawstyle=None, linestyle=None, linewidth=None, color=None, marker=None, markersize=None):
     params = {k:v for k, v in locals().items() if k is not None and k not in ["key", "xs", "ys"]}
     return plt.plot(xs, ys, **params)
 
-# key?
+
 def hist(x, bins, density=False, weights=None, cumulative=False, bottom=None, histtype='stepfilled', color=None, label=None, linewidth=None,
          underflows=0.0, overflows=0.0, minvalue=math.nan, maxvalue=math.nan):
 
--- ui/org.omnetpp.scave.pychart/python/omnetpp/scave/impl_ide/plot.py
+++ ui/org.omnetpp.scave.pychart/python/omnetpp/scave/impl_ide/plot.py
@@ -12,31 +12,24 @@ if platform.system() in ['Linux', 'Darwin']:
 import mmap
 
 import functools
-from math import inf
 print = functools.partial(print, flush=True)
 
-from omnetpp.scave import results, chart, plot
+from omnetpp.scave import results
+from omnetpp.scave.utils import *
 from omnetpp.internal import Gateway
 
-from omnetpp.scave.utils import *
-
+import py4j
 from py4j.java_collections import MapConverter
 
-vector_data_counter = 0
+_vector_data_counter = 0
 
 def _assert_is_native_chart():
     if not Gateway.chart_plotter:
         raise RuntimeError("This method can only be used on native charts")
 
-def _list_to_bytes(l):
-    """
-    Internal. Encodes/serializes a list or `np.array` of numbers as
-    8-byte floats (doubles) into an `np.array` of bytes.
-    """
-    return np.array(np.array(l), dtype=np.dtype('>f8')).tobytes()
-
 
 def plot(xs, ys, key=None, label=None, drawstyle=None, linestyle=None, linewidth=None, color=None, marker=None, markersize=None):
+    _assert_is_native_chart()
     props = {}
     if drawstyle:
         props["Line.DrawStyle"] = _translate_drawstyle(drawstyle)
@@ -51,31 +44,18 @@ def plot(xs, ys, key=None, label=None, drawstyle=None, linestyle=None, linewidth
     if markersize:
         props["Symbols.Size"] = str(markersize)
 
-    return plot_vector(label, xs, ys, key, props)
-
-# minvalue, maxvalue, underflows and overflows are NOT accepted by mpl.pyplot.hist
-def hist(x, bins, density=False, weights=None, cumulative=False, bottom=None, histtype='stepfilled', color=None, label=None, linewidth=None,
-         underflows=0.0, overflows=0.0, minvalue=math.nan, maxvalue=math.nan):
-    props = {}
-
-    props["Hist.Cumulative"] = str(cumulative)
-    props["Hist.Density"] = str(density)
-
-    props["Hist.Color"] = _translate_color(color)
-    props["Hist.Bar"] = _translate_histtype(histtype)
-
-    props["Bars.Baseline"] = str(bottom)
-
-    # check if we really got a precomputed histogram, using the trick documented for pyplot.hist
-    if not np.array_equal(x, bins[:-1]):
-        raise ValueError("Histogram computation is not performed.")
-
-    plot_histogram(label, bins, weights, underflows=underflows, overflows=overflows, minvalue=minvalue, maxvalue=maxvalue, props=props)
+    plot_lines(pd.DataFrame({
+        "key": [key],
+        "label": [label],
+        "xs": [np.array(xs)],
+        "ys": [np.array(ys)]
+        }
+    ), props)
 
 
 def bar(x, height, width=0.8, key=None, label=None, color=None, edgecolor=None):
-    props = {}
     _assert_is_native_chart()
+    props = {}
 
     if color:
         props["Bar.Color"] = _translate_color(color)
@@ -88,19 +68,36 @@ def bar(x, height, width=0.8, key=None, label=None, color=None, edgecolor=None):
         }
     ), props)
 
-def plot_bars(df, props=dict()):
-    # TODO: add check for one-layer indices? numbers-only data?
+
+# minvalue, maxvalue, underflows and overflows are NOT accepted by mpl.pyplot.hist
+def hist(x, bins, density=False, weights=None, cumulative=False, bottom=None, histtype='stepfilled', color=None, key=None, label=None, linewidth=None,
+         underflows=0.0, overflows=0.0, minvalue=math.nan, maxvalue=math.nan):
     _assert_is_native_chart()
 
-    Gateway.chart_plotter.plotScalars(pl.dumps([
-        {
-            "key": row.key,
-            "title": row.label,
-            "values": _list_to_bytes(row.values),
-        }
-        for row in df.itertuples(index=False)
-    ]), props)
+    # check if we really got a precomputed histogram, using the trick documented for pyplot.hist
+    if not np.array_equal(x, bins[:-1]):
+        raise ValueError("Histogram computation is not performed.")
 
+    props = {}
+
+    props["Hist.Cumulative"] = str(cumulative)
+    props["Hist.Density"] = str(density)
+
+    props["Hist.Color"] = _translate_color(color)
+    props["Hist.Bar"] = _translate_histtype(histtype)
+
+    props["Bars.Baseline"] = str(bottom)
+
+    plot_histograms(pd.DataFrame({
+        "key": [key],
+        "label": [label],
+        "binedges": [np.array(bins)],
+        "binvalues": [np.array(weights)],
+        "underflows": [underflows],
+        "overflows": [overflows],
+        "min": [float(np.min(binedges)) if math.isnan(minvalue) else minvalue],
+        "max": [float(np.max(binedges)) if math.isnan(maxvalue) else maxvalue]
+    }), props)
 
 
 def plot_lines(df, props = dict()):  # key, label, xs, ys
@@ -128,10 +125,25 @@ def plot_lines(df, props = dict()):  # key, label, xs, ys
         o.unlink()
 
 
-def plot_histogram_data(df, props=dict()):  # key, label, binedges, binvalues, underflows, overflows, min, max
+def plot_bars(df, props=dict()):
+    # TODO: add check for one-layer indices? numbers-only data?
     _assert_is_native_chart()
+
+    Gateway.chart_plotter.plotScalars(pl.dumps([
+        {
+            "key": row.key,
+            "title": row.label,
+            "values": _list_to_bytes(row.values),
+        }
+        for row in df.itertuples(index=False)
+    ]), props)
+
+
+def plot_histograms(df, props=dict()):  # key, label, binedges, binvalues, underflows, overflows, min, max
+    _assert_is_native_chart()
+
     if sorted(list(df.columns)) != sorted(["key", "label", "binedges", "binvalues", "underflows", "overflows", "min", "max"]):
-        raise RuntimeError("Invalid DataFrame format in plot_histogram_data")
+        raise RuntimeError("Invalid DataFrame format in plot_histogram")
 
     Gateway.chart_plotter.plotHistograms(pl.dumps([
         {
@@ -154,6 +166,14 @@ def plot_histogram_data(df, props=dict()):  # key, label, binedges, binvalues, u
     ]), MapConverter().convert(props, Gateway.gateway._gateway_client))
 
 
+def _list_to_bytes(l):
+    """
+    Internal. Encodes/serializes a list or `np.array` of numbers as
+    8-byte floats (doubles) into an `np.array` of bytes.
+    """
+    return np.array(np.array(l), dtype=np.dtype('>f8')).tobytes()
+
+
 def _to_label(x):
     """
     Internal. Turns various types of objects into a string, so they can be used as labels.
@@ -170,55 +190,6 @@ def _to_label(x):
         return str(x)
 
 
-def _plot_scalars_lists(row_label, labels, values):
-    if labels and len(labels) != len(values):
-        raise RuntimeError("Not the same number of labels as values!")
-    rows = [row_label if row_label else ""],
-    columns = labels if labels else [""] * len(values),
-
-    df = pd.DataFrame({row_label : values}, index=labels).transpose()
-
-    plot_bars(df)
-
-
-def _plot_scalars_DF_simple(df):
-    print("plot DF simple not yet done")
-    pass
-    #plot_bars(df)
-    # TODO: detect single-valued index/ column header levels, drop them
-
-
-def _plot_scalars_DF_scave(df):
-    # TODO: pivot in the other direction (transposed)?
-    # TODO: this is... wrong?
-    
-    title_col, legend_cols = extract_label_columns(df)
-
-    df2 = pd.DataFrame.from_records([(
-            str(i),
-            make_legend_label(legend_cols, row),
-            row.vectime,
-            row.vecvalue
-        )
-        for i, row in enumerate(df.itertuples(index=False))
-        if row.vectime is not None and row.vecvalue is not None
-    ], columns=["key", "label", "values"])
-
-    plot_bars(df2)
-
-# This method only does dynamic dispatching based on its first argument.
-def plot_scalars(df_or_values, labels=None, row_label=None):
-    _assert_is_native_chart()
-    if isinstance(df_or_values, pd.DataFrame):
-        df = df_or_values
-        if "value" in df.columns and "module" in df.columns and "name" in df.columns:
-            _plot_scalars_DF_scave(df)
-        else:
-            _plot_scalars_DF_simple(df)
-    else:
-        _plot_scalars_lists(row_label, labels, df_or_values)
-
-
 def _put_array_in_shm(arr, shm_objs, mmap_objs):
     """
     Internal. Turns a `np.array` into a byte sequence, and writes it into
@@ -229,10 +200,10 @@ def _put_array_in_shm(arr, shm_objs, mmap_objs):
     if arr.size == 0:
         return "<EMPTY> 0"
 
-    global vector_data_counter
+    global _vector_data_counter
     # they only need to start with a / on POSIX, but Windows doesn't seem to mind, so just putting it there always...
-    name = "/vecdata-" + str(os.getpid()) + "-" + str(vector_data_counter)
-    vector_data_counter += 1
+    name = "/vecdata-" + str(os.getpid()) + "-" + str(_vector_data_counter)
+    _vector_data_counter += 1
 
     system = platform.system()
 
@@ -273,6 +244,7 @@ def _translate_drawstyle(drawstyle):
         raise ValueError("Unrecognized drawstyle '{}'".format(drawstyle))
     return mapping[drawstyle]
 
+
 def _translate_linestyle(linestyle):
     if linestyle in ["none", "solid", "dashed", "dashdot", "dotted"]:
         return linestyle
@@ -286,6 +258,7 @@ def _translate_linestyle(linestyle):
     if linestyle not in mapping:
         raise ValueError("Unrecognized linestyle '{}'".format(linestyle))
     return mapping[linestyle]
+
 
 def _translate_color(color):
     if color and len(color) >= 2 and color[:1] == "C" and color[1:].isdigit():
@@ -302,6 +275,7 @@ def _translate_color(color):
         "w" : "white"
     }
     return mapping[color] if color in mapping else color
+
 
 def _translate_marker(marker):
     mapping = {
@@ -345,192 +319,30 @@ def _translate_histtype(histtype):
     return mapping[histtype]
 
 
-def plot_vector(label, xs, ys, key=None, props = dict()):
-    _assert_is_native_chart()
-    plot_lines(pd.DataFrame({
-        "key": [key],
-        "label": [label],
-        "xs": [np.array(xs)],
-        "ys": [np.array(ys)]
-        }
-    ), props)
-
-
-def _plot_vectors_tuplelist(vectors):
-    df = pd.DataFrame.from_records(vectors, columns=["label", "xs", "ys"])
-    df["key"] = [str(i) for i in range(len(vectors))]
-    df["xs"] = df["xs"].map(np.array)
-    df["ys"] = df["ys"].map(np.array)
-    plot_lines(df)
-
-
-def _plot_vectors_DF_simple(df):
-    xcolumn = None
-    for c in ["x", "xs", "time"]:
-        if c in df:
-            xcolumn = c
-            break
-    xs = df[xcolumn] if xcolumn else np.array(range(len(df.index)))
-
-    df2 = pd.DataFrame.from_records([(
-            str(i),
-            str(column),
-            np.array(xs[~np.isnan(df[column])]),
-            np.array(df[column].values[~np.isnan(df[column])])
-        )
-        for i, column in enumerate(df) if column != xcolumn
-    ], columns=["key", "label", "xs", "ys"])
-
-    plot_lines(df2)
-
-
-def _plot_vectors_DF_scave(df):
-    title_col, legend_cols = extract_label_columns(df)
-
-    df2 = pd.DataFrame.from_records([(
-            str(i),
-            make_legend_label(legend_cols, row),
-            row.vectime,
-            row.vecvalue
-        )
-        for i, row in enumerate(df.itertuples(index=False))
-        if row.vectime is not None and row.vecvalue is not None
-    ], columns=["key", "label", "xs", "ys"])
-
-    plot_lines(df2)
-
-    properties = dict()
-    for i, t in enumerate(df.itertuples(index=False)):
-        key = str(i)
-        interpolation = t.interpolationmode if 'interpolationmode' in df else 'sample-hold' if 'enum' in df else None
-        if interpolation:
-            if interpolation == "none":
-                properties["Line.Type/" + key] = "Dots"
-            elif interpolation == "linear":
-                properties["Line.Type/" + key] = "Linear"
-            elif interpolation == "sample-hold":
-                properties["Line.Type/" + key] = "Sample-Hold"
-            elif interpolation == "backward-sample-hold":
-                properties["Line.Type/" + key] = "Backward Sample-Hold"
-
-    set_properties(properties)
-
-
-def plot_vectors(df_or_list):
-    _assert_is_native_chart()
-    if isinstance(df_or_list, pd.DataFrame):
-        df = df_or_list
-        if "vectime" in df.columns and "vecvalue" in df.columns:
-            _plot_vectors_DF_scave(df)
-        else:
-            _plot_vectors_DF_simple(df)
-    else:
-        _plot_vectors_tuplelist(df_or_list)
-
-
-def plot_scatter(df, xdata, iso_column=None):
-    _assert_is_native_chart()
-    # names = set(df["name"].values)
-    # names.discard(None)
-    # names.discard(xdata)
-    # names.discard(iso_column)
-
-    # TODO: compute mean if results are vectors?
-    # TODO: add option to sort by a third column, instead of the X axis
-
-    df = pd.pivot_table(df, index=xdata, columns=iso_column, values="value")
-
-    renaming = dict()
-
-    if iso_column:
-        for c in list(df.columns):
-            renaming[c] = str(iso_column) + "=" + str(c)
-
-    df = df.reset_index()
-
-    df.dropna(inplace=True)
-
-    df[xdata] = pd.to_numeric(df[xdata])
-
-    df.sort_values(by=xdata, inplace=True)
-    if df.columns.nlevels > 1:
-        df.columns = [' '.join(col).strip() for col in df.columns.values]
-
-    # todo only return df
-    """
-    # only used on posix, to unlink them later
-    shm_objs = list()
-    # only used on windows, to prevent gc
-    mmap_objs = list()
-
-    Gateway.chart_plotter.plotVectors(pl.dumps([
-        {
-            "title": column,
-            "key": column,
-            "xs": _put_array_in_shm(np.array(df[xdata]), shm_objs, mmap_objs),
-            "ys": _put_array_in_shm(np.array(df[column]), shm_objs, mmap_objs)
-        }
-        for column in df if column != xdata
-    ]))
-
-    # this is a no-op on Windows
-    for o in shm_objs:
-        o.unlink()
-
-    # TODO: set_property('Y.Axis.Title', ', '.join(names))
-    set_property('X.Axis.Title', xdata)
-    set_property('Plot.Title', get_name())
-    """
-
-
-def plot_histogram(label, binedges, binvalues, underflows=0.0, overflows=0.0, minvalue=math.nan, maxvalue=math.nan, key=None, props=dict()):
-    _assert_is_native_chart()
-    plot_histogram_data(pd.DataFrame({
-        "key": [key],
-        "label": [label],
-        "binedges": [np.array(binedges)],
-        "binvalues": [np.array(binvalues)],
-        "underflows": [underflows],
-        "overflows": [overflows],
-        "min": [float(np.min(binedges)) if math.isnan(minvalue) else minvalue],
-        "max": [float(np.max(binedges)) if math.isnan(maxvalue) else maxvalue]
-    }), props)
-
-
-def _plot_histograms_DF_scave(df):
-
-    title_col, legend_cols = extract_label_columns(df)
-
-    plot_histogram_data(pd.DataFrame.from_records([(
-            str(i),
-            make_legend_label(legend_cols, row),
-            np.array(row.binedges),
-            np.array(row.binvalues),
-            row.underflows,
-            row.overflows,
-            float(min(row.binedges)) if row.min == math.nan else row.min,
-            float(max(row.binedges)) if row.max == math.nan else row.max
-        )
-        for i, row in enumerate(df.itertuples(index=False))
-    ], columns=["key", "label", "binedges", "binvalues", "underflows", "overflows", "min", "max"]))
-
-
-def plot_histograms(df):
-    _assert_is_native_chart()
-    if "binedges" in df.columns and "binvalues" in df.columns:
-        _plot_histograms_DF_scave(df)
-    else:
-        pass  # TODO - add other formats as well?
-
-
 def set_property(key, value):
     _assert_is_native_chart()
-    Gateway.chart_plotter.setProperty(key, value)
+    msg = None
+    try:
+        Gateway.chart_plotter.setProperty(key, value)
+    except py4j.protocol.Py4JJavaError as e:
+        msg = e.java_exception.getMessage()
+
+    if msg is not None:
+        raise RuntimeError(msg)
+
 
 
 def set_properties(props):
     _assert_is_native_chart()
-    Gateway.chart_plotter.setProperties(props)
+    msg = None
+    try:
+        Gateway.chart_plotter.setProperties(dict(props))
+    except py4j.protocol.Py4JJavaError as e:
+        msg = e.java_exception.getMessage()
+
+
+    if msg is not None:
+        raise RuntimeError(msg)
 
 
 def get_supported_property_keys():
@@ -539,6 +351,7 @@ def get_supported_property_keys():
 
 
 def set_warning(warning):
+    _assert_is_native_chart()
     # TODO maybe we need a common interface?
     if Gateway.widget_provider:
         Gateway.widget_provider.setWarning(warning)
@@ -601,7 +414,6 @@ def yscale(value):
 def xticks(ticks=None, labels=None, rotation=0):
     Gateway.chart_plotter.setGroupTitles(labels)
     set_property("X.Label.RotateBy", str(-int(rotation))) # matplotlib and our native widgets have different ideas about label rotation
-
 
 def grid(b=True, which="major"):
     if which not in ["major", "both"]:
