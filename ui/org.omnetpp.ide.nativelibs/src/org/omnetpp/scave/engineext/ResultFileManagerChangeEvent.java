93d8ddd21b80194ea479100d3bd390ac524dca83
Andras Varga <andras@omnetpp.org>
2020-03-12 14:08:36 +0100

    ide/scave: delete remains of "computed scalars" from old Scave

--- ui/org.omnetpp.ide.nativelibs/src/org/omnetpp/scave/engineext/ResultFileManagerChangeEvent.java
+++ ui/org.omnetpp.ide.nativelibs/src/org/omnetpp/scave/engineext/ResultFileManagerChangeEvent.java
@@ -8,40 +8,18 @@ public class ResultFileManagerChangeEvent {
     public enum ChangeType {
         LOAD,
         UNLOAD,
-        COMPUTED_ITEM_ADDED,
-        COMPUTED_SCALARS_CLEARED,
     }
 
     private ResultFileManager manager;
     private ChangeType change;
     private String filename;
-    private long computedItemID;
 
     public ResultFileManagerChangeEvent(ResultFileManager manager, ChangeType change, String filename)
     {
         Assert.isLegal(manager != null);
-        Assert.isLegal(change == ChangeType.LOAD || change == ChangeType.UNLOAD);
-
         this.manager = manager;
         this.change = change;
         this.filename = filename;
-    }
-
-    public ResultFileManagerChangeEvent(ResultFileManager manager, ChangeType change, long computedItemID)
-    {
-        Assert.isLegal(manager != null);
-        Assert.isLegal(change == ChangeType.COMPUTED_ITEM_ADDED);
-        this.manager = manager;
-        this.change = change;
-        this.computedItemID = computedItemID;
-    }
-
-    public ResultFileManagerChangeEvent(ResultFileManager manager, ChangeType change)
-    {
-        Assert.isLegal(manager != null);
-        Assert.isLegal(change == ChangeType.COMPUTED_SCALARS_CLEARED);
-        this.manager = manager;
-        this.change = change;
     }
 
     public ResultFileManager getResultFileManager() {
@@ -54,9 +32,5 @@ public class ResultFileManagerChangeEvent {
 
     public String getFilename() {
         return filename;
-    }
-
-    public long getComputedItemID() {
-        return computedItemID;
     }
 }
