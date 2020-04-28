f664b1ed02ff5ce1e2de8865db2f1593ede0253c
Andras Varga <andras@omnetpp.org>
2020-04-28 18:35:21 +0200

    ide/scave: TimeTriggeredProgressMonitorDialog2: added InterruptedFlag support
    At the cost of significant ugliness.

--- ui/org.omnetpp.common.core/src/org/omnetpp/common/ui/TimeTriggeredProgressMonitorDialog2.java
+++ ui/org.omnetpp.common.core/src/org/omnetpp/common/ui/TimeTriggeredProgressMonitorDialog2.java
@@ -10,6 +10,7 @@ package org.omnetpp.common.ui;
 
 import java.lang.reflect.InvocationTargetException;
 
+import org.eclipse.core.runtime.Assert;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.dialogs.ProgressMonitorDialog;
@@ -18,6 +19,7 @@ import org.eclipse.swt.custom.BusyIndicator;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.omnetpp.common.CommonCorePlugin;
+import org.omnetpp.scave.engine.InterruptedFlag;
 
 /**
  * TimeTriggeredProgressMonitorDialog2 is a progress monitor dialog that only
@@ -37,6 +39,14 @@ import org.omnetpp.common.CommonCorePlugin;
  * In addition, the class provides utility methods (runWithDialog() and variants)
  * for convenience.
  *
+ * In addition to IProgressMonitor.isCanceled(), cancellation is also signaled via
+ * an InterruptedFlag instance. Use the following expression to access the relevant
+ * InterruptedFlag instance:
+ *
+ * <pre>
+ * TimeTriggeredProgressMonitorDialog2.getActiveInstance().getInterruptedFlag()
+ * </pre>
+ *
  * @author andras
  */
 public class TimeTriggeredProgressMonitorDialog2 extends ProgressMonitorDialog {
@@ -46,6 +56,16 @@ public class TimeTriggeredProgressMonitorDialog2 extends ProgressMonitorDialog {
      * The time considered to be the long operation time.
      */
     private int delayMillis;
+
+    /**
+     * This flag will also be set when the Cancel button is clicked.
+     */
+    private InterruptedFlag interruptedFlag = new InterruptedFlag();
+
+    /**
+     * The currently running instance
+     */
+    private static TimeTriggeredProgressMonitorDialog2 activeInstance;
 
     /**
      * Create a new instance of the receiver.
@@ -63,36 +83,61 @@ public class TimeTriggeredProgressMonitorDialog2 extends ProgressMonitorDialog {
     }
 
     @Override
+    protected void cancelPressed() {
+        super.cancelPressed();
+        interruptedFlag.setFlag(true);
+    }
+
+    /**
+     * Allows the runnable to access the InterruptedFlag of the active dialog.
+     */
+    public static TimeTriggeredProgressMonitorDialog2 getActiveInstance() {
+        return activeInstance;
+    }
+
+    public InterruptedFlag getInterruptedFlag() {
+        return interruptedFlag;
+    }
+
+    @Override
     public void run(boolean fork, final boolean cancelable, final IRunnableWithProgress runnable) throws InvocationTargetException, InterruptedException {
+        Assert.isTrue(activeInstance == null); // no nesting
+        try {
+            activeInstance = this;
 
-        Display display = Display.getCurrent();
-        if (display == null) {
-            runnable.run(new NullProgressMonitor());
-            return;
-        }
-
-        Runnable openDialogRunnable = () -> open();
-        display.timerExec(delayMillis, openDialogRunnable);
-
-        InvocationTargetException[] invokes = new InvocationTargetException[1];
-        InterruptedException[] interrupt = new InterruptedException[1];
-        BusyIndicator.showWhile(display, () -> {
-            try {
-                TimeTriggeredProgressMonitorDialog2.super.run(fork, cancelable, runnable);
-            } catch (InvocationTargetException e) {
-                invokes[0] = e;
-            } catch (InterruptedException e) {
-                interrupt[0]= e;
+            Display display = Display.getCurrent();
+            if (display == null) {
+                runnable.run(new NullProgressMonitor());
+                return;
             }
-        });
 
-        if (!display.isDisposed())
-            display.timerExec(-1, openDialogRunnable);
+            Runnable openDialogRunnable = () -> open();
+            display.timerExec(delayMillis, openDialogRunnable);
 
-        if (invokes[0] != null)
-            throw invokes[0];
-        if (interrupt[0] != null)
-            throw interrupt[0];
+            InvocationTargetException[] invokes = new InvocationTargetException[1];
+            InterruptedException[] interrupt = new InterruptedException[1];
+            BusyIndicator.showWhile(display, () -> {
+                try {
+                    TimeTriggeredProgressMonitorDialog2.super.run(fork, cancelable, runnable);
+                } catch (InvocationTargetException e) {
+                    invokes[0] = e;
+                } catch (InterruptedException e) {
+                    interrupt[0]= e;
+                }
+            });
+
+            if (!display.isDisposed())
+                display.timerExec(-1, openDialogRunnable);
+
+            if (invokes[0] != null)
+                throw invokes[0];
+            if (interrupt[0] != null)
+                throw interrupt[0];
+
+        }
+        finally {
+            activeInstance = null;
+        }
      }
 
     /**
