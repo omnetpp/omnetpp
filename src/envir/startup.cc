f6281791ffb9b07c7e8ce7a0451528165be3594d
Rudolf Hornig <rudi@omnetpp.org>
2020-05-05 14:55:12 +0200

    qtenv/ide: updated copyright date in banners and about dialog

--- src/envir/startup.cc
+++ src/envir/startup.cc
@@ -120,7 +120,7 @@ int setupUserInterface(int argc, char *argv[])
 
         verbose = !args.optionGiven('s');  // "not silent"
         if (verbose) {
-            std::cout << OMNETPP_PRODUCT " Discrete Event Simulation  (C) 1992-2018 Andras Varga, OpenSim Ltd." << endl;
+            std::cout << OMNETPP_PRODUCT " Discrete Event Simulation  (C) 1992-2020 Andras Varga, OpenSim Ltd." << endl;
             std::cout << "Version: " OMNETPP_VERSION_STR ", build: " OMNETPP_BUILDID ", edition: " OMNETPP_EDITION << endl;
             std::cout << "See the license for distribution terms and warranty disclaimer" << endl;
             std::cout << endl;
