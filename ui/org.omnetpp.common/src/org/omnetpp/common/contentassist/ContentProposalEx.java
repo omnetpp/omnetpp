247205e8d6ba5c21879ab22f87e31b91e7ceaa4e
Andras Varga <andras@omnetpp.org>
2020-05-19 17:26:10 +0200

    ide/common: ContentProposalEx: decorators are no longer in use, removed

--- ui/org.omnetpp.common/src/org/omnetpp/common/contentassist/ContentProposalEx.java
+++ ui/org.omnetpp.common/src/org/omnetpp/common/contentassist/ContentProposalEx.java
@@ -21,29 +21,12 @@ import org.omnetpp.common.engine.Common;
  * @author Andras
  */
 public class ContentProposalEx implements IContentProposalEx, Comparable<Object> {
-    // decorators
-    public static final int DEC_NONE       = 0x00;
-    public static final int DEC_OP         = 0x01;
-    public static final int DEC_CP         = 0x02;
-    public static final int DEC_QUOTE      = 0x04;
-    public static final int DEC_SP_BEFORE  = 0x08;
-    public static final int DEC_SP_AFTER   = 0x10;
-    public static final int DEC_MATCHES    = 0x20;
-
     private String content;
-
     private String label;
-
     private String description;
-
     private int cursorPosition; // position of the cursor relative to content after the proposal accepted
-
-    private int decorators; // additional characters added to the content
-
     private int startIndex; // start of the replaced range
-
     private int endIndex; // end of the replaced range
-
     private Image image;
 
     public ContentProposalEx(String content) {
@@ -72,13 +55,7 @@ public class ContentProposalEx implements IContentProposalEx, Comparable<Object>
     }
 
     public String getContent() {
-        StringBuffer result = new StringBuffer(((decorators & DEC_QUOTE) != 0 ? quoteStringIfNeeded(content) : content));
-        if ((decorators & DEC_SP_BEFORE) != 0) result.insert(0, ' ');
-        if ((decorators & DEC_OP) != 0) result.append('(');
-        if ((decorators & DEC_CP) != 0) result.append(") ");
-        if ((decorators & DEC_SP_AFTER) != 0) result.append(' ');
-        if ((decorators & DEC_MATCHES) != 0) result.append(" =~ ");
-        return result.toString();
+        return content;
     }
 
     public int getStartIndex() {
@@ -90,14 +67,7 @@ public class ContentProposalEx implements IContentProposalEx, Comparable<Object>
     }
 
     public int getCursorPosition() {
-        int position = cursorPosition;
-        if ((decorators & DEC_QUOTE) != 0) position += quoteStringIfNeeded(content).length() - content.length(); // put cursor after the ending '"'
-        if ((decorators & DEC_SP_BEFORE) != 0) position++;
-        if ((decorators & DEC_OP) != 0) position++; // put cursor after the '('
-        else if ((decorators & DEC_CP) != 0) position += 2; // put cursor after the ')' if no opening inserted
-        if ((decorators & DEC_SP_AFTER) != 0) position++;
-        if ((decorators & DEC_MATCHES) != 0) position += 4;
-        return position;
+        return cursorPosition;
     }
 
     public String getDescription() {
@@ -118,10 +88,6 @@ public class ContentProposalEx implements IContentProposalEx, Comparable<Object>
 
     public void setEndIndex(int index) {
         endIndex = index;
-    }
-
-    public void setDecorators(int decorators) {
-        this.decorators = decorators;
     }
 
     public boolean startsWith(String prefix) {
