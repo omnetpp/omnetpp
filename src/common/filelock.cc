2ebd5cf8d322990e69c346045f9b828db3183056
Levente Mészáros <levy@omnetpp.org>
2020-12-10 16:05:02 +0100

    common: Added a generic platform independent FileLock class.

---
+++ src/common/filelock.cc
@@ -0,0 +1,103 @@
+//=========================================================================
+//  FILELOCK.CC - part of
+//                  OMNeT++/OMNEST
+//           Discrete System Simulation in C++
+//
+//  Author: Levente Meszaros
+//
+//=========================================================================
+
+/*--------------------------------------------------------------*
+  Copyright (C) 2006-2008 OpenSim Ltd.
+
+  This file is distributed WITHOUT ANY WARRANTY. See the file
+  `license' for details on this and other legal matters.
+*--------------------------------------------------------------*/
+
+#include <stdio.h>
+#include <errno.h>
+#include <fcntl.h>
+#include "filelock.h"
+#include "exception.h"
+#include "commonutil.h"
+
+namespace omnetpp {
+namespace common {
+
+FileLock::FileLock(FILE *file, const char *fileName)
+{
+    Assert(file);
+    this->file = file;
+    this->fileName = fileName;
+    this->counter = 0;
+#ifdef _WIN32
+    this->handle = (HANDLE)_get_osfhandle(fileno(file));
+#endif
+}
+
+void FileLock::lock(FileLockType fileLockType)
+{
+    if (counter == 0) {
+#ifdef _WIN32
+        OVERLAPPED overlapped;
+        overlapped.hEvent = 0;
+        overlapped.Offset = overlapped.OffsetHigh = 0;
+        overlapped.Internal = overlapped.InternalHigh = 0;
+        if (LockFileEx(handle, fileLockType == FILE_LOCK_EXCLUSIVE ? LOCKFILE_EXCLUSIVE_LOCK : 0, 0, 0xFFFFFFFF, 0xFFFFFFFF, &overlapped) == 0)
+            throw opp_runtime_error("Cannot lock file '%s', error code %d", fileName ? fileName : "", GetLastError());
+#else
+        struct flock flock;
+        flock.l_type = fileLockType == FILE_LOCK_EXCLUSIVE ? F_WRLCK : F_RDLCK;
+        flock.l_whence = SEEK_SET;
+        flock.l_start = 0;
+        flock.l_len = 0;
+        flock.l_pid = getpid();
+        if (fcntl(fileno(file), F_SETLKW, &flock) == -1)
+            throw opp_runtime_error("Cannot lock file '%s', error code %d", fileName ? fileName : "", errno);
+#endif
+    }
+    counter++;
+}
+
+void FileLock::unlock(FileLockType fileLockType)
+{
+    counter--;
+    if (counter == 0) {
+#ifdef _WIN32
+        OVERLAPPED overlapped;
+        overlapped.hEvent = 0;
+        overlapped.Offset = overlapped.OffsetHigh = 0;
+        overlapped.Internal = overlapped.InternalHigh = 0;
+        if (UnlockFileEx(handle, 0, 0xFFFFFFFF, 0xFFFFFFFF, &overlapped) == 0)
+            throw opp_runtime_error("Cannot unlock file '%s', error code %d", fileName ? fileName : "", GetLastError());
+#else
+        struct flock flock;
+        flock.l_type = F_UNLCK;
+        flock.l_whence = SEEK_SET;
+        flock.l_start = 0;
+        flock.l_len = 0;
+        flock.l_pid = getpid();
+        if (fcntl(fileno(file), F_SETLKW, &flock) == -1)
+            throw opp_runtime_error("Cannot unlock file '%s', error code %d", fileName ? fileName : "", errno);
+#endif
+    }
+}
+
+FileLockAcquirer::FileLockAcquirer(FileLock *fileLock, FileLockType fileLockType, bool enableLocking)
+{
+    Assert(fileLock);
+    this->fileLock = fileLock;
+    this->fileLockType = fileLockType;
+    this->enableLocking = enableLocking;
+    if (enableLocking)
+        fileLock->lock(fileLockType);
+}
+
+FileLockAcquirer::~FileLockAcquirer()
+{
+    if (enableLocking)
+        fileLock->unlock(fileLockType);
+}
+
+}  // namespace common
+}  // namespace omnetpp
---
+++ src/common/filelock.h
@@ -0,0 +1,73 @@
+//=========================================================================
+//  FILELOCK.H - part of
+//                  OMNeT++/OMNEST
+//           Discrete System Simulation in C++
+//
+//  Author: Levente Meszaros
+//
+//=========================================================================
+
+/*--------------------------------------------------------------*
+  Copyright (C) 2006-2008 OpenSim Ltd.
+
+  This file is distributed WITHOUT ANY WARRANTY. See the file
+  `license' for details on this and other legal matters.
+*--------------------------------------------------------------*/
+
+#ifndef __FILELOCK_H_
+#define __FILELOCK_H_
+
+#include "omnetpp/platdep/platmisc.h"
+#include "commondefs.h"
+
+namespace omnetpp {
+namespace common {
+
+enum FileLockType
+{
+    FILE_LOCK_SHARED,
+    FILE_LOCK_EXCLUSIVE
+};
+
+/**
+ * This class provides a recursive almost portable file lock.
+ * Unfortunatly Unix systems support both advisory and mandatory locking,
+ * while Windows supports only mandatory locking. Therefore all processes
+ * must use this locking mechanism properly to get correct portable file
+ * locking.
+ */
+class COMMON_API FileLock
+{
+    private:
+        FILE *file; // cannot be NULL
+        const char *fileName; // may be NULL
+        int counter; // 0 means unlocked
+#ifdef _WIN32
+        HANDLE handle; // os specific handle
+#endif
+
+    public:
+        FileLock(FILE *file, const char *fileName);
+        void lock(FileLockType fileLockType);
+        void unlock(FileLockType fileLockType);
+};
+
+/**
+ * This class provides an unwind safe way of grabbing and releasing a lock.
+ */
+class COMMON_API FileLockAcquirer
+{
+    private:
+        bool enableLocking;
+        FileLock *fileLock;
+        FileLockType fileLockType;
+
+    public:
+        FileLockAcquirer(FileLock *fileLock, FileLockType fileLockType, bool enableLocking = true);
+        ~FileLockAcquirer();
+};
+
+}  // namespace common
+}  // namespace omnetpp
+
+#endif
