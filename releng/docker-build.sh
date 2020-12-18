54e6ae4d0e5c93e5d177c281f01875bdba24785a
Rudolf Hornig <rudi@omnetpp.org>
2020-12-18 21:17:20 +0100

    prof: rebuild the distro-build images because of a an issue in the macOS JRE archive layout.

--- releng/docker-build.sh
+++ releng/docker-build.sh
@@ -5,6 +5,6 @@ OMNETPP_ROOT=`pwd`
 
 docker run --network none -v "$OMNETPP_ROOT":/root/omnetpp-repo -e what -e GIT_VERSION -e BUILD_DOC \
        -e BUILD_CORE_DISTRO -e BUILD_LINUX_DISTRO -e BUILD_WIN64_DISTRO -e BUILD_MACOSX_DISTRO \
-       omnetpp/distrobuild:eclipse4.18-tools201211-201217 && \
+       omnetpp/distrobuild:eclipse4.18-tools201218-201218 && \
 docker cp $(docker ps -lq):/root/omnetpp/out/dist/ releng && \
 docker rm $(docker ps -lq)
