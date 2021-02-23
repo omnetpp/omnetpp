e134f07e32422f5fab2cdabe55e235ea6d7fea10
Andras Varga <andras@omnetpp.org>
2021-02-23 19:01:37 +0100

    test_exceptions.py: make empty file so that opp_charttool exits with code=0

--- test/scave/charttemplates/test_exceptions.py
+++ test/scave/charttemplates/test_exceptions.py
@@ -1,4 +1,4 @@
-from omnetpp.scave import plot
+from omnetpp.scave import plot, utils, chart
 
 messages = []
 
@@ -13,7 +13,12 @@ def handle_exception(expected, e):
     msgs_str = str(e)
     if messages:
         msgs_str += "; Status message: " + "; ".join(messages)
-        
+
     print("EXPECTED:", expected)
     print("GOT:", msgs_str)
     assert expected in msgs_str
+
+    # make empty file so that opp_charttool doesn't complain
+    fname = utils.get_image_export_filepath(chart.get_properties())
+    with open(fname, 'wt') as f:
+        pass
