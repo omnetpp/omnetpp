8df8b5c63432585b135ea2cff4d0b0586cc3b18d
Rudolf Hornig <rudi@omnetpp.org>
2021-05-04 16:11:24 +0200

    prod: version bump to 6.0pre12

--- include/omnetpp/simkerneldefs.h
+++ include/omnetpp/simkerneldefs.h
@@ -28,7 +28,7 @@ namespace omnetpp {
 #define OMNETPP_VERSION  0x0600
 
 // OMNeT++ build number
-#define OMNETPP_BUILDNUM   1513
+#define OMNETPP_BUILDNUM   1514
 
 #if defined(SIM_EXPORT)
 #  define SIM_API OPP_DLLEXPORT
