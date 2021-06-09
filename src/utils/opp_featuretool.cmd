16b40d446afb63a5d904d568972d98406b351575
Rudolf Hornig <rudi@omnetpp.org>
2021-06-09 13:20:23 +0200

    utils: removed unused files - removed .cmd files as they are no longer used even on windows - opp_onfigfilepath is generated directly in the bin dir - fixed missing files in the clean target

--- src/utils/opp_featuretool.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-PATH %~dp0;%PATH%
-python %~dpn0\opp_featuretool %*
--- src/utils/splitvec.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-rem splitvec.bat:
-rem   Splits an output vector (.vec) file into files with one vector in each
-rem	ie: foo.vec --> foo-1.vec, foo-2.vec, foo-3.vec,...
-rem Required gawk.exe can be found in CYGWIN or DJGPP
-
-PATH %~dp0;%PATH%
-gawk -f %~dpn0 %1
