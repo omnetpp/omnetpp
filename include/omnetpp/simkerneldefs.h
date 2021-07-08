7189af471fdac572e74bbb77a3cdef94b08842a5
Andras Varga <andras@omnetpp.org>
2021-07-08 14:40:31 +0200

    OMNETPP_BUILDNUM bumped

--- include/omnetpp/simkerneldefs.h
+++ include/omnetpp/simkerneldefs.h
@@ -28,7 +28,7 @@ namespace omnetpp {
 #define OMNETPP_VERSION  0x0600
 
 // OMNeT++ build number
-#define OMNETPP_BUILDNUM   1522
+#define OMNETPP_BUILDNUM   1525
 
 #if defined(SIM_EXPORT)
 #  define SIM_API OPP_DLLEXPORT
