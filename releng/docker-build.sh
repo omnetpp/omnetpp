99b6daffb6a3736896b907455d1dc6d16d75b40b
Rudolf Hornig <rudi@omnetpp.org>
2021-07-12 16:38:04 +0200

    prod: added linux.aarch64 IDE native library support for ARM-based linux distros

--- releng/docker-build.sh
+++ releng/docker-build.sh
@@ -4,7 +4,8 @@ cd $(dirname $0)/..
 OMNETPP_ROOT=$(pwd)
 
 docker run --network none -v "$OMNETPP_ROOT":/root/omnetpp-repo -e what -e GIT_VERSION -e BUILD_DOC \
-       -e BUILD_CORE_DISTRO -e BUILD_LINUX_X86_64_DISTRO -e BUILD_WIN32_X86_64_DISTRO -e BUILD_MACOSX_DISTRO \
+       -e BUILD_CORE_DISTRO -e BUILD_LINUX_X86_64_DISTRO -e BUILD_LINUX_AARCH64_DISTRO \
+       -e BUILD_WIN32_X86_64_DISTRO -e BUILD_MACOSX_DISTRO \
        omnetpp/distrobuild:eclipse4.20-tools210713-210726 && \
 docker cp $(docker ps -lq):/root/omnetpp/out/dist/ releng && \
 docker rm $(docker ps -lq)
