98227bfad40b0e9c2e66a4a6af9e21253b823230
Rudolf Hornig <rudi@omnetpp.org>
2021-08-05 15:29:08 +0200

    prod: adjusted build process to use the same ubuntu20.04 based image as the 6.x branch
    The makedist files are in sync now with omnetpp 6.0pre12 files (except that they do not support
    aarch64 linux target.

--- releng/docker-build.sh
+++ releng/docker-build.sh
@@ -3,8 +3,9 @@
 cd $(dirname $0)/..
 OMNETPP_ROOT=$(pwd)
 
-docker run --network none -v "$OMNETPP_ROOT":/root/omnetpp-repo -e what -e GIT_VERSION -e BUILD_DOC \
-       -e BUILD_CORE_DISTRO -e BUILD_LINUX_DISTRO -e BUILD_WIN64_DISTRO -e BUILD_MACOSX_DISTRO \
-       omnetpp/distrobuild-opp5:eclipse4.20-tools201218-210607 && \
+docker run --network none -v "$OMNETPP_ROOT":/root/omnetpp-repo -e what -e GIT_VERSION -e BUILD_DOC -e BUILD_IDE \
+       -e BUILD_CORE_DISTRO -e BUILD_LINUX_X86_64_DISTRO \
+       -e BUILD_WIN32_X86_64_DISTRO -e BUILD_MACOSX_DISTRO \
+       omnetpp/distrobuild-opp5:eclipse4.20-tools210713-210726 && \
 docker cp $(docker ps -lq):/root/omnetpp/out/dist/ releng && \
 docker rm $(docker ps -lq)
