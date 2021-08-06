f7e6936b96d0ce9fa16fd094ab7660eda5b5f80c
TÖRÖK Attila <torokati44@gmail.com>
2021-08-06 16:24:32 +0200

    sim: OMNETPP_BUILDNUM bumped because cEnvir::pausePoint() got added

--- include/omnetpp/simkerneldefs.h
+++ include/omnetpp/simkerneldefs.h
@@ -28,7 +28,7 @@ namespace omnetpp {
 #define OMNETPP_VERSION  0x0600
 
 // OMNeT++ build number
-#define OMNETPP_BUILDNUM   1525
+#define OMNETPP_BUILDNUM   1526
 
 #if defined(SIM_EXPORT)
 #  define SIM_API OPP_DLLEXPORT
