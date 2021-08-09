3340457ffcda1c58b46fc85b3888d0e57a7c1aa8
Rudolf Hornig <rudi@omnetpp.org>
2021-08-09 17:35:37 +0200

    prod: updated windows toolchain and build docker images.

--- releng/docker-build.sh
+++ releng/docker-build.sh
@@ -6,6 +6,6 @@ OMNETPP_ROOT=$(pwd)
 docker run --network none -v "$OMNETPP_ROOT":/root/omnetpp-repo -e what -e GIT_VERSION -e BUILD_DOC \
        -e BUILD_CORE_DISTRO -e BUILD_LINUX_X86_64_DISTRO -e BUILD_LINUX_AARCH64_DISTRO \
        -e BUILD_WIN32_X86_64_DISTRO -e BUILD_MACOSX_DISTRO \
-       omnetpp/distrobuild:eclipse4.20-tools210713-210726 && \
+       omnetpp/distrobuild:eclipse4.20-tools210809-210809 && \
 docker cp $(docker ps -lq):/root/omnetpp/out/dist/ releng && \
 docker rm $(docker ps -lq)
