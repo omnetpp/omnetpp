cdf523bd3434f36d4ba5433db3242e2263e0099b
Rudolf Hornig <rudi@omnetpp.org>
2021-10-26 16:00:28 +0200

    prod: use new build image for docker buils

--- releng/docker-build.sh
+++ releng/docker-build.sh
@@ -6,6 +6,6 @@ OMNETPP_ROOT=$(pwd)
 docker run --network none -v "$OMNETPP_ROOT":/root/omnetpp-repo -e what -e GIT_VERSION -e BUILD_DOC \
        -e BUILD_CORE_DISTRO -e BUILD_LINUX_X86_64_DISTRO -e BUILD_LINUX_AARCH64_DISTRO \
        -e BUILD_WIN32_X86_64_DISTRO -e BUILD_MACOSX_DISTRO \
-       omnetpp/distrobuild:eclipse4.21-tools210809-210927 && \
+       omnetpp/distrobuild:eclipse4.21-tools211026-211026 && \
 docker cp $(docker ps -lq):/root/omnetpp/out/dist/ releng && \
 docker rm $(docker ps -lq)
