5c0588601eff47eef917addb6adc9143e15425cb
Rudolf Hornig <rudi@omnetpp.org>
2021-10-29 12:22:46 +0200

    prod: files in overlay directory should not overlap with the content of tools archives
    - do not modify bash.bashrc in mingw, istead add an omnetpp.sh in profiles.d

--- releng/overlay/win64/tools/win64/etc/profile.d/omnetpp.sh
+++ releng/overlay/win64/tools/win64/etc/profile.d/omnetpp.sh
@@ -1,6 +1,18 @@
-# add extra path elements passed from the calling process
-if [ -n "$__EXTRA_PATH" ]; then
-  COMPILER_EXTRA_PATH=$(cygpath -u -p "$__EXTRA_PATH")
-  PATH="$COMPILER_EXTRA_PATH:$PATH"
-  unset COMPILER_EXTRA_PATH
+##############################################################
+# OMNeT++ specific changes
+
+export TERM=xterm-256color
+
+# source the git completion script if exsist
+if [ -f "/usr/share/bash-completion/completions/git" ] ; then
+  source "/usr/share/bash-completion/completions/git"
+fi
+if [ -f "/usr/share/git/git-prompt.sh" ] ; then
+  source "/usr/share/git/git-prompt.sh"
+fi
+
+PS1='\[\e]0;\w\a\]\n\[\e[32m\]\[\e[33m\]$(__git_ps1 "(%s) ")\w\[\e[0m\]\$ '
+
+if [ -f "$HOME/setenv" ] ; then
+  source $HOME/setenv
 fi
