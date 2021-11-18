55c9e9decd35d2d25b14268d96fd7c7d289cf7a2
Rudolf Hornig <rudi@omnetpp.org>
2021-11-18 12:49:55 +0100

    prod: introduced aarch64 build output for macOS

--- releng/docker-build.sh
+++ releng/docker-build.sh
@@ -5,7 +5,7 @@ OMNETPP_ROOT=$(pwd)
 
 docker run --network none -v "$OMNETPP_ROOT":/root/omnetpp-repo -e what -e GIT_VERSION -e BUILD_DOC \
        -e BUILD_CORE_DISTRO -e BUILD_LINUX_X86_64_DISTRO -e BUILD_LINUX_AARCH64_DISTRO \
-       -e BUILD_WIN32_X86_64_DISTRO -e BUILD_MACOSX_DISTRO \
-       omnetpp/distrobuild:eclipse4.21-tools211029-211029 && \
+       -e BUILD_WIN32_X86_64_DISTRO -e BUILD_MACOS_X86_64_DISTRO -e BUILD_MACOS_AARCH64_DISTRO \
+       omnetpp/distrobuild:eclipse4.22-tools211029-211118 && \
 docker cp $(docker ps -lq):/root/omnetpp/out/dist/ releng && \
 docker rm $(docker ps -lq)
