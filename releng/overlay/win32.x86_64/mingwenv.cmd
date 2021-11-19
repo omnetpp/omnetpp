cc13fdf0fa5ec8eff13a3f57c47ab0a0f5ba4afc
Rudolf Hornig <rudi@omnetpp.org>
2021-11-19 22:08:48 +0100

    prod: tools/win64 moved to tools/win32.x86_64 for consistency.

--- releng/overlay/win32.x86_64/mingwenv.cmd
+++ releng/overlay/win32.x86_64/mingwenv.cmd
@@ -8,11 +8,11 @@ echo.
 echo We need to unpack the MinGW toolchain before continuing.
 echo This can take a while, please be patient.
 echo.
-7za x -aos -y -owin64 opp-tools-win64-mingw64-toolchain.7z && del opp-tools-win64-mingw64-toolchain.7z
+7za x -aos -y -owin32.x86_64 opp-tools-win64-mingw64-toolchain.7z && del opp-tools-win64-mingw64-toolchain.7z
 )
 
 IF EXIST "opp-tools-win64-mingw64-dependencies.7z" (
-7za x -aos -y -owin64\opt\mingw64 opp-tools-win64-mingw64-dependencies.7z && del opp-tools-win64-mingw64-dependencies.7z && "win64\opt\mingw64\bin\qtbinpatcher.exe" --qt-dir=win64\opt\mingw64
+7za x -aos -y -owin32.x86_64\opt\mingw64 opp-tools-win64-mingw64-dependencies.7z && del opp-tools-win64-mingw64-dependencies.7z && "win32.x86_64\opt\mingw64\bin\qtbinpatcher.exe" --qt-dir=win32.x86_64\opt\mingw64
 echo *** MinGW-64 toolchain extracted. ***
 )
 
@@ -20,7 +20,7 @@ cd "%HOME%"
 rem Open the MinGW command shell (you may add -full-path to force the MSYS shell to inherit the current system path)
 if "%1" == "ide" (
 rem if the first paramter is "ide" we start the IDE instead of the shell. This can be used to start the IDE from a shortcut
-call "%HOME%\tools\win64\msys2_shell.cmd" -mingw64 -c "nohup >/dev/null 2>/dev/null $HOME/bin/omnetpp"
+call "%HOME%\tools\win32.x86_64\msys2_shell.cmd" -mingw64 -c "nohup >/dev/null 2>/dev/null $HOME/bin/omnetpp"
 ) else (
-call "%HOME%\tools\win64\msys2_shell.cmd" -mingw64 %*
+call "%HOME%\tools\win32.x86_64\msys2_shell.cmd" -mingw64 %*
 )
--- releng/overlay/win32.x86_64/vcenv.cmd
+++ releng/overlay/win32.x86_64/vcenv.cmd
@@ -15,7 +15,7 @@ echo.
 echo We need to unpack the MinGW toolchain before continuing.
 echo This can take a while, please be patient.
 echo.
-7za x -aos -y -owin64 opp-tools-win64-mingw64-toolchain.7z && del opp-tools-win64-mingw64-toolchain.7z
+7za x -aos -y -owin32.x86_64 opp-tools-win64-mingw64-toolchain.7z && del opp-tools-win64-mingw64-toolchain.7z
 )
 
 IF EXIST "opp-tools-win64-visualc-dependencies.7z" (
@@ -24,7 +24,7 @@ echo.
 echo We need to unpack the libraries required by the ms-clang toolchain before continuing.
 echo This can take a while, please be patient.
 echo.
-7za x -aos -y -owin64\opt\visualc opp-tools-win64-visualc-dependencies.7z && del opp-tools-win64-visualc-dependencies.7z && win64\opt\visualc\bin\qtbinpatcher.exe --qt-dir=win64\opt\visualc
+7za x -aos -y -owin32.x86_64\opt\visualc opp-tools-win64-visualc-dependencies.7z && del opp-tools-win64-visualc-dependencies.7z && win32.x86_64\opt\visualc\bin\qtbinpatcher.exe --qt-dir=win32.x86_64\opt\visualc
 echo *** Files for Microsoft ABI compatible toolchain extracted. ***
 )
 
@@ -40,9 +40,9 @@ set VS_INSTALL_DIR=
 rem Open the MinGW command shell (you may add -full-path to force the MSYS shell to inherit the current system path)
 if "%1" == "ide" (
 rem if the first paramter is "ide" we start the IDE instead of the shell. This can be used to start the IDE from a shortcut
-call "%HOME%\tools\win64\msys2_shell.cmd" -mingw64 -c "nohup >/dev/null 2>/dev/null $HOME/bin/omnetpp"
+call "%HOME%\tools\win32.x86_64\msys2_shell.cmd" -mingw64 -c "nohup >/dev/null 2>/dev/null $HOME/bin/omnetpp"
 ) else (
-call "%HOME%\tools\win64\msys2_shell.cmd" -mingw64 %*
+call "%HOME%\tools\win32.x86_64\msys2_shell.cmd" -mingw64 %*
 )
 
 goto EOF
