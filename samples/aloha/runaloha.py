466a8f560f046f320c52caf9d094bd7b7d576d6a
Andras Varga <andras@omnetpp.org>
2022-03-19 11:39:08 +0100

    sim,envir: START EXTENSIVE REFACTORING OF ENVIR
    Goal: Make it possible to use OMNeT++ from Python as a library,
    with the help of the cppyy package [1,2].
    
    [1] https://github.com/wlav/cppyy/
    [2] https://cppyy.readthedocs.io/
    
    This commit adds an initial working Python script that loads
    OMNeT++ libraries and runs the Aloha example simulation.
    
    An additional goal is to allow the use of multiple theads/cores
    for running simulations. Each simulation would be STRICTLY tied
    to one thread, but the use of multiple threads would be allowed.
    This is largely achieved by having per-thread copies of global
    variables, via the use of the "thread_local" C++ modifier.
    
    Refactoring involves:
    
    - Breaking up EnvirBase, Cmdenv, Qtenv into library classes which
      are usable as building blocks from Python;
    
    - Make multiple instances of cSimulation possible, and even, the norm.
    
    - cEnvir/cRunnableEnvir/EnvirBase now serve a dual role: "the thing
      that provides services for cSimulation", and "the app that sets up
      and runs simulations". These two need to be separated, and the latter
      functionality factored out into distict classes. cEnvir and subclasses
      should purely be a class that plugs into cSimulation and provides
      services (such as recording simulation results) for it.
    
    - Getting rid of singletons and global state as much as possible.
      (Note that it is not possible to fully remove globals, as it would
      imply changing the simulation kernel API in a way that would
      severely inconvenience model authors.)

---
+++ samples/aloha/runaloha.py
@@ -0,0 +1,80 @@
+import re
+import cppyy
+
+omnetpp_root = "/home/andras/projects/omnetpp-dev"
+libsuffix = "_dbg"
+
+# setup omnetpp include path
+cppyy.add_include_path(omnetpp_root + "/include")
+cppyy.add_include_path(omnetpp_root + "/src")
+
+# setup omnetpp library path
+cppyy.add_library_path(omnetpp_root + "/lib")
+
+# load omnetpp libraries
+cppyy.load_library("liboppcmdenv" + libsuffix)
+cppyy.load_library("liboppcommon" + libsuffix)
+cppyy.load_library("liboppenvir" + libsuffix)
+cppyy.load_library("liboppeventlog" + libsuffix)
+cppyy.load_library("libopplayout" + libsuffix)
+cppyy.load_library("liboppnedxml" + libsuffix)
+#cppyy.load_library("liboppqtenv-osg" + libsuffix)
+#cppyy.load_library("liboppqtenv" + libsuffix)
+#cppyy.load_library("liboppscave" + libsuffix)
+cppyy.load_library("liboppsim" + libsuffix)
+
+# include omnetpp header files
+cppyy.include("omnetpp.h")
+cppyy.include("cmdenv/cmdenv.h")
+cppyy.include("envir/inifilereader.h")
+cppyy.include("envir/sectionbasedconfig.h")
+
+# setup omnetpp namespace
+from cppyy.gbl import omnetpp
+cStaticFlag = omnetpp.cStaticFlag
+SimTime = omnetpp.SimTime
+CodeFragments = omnetpp.CodeFragments
+cSimulation = omnetpp.cSimulation
+cEvent = omnetpp.cEvent
+Cmdenv = omnetpp.cmdenv.Cmdenv
+InifileReader = omnetpp.envir.InifileReader
+SectionBasedConfiguration = omnetpp.envir.SectionBasedConfiguration
+
+# define a modified Cmdenv
+class PythonCmdenv(Cmdenv):
+    def loadNEDFiles(self):
+        pass
+
+# startup
+CodeFragments.executeAll(CodeFragments.STARTUP)
+#SimTime.setScaleExp(-12)
+static_flag = cStaticFlag()
+static_flag.__python_owns__ = False
+cSimulation.loadNedSourceFolder(omnetpp_root + "/samples/aloha")
+cSimulation.doneLoadingNedFiles()
+iniReader = InifileReader()
+iniReader.readFile("omnetpp.ini")
+configuration = SectionBasedConfiguration()
+configuration.setConfigurationReader(iniReader)
+iniReader.__python_owns__ = False
+
+cppyy.load_library("libaloha" + libsuffix)
+CodeFragments.executeAll(CodeFragments.STARTUP)
+args = [ "<progname>", "-c", "PureAloha1" ]
+extra_config_options = { "cpu-time-limit" : "1s" }
+configuration.setCommandLineConfigOptions(extra_config_options, ".")
+
+environment = PythonCmdenv()
+#environment.loggingEnabled = False
+simulation = cSimulation("simulation", environment)
+environment.__python_owns__ = False
+cSimulation.setActiveSimulation(simulation)
+
+# run
+returncode = environment.run(args, configuration)
+
+# shutdown
+cSimulation.setActiveSimulation(cppyy.nullptr)
+simulation.__python_owns__ = False
+CodeFragments.executeAll(CodeFragments.SHUTDOWN)
+del static_flag
