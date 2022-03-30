55112e417beb580547ba9428088db5d730ffa3e5
Rudolf Hornig <rudi@omnetpp.org>
2022-03-30 18:17:37 +0200

    prod: new build image with updated visual c dependencies (qt updated to 5.15.2)

--- releng/docker-build.sh
+++ releng/docker-build.sh
@@ -6,6 +6,6 @@ OMNETPP_ROOT=$(pwd)
 docker run --network none -v "$OMNETPP_ROOT":/root/omnetpp-repo -e what -e GIT_VERSION -e BUILD_DOC \
        -e BUILD_CORE_DISTRO -e BUILD_LINUX_X86_64_DISTRO -e BUILD_LINUX_AARCH64_DISTRO \
        -e BUILD_WIN32_X86_64_DISTRO -e BUILD_MACOS_X86_64_DISTRO -e BUILD_MACOS_AARCH64_DISTRO \
-       omnetpp/distrobuild:eclipse4.22-tools211209-220329 && \
+       omnetpp/distrobuild:eclipse4.22-tools220331-220331 && \
 docker cp $(docker ps -lq):/root/omnetpp/out/dist/ releng && \
 docker rm $(docker ps -lq)
