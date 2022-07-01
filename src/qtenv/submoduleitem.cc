omnetpp/omnetpp

commit fddd71e99ee8e51cec46c81ebfdeceae908f06e1

qtenv: Use the new DisplayStringAccess class everywhere to query display strings

src/qtenv/submoduleitem.cc
@@ -19,6 +19,7 @@
 #include "qtenv.h"
 #include "qtutil.h"
 #include "graphicsitems.h"
+#include "displaystringaccess.h"
 
 #include <QtCore/QDebug>
 #include <QtGui/QPainter>
@@ -42,13 +43,12 @@ void SubmoduleItemUtil::setupFromDisplayString(SubmoduleItem *si, cModule *mod)
     // making a copy, so we can modify freely
     cDisplayString ds = mod->getDisplayString();
 
-    // replacing $param args with the actual parameter values
-    std::string buffer;
-    ds = substituteDisplayStringParamRefs(ds, buffer, mod, true);
+    std::string buffer; // stores getTagArg return values after substitution
+    DisplayStringAccess dsa(&ds, mod);
 
     bool widthOk, heightOk;
-    double shapeWidth = QString(ds.getTagArg("b", 0)).toDouble(&widthOk);
-    double shapeHeight = QString(ds.getTagArg("b", 1)).toDouble(&heightOk);
+    double shapeWidth = dsa.getTagArgAsDouble("b", 0, 0.0, &widthOk);
+    double shapeHeight = dsa.getTagArgAsDouble("b", 1, 0.0, &heightOk);
 
     if (!widthOk)
         shapeWidth = shapeHeight;
@@ -62,45 +62,45 @@ void SubmoduleItemUtil::setupFromDisplayString(SubmoduleItem *si, cModule *mod)
     si->setWidth(shapeWidth);
     si->setHeight(shapeHeight);
 
-    QString shapeName = QString(ds.getTagArg("b", 2));
+    const char *shapeName = dsa.getTagArg("b", 2, buffer);
 
     auto shape = (ds.getNumArgs("b") <= 0)
                    ? SubmoduleItem::SHAPE_NONE
-                   : shapeName == "oval"
+                   : opp_streq(shapeName, "oval")
                        ? SubmoduleItem::SHAPE_OVAL
                        : SubmoduleItem::SHAPE_RECT; // if unknown, this is the default
     si->setShape(shape);
 
-    si->setFillColor(parseColor(ds.getTagArg("b", 3)));
-    si->setOutlineColor(parseColor(ds.getTagArg("b", 4)));
-    bool ok;
-    int outlineWidth = QString(ds.getTagArg("b", 5)).toInt(&ok);
-    si->setOutlineWidth(ok ? outlineWidth : 2);
+    si->setFillColor(parseColor(dsa.getTagArg("b", 3, buffer)));
+    si->setOutlineColor(parseColor(dsa.getTagArg("b", 4, buffer)));
+    si->setOutlineWidth(dsa.getTagArgAsLong("b", 5, 2));
 
-    const char *imageName = ds.getTagArg("i", 0);
+    // storing in a std::string because it will be still needed after we use `buffer` again
+    std::string imageName = dsa.getTagArg("i", 0, buffer);
 
-    if (!imageName[0] && shape == SubmoduleItem::SHAPE_NONE)
+    if (imageName.empty() && shape == SubmoduleItem::SHAPE_NONE)
         imageName = SubmoduleItem::DEFAULT_ICON;
 
-    if (imageName[0]) {
-        const char *imageColor = ds.getTagArg("i", 1);
-        double tintAmount = ((ds.getNumArgs("i") == 2 && strlen(ds.getTagArg("i", 1)) > 0)
+    if (!imageName.empty()) {
+        const char *imageColor = dsa.getTagArg("i", 1, buffer);
+        double tintAmount = ((ds.getNumArgs("i") == 2 && strlen(imageColor) > 0)
                 ? 30  // color given, but no percentage
-                : QString(ds.getTagArg("i", 2)).toDouble()) / 100.0;
+                : dsa.getTagArgAsDouble("i", 2) / 100.0);
 
         si->setIcon(getQtenv()->icons.getTintedPixmap(
-                        imageName, ds.getTagArg("is", 0), parseColor(imageColor), tintAmount));
+                        imageName.c_str(), dsa.getTagArg("is", 0, buffer), parseColor(imageColor), tintAmount));
     }
     else {
         si->setIcon(QPixmap());
     }
 
-    const char *decoratorImageName = ds.getTagArg("i2", 0);
+    const char *decoratorImageName = dsa.getTagArg("i2", 0, buffer);
     if (decoratorImageName[0]) {
-        const char *decoratorImageColor = ds.getTagArg("i2", 1);
-        double tintAmount = ((ds.getNumArgs("i2") == 2 && strlen(ds.getTagArg("i2", 1)) > 0)
+        std::string buffer2; // can't reuse `buffer` yet, still need `decoratorImageName`
+        const char *decoratorImageColor = dsa.getTagArg("i2", 1, buffer2);
+        double tintAmount = ((ds.getNumArgs("i2") == 2 && strlen(decoratorImageColor) > 0)
                 ? 30  // color given, but no percentage
-                : QString(ds.getTagArg("i2", 2)).toDouble()) / 100.0;
+                : dsa.getTagArgAsDouble("i2", 2) / 100.0);
 
         si->setDecoratorIcon(getQtenv()->icons.getTintedPixmap(
                                  decoratorImageName, parseColor(decoratorImageColor), tintAmount));
@@ -109,19 +109,19 @@ void SubmoduleItemUtil::setupFromDisplayString(SubmoduleItem *si, cModule *mod)
         si->setDecoratorIcon(QPixmap());
     }
 
-    const char *text = ds.getTagArg("t", 0);
-    const char *textPosition = ds.getTagArg("t", 1);
-    const char *textColor = ds.getTagArg("t", 2);
-
+    const char *textPosition = dsa.getTagArg("t", 1, buffer);
     SubmoduleItem::TextPos textPos;
     switch (textPosition[0]) {
         case 'l': textPos = SubmoduleItem::TEXTPOS_LEFT;  break;
         case 'r': textPos = SubmoduleItem::TEXTPOS_RIGHT; break;
         default:  textPos = SubmoduleItem::TEXTPOS_TOP;   break;
     }
-    auto color = parseColor(textColor);
-    if (!color.isValid())
-        color = QColor("blue");
+
+    const char *textColor = dsa.getTagArg("t", 2, buffer);
+    auto color = parseColor(textColor, QColor("blue"));
+
+    // have to be last, position and color cobble `buffer`
+    const char *text = dsa.getTagArg("t", 0, buffer);
     si->setInfoText(text, textPos, color);
 
     si->clearRangeItems();
@@ -139,13 +139,11 @@ void SubmoduleItemUtil::setupFromDisplayString(SubmoduleItem *si, cModule *mod)
         if (!ok)
             continue;
 
-        double r = QString(ds.getTagArg(i, 0)).toDouble();
-        QColor rangeFillColor = parseColor(ds.getTagArg(i, 1));
-        QColor rangeOutlineColor = parseColor(ds.getTagArg(i, 2));
+        double r = dsa.getTagArgAsDouble(i, 0);
+        QColor rangeFillColor = parseColor(dsa.getTagArg(i, 1, buffer));
+        QColor rangeOutlineColor = parseColor(dsa.getTagArg(i, 2, buffer));
 
-        int rangeOutlineWidth = QString(ds.getTagArg(i, 3)).toInt(&ok);
-        if (!ok)
-            rangeOutlineWidth = 1;
+        int rangeOutlineWidth = dsa.getTagArgAsLong(i, 3, 1);
         if (!rangeOutlineColor.isValid() && rangeOutlineWidth > 0)
             rangeOutlineColor = QColor("black");
         si->addRangeItem(r, rangeOutlineWidth, rangeFillColor, rangeOutlineColor);
@@ -160,11 +158,11 @@ void SubmoduleItemUtil::updateQueueSizeLabel(SubmoduleItem *si, cModule *mod)
 {
     cDisplayString ds = mod->getDisplayString();
     std::string buffer;
-    ds = substituteDisplayStringParamRefs(ds, buffer, mod, true);
+    DisplayStringAccess dsa(&ds, mod);
 
     QString queueText;
     if (ds.containsTag("q"))
-        if (cQueue *q = dynamic_cast<cQueue *>(mod->findObject(ds.getTagArg("q", 0))))
+        if (cQueue *q = dynamic_cast<cQueue *>(mod->findObject(dsa.getTagArg("q", 0, buffer))))
             queueText = QString("q: %1").arg(q->getLength());
     si->setQueueText(queueText);
 }

src/qtenv/compoundmoduleitem.cc
@@ -24,6 +24,7 @@
 #include <omnetpp/cdisplaystring.h>
 #include "qtenv.h"
 #include "qtutil.h"
+#include "displaystringaccess.h"
 
 namespace omnetpp {
 namespace qtenv {
@@ -317,9 +318,8 @@ void CompoundModuleItemUtil::setupFromDisplayString(CompoundModuleItem *cmi, cMo
             ? mod->getDisplayString()
             : cDisplayString();
 
-    // replacing $param args with the actual parameter values
-    std::string buffer;
-    ds = substituteDisplayStringParamRefs(ds, buffer, mod, true);
+    std::string buffer; // stores getTagArg return values after substitution
+    DisplayStringAccess dsa(&ds, mod);
 
     QRectF border = submodulesRect;
 
@@ -343,8 +343,8 @@ void CompoundModuleItemUtil::setupFromDisplayString(CompoundModuleItem *cmi, cMo
 
     bool widthOk, heightOk;
     double width, height;
-    width = QString(ds.getTagArg("bgb", 0)).toDouble(&widthOk) * zoomFactor;
-    height = QString(ds.getTagArg("bgb", 1)).toDouble(&heightOk) * zoomFactor;
+    width = dsa.getTagArgAsDouble("bgb", 0, 0.0, &widthOk) * zoomFactor;
+    height = dsa.getTagArgAsDouble("bgb", 1, 0.0, &heightOk) * zoomFactor;
 
     if (widthOk)
         border.setWidth(width);
@@ -360,24 +360,18 @@ void CompoundModuleItemUtil::setupFromDisplayString(CompoundModuleItem *cmi, cMo
     cmi->setZoomFactor(zoomFactor);
     cmi->setArea(border);
 
-    cmi->setBackgroundColor(parseColor(ds.getTagArg("bgb", 2), parseColor("grey82")));
-    cmi->setOutlineColor(parseColor(ds.getTagArg("bgb", 3), QColor("black")));
-
-    double outlineWidth;
-    bool ok;
-    outlineWidth = QString(ds.getTagArg("bgb", 4)).toDouble(&ok);
-    if (!ok)
-        outlineWidth = 2;
-    cmi->setOutlineWidth(outlineWidth);
+    cmi->setBackgroundColor(parseColor(dsa.getTagArg("bgb", 2, buffer), parseColor("grey82")));
+    cmi->setOutlineColor(parseColor(dsa.getTagArg("bgb", 3, buffer), QColor("black")));
+    cmi->setOutlineWidth(dsa.getTagArgAsDouble("bgb", 4, 2.0));
 
     cmi->setData(ITEMDATA_COBJECT, QVariant::fromValue((cObject *)mod));
-    cmi->setData(ITEMDATA_TOOLTIP, QString(ds.getTagArg("bgtt", 0)));
+    cmi->setData(ITEMDATA_TOOLTIP, QString(dsa.getTagArg("bgtt", 0, buffer)));
 
     // background image
-    const char *imageName = ds.getTagArg("bgi", 0);
-    const char *imageMode = ds.getTagArg("bgi", 1);
-
+    const char *imageName = dsa.getTagArg("bgi", 0, buffer);
     cmi->setImage(imageName[0] ? getQtenv()->icons.getImage(imageName) : nullptr);
+
+    const char *imageMode = dsa.getTagArg("bgi", 1, buffer);
     switch (imageMode[0]) {
         case 's': cmi->setImageMode(CompoundModuleItem::MODE_STRETCH); break;
         case 'c': cmi->setImageMode(CompoundModuleItem::MODE_CENTER);  break;
@@ -387,10 +381,10 @@ void CompoundModuleItemUtil::setupFromDisplayString(CompoundModuleItem *cmi, cMo
 
     // grid
     // if failed to parse, the default 0 will disable the grid
-    cmi->setGridMajorDistance(QString(ds.getTagArg("bgg", 0)).toDouble());
+    cmi->setGridMajorDistance(dsa.getTagArgAsDouble("bgg", 0));
     // if failed to parse, the default 0 will disable the minor lines
-    cmi->setGridMinorNum(QString(ds.getTagArg("bgg", 1)).toInt());
-    cmi->setGridColor(parseColor(ds.getTagArg("bgg", 2), QColor("grey")));
+    cmi->setGridMinorNum(dsa.getTagArgAsLong("bgg", 1));
+    cmi->setGridColor(parseColor(dsa.getTagArg("bgg", 2, buffer), QColor("grey")));
 
     // the text in the top left corner
     cmi->setModulePath(mod->getFullPath().c_str());
@@ -404,10 +398,10 @@ void CompoundModuleItemUtil::setupFromDisplayString(CompoundModuleItem *cmi, cMo
             break;
 
         cmi->addText(QPointF(
-                         QString(ds.getTagArg(tagName.c_str(), 0)).toDouble(),
-                         QString(ds.getTagArg(tagName.c_str(), 1)).toDouble()),
-                     ds.getTagArg(tagName.c_str(), 2),
-                     parseColor(ds.getTagArg(tagName.c_str(), 3), QColor("black")));
+                         dsa.getTagArgAsDouble(tagName.c_str(), 0),
+                         dsa.getTagArgAsDouble(tagName.c_str(), 1)),
+                     dsa.getTagArg(tagName.c_str(), 2, buffer),
+                     parseColor(dsa.getTagArg(tagName.c_str(), 3, buffer), QColor("black")));
 
         ++textIndex;
     }

src/qtenv/connectionitem.cc
@@ -21,6 +21,7 @@
 #include <omnetpp/cgate.h>
 #include <omnetpp/cchannel.h>
 #include "graphicsitems.h"
+#include "displaystringaccess.h"
 #include "qtutil.h"
 
 namespace omnetpp {
@@ -36,32 +37,32 @@ void ConnectionItemUtil::setupFromDisplayString(ConnectionItem *ci, cGate *gate,
 
     // replacing $param args with the actual parameter values
     std::string buffer;
-    ds = substituteDisplayStringParamRefs(ds, buffer, chan, true);
+    DisplayStringAccess dsa(&ds, chan);
 
     ci->setData(ITEMDATA_COBJECT, QVariant::fromValue((cObject *)gate));
-    ci->setData(ITEMDATA_TOOLTIP, ds.getTagArg("tt", 0));
+    ci->setData(ITEMDATA_TOOLTIP, dsa.getTagArg("tt", 0, buffer));
 
-    ci->setColor(parseColor(ds.getTagArg("ls", 0), QColor("black")));
+    ci->setColor(parseColor(dsa.getTagArg("ls", 0, buffer), QColor("black")));
 
     bool ok;
-    double width = QString(ds.getTagArg("ls", 1)).toDouble(&ok);
+    double width = dsa.getTagArgAsDouble("ls", 1, 0.0, &ok);
     ci->setWidth(width);  // will display even with 0 width, as hairline
 
     // explicit 0 width, so hiding the line completely
     if (ok && width == 0)
         ci->setColor(QColor("transparent"));
 
-    const char *style = ds.getTagArg("ls", 2);
+    const char *style = dsa.getTagArg("ls", 2, buffer);
     ci->setLineStyle(style[0] == 'd'
                       ? style[1] == 'a'
                          ? Qt::DashLine
                          : Qt::DotLine
                       : Qt::SolidLine);
 
-    const char *text = ds.getTagArg("t", 0);
+    const char *text = dsa.getTagArg("t", 0, buffer);
     ci->setText(text);
 
-    const char *textPos = ds.getTagArg("t", 1);
+    const char *textPos = dsa.getTagArg("t", 1, buffer);
 
     switch (textPos[0]) {
         case 'l': ci->setTextPosition(Qt::AlignLeft);   break;
@@ -69,7 +70,7 @@ void ConnectionItemUtil::setupFromDisplayString(ConnectionItem *ci, cGate *gate,
         default:  ci->setTextPosition(Qt::AlignCenter); break;
     }
 
-    ci->setTextColor(parseColor(ds.getTagArg("t", 2), QColor("#005030")));
+    ci->setTextColor(parseColor(dsa.getTagArg("t", 2, buffer), QColor("#005030")));
 
     bool twoWay = isTwoWayConnection(gate);
 

src/qtenv/displaystringaccess.h
@@ -97,6 +97,9 @@ class DisplayStringAccess {
     const char *getTagArg(const char *tagname, int index, std::string& buffer) const;
     const char *getTagArg(int tagindex, int index, std::string& buffer) const;
 
+    std::string getTagArg(const char *tagname, int index) const;
+    std::string getTagArg(int tagindex, int index) const;
+
     bool getTagArgAsBool(const char *tagname, int index, bool defaultValue = false, bool *okp = nullptr) const;
     bool getTagArgAsBool(int tagindex, int index, bool defaultValue = false, bool *okp = nullptr) const;
 

src/qtenv/layouterenv.cc
@@ -35,23 +35,23 @@ namespace omnetpp {
 namespace qtenv {
 
 QtenvGraphLayouterEnvironment::QtenvGraphLayouterEnvironment(cModule *parentModule, const cDisplayString& displayString, QGraphicsScene *scene)
-    : parentModule(parentModule), displayString(displayString), scene(scene)
+    : parentModule(parentModule), displayString(displayString), dsa(&this->displayString, this->parentModule), scene(scene)
 {
 }
 
 bool QtenvGraphLayouterEnvironment::getBoolParameter(const char *tagName, int index, bool defaultValue)
 {
-    return resolveBoolDispStrArg(displayString.getTagArg(tagName, index), parentModule, defaultValue);
+    return dsa.getTagArgAsBool(tagName, index, defaultValue);
 }
 
 long QtenvGraphLayouterEnvironment::getLongParameter(const char *tagName, int index, long defaultValue)
 {
-    return resolveLongDispStrArg(displayString.getTagArg(tagName, index), parentModule, defaultValue);
+    return dsa.getTagArgAsLong(tagName, index, defaultValue);
 }
 
 double QtenvGraphLayouterEnvironment::getDoubleParameter(const char *tagName, int index, double defaultValue)
 {
-    return resolveDoubleDispStrArg(displayString.getTagArg(tagName, index), parentModule, defaultValue);
+    return dsa.getTagArgAsDouble(tagName, index, defaultValue);
 }
 
 void QtenvGraphLayouterEnvironment::showGraphics(const char *text)

src/qtenv/layouterenv.h
@@ -23,6 +23,8 @@
 #include <QtCore/QObject>
 #include <climits>
 #include "qtenvdefs.h"
+#include "qtutil.h"
+#include "displaystringaccess.h"
 
 namespace omnetpp {
 
@@ -41,6 +43,7 @@ protected:
     // configuration
     cModule *parentModule;
     const cDisplayString& displayString;
+    DisplayStringAccess dsa;
     QGraphicsScene *scene;
 
     // state

src/qtenv/messageitem.cc
@@ -96,6 +96,11 @@ void MessageItemUtil::setupSymbolFromDisplayString(SymbolMessageItem *mi, cMessa
         mi->setImage(nullptr);
     }
     else {  // as defined in the dispstr
+        // Not using DisplaySringAccess here, because there is no cComponent
+        // here to get parameters from. So expressions aren't evaluated either.
+        // Since display strings for messages can't be set directly from NED,
+        // users should compute whatever expressions they want in their
+        // overrides of cMessage::getDisplayString().
         bool widthOk, heightOk;
         double shapeWidth = QString(ds.getTagArg("b", 0)).toDouble(&widthOk);
         double shapeHeight = QString(ds.getTagArg("b", 1)).toDouble(&heightOk);

src/qtenv/modulecanvasviewer.cc
@@ -676,23 +676,23 @@ QLineF ModuleCanvasViewer::getConnectionLine(cGate *gate)
         cDisplayString ds = channel->getDisplayString();
 
         std::string buffer;
-        ds = substituteDisplayStringParamRefs(ds, buffer, channel, true);
+        DisplayStringAccess dsa(&ds, channel);
 
-        const char *modeString = ds.getTagArg("m", 0);
+        const char *modeString = dsa.getTagArg("m", 0, buffer);
         if (modeString[0] && QString("amnews").contains(modeString[0]))
             mode = modeString[0];
 
         bool xOk, yOk;
-        int x = QString(ds.getTagArg("m", 1)).toInt(&xOk);
-        int y = QString(ds.getTagArg("m", 2)).toInt(&yOk);
+        int x = dsa.getTagArgAsLong("m", 1, 0.0, &xOk);
+        int y = dsa.getTagArgAsLong("m", 2, 0.0, &yOk);
 
         if (xOk)
             srcAnch.setX(x);
         if (yOk)
             srcAnch.setY(y);
 
-        x = QString(ds.getTagArg("m", 3)).toInt(&xOk);
-        y = QString(ds.getTagArg("m", 4)).toInt(&yOk);
+        x = dsa.getTagArgAsLong("m", 3, 0.0, &xOk);
+        y = dsa.getTagArgAsLong("m", 4, 0.0, &yOk);
 
         if (xOk)
             destAnch.setX(x);

commit 12d170d7a3f99df6dd9137db4c8221ef55ef936e

qtenv: Fix SubmoduleItem regression: Overflowing tint amount of images
This was introduced by fddd71e99e where a closing parenthesis
was accidentally moved.
Also change the literal default to a double, so it won't truncate
the non-default value as well due to ?: operator rules.
And do the division by a hundred afterwards, to improve readability.

src/qtenv/submoduleitem.cc
@@ -83,12 +83,13 @@ void SubmoduleItemUtil::setupFromDisplayString(SubmoduleItem *si, cModule *mod)
 
     if (!imageName.empty()) {
         const char *imageColor = dsa.getTagArg("i", 1, buffer);
-        double tintAmount = ((ds.getNumArgs("i") == 2 && strlen(imageColor) > 0)
-                ? 30  // color given, but no percentage
-                : dsa.getTagArgAsDouble("i", 2) / 100.0);
+        double tintPercentage = (ds.getNumArgs("i") == 2 && strlen(imageColor) > 0)
+                ? 30.0  // color given, but no percentage
+                : dsa.getTagArgAsDouble("i", 2);
 
         si->setIcon(getQtenv()->icons.getTintedPixmap(
-                        imageName.c_str(), dsa.getTagArg("is", 0, buffer), parseColor(imageColor), tintAmount));
+                        imageName.c_str(), dsa.getTagArg("is", 0, buffer),
+                        parseColor(imageColor), tintPercentage / 100.0));
     }
     else {
         si->setIcon(QPixmap());
@@ -98,12 +99,13 @@ void SubmoduleItemUtil::setupFromDisplayString(SubmoduleItem *si, cModule *mod)
     if (decoratorImageName[0]) {
         std::string buffer2; // can't reuse `buffer` yet, still need `decoratorImageName`
         const char *decoratorImageColor = dsa.getTagArg("i2", 1, buffer2);
-        double tintAmount = ((ds.getNumArgs("i2") == 2 && strlen(decoratorImageColor) > 0)
-                ? 30  // color given, but no percentage
-                : dsa.getTagArgAsDouble("i2", 2) / 100.0);
+        double tintPercentage = (ds.getNumArgs("i2") == 2 && strlen(decoratorImageColor) > 0)
+                ? 30.0  // color given, but no percentage
+                : dsa.getTagArgAsDouble("i2", 2);
 
-        si->setDecoratorIcon(getQtenv()->icons.getTintedPixmap(
-                                 decoratorImageName, parseColor(decoratorImageColor), tintAmount));
+        si->setDecoratorIcon(
+            getQtenv()->icons.getTintedPixmap(decoratorImageName,
+                parseColor(decoratorImageColor), tintPercentage / 100.0));
     }
     else {
         si->setDecoratorIcon(QPixmap());