12dfea52f461711e5276155cf70a228b9aeae6ee
Andras Varga <andras@omnetpp.org>
2022-08-15 09:16:15 +0200

    runaloha.py updated
    There's no need for separate initialization of the simulation library (SimTime,cCoroutine)

--- samples/aloha/runaloha.py
+++ samples/aloha/runaloha.py
@@ -1,20 +1,11 @@
 from opplib import *
 
-# setup: load ini,ned,libs,etc.
-ini = InifileContents("omnetpp.ini")
-
-globalCfg = ini.extractGlobalConfig()
-SimTime.configure(globalCfg)
-cCoroutine.configure(globalCfg)
-
 loadExtensionLibrary("aloha")
 CodeFragments.executeAll(CodeFragments.STARTUP) # setup complete
 
-nedLoader = CmdenvSimulationHolder.loadNEDFiles(globalCfg)
-
-# run simulation
-# TODO { "cpu-time-limit" : "1s" }
-sim = CmdenvSimulationHolder(nedLoader)
+ini = InifileContents("omnetpp.ini") # + args?
 cfg = ini.extractConfig("PureAloha1")
+nedLoader = CmdenvSimulationHolder.loadNEDFiles(cfg)
+sim = CmdenvSimulationHolder(nedLoader)
 sim.runCmdenvSimulation(cfg)
 
