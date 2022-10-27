125c271137c1b5250bcb732536d2e425c6507588
Andras Varga <andras@omnetpp.org>
2022-10-27 12:56:19 +0200

    samples/routing: L2Queue: prepare for disconnection or channel becoming disabled

--- samples/routing/node/L2Queue.cc
+++ samples/routing/node/L2Queue.cc
@@ -17,12 +17,13 @@ using namespace omnetpp;
  * Point-to-point interface module. While one frame is transmitted,
  * additional frames get queued up; see NED file for more info.
  */
-class L2Queue : public cSimpleModule
+class L2Queue : public cSimpleModule, public cListener
 {
   private:
     intval_t frameCapacity;
 
     cPacketQueue queue;
+    cChannel *transmissionChannel = nullptr;
     cMessage *endTransmissionEvent = nullptr;
     bool isBusy; // i.e. transmitting
 
@@ -39,9 +40,11 @@ class L2Queue : public cSimpleModule
   protected:
     virtual void initialize() override;
     virtual void handleMessage(cMessage *msg) override;
-    virtual void refreshDisplay() const override;
+    virtual bool isInterfaceUp();
     virtual void startTransmitting(cPacket *packet);
     virtual void startTransmittingFromQueue();
+    virtual void receiveSignal(cComponent *source, simsignal_t signalID, cObject *obj, cObject *details) override;
+    virtual void refreshDisplay() const override;
 };
 
 Define_Module(L2Queue);
@@ -59,6 +62,10 @@ void L2Queue::initialize()
     if (par("useCutThroughSwitching"))
         gate("line$i")->setDeliverImmediately(true);
 
+    subscribe(POST_MODEL_CHANGE, this);
+
+    transmissionChannel = gate("line$o")->findTransmissionChannel();
+
     frameCapacity = par("frameCapacity");
 
     qlenSignal = registerSignal("qlen");
@@ -73,17 +80,32 @@ void L2Queue::initialize()
     isBusy = false;
 }
 
+bool L2Queue::isInterfaceUp()
+{
+    return transmissionChannel && !transmissionChannel->isDisabled();
+}
+
+void L2Queue::receiveSignal(cComponent *source, simsignal_t signalID, cObject *obj, cObject *details)
+{
+    if (getSimulation()->getStage() == cSimulation::STAGE_CLEANUP)
+        return;
+
+    transmissionChannel = gate("line$o")->findTransmissionChannel();
+    if (!isBusy && !queue.isEmpty() && isInterfaceUp())
+        startTransmittingFromQueue();
+}
+
 void L2Queue::handleMessage(cMessage *msg)
 {
     if (msg == endTransmissionEvent) {
         // transmission finished, we can start transmitting next one from the queue
         EV << "Transmission finished.\n";
-        if (queue.isEmpty()) {
-            isBusy = false;
-            emit(busySignal, false);
+        if (!queue.isEmpty() && isInterfaceUp()) {
+            startTransmittingFromQueue();
         }
         else {
-            startTransmittingFromQueue();
+            isBusy = false;
+            emit(busySignal, false);
         }
     }
     else if (msg->arrivedOn("line$i")) {
@@ -94,8 +116,13 @@ void L2Queue::handleMessage(cMessage *msg)
     }
     else {  // arrived on gate "in"
         cPacket *packet = check_and_cast<cPacket *>(msg);
-        if (endTransmissionEvent->isScheduled()) {
-            // We are currently busy, so just queue up the packet.
+        if (!isInterfaceUp()) {
+            EV << "Received " << packet << " but interface is down\n";
+            emit(dropSignal, (intval_t)packet->getByteLength());
+            delete packet;
+        }
+        else if (isBusy) {
+            // queue up the packet if there's space left in the queue
             if (frameCapacity && queue.getLength() >= frameCapacity) {
                 EV << "Received " << packet << " but transmitter busy and queue full: discarding\n";
                 emit(dropSignal, (intval_t)packet->getByteLength());
