35ed964565dfb8e9240ae78acc278f3e8909e5f8
Andras Varga <andras@omnetpp.org>
2023-01-12 14:09:23 +0100

    test/core: dump.cc: cosmetics (rename local vars)

--- test/core/lib/dump.cc
+++ test/core/lib/dump.cc
@@ -61,18 +61,18 @@ void Dump::dump(cModule *mod, std::string currentIndent)
 
     cProperties *props = mod->getProperties();
     ASSERT(props != nullptr);
-    bool paramheadingprinted = false;
+    bool parametersHeadingPrinted = false;
     for (int i = 0; i < props->getNumProperties(); i++) {
-        if (!paramheadingprinted) {
+        if (!parametersHeadingPrinted) {
             printf("%s    parameters:\n", indent);
-            paramheadingprinted = true;
+            parametersHeadingPrinted = true;
         }
         printf("%s        %s\n", indent, props->get(i)->str().c_str());
     }
     for (int i = 0; i < mod->getNumParams(); i++) {
-        if (!paramheadingprinted) {
+        if (!parametersHeadingPrinted) {
             printf("%s    parameters:\n", indent);
-            paramheadingprinted = true;
+            parametersHeadingPrinted = true;
         }
         cPar& par = mod->par(i);
         std::string valueString = par.str();
@@ -87,12 +87,12 @@ void Dump::dump(cModule *mod, std::string currentIndent)
         printf("%s        %s%s = %s\n", indent, mod->par(i).getFullName(), props2str(par.getProperties()).c_str(), valueString.c_str());
     }
 
-    bool gateheadingprinted = false;
+    bool gatesHeadingPrinted = false;
     for (cModule::GateIterator it(mod); !it.end(); it++) {
         cGate *gate = *it;
-        if (!gateheadingprinted) {
+        if (!gatesHeadingPrinted) {
             printf("%s    gates:\n", indent);
-            gateheadingprinted = true;
+            gatesHeadingPrinted = true;
         }
         printf("%s        %s%s: %s", indent, gate->getFullName(), props2str(gate->getProperties()).c_str(), gate->str().c_str());
         if (printClassNames && gate->getChannel() != nullptr)
@@ -100,11 +100,11 @@ void Dump::dump(cModule *mod, std::string currentIndent)
         printf("\n");
     }
 
-    bool submodheadingprinted = false;
+    bool submodulesHeadingPrinted = false;
     for (cModule::SubmoduleIterator it(mod); !it.end(); it++) {
-        if (!submodheadingprinted) {
+        if (!submodulesHeadingPrinted) {
             printf("%s    submodules:\n", indent);
-            submodheadingprinted = true;
+            submodulesHeadingPrinted = true;
         }
         dump(*it, currentIndent+"        ");
     }
