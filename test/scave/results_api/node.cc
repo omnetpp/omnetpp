6c6710060e889935d88fe5a48218221a34a1c916
TÖRÖK Attila <torokati44@gmail.com>
2023-02-06 15:22:10 +0100

    test/scave/results_api: Add a zero result to check conversion from string

--- test/scave/results_api/node.cc
+++ test/scave/results_api/node.cc
@@ -9,6 +9,7 @@ class Node : public cSimpleModule
 
         simsignal_t foo1 = registerSignal("foo1");
         simsignal_t foo2 = registerSignal("foo2");
+
         for (int i = 0; i < 100; i++) {
             cTimestampedValue tmp1((simtime_t)i, (double)binomial(40, 0.5) * bar);
             emit(foo1, &tmp1);
@@ -16,6 +17,10 @@ class Node : public cSimpleModule
             cTimestampedValue tmp2((simtime_t)i, (double)exponential(10) * bar);
             emit(foo2, &tmp2);
         }
+
+        simsignal_t zero = registerSignal("zero");
+        emit(zero, 0.0);
+
     }
 };
 
