036d0de668ee57025a653bcffcceed9cc0760099
Andras Varga <andras@omnetpp.org>
2023-03-09 17:01:10 +0100

    common: UnitConversion: getBestUnit() reimplemented, accepts Options struct; etc
    NOTE: now it convers minutes, hours, days to seconds

--- test/common/lib/testutil.h
+++ test/common/lib/testutil.h
@@ -5,17 +5,13 @@
 
 using namespace omnetpp;
 
-namespace testlib {
-
-void printEnum(const char *enumname);
-
-bool isRunningTest(const char *testname);
-
 // Assertion that is not only is effective in DEBUG mode.
 #define TEST_ASSERT(expr) \
   ((void) ((expr) ? 0 : (throw omnetpp::cRuntimeError("TEST_ASSERT: Condition '%s' does not hold in function '%s' at %s:%d", \
                                    #expr, __FUNCTION__, __FILE__, __LINE__), 0)))
 
+
+namespace testlib {
 }
 
 #endif
