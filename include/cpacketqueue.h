07088908331e65f92b8b8f11f94e9612d29a3618
Andras Varga <andras@omnetpp.org>
2023-03-30 12:22:39 +0200

    sim: backported cPacketQueue fixes
    1. some cQueue methods like insert/remove were hidden in cPacketQueue by
       similarly named functions of different signature, leading to wrong
       behavior
    
    2. fix: cPacketQueue's clear() didn't zero the bit length
    
    Test was also faulty! (clear() and copy ctor test missing, op= test printed the wrong bit length!!)
    Reported by Guglielmo Morandin.
    
    cherry-picked from omnetpp-4.3.x

--- include/cpacketqueue.h
+++ include/cpacketqueue.h
@@ -36,9 +36,17 @@ class SIM_API cPacketQueue : public cQueue
   private:
     int64 bitlength;
 
+  private:
+    void copy(const cPacketQueue& other);
+    virtual void insert(cObject *obj);
+    virtual void insertBefore(cObject *where, cObject *obj);
+    virtual void insertAfter(cObject *where, cObject *obj);
+    virtual cObject *remove(cObject *obj);
+
   protected:
     // internal
-    void addLen(cPacket *pkt);
+    void addLength(cPacket *pkt);
+    cPacket *checkPacket(cObject *obj);
 
   public:
     /** @name Constructors, destructor, assignment. */
@@ -133,6 +141,12 @@ class SIM_API cPacketQueue : public cQueue
      * is empty, cRuntimeError is thrown.
      */
     virtual cPacket *pop();
+
+    /**
+     * Empties the container. Contained objects that were owned by the
+     * queue (see getTakeOwnership()) will be deleted.
+     */
+    virtual void clear();
     //@}
 
     /** @name Query functions. */
--- src/sim/cpacketqueue.cc
+++ src/sim/cpacketqueue.cc
@@ -82,41 +82,74 @@ void cPacketQueue::parsimUnpack(cCommBuffer *buffer)
 #endif
 }
 
-void cPacketQueue::addLen(cPacket *pkt)
+void cPacketQueue::addLength(cPacket *pkt)
 {
-    if (pkt)
-        bitlength += pkt->getBitLength();
+    bitlength += pkt->getBitLength();
+}
+
+cPacket *cPacketQueue::checkPacket(cObject *obj)
+{
+    cPacket *pkt = dynamic_cast<cPacket *>(obj);
+    if (!pkt)
+        throw cRuntimeError(this, "insert...(): Cannot cast (%s)%s to cPacket", obj->getClassName(), obj->getFullName());
+    return pkt;
 }
 
 void cPacketQueue::insert(cPacket *pkt)
 {
-    addLen(pkt);
     cQueue::insert(pkt);
+    addLength(pkt);
 }
 
 void cPacketQueue::insertBefore(cPacket *where, cPacket *pkt)
 {
-    addLen(pkt);
     cQueue::insertBefore(where, pkt);
+    addLength(pkt);
 }
 
 void cPacketQueue::insertAfter(cPacket *where, cPacket *pkt)
 {
-    addLen(pkt);
     cQueue::insertAfter(where, pkt);
+    addLength(pkt);
 }
 
 cPacket *cPacketQueue::remove(cPacket *pkt)
 {
-    cPacket *msg1 = (cPacket *)cQueue::remove(pkt);
-    if (pkt)
-        bitlength -= msg1->getBitLength();
-    return msg1;
+    return (cPacket *)remove((cObject *)pkt);
 }
 
 cPacket *cPacketQueue::pop()
 {
     cPacket *pkt = (cPacket *)cQueue::pop();
+    if (pkt)
+        bitlength -= pkt->getBitLength();
+    return pkt;
+}
+
+void cPacketQueue::clear()
+{
+    cQueue::clear();
+    bitlength = 0;
+}
+
+void cPacketQueue::insert(cObject *obj)
+{
+    insert(checkPacket(obj));
+}
+
+void cPacketQueue::insertBefore(cObject *where, cObject *obj)
+{
+    insertBefore(checkPacket(where), checkPacket(obj));
+}
+
+void cPacketQueue::insertAfter(cObject *where, cObject *obj)
+{
+    insertAfter(checkPacket(where), checkPacket(obj));
+}
+
+cObject *cPacketQueue::remove(cObject *obj)
+{
+    cPacket *pkt = (cPacket *)cQueue::remove(obj);
     if (pkt)
         bitlength -= pkt->getBitLength();
     return pkt;
