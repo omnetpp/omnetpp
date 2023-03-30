835ce5ef3de21c8db0bb69ebbdcad27355aae84e
Andras Varga <andras@omnetpp.org>
2023-03-30 09:36:42 +0200

    sim: cADByStddev: fix in operator=

--- src/sim/cdetect.cc
+++ src/sim/cdetect.cc
@@ -247,7 +247,7 @@ cADByStddev& cADByStddev::operator=(const cADByStddev& res)
    // setHostObject(res.hostObject());
    go=res.go; resaccval=res.resaccval;
    accuracy=res.accuracy; sctr=res.sctr;
-   ssum=res.ssum; sqrsum=sqrsum;
+   ssum=res.ssum; sqrsum=res.sqrsum;
    repeats=res.repeats; detreps=res.detreps;
    pdf=res.pdf; pdfdata=res.pdfdata;
    return *this;
