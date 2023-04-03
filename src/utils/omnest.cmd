01c6a2cb88e747481c356755999e605123868f5f
Andras Varga <andras@omnetpp.org>
2023-04-03 20:25:57 +0200

    minor adjustments

--- src/utils/omnest.cmd
+++ src/utils/omnest.cmd
@@ -1,7 +1,7 @@
 @echo off
 SET IDEDIR=%~dp0\..\ide                                                                                        
-SET LAUNCHER=omnest
+SET LAUNCHER=omnetpp
 cd %IDEDIR%
 SET PATH=%~dp0;%~dp0\..\msys\bin;%~dp0\..\mingw\bin;%PATH%
-echo Starting the OMNEST IDE...
+echo Starting the OMNeT++ IDE...
 start %LAUNCHER% >out.log
