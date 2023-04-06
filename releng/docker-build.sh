77a3b2ac8959adb729c06b3550fb335bb2bce896
Rudolf Hornig <rudi@omnetpp.org>
2023-04-06 11:48:52 +0200

    prod: docker build images updated to fix maven crash on java 17

--- releng/docker-build.sh
+++ releng/docker-build.sh
@@ -6,6 +6,6 @@ OMNETPP_ROOT=$(pwd)
 docker run --network none -v "$OMNETPP_ROOT":/root/omnetpp-repo -e what -e GIT_VERSION -e BUILD_DOC \
        -e BUILD_CORE_DISTRO -e BUILD_LINUX_X86_64_DISTRO -e BUILD_LINUX_AARCH64_DISTRO \
        -e BUILD_WIN32_X86_64_DISTRO -e BUILD_MACOS_X86_64_DISTRO -e BUILD_MACOS_AARCH64_DISTRO \
-       ghcr.io/omnetpp/distrobuild:eclipse4.25-tools220810-230323 && \
+       ghcr.io/omnetpp/distrobuild:eclipse4.27-tools220810-230406 && \
 docker cp $(docker ps -lq):/root/omnetpp/out/dist/ releng && \
 docker rm $(docker ps -lq)
