7b704e0846a5cbcb71a58e4ef6465ffbd64bba31
TÖRÖK Attila <torokati44@gmail.com>
2023-05-31 16:02:55 +0200

    ide/scave: swtplot ported from posix_ipc to multiprocessing.shared_memory

--- ui/org.omnetpp.scave.pychart/python/omnetpp/scave/impl_ide/swtplot.py
+++ ui/org.omnetpp.scave.pychart/python/omnetpp/scave/impl_ide/swtplot.py
@@ -1,18 +1,13 @@
 import os
-import platform
 import numpy as np
 import pandas as pd
 import pickle as pl
-
-# posix_ipc is required for POSIX shm on Linux and Mac
-if platform.system() in ['Linux', 'Darwin']:
-    import posix_ipc
-import mmap
 
 import functools
 print = functools.partial(print, flush=True)
 
 from omnetpp.internal import Gateway
+from omnetpp.internal.shmutil import shm_ctor_wrapper
 
 import py4j
 from py4j.java_collections import MapConverter
@@ -109,18 +104,15 @@ def plot_lines(df, props):
     if sorted(list(df.columns)) != sorted(["key", "label", "xs", "ys"]):
         raise RuntimeError("Invalid DataFrame format in plot_lines")
 
-    # only used on posix, to unlink them later
     shm_objs = list()
-    # only used on windows, to prevent gc
-    mmap_objs = list()
 
     # the key of each row is appended to the property names in Java
     Gateway.chart_plotter.plotVectors(pl.dumps([
         {
             "key": row.key,
             "title": str(row.label),
-            "xs": _put_array_in_shm(row.xs, shm_objs, mmap_objs),
-            "ys": _put_array_in_shm(row.ys, shm_objs, mmap_objs)
+            "xs": _put_array_in_shm(row.xs, shm_objs),
+            "ys": _put_array_in_shm(row.ys, shm_objs)
         }
         for row in df.itertuples(index=False)
     ]), MapConverter().convert(props, Gateway.gateway._gateway_client))
@@ -219,7 +211,7 @@ def _list_to_bytes(l):
     return np.array(np.array(l), dtype=np.dtype('>f8')).tobytes()
 
 
-def _put_array_in_shm(arr, shm_objs, mmap_objs):
+def _put_array_in_shm(arr, shm_objs):
     """
     Internal. Turns a `np.array` into a byte sequence, and writes it into
     a newly created (platform-specific) SHM object with the given name.
@@ -234,27 +226,10 @@ def _put_array_in_shm(arr, shm_objs, mmap_objs):
     name = "/vecdata-" + str(os.getpid()) + "-" + str(_vector_data_counter)
     _vector_data_counter += 1
 
-    system = platform.system()
+    name, shm = shm_ctor_wrapper(name, create=True, size=arr.nbytes)
 
-    if system in ['Linux', 'Darwin']:
-        mem = posix_ipc.SharedMemory(name, posix_ipc.O_CREAT | posix_ipc.O_EXCL, size=arr.nbytes)
-
-        shm_objs.append(mem)
-
-        if system == 'Darwin':
-            # for some reason we can't write to the shm fd directly on mac, only after mmap-ing it
-            with mmap.mmap(mem.fd, length=mem.size) as mf:
-                mf.write(arr.astype(np.dtype('>f8')).tobytes())
-        else:
-            with open(mem.fd, 'wb') as mf:
-                arr.astype(np.dtype('>f8')).tofile(mf)
-
-    elif system == 'Windows':
-        # on Windows, the mmap module in itself provides shared memory functionality
-        mm = mmap.mmap(-1, arr.nbytes, tagname=name)
-        mmap_objs.append(mm)
-        mm.write(arr.astype(np.dtype('>f8')).tobytes())
-    else:
-        raise RuntimeError("unsupported platform")
+    shm_objs.append(shm)
+    # shm object may be oversized (rounded up to whole page), hence the [:arr.nbytes]
+    shm.buf[:arr.nbytes] = arr.astype(np.dtype('>f8')).tobytes()[:]
 
     return name + " " + str(arr.nbytes)
