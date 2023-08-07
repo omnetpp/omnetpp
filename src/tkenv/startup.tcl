ed2776b5ae5dedd521055f74ed41971ff4b1d8fe
Andras Varga <andras@omnetpp.org>
2023-08-07 12:29:55 +0200

    tkenv: fix runtime error in startup code on Macs with ARM CPU
    tkdock is unavailable for arm64

--- src/tkenv/startup.tcl
+++ src/tkenv/startup.tcl
@@ -424,7 +424,9 @@ proc setOSXDockIcon {} {
     }
 
     # set the icon
-    tkdock::switchIcon $icon
+    if [catch {tkdock::switchIcon $icon}] {
+        debug "tkdock::switchIcon failed"
+    }
 }
 
 
