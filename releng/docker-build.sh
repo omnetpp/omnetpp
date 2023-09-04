093d38c6c643b597a75a973c376db7cc3c121729
Rudolf Hornig <rudi@omnetpp.org>
2023-09-04 12:33:26 +0200

    prod: eclipse and tycho and build image updates
    - tycho 2.7.5, eclipse 4.27, cdt 11.1
    - updated butik plugin dependencies + compatibility fixes for SVG export
    - updated build docker images (to include eclipse 4.27)

--- releng/docker-build.sh
+++ releng/docker-build.sh
@@ -6,6 +6,6 @@ OMNETPP_ROOT=$(pwd)
 docker run --network none -v "$OMNETPP_ROOT":/root/omnetpp-repo -e what -e GIT_VERSION -e BUILD_DOC -e BUILD_IDE \
        -e BUILD_CORE_DISTRO -e BUILD_LINUX_X86_64_DISTRO \
        -e BUILD_WIN32_X86_64_DISTRO -e BUILD_MACOSX_DISTRO \
-       omnetpp/distrobuild-opp5:eclipse4.21-tools210809-210927 && \
+       omnetpp/distrobuild-opp5:eclipse4.27-tools210809-230907 && \
 docker cp $(docker ps -lq):/root/omnetpp/out/dist/ releng && \
 docker rm $(docker ps -lq)
