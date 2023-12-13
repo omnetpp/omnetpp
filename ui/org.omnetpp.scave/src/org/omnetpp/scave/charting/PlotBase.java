omnetpp/omnetpp

commit 32934a22e713a745ccc28dff59981291ad5f8b03

ide/scave: native plots: turn off adjusting zoom on legend item enablement changes
Reason: when it's on, view adjustments make the plot contents jump
around when items are enabled/disabled, making it hard to follow or
compare the plotted lines.

ui/org.omnetpp.scave/src/org/omnetpp/scave/charting/PlotBase.java
@@ -94,6 +94,7 @@ public abstract class PlotBase extends ZoomableCachingCanvas implements IPlotVie
     protected String titleText;
     protected Legend legend = new Legend(this);
     protected LegendTooltip legendTooltip;
+    private boolean adjustViewOnLegendItemEnablementChange = false; //TODO make user-settable?
 
     private String statusText = "No data available."; // displayed when there's no dataset
     private String warningText = null;
@@ -675,30 +676,32 @@ public abstract class PlotBase extends ZoomableCachingCanvas implements IPlotVie
     }
 
     public void legendItemEnablementChanged() {
-        double zx = getZoomX();
-        double zy = getZoomY();
-        long vt = getViewportTop();
-        long vl = getViewportLeft();
+        if (adjustViewOnLegendItemEnablementChange) {
+            double zx = getZoomX();
+            double zy = getZoomY();
+            long vt = getViewportTop();
+            long vl = getViewportLeft();
 
-        boolean shouldZoomOutX = getZoomX()==0 || isZoomedOutX();
-        boolean shouldZoomOutY = getZoomY()==0 || isZoomedOutY();
+            boolean shouldZoomOutX = getZoomX()==0 || isZoomedOutX();
+            boolean shouldZoomOutY = getZoomY()==0 || isZoomedOutY();
 
-        chartChanged();
-        chartArea = calculatePlotArea();
-        updateArea();
+            chartChanged();
+            chartArea = calculatePlotArea();
+            updateArea();
 
-        if (shouldZoomOutX)
-            zoomToFitX();
-        else {
-            setZoomX(zx);
-            scrollHorizontalTo(vl);
-        }
+            if (shouldZoomOutX)
+                zoomToFitX();
+            else {
+                setZoomX(zx);
+                scrollHorizontalTo(vl);
+            }
 
-        if (shouldZoomOutY)
-            zoomToFitY();
-        else {
-            setZoomY(zy);
-            scrollVerticalTo(vt);
+            if (shouldZoomOutY)
+                zoomToFitY();
+            else {
+                setZoomY(zy);
+                scrollVerticalTo(vt);
+            }
         }
     }
 

commit 6b7116870f900e0c35f50893ff8af131f3917331

ide/scave: Fix chart not updating when series visibility is toggled by the legend
This regressed in 32934a22e713a745ccc28dff59981291ad5f8b03,
"ide/scave: native plots: turn off adjusting zoom on legend item enablement changes".

ui/org.omnetpp.scave/src/org/omnetpp/scave/charting/PlotBase.java
@@ -704,6 +704,9 @@ public abstract class PlotBase extends ZoomableCachingCanvas implements IPlotVie
                 scrollVerticalTo(vt);
             }
         }
+        else {
+            chartChanged();
+        }
     }
 
     protected void paintInsets(Graphics graphics) {