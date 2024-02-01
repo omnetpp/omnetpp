b9f16a8f5f88bfc1aba4866358e88f645447797e
Rudolf Hornig <rudi@omnetpp.org>
2024-02-01 10:49:21 +0100

    ide: detect if IDE was started from opp_env and fine tune the model installation instructions
    - the Help / Install Simulation Models menu item warns that opp_env should be preferred
    - the FirstStep dialog on an empty workspace provides a warning for the INET
      item and comes up in unchecked state by default to encourage installation
      using opp_env.

--- ui/org.omnetpp.main/src/org/omnetpp/ide/installer/InstallSimulationModelsHandler.java
+++ ui/org.omnetpp.main/src/org/omnetpp/ide/installer/InstallSimulationModelsHandler.java
@@ -3,11 +3,31 @@ package org.omnetpp.ide.installer;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.ui.PlatformUI;
+import org.omnetpp.common.OmnetppUtils;
 
 public class InstallSimulationModelsHandler extends AbstractHandler {
     @Override
     public Object execute(ExecutionEvent event) throws ExecutionException {
-        new InstallSimulationModelsDialog(null).open();
+    	
+		if (OmnetppUtils.isInsideOppEnv()) {
+    		boolean wantExit = MessageDialog.openQuestion(null, "Model Installation", 
+    				"We detected that the IDE and OMNeT++ were installed via opp_env. "
+    				+ "For consistency and optimal performance, it is recommended to install "
+    				+ "simulation models via opp_env, not from the IDE. \n"
+    				+ "\n"
+    				+ "Do you want to exit the IDE and install models using opp_env?");	    	
+    		if (wantExit) {
+    			if (MessageDialog.openConfirm(null, "Confirm", "Confirm exiting the IDE."))
+    				PlatformUI.getWorkbench().close();
+    			
+    			return null;
+    		}
+    	}
+		
+		new InstallSimulationModelsDialog(null).open();
         return null;
     }
+
 }
