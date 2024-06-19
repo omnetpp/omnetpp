omnetpp/omnetpp

commit b24f5617294b2bdfc57f6431d1ccb8a840b43a23

python/scave: proofreading; corresponding appendix in manual re-generated

python/omnetpp/scave/ideplot.py
@@ -41,7 +41,7 @@ else:
 
 
 
-def plot(xs, ys, key=None, label=None, drawstyle=None, linestyle=None, linewidth=None, color=None, marker=None, markersize=None):
+def plot(x, y, key=None, label=None, drawstyle=None, linestyle=None, linewidth=None, color=None, marker=None, markersize=None):
     """
     Plot y versus x as lines and/or markers. Call `plot` multiple times
     to plot multiple sets of data.
@@ -52,7 +52,7 @@ def plot(xs, ys, key=None, label=None, drawstyle=None, linestyle=None, linewidth
     - `key` (string): Identifies the series in the native plot widget.
     - `label` (string): Series label for the legend
     - `drawstyle` (string): Matplotlib draw style ('default', 'steps', 'steps-pre', 'steps-mid', 'steps-post')
-    - `linestyle` (string): Matplotlib line style ('-', '--', '-.', ':', etc}
+    - `linestyle` (string): Matplotlib line style ('-', '--', '-.', ':', etc)
     - `linewidth` (float): Line width in pixels
     - `color` (string): Matplotlib color name or abbreviation ('b' for blue, 'g' for green, etc.)
     - `marker` (string): Matplotlib marker name ('.', ',', 'o', 'x', '+', etc.)
@@ -65,13 +65,13 @@ def plot(xs, ys, key=None, label=None, drawstyle=None, linestyle=None, linewidth
 def hist(x, bins, key=None, density=False, weights=None, cumulative=False, bottom=None, histtype='stepfilled', color=None, label=None, linewidth=None,
          underflows=0.0, overflows=0.0, minvalue=math.nan, maxvalue=math.nan):
     """
-    Make a histogram plot. This function adds one histogram the bar plot; make
+    Make a histogram plot. This function adds one histogram to the bar plot; make
     multiple calls to add multiple histograms.
 
     Parameters:
 
     - `x` (array-like): Input values.
-    - `bins` (array-like): Bin edges, including left edge of first bin and right edge of last bin.
+    - `bins` (array-like): Bin edges, including the left edge of the first bin and the right edge of the last bin.
     - `key` (string): Identifies the series in the native plot widget.
     - `density` (bool): See `mpl.hist()`.
     - `weights` (array-like): Weights.
@@ -86,7 +86,7 @@ def hist(x, bins, key=None, density=False, weights=None, cumulative=False, botto
 
     Restrictions:
 
-    1. Overflow bin data (minvalue, maxvalue, underflows and overflows) is not accepted by `pyplot.hist()`.
+    1. Overflow bin data (minvalue, maxvalue, underflows, and overflows) is not accepted by `pyplot.hist()`.
     2. The native plot widget only accepts a precomputed histogram (using the trick documented for `pyplot.hist()`)
     """
     return impl.hist(**locals())
@@ -109,30 +109,30 @@ def bar(x, height, width=0.8, key=None, label=None, color=None, edgecolor=None):
     - `height` (scalar or sequence of scalars): The height(s) of the bars.
     - `width` (scalar or array-like): The width(s) of the bars.
     - `key` (string): Identifies the series in the native plot widget.
-    - `label` (string): The label of the series the bars represent .
+    - `label` (string): The label of the series the bars represent.
     - `color` (string): The fill color of the bars.
     - `edgecolor` (string): The edge color of the bars.
 
     The native plot implementation has the following restrictions:
 
-    - widths are automatic (parameter is ignored)
+    - widths are automatic (the parameter is ignored)
     - x coordinates are automatic (values are ignored)
     - height must be a sequence (cannot be a scalar)
     - in multiple calls to bar(), the lengths of the height sequence must be
       equal (i.e. all series must have the same number of values)
-    - default color is grey (Matplotlib assigns a different color to each series)
+    - the default color is grey (Matplotlib assigns a different color to each series)
     """
     return impl.bar(**locals())
 
 def set_property(key, value):
     """
     Sets one property of the native plot widget to the given value. When invoked
-    outside the contex of a native plot widget, the function does nothing.
+    outside the context of a native plot widget, the function does nothing.
 
     Parameters:
 
     - `key` (string): Name of the property.
-    - `value` (string): The value to set. If any other type than string is passed in, it will be converted to string.
+    - `value` (string): The value to set. If any other type other than a string is passed in, it will be converted to a string.
     """
     return impl.set_property(**locals())
 
@@ -140,7 +140,7 @@ def set_properties(props):
     """
     Sets several properties of the native plot widget. It is functionally equivalent to
     repeatedly calling `set_property` with the entries of the `props` dictionary.
-    When invoked outside the contex of a native plot widget (TODO?), the function does nothing.
+    When invoked outside the context of a native plot widget (TODO?), the function does nothing.
 
     Parameters:
 
@@ -151,9 +151,9 @@ def set_properties(props):
 def get_supported_property_keys():
     """
     Returns the list of property names that the native plot widget supports, such as
-    'Plot.Title', 'X.Axis.Max' and 'Legend.Display', among many others.
+    'Plot.Title', 'X.Axis.Max', and 'Legend.Display', among many others.
 
-    Note: This method has no equivalent in `pyplot`. When the script runs outside the IDE  (TODO?),
+    Note: This method has no equivalent in `pyplot`. When the script runs outside the IDE (TODO?),
     the method returns an empty list.
     """
     return impl.get_supported_property_keys(**locals())
@@ -172,19 +172,19 @@ def title(label: str):
 
 def xlabel(xlabel: str):
     """
-    Sets the label of the X axis to the given string.
+    Sets the label of the X-axis to the given string.
     """
     impl.xlabel(**locals())
 
 def ylabel(ylabel: str):
     """
-    Sets the label of the Y axis to the given string..
+    Sets the label of the Y-axis to the given string.
     """
     impl.ylabel(**locals())
 
 def xlim(left=None, right=None):
     """
-    Sets the limits of the X axis.
+    Sets the limits of the X-axis.
 
     Parameters:
 
@@ -195,7 +195,7 @@ def xlim(left=None, right=None):
 
 def ylim(bottom=None, top=None):
     """
-    Sets the limits of the Y axis.
+    Sets the limits of the Y-axis.
 
     Parameters:
 
@@ -206,13 +206,13 @@ def ylim(bottom=None, top=None):
 
 def xscale(value: str):
     """
-    Sets the scale of the X axis. Possible values are 'linear' and 'log'.
+    Sets the scale of the X-axis. Possible values are 'linear' and 'log'.
     """
     impl.xscale(**locals())
 
 def yscale(value: str):
     """
-    Sets the scale of the Y axis.
+    Sets the scale of the Y-axis.
     """
     impl.yscale(**locals())
 
@@ -235,7 +235,7 @@ def grid(b=True, which="major"):
     Parameters:
 
     - `b` (bool or `None`): Whether to show the grid lines.
-    - `which` ('major', 'minor' or 'both'): The grid lines to apply the changes on.
+    - `which` ('major', 'minor', or 'both'): The grid lines to apply the changes to.
     """
     impl.grid(**locals())
 

python/omnetpp/scave/chart.py
@@ -31,7 +31,7 @@ else:
 def get_properties():
     """
     Returns the currently set properties of the chart as a `dict`
-    whose keys and values are both all strings.
+    whose keys and values are both strings.
     """
     return impl.get_properties(**locals())
 
@@ -51,7 +51,7 @@ def get_name():
 
 def get_chart_type():
     """
-    Returns the chart type, one of the strings "bar"/"histogram"/"line"/"matplotlib"
+    Returns the chart type, which is one of the strings "BAR", "LINE", "HISTOGRAM", and "MATPLOTLIB".
     """
     return impl.get_chart_type(**locals())
 
@@ -71,6 +71,6 @@ def set_suggested_chart_name(name):
 def set_observed_column_names(column_names):
     """
     Sets the DataFrame column names observed during the chart script.
-    The IDE may use it for content assist when the user edits the legend format string.
+    The IDE may use them for content assist when the user edits the legend format string.
     """
     return impl.set_observed_column_names(**locals())

python/omnetpp/scave/results.py
@@ -216,8 +216,8 @@ def _guarded_result_query_func(func):
     return inner
 
 
-def _fix_ndarray_shapes(df : pd.DataFrame, columns : List[str]):
-    """ A workaround for: https://github.com/pandas-dev/pandas/issues/53565 """
+def _fix_ndarray_shapes(df: pd.DataFrame, columns: List[str]):
+    """A workaround for: https://github.com/pandas-dev/pandas/issues/53565"""
     for col in columns:
         df[col] = df[col].apply(lambda a: a.reshape(1)
                                             if isinstance(a, np.ndarray) and a.shape == ()
@@ -239,7 +239,7 @@ def _dropna_except(df : pd.DataFrame, keep_columns : List[str]):
 def get_serial():
     """
     Returns an integer that is incremented every time the set of loaded results
-    change, typically as a result of the IDE loading, reloading or unloading
+    changes, typically as a result of the IDE loading, reloading or unloading
     a scalar or vector result file. The serial can be used for invalidating
     cached intermediate results when their input changes.
     """
@@ -248,10 +248,10 @@ def get_serial():
 def set_inputs(filenames):
     """
     Specifies the set of simulation result files (.vec, .sca) to use as input
-    for the query functions. The argument may be a single string, or a list of
-    strings. Each string is interpreted as a file or directory path, and may
+    for the query functions. The argument may be a single string or a list of
+    strings. Each string is interpreted as a file or directory path and may
     also contain wildcards. In addition to `?` and `*`, `**` (which is able to
-    match several directory levels) is also accepted as wildcard. If a path
+    match several directory levels) is also accepted as a wildcard. If a path
     corresponds to a directory, it is interpreted as `[ "<dir>/**/*.sca",
     "<dir>/**/*.vec" ]`, that is, all result files will be loaded from that
     directory and recursively all its subdirectories.
@@ -264,8 +264,8 @@ def set_inputs(filenames):
 def add_inputs(filenames):
     """
     Appends to the set of simulation result files (.vec, .sca) to use as input
-    for the query functions.  The argument may be a single string, or a list of
-    strings. Each string is interpreted as a file or directory path, and may
+    for the query functions. The argument may be a single string or a list of
+    strings. Each string is interpreted as a file or directory path and may
     also contain wildcards (`?`, `*`, `**`). See `set_inputs()` for more details.
     """
     impl.add_inputs(filenames)
@@ -273,20 +273,20 @@ def add_inputs(filenames):
 def read_result_files(filenames, filter_expression=None, include_fields_as_scalars=False, vector_start_time=-inf, vector_end_time=inf):
     """
     Loads the simulation result files specified in the first argument
-    `filenames`, and returns the filtered set of results and metadata as a
+    `filenames` and returns the filtered set of results and metadata as a
     Pandas `DataFrame`.
 
     The `filenames` argument specifies the set of simulation result files (.vec,
-    .sca) to load.  The argument may be a single string, or a list of strings.
-    Each string is interpreted as a file or directory path, and may also contain
+    .sca) to load. The argument may be a single string or a list of strings.
+    Each string is interpreted as a file or directory path and may also contain
     wildcards (`?`, `*`, `**`). See `set_inputs()` for more details on this
     format.
 
     It is possible to limit the set of results to return by specifying a filter
-    expression, and vector start/end times.
+    expression and vector start/end times.
 
     Parameters:
-    - `filenames` (string, or list of strings): Specifies the result files to
+    - `filenames` (string or list of strings): Specifies the result files to
       load.
     - `filter_expression` (string): The filter expression to select the desired
       items to load. Example: `module =~ "*host*" AND name =~ "numPacket*"`
@@ -326,8 +326,7 @@ def get_results(filter_or_dataframe="", row_types=None, omit_unused_columns=True
       statistics and histograms (`:min`, `:mean`, etc.) are also returned as
       synthetic scalars.
     - `start_time`, `end_time` (double): Optional time limits to trim the data
-      of vector type results. The unit is seconds, both the `vectime` and
-      `vecvalue` arrays will be affected, the interval is left-closed,
+      of vector type results. The unit is seconds, the interval is left-closed,
       right-open.
 
     Returns: a `DataFrame` in the "raw" format (see the corresponding section of
@@ -505,8 +504,8 @@ def get_scalars(filter_or_dataframe="", include_attrs=False, include_fields=Fals
       desired scalars, or a dataframe in the "raw" format. Example: `name =~
       "channelUtilization*" AND runattr:replication =~ "#0"`
     - `include_attrs` (bool): Optional. When set to `True`, result attributes
-      (like `unit` or `source` for example) are appended to the DataFrame,
-      pivoted into columns.
+      (like `unit` for example) are appended to the DataFrame, pivoted into
+      columns.
     - `include_fields` (bool): Optional. If `True`, the fields of statistics and
       histograms (`:min`, `:mean`, etc.) are also returned as synthetic scalars.
     - `include_runattrs`, `include_itervars`, `include_param_assignments`,
@@ -573,8 +572,7 @@ def get_parameters(filter_or_dataframe="", include_attrs=False, include_runattrs
       desired parameters, or a dataframe in the "raw" format. Example: `name =~
       "x" AND module =~ Aloha.server`
     - `include_attrs` (bool): Optional. When set to `True`, result attributes
-      (like `unit` for example) are appended to the DataFrame, pivoted into
-      columns.
+      (like `unit`) are appended to the DataFrame, pivoted into columns.
     - `include_runattrs`, `include_itervars`, `include_param_assignments`,
       `include_config_entries` (bool): Optional. When set to `True`, additional
       pieces of metadata about the run is appended to the DataFrame, pivoted
@@ -619,8 +617,8 @@ def get_vectors(filter_or_dataframe="", include_attrs=False, include_runattrs=Fa
       desired vectors, or a dataframe in the "raw" format. Example: `name =~
       "radioState*" AND runattr:replication =~ "#0"`
     - `include_attrs` (bool): Optional. When set to `True`, result attributes
-      (like `unit` or `source` for example) are appended to the DataFrame,
-      pivoted into columns.
+      (like `unit` or `source`) are appended to the DataFrame, pivoted into
+      columns.
     - `include_runattrs`, `include_itervars`, `include_param_assignments`,
       `include_config_entries` (bool): Optional. When set to `True`, additional
       pieces of metadata about the run is appended to the DataFrame, pivoted
@@ -698,8 +696,8 @@ def get_statistics(filter_or_dataframe="", include_attrs=False, include_runattrs
       desired statistics, or a dataframe in the "raw" format. Example: `name =~
       "collisionLength:stat" AND itervar:iaMean =~ "5"`
     - `include_attrs` (bool): Optional. When set to `True`, result attributes
-      (like `unit` or `source` for example) are appended to the DataFrame,
-      pivoted into columns.
+      (like `unit` or `source`) are appended to the DataFrame, pivoted into
+      columns.
     - `include_runattrs`, `include_itervars`, `include_param_assignments`,
       `include_config_entries` (bool): Optional. When set to `True`, additional
       pieces of metadata about the run is appended to the DataFrame, pivoted
@@ -714,7 +712,7 @@ def get_statistics(filter_or_dataframe="", include_attrs=False, include_runattrs
     - `runID` (string): Identifies the simulation run
     - `module` (string): Hierarchical name (a.k.a. full path) of the module that
       recorded the result item
-    - `name` (string): The name of the vector
+    - `name` (string): The name of the statistic
     - `count`, `sumweights`, `mean`, `stddev`, `min`, `max` (double): The
       characteristic mathematical properties of the statistics result
     - Additional metadata items (result attributes, run attributes, iteration

commit 11303bfa3c2f2408d72e568cde7b097cf188e4c7

scave/python: Restore `ideplot.plot` parameter names, update the manual
This got regressed accidentally by b24f561729.

doc/src/manual/appendix-chart-api.tex
@@ -858,16 +858,16 @@ widget, and False otherwise.
 \label{cha:chart-api:omnetpp.scave.ideplot:plot}
 
 \begin{flushleft}
-\ttt{plot(x, y, key=None, label=None, drawstyle=None, linestyle=None, linewidth=None, color=None, marker=None, markersize=None)}
+\ttt{plot(xs, ys, key=None, label=None, drawstyle=None, linestyle=None, linewidth=None, color=None, marker=None, markersize=None)}
 \end{flushleft}
 
-\par Plot y versus x as lines and/or markers. Call \ttt{plot} multiple times
+\par Plot ys versus xs as lines and/or markers. Call \ttt{plot} multiple times
 to plot multiple sets of data.
 
 \par Parameters:
 
 \begin{itemize}
-  \item \ttt{x}, \ttt{y} \textit{(array-like or scalar)}: The horizontal / vertical coordinates of the data points.
+  \item \ttt{xs}, \ttt{ys} \textit{(array-like or scalar)}: The horizontal / vertical coordinates of the data points.
   \item \ttt{key} \textit{(string)}: Identifies the series in the native plot widget.
   \item \ttt{label} \textit{(string)}: Series label for the legend
   \item \ttt{drawstyle} \textit{(string)}: Matplotlib draw style ('default', 'steps', 'steps-pre', 'steps-mid', 'steps-post')

python/omnetpp/scave/ideplot.py
@@ -41,14 +41,14 @@ else:
 
 
 
-def plot(x, y, key=None, label=None, drawstyle=None, linestyle=None, linewidth=None, color=None, marker=None, markersize=None):
+def plot(xs, ys, key=None, label=None, drawstyle=None, linestyle=None, linewidth=None, color=None, marker=None, markersize=None):
     """
-    Plot y versus x as lines and/or markers. Call `plot` multiple times
+    Plot ys versus xs as lines and/or markers. Call `plot` multiple times
     to plot multiple sets of data.
 
     Parameters:
 
-    - `x`, `y` (array-like or scalar): The horizontal / vertical coordinates of the data points.
+    - `xs`, `ys` (array-like or scalar): The horizontal / vertical coordinates of the data points.
     - `key` (string): Identifies the series in the native plot widget.
     - `label` (string): Series label for the legend
     - `drawstyle` (string): Matplotlib draw style ('default', 'steps', 'steps-pre', 'steps-mid', 'steps-post')