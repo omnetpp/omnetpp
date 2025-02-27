66545ea9498c617e28e82c123354dc8c3cd778b0
TÖRÖK Attila <torokati44@gmail.com>
2025-02-27 15:06:48 +0100

    qtenv: Various improvements to circular_buffer
    - Add a (random access) iterator so standard binary search
      (std::lower_bound, std::upper_bound) can be used on it.
    - Allow growing even if the buffer is not completely full.
    - Allow growing to a custom new size, not just doubling it.
    - Add pop_front(int) for quickly dropping multiple elements.
    - Add resize(int) to shrink or grow the buffer, extending it
      with default-initialized new values as needed.
    - Add prepare_fill(int) which is useful for quickly
      overwriting the entire contents, without unnecessary
      reallocation, copies, or range checks.
    - Make element access methods (front, back, operator[]) return
      references to elements, so they avoid copies and can be used
      to change contents.
    - Extract wrapping index calculation to one place: wrapIndex
    - Rename members for clarity:
        cb -> storage
        cbsize -> capacity
        cbhead -> head
        cbtail -> tail

--- src/qtenv/circularbuffer.h
+++ src/qtenv/circularbuffer.h
@@ -20,6 +20,7 @@
 #include "omnetpp/simkerneldefs.h" // for ASSERT
 #include "omnetpp/cexception.h"
 #include "qtenvdefs.h"
+#include <bitset> // only for an ASSERT
 
 namespace omnetpp {
 namespace qtenv {
@@ -31,69 +32,200 @@ template <typename T>
 class QTENV_API circular_buffer
 {
     private:
-        T *cb;           // size of the circular buffer
-        int cbsize;      // always power of 2
-        int cbhead = 0;  // inclusive
-        int cbtail = 0;  // exclusive
+        T *storage;
+        int capacity;  // size of storage, always a power of 2
+        int head = 0;  // index of the first element in storage (inclusive)
+        int tail = 0;  // index after the last element in storage (exclusive)
 
     private:
-        void grow() {
-            ASSERT(cbhead == cbtail); // here it means full, not empty
+        inline int wrapIndex(int index) const {
+            // This is a % in disguise, and it works because capacity is always a power of 2.
+            return index & (capacity - 1);
+        }
+        void grow() { grow(capacity * 2); }
+        void grow(int newCapacity) {
+            int oldSize = size(); // this is valid element count
+            ASSERT(newCapacity > capacity); // this is backing buffer capacity
+            ASSERT(std::bitset<sizeof(int)*8>(newCapacity).count() == 1); // must be a power of 2
+            T *newStorage = new T[newCapacity];
 
-            int newsize = 2*cbsize;
-            T *newcb = new T[newsize];
+            // There is no std::move_n, but adding std::make_move_iterator achieves the same.
+            if (head < tail)
+                // Current contents are contiguous, one copy is enough.
+                std::copy_n(std::make_move_iterator(storage + head), tail - head, newStorage);
+            else if (head > tail) {
+                // Copy the first part of the range (at the end of the current buffer)
+                // to the beginning of the new one.
+                std::copy_n(std::make_move_iterator(storage + head), capacity - head, newStorage);
+                // Then copy the rest (at the beginning of the current buffer) after them.
+                std::copy_n(std::make_move_iterator(storage), tail, newStorage + (capacity - head));
+            } // else: empty buffer, nothing to copy
 
-            std::copy_n(cb + cbhead, cbsize - cbhead, newcb);
-            std::copy_n(cb, cbtail, newcb + (cbsize - cbhead));
+            delete[] storage;
+            storage = newStorage;
 
-            delete[] cb;
-            cb = newcb;
-
-            cbhead = 0;
-            cbtail = cbsize;
-            cbsize = newsize;
+            head = 0;
+            tail = oldSize;
+            capacity = newCapacity;
         }
 
         // unimplemented:
-        circular_buffer(const circular_buffer&);
-        void operator=(const circular_buffer&);
+        circular_buffer(const circular_buffer&) = delete;
+        void operator=(const circular_buffer&) = delete;
 
     public:
+        class iterator : public std::iterator<std::random_access_iterator_tag, T, int> {
+            private:
+                circular_buffer<T> *buffer;
+                // This is an ordinary index, as seen from the outside, not one into the internal
+                // storage - the wraparound logic is handled entirely by the buffer.
+                int position;
+
+            public:
+                iterator(circular_buffer<T> *buf, int pos) : buffer(buf), position(pos) {}
+                T& operator*() const {
+                    return (*buffer)[position];
+                }
+                iterator& operator++() {
+                    ++position;
+                    return *this;
+                }
+                iterator operator++(int) {
+                    iterator tmp(*this);
+                    ++position;
+                    return tmp;
+                }
+                iterator& operator--() {
+                    --position;
+                    return *this;
+                }
+                iterator operator--(int) {
+                    iterator tmp(*this);
+                    --position;
+                    return tmp;
+                }
+                iterator& operator+=(int n) {
+                    position += n;
+                    return *this;
+                }
+                iterator operator+(int n) const {
+                    iterator tmp(*this);
+                    tmp += n;
+                    return tmp;
+                }
+                iterator& operator-=(int n) {
+                    position -= n;
+                    return *this;
+                }
+                iterator operator-(int n) const {
+                    iterator tmp(*this);
+                    tmp -= n;
+                    return tmp;
+                }
+                int operator-(const iterator& other) const {
+                    return position - other.position;
+                }
+                T& operator[](int n) const {
+                    return (*buffer)[position + n];
+                }
+                bool operator==(const iterator& other) const {
+                    return buffer == other.buffer && position == other.position;
+                }
+                bool operator!=(const iterator& other) const {
+                    return buffer != other.buffer || position != other.position;
+                }
+        };
+
         circular_buffer(int capacity=32) :
-            cb(new T[capacity]), cbsize(capacity) { }
+            storage(new T[capacity]), capacity(capacity) { }
         ~circular_buffer() {
-            delete[] cb;
+            delete[] storage;
         }
         void push_back(T d) {
-            cb[cbtail] = d;
-            cbtail = (cbtail + 1) & (cbsize-1);
-            if (cbtail == cbhead)
+            // can't let it get full, it would look empty
+            if (wrapIndex(tail + 1) == head)
                 grow();
+            storage[tail] = d;
+            tail = wrapIndex(tail + 1);
         }
         void pop_front() {
-            ASSERT(cbhead != cbtail);
-            cbhead = (cbhead + 1) & (cbsize-1);
+            ASSERT(head != tail);
+            head = wrapIndex(head + 1);
         }
-        T front() const {
-            ASSERT(cbhead != cbtail);
-            return cb[cbhead];
+        void pop_front(int n) {
+            ASSERT(size() >= n);
+            head = wrapIndex(head + n);
         }
-        T back() const {
-            ASSERT(cbhead != cbtail);
-            return cb[(cbtail - 1) & (cbsize-1)];
+        T& front() const {
+            ASSERT(head != tail);
+            return storage[head];
         }
-        T operator[](int i) const {
-            ASSERT(cbhead != cbtail);
-            return cb[(cbhead + i) & (cbsize-1)];
+        T& back() const {
+            ASSERT(head != tail);
+            return storage[wrapIndex(tail - 1)];
+        }
+        T& operator[](int i) const {
+            ASSERT(size() > i);
+            return storage[wrapIndex(head + i)];
         }
         int size() const {
-            return (cbtail - cbhead) & (cbsize-1);
+            return wrapIndex(tail - head);
+        }
+        void resize(int newSize) {
+            if (newSize < size())
+                // shrinking is trivial
+                tail = wrapIndex(head + newSize);
+            else if (newSize > size()) {
+                // growing, check if need to reallocate buffer
+                if (newSize >= capacity) { // new size must be less than (not equal) to cbsize, because full and empty buffer would be indistinguishable
+                    int minCapacity = capacity;
+                    // can't allocate to exactly the new size because there's no way
+                    // to tell apart full and empty buffer
+                    while (minCapacity <= newSize)
+                        minCapacity *= 2;
+                    // reallocates unconditionally, only copies valid
+                    // elements, keeps the rest default-constructed
+                    grow(minCapacity);
+                }
+                else {
+                    // no need to reallocate, but need to fill with default
+                    for (int i = size(); i < newSize; i++)
+                        storage[wrapIndex(head + i)] = T();
+                }
+                tail = wrapIndex(head + newSize);
+            }
+            ASSERT(size() == newSize);
+        }
+        // Set the size of the buffer, reallocating if necessary,
+        // leaving its contents undefined. Caller is responsible for
+        // assigning a valid new value to all values in the returned
+        // pointer all through [0] to [count-1] (inclusive).
+        T *prepare_fill(int count) {
+            int minCapacity = 32;
+            // can't allocate to exactly the new size because there's no way
+            // to tell apart full and empty buffer
+            while (minCapacity <= count)
+                minCapacity *= 2;
+            if (capacity < minCapacity) {
+                delete[] storage;
+                storage = new T[minCapacity];
+                capacity = minCapacity;
+            }
+            head = 0;
+            tail = count;
+            return storage;
         }
         bool empty() const {
-            return cbhead == cbtail;
+            return head == tail;
         }
         void clear() {
-            cbhead = cbtail = 0;
+            head = tail = 0;
+        }
+        iterator begin() {
+            return iterator(this, 0);
+        }
+        iterator end() {
+            return iterator(this, size());
         }
 };
 
