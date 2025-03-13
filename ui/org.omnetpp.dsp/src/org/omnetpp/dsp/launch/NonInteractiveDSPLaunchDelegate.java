70f0b523b9dffae59da7cc8a6bd7c653e49eaec5
Levente Meszaros <levy@omnetpp.org>
2025-03-13 15:10:54 +0100

    ide: Replaced access to private fields with method calls.

--- ui/org.omnetpp.dsp/src/org/omnetpp/dsp/launch/NonInteractiveDSPLaunchDelegate.java
+++ ui/org.omnetpp.dsp/src/org/omnetpp/dsp/launch/NonInteractiveDSPLaunchDelegate.java
@@ -6,13 +6,14 @@ import java.util.function.Supplier;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.SubMonitor;
 import org.eclipse.core.variables.VariablesPlugin;
 import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.model.IDebugTarget;
-import org.eclipse.jdt.annotation.Nullable;
 import org.eclipse.lsp4e.debug.DSPPlugin;
 import org.eclipse.lsp4e.debug.debugmodel.JsonParserWithStringSubstitution;
 import org.eclipse.lsp4e.debug.debugmodel.TransportStreams;
@@ -30,30 +31,35 @@ public class NonInteractiveDSPLaunchDelegate extends DSPLaunchDelegate {
 		return target;
 	}
 
-
     @Override
-    public void launch(ILaunchConfiguration configuration, String mode, ILaunch launch, @Nullable IProgressMonitor monitor) throws CoreException {
+    public void launch(ILaunchConfiguration configuration, String mode, ILaunch launch, IProgressMonitor monitor) throws CoreException {
         final var builder = new DSPLaunchDelegateLaunchBuilder(configuration, mode, launch, monitor);
-        builder.launchNotConnect = DSPPlugin.DSP_MODE_LAUNCH
-                .equals(configuration.getAttribute(DSPPlugin.ATTR_DSP_MODE, DSPPlugin.DSP_MODE_LAUNCH));
-        builder.debugCmd = configuration.getAttribute(DSPPlugin.ATTR_DSP_CMD, "");
-        builder.debugCmdArgs = configuration.getAttribute(DSPPlugin.ATTR_DSP_ARGS, List.of());
-        builder.debugCmdEnvVars = DebugPlugin.getDefault().getLaunchManager().getEnvironment(configuration);
-        builder.monitorDebugAdapter = configuration.getAttribute(DSPPlugin.ATTR_DSP_MONITOR_DEBUG_ADAPTER, false);
-        builder.server = configuration.getAttribute(DSPPlugin.ATTR_DSP_SERVER_HOST, "");
-        builder.port = configuration.getAttribute(DSPPlugin.ATTR_DSP_SERVER_PORT, 0);
+        var launchNotConnect = DSPPlugin.DSP_MODE_LAUNCH.equals(configuration.getAttribute(DSPPlugin.ATTR_DSP_MODE, DSPPlugin.DSP_MODE_LAUNCH));
+        if (launchNotConnect) {
+            var debugCmd = configuration.getAttribute(DSPPlugin.ATTR_DSP_CMD, "");
+            var debugCmdArgs = configuration.getAttribute(DSPPlugin.ATTR_DSP_ARGS, List.of());
+            var debugCmdEnvVars = DebugPlugin.getDefault().getLaunchManager().getEnvironment(configuration);
+            builder.setLaunchDebugAdapter(debugCmd, debugCmdArgs, debugCmdEnvVars);
+        }
+        else {
+            var server = configuration.getAttribute(DSPPlugin.ATTR_DSP_SERVER_HOST, "");
+            var port = configuration.getAttribute(DSPPlugin.ATTR_DSP_SERVER_PORT, 0);
+            builder.setAttachDebugAdapter(server, port);
+        }
+        boolean monitorDebugAdapter = configuration.getAttribute(DSPPlugin.ATTR_DSP_MONITOR_DEBUG_ADAPTER, false);
+        builder.setMonitorDebugAdapter(monitorDebugAdapter);
 
         String dspParametersJson = configuration.getAttribute(DSPPlugin.ATTR_DSP_PARAM, "{}");
         try {
             final var jsonUtils = new JsonParserWithStringSubstitution(
                     VariablesPlugin.getDefault().getStringVariableManager());
             Map<String, Object> customParams = jsonUtils.parseJsonObjectAndRemoveNulls(dspParametersJson);
-            builder.dspParameters.putAll(customParams);
+            builder.setDspParameters(customParams);
         } catch (IllegalStateException e) {
-            throw new CoreException(createErrorStatus("Json launch parameters were not correctly formatted.", null));
+            var status = new Status(IStatus.ERROR, DSPPlugin.PLUGIN_ID, 0, "Json launch parameters were not correctly formatted.", null);
+            throw new CoreException(status);
         }
 
         launch(builder);
     }
-
 }
